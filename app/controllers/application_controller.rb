adf66098ec7d259bc804fec4168f235f9ed989f7
Wang Qiang <rovingbreeze@gmail.com>
2014-08-18 00:49:34 +0800

    Using cookies to remember locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_locale
+
+  def set_locale
+    if params[:locale]
+      cookies.permanent[:locale] = I18n.locale = params[:locale]
+    else
+      if cookies.permanent[:locale]
+        I18n.locale = cookies.permanent[:locale]
+      else
+        I18n.locale = I18n.default_locale
+      end
+    end
+  end
 end
