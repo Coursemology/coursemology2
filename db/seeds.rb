b870e1ea4c1030e373ae8388f6da5ae50497751b
Joel Low <joel@joelsplace.sg>
2014-12-04 09:19:52 +0800

    Loading seeds should be part of the Rails helper.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Instance.create(host: '*')
+#
+# Remember to ensure that the commands in this file are idempotent!
+Instance.find_or_create_by(host: '*')
