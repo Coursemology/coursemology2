d0712800c3c70fd53d05c0639cd15b7e8e37aac8
Joel Low <joel@joelsplace.sg>
2014-12-10 08:09:01 +0800

    Try to get both Coveralls and Code Climate to coexist.

--- spec/coverage_helper.rb
+++ spec/coverage_helper.rb
@@ -1,24 +1,43 @@
-# Helper to include Coveralls/Code Climate coverage, but not require developers to install the gem.
-#
-# @param name [String] The name of the module to require.
-# @param initialiser [Proc] The block to execute when the module is required successfully.
-def load_provider(name, &initialiser)
-  require name
-  initialiser.call
-rescue LoadError => e
-  if e.path == name
-    puts 'Cannot find \'%s\', ignoring' % [name] if ENV['CI']
-  else
-    raise e
+require 'simplecov'
+
+module CoverageHelper
+  # Helper to include Coveralls/Code Climate coverage, but not require developers to install the gem.
+  #
+  # @param name [String] The name of the module to require.
+  # @param initialiser [Proc] The block to execute when the module is required successfully.
+  def self.load(name, &initialiser)
+    old_simplecov_formatter = SimpleCov.formatter
+    require name
+    initialiser.call
+
+    if old_simplecov_formatter != SimpleCov.formatter
+      formatters.unshift(SimpleCov.formatter)
+      SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[*formatters]
+    end
+  rescue LoadError => e
+    if e.path == name
+      puts 'Cannot find \'%s\', ignoring' % [name] if ENV['CI']
+    else
+      raise e
+    end
+  end
+
+  private
+
+  # Tracks the formatters which need to be enabled
+  #
+  # @return [Array] The formatters which have been currently defined.
+  def self.formatters
+    @formatters ||= []
   end
 end
 
 # Coveralls
-load_provider('coveralls') do
+CoverageHelper.load('coveralls') do
   Coveralls.wear!('rails')
 end
 
 # Code Climate
-load_provider('codeclimate-test-reporter') do
+CoverageHelper.load('codeclimate-test-reporter') do
   CodeClimate::TestReporter.start
 end
