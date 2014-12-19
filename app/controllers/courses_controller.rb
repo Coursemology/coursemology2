2e1fb9b1f0d5aefc5a998704c53cd3869cd63cd5
Wang Qiang <rovingbreeze@gmail.com>
2014-12-19 12:00:53 +0800

    new and create action

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -8,6 +8,12 @@ class CoursesController < ApplicationController
   end
 
   def create
+    @course.course_users.build(user: current_user, name: 'name', role: :owner)
+    if @course.save
+      redirect_to edit_course_path(@course)
+    else
+      render 'new'
+    end
   end
 
   def edit
--- app/models/course.rb
+++ app/models/course.rb
@@ -7,4 +7,13 @@ class Course < ActiveRecord::Base
   belongs_to :creator, class_name: User.name
   has_many :course_users, inverse_of: :course, dependent: :destroy
   has_many :users, through: :course_users
+
+  before_validation :set_default_values, on: :create
+
+  private
+
+  def set_default_values
+    self.start_at ||= Time.now
+    self.end_at ||= 1.month.from_now
+  end
 end
