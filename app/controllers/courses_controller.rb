35dd885b1b246bc83683025061d0d369213ca491
Wang Qiang <rovingbreeze@gmail.com>
2014-12-19 11:30:57 +0800

    default empty actions and routes

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,28 @@
 class CoursesController < ApplicationController
+  load_and_authorize_resource
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def course_params
+    params.require(:course).
+      permit(:title, :description, :status, :start_at, :end_at)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,5 @@ Rails.application.routes.draw do
   #   end
 
   devise_for :users
+  resources :courses
 end
