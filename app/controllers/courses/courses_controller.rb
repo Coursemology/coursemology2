a551c4bd5cd7549606e02458a4b3461582a346e4
Wang Qiang <rovingbreeze@gmail.com>
2014-12-19 12:00:53 +0800

    new and create action

--- app/controllers/courses/courses_controller.rb
+++ app/controllers/courses/courses_controller.rb
@@ -8,6 +8,13 @@ class Courses::CoursesController < Courses::Controllers
   end
 
   def create #:nodoc:
+    @course.course_users.build(user: current_user, name: 'name', role: :owner)
+
+    if @course.save
+      redirect_to edit_course_path(@course)
+    else
+      render 'new'
+    end
   end
 
   def edit #:nodoc:
--- app/models/course.rb
+++ app/models/course.rb
@@ -2,8 +2,19 @@ class Course < ActiveRecord::Base
   acts_as_tenant(:instance)
   stampable
 
+  after_initialize :set_defaults, if: :new_record?
+
   enum status: { closed: 0, published: 1, opened: 2 }
+
   belongs_to :creator, class_name: User.name
   has_many :course_users, inverse_of: :course, dependent: :destroy
   has_many :users, through: :course_users
+
+  private
+
+  # Set default values
+  def set_defaults
+    self.start_at ||= Time.now
+    self.end_at ||= 1.month.from_now
+  end
 end
