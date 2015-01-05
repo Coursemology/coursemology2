bbef26c61e97e58732ff6891581a8cb04c19e25d
Wang Qiang <rovingbreeze@gmail.com>
2015-01-05 19:03:14 +0800

    essential configrations for capybara

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,7 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'coverage_helper'
 require 'should_not/rspec'
+require 'capybara/rspec'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
