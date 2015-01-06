f7ad012f4ba0ebfef91e8711c1f663c1bb82388e
Wang Qiang <rovingbreeze@gmail.com>
2015-01-06 14:09:36 +0800

    rails generate controller, modularize

---
+++ app/controllers/admin/instances_controller.rb
@@ -0,0 +1,19 @@
+class Admin::InstancesController < Admin::Controller
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
