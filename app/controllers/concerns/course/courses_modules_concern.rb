2da31fb7e12490721c8c474d45a338aca45fede2
Joel Low <joel@joelsplace.sg>
2015-01-10 16:14:54 +0800

    Define the API for declaring sidebar elements.

--- app/controllers/concerns/course/courses_modules_concern.rb
+++ app/controllers/concerns/course/courses_modules_concern.rb
@@ -3,34 +3,45 @@ module Course::CoursesModulesConcern
   included do
     include Modular
 
+    # Open the Modular Base Module.
     const_get(:Module).module_eval do
       const_set(:ClassMethods, Module.new) unless const_defined?(:ClassMethods)
       class_methods_module = const_get(:ClassMethods)
 
+      # Inject our sidebar definition methods into ClassMethods.
       class_methods_module.module_eval do
         include Sidebar
       end
     end
+
+    # Eager load all the modules declared.
+    eager_load_modules(Dir.open(File.join(__dir__, '../../modules')))
   end
 
   module Sidebar
+    # Class method to declare the proc handling the sidebar menu items.
+    #
+    # @param proc [Proc] The proc handling the sidebar for the given module. The proc will be
+    #                    `instance_eval`ed in the context of the controller handling the current
+    #                    request. This proc must return an array of hashes, each describing one
+    #                    menu item.
     def sidebar(&proc)
-      if proc
-        sidebar_set(proc)
-      else
-        sidebar_get
-      end
+      self.sidebar_proc = proc
+    end
+
+    # Class method to get the sidebar items from this module, in the context of the given
+    # controller instance.
+    #
+    # @param controller [Course::Controller] The controller handling the current request.
+    # @return [Array] An array of hashes containing the sidebar items exposed by this module.
+    def get_sidebar_items(controller)
+      return [] unless sidebar_proc
+
+      controller.instance_exec(&sidebar_proc)
     end
 
     private
 
-    def sidebar_set(proc)
-      @sidebar = proc
-    end
-
-    def sidebar_get
-      proc = @sidebar || ->() { [] }
-      proc.call()
-    end
+    attr_accessor :sidebar_proc
   end
 end
--- app/controllers/course/controller.rb
+++ app/controllers/course/controller.rb
@@ -1,7 +1,23 @@
 class Course::Controller < ApplicationController
   load_and_authorize_resource :course
 
+  # Gets the sidebar elements.
+  #
+  # Sidebar elements have the given format:
+  #
+  # ```
+  # {
+  #    title: 'Sidebar Item Title'
+  #    unread: 0 # or nil
+  # }
+  # ```
+  #
+  # The elements are rendered on all Course controller subclasses as part of a nested template.
   def sidebar
-    Course::CoursesController.modules.map { |module_| module_.sidebar }
+    array_of_module_arrays = Course::CoursesController.modules.map do |module_|
+      module_.get_sidebar_items(self)
+    end
+
+    array_of_module_arrays.flatten!
   end
 end
