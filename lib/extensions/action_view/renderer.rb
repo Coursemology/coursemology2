92c3ab90aa5b196d462edd13fdc279965aafb0b4
Joel Low <joel@joelsplace.sg>
2015-01-11 17:17:42 +0800

    Add specs for `render within_layout`.

--- lib/extensions/action_view/renderer.rb
+++ lib/extensions/action_view/renderer.rb
@@ -1,5 +1,5 @@
 module Extensions::ActionView::Renderer
-  def render_within_layout(context, layout)
+  def render_within_layout(context, layout, *_)
     context.view_flow.set(:layout, context.capture { yield })
     context.render template: "layouts/#{layout}"
   end
---
+++ spec/libraries/render_within_layout_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'render_within_layout', type: :view do
+  let(:views_directory) do
+    path = Pathname.new("#{__dir__}/../fixtures/libraries/render_within_layout")
+    path.realpath
+  end
+
+  before do
+    controller.prepend_view_path views_directory
+  end
+
+  it 'properly nests' do
+    render template: 'content', layout: 'inner_layout'
+    expect(rendered).to have_tag('div.outer') do
+      with_tag('div.inner') do
+        with_text /test!/
+      end
+    end
+  end
+end
