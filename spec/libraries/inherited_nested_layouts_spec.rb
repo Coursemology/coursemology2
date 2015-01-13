30ceb4e5759fbd7b40aaa214f81f095f46fd0962
Joel Low <joel@joelsplace.sg>
2015-01-13 09:28:40 +0800

    Support nested layout querying to any depth.

--- spec/libraries/inherited_nested_layouts_spec.rb
+++ spec/libraries/inherited_nested_layouts_spec.rb
@@ -13,14 +13,31 @@ describe 'Inherited Nested Layouts', type: :controller do
     end
   end
 
-  controller(ControllerB) do
+  class ControllerC < ControllerB
+    layout 'testC'
+  end
+
+  controller(ControllerC) do
   end
 
   it 'gets the correct current layout' do
-    expect(controller.current_layout).to eq('testB')
+    expect(controller.current_layout).to eq('testC')
   end
 
   it 'gets the correct parent layout' do
-    expect(controller.parent_layout).to eq('testA')
+    expect(controller.parent_layout).to eq('testB')
+  end
+
+  it 'gets the correct parent layout of the specified parent' do
+    expect(controller.parent_layout(of_layout: 'testB')).to eq('testA')
+  end
+
+  it 'gets the correct layout hierarchy' do
+    expect(controller.layout_hierarchy).to eq([
+      'default',
+      'testA',
+      'testB',
+      'testC'
+    ])
   end
 end
