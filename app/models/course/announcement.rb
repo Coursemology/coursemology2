d9e1bf6f9f996ff9eba739f95fcd5b6ff38ed5f6
Wang Qiang <rovingbreeze@gmail.com>
2015-01-14 15:52:14 +0800

    initial default values for announcement

--- app/models/course/announcement.rb
+++ app/models/course/announcement.rb
@@ -1,6 +1,14 @@
 class Course::Announcement < ActiveRecord::Base
   stampable
 
+  after_initialize :set_defaults, if: :new_record?
+
   belongs_to :creator, class_name: User.name
   belongs_to :course, inverse_of: :announcements
+
+  # Set default values
+  def set_defaults
+    self.valid_from ||= Time.now
+    self.valid_to ||= 7.days.from_now
+  end
 end
