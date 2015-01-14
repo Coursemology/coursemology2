95242e6b7ccb6005f0f421cac8962a8399b54661
Wang Qiang <rovingbreeze@gmail.com>
2015-01-14 10:59:12 +0800

    factories and specs

--- spec/models/course/announcement_spec.rb
+++ spec/models/course/announcement_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Course::Announcement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:instance) { create(:instance) }
+  with_tenant(:instance) do
+    context 'when title is not present' do
+      subject { build(:course_announcement, title: '') }
+
+      it { is_expected.not_to be_valid }
+    end
+  end
 end
