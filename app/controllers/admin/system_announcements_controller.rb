e92e5c5775313a33d150c2d5cce2e9368a83e3de
Wang Qiang <rovingbreeze@gmail.com>
2015-01-29 13:54:36 +0800

    implement system announcement index

--- app/controllers/admin/system_announcements_controller.rb
+++ app/controllers/admin/system_announcements_controller.rb
@@ -1,2 +1,7 @@
-class Admin::SystemAnnouncementsController < ApplicationController
+class Admin::SystemAnnouncementsController < Admin::Controller
+  load_and_authorize_resource :system_announcement, class: SystemAnnouncement.name
+
+  def index
+    @system_announcements = @system_announcements.includes(:creator)
+  end
 end
