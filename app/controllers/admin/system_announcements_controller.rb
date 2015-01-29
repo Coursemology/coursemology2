65d48d0327793b5e7fd32cdbd035c9ed44d5046d
Wang Qiang <rovingbreeze@gmail.com>
2015-01-29 14:42:26 +0800

    implement creating system announcement

--- app/controllers/admin/system_announcements_controller.rb
+++ app/controllers/admin/system_announcements_controller.rb
@@ -4,4 +4,22 @@ class Admin::SystemAnnouncementsController < Admin::Controller
   def index
     @system_announcements = @system_announcements.includes(:creator)
   end
+
+  def new
+  end
+
+  def create
+    if @system_announcement.save
+      redirect_to admin_system_announcements_path,
+                  notice: t('.notice', title: @system_announcement.title)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def system_announcement_params
+    params.require(:system_announcement).permit(:title, :content, :valid_from, :valid_to)
+  end
 end
