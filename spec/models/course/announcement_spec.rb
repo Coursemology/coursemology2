4875d142c87ecc72b679953c20718251d9c47f4c
Jason Yeo <jasonyeo88@gmail.com>
2015-02-15 01:43:48 +0800

    Use shoulda to test validations

--- spec/models/course/announcement_spec.rb
+++ spec/models/course/announcement_spec.rb
@@ -3,13 +3,5 @@ require 'rails_helper'
 RSpec.describe Course::Announcement, type: :model do
   it { is_expected.to belong_to(:creator).class_name(User.name) }
   it { is_expected.to belong_to(:course).inverse_of(:announcements) }
-
-  let!(:instance) { create(:instance) }
-  with_tenant(:instance) do
-    context 'when title is not present' do
-      subject { build(:course_announcement, title: '') }
-
-      it { is_expected.not_to be_valid }
-    end
-  end
+  it { is_expected.to validate_presence_of(:title) }
 end
