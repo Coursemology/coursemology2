d7ca7b1db61f6dd3e8c335bbcc9c62e156deb21a
Jason Yeo <jasonyeo88@gmail.com>
2015-02-15 01:53:29 +0800

    Test CourseUser's enum

--- spec/models/course_user_spec.rb
+++ spec/models/course_user_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe CourseUser, type: :model do
   it { is_expected.to belong_to(:user).inverse_of(:course_users) }
   it { is_expected.to belong_to(:course).inverse_of(:course_users) }
+  it { is_expected.to define_enum_for(:role) }
 
   context 'when course_user is created' do
     subject { CourseUser.new }
