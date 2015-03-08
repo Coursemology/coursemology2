7c4bbaa055e98a552eb0ac18f04770b511e5e14d
Han Lin <hanlin.dev@gmail.com>
2015-03-08 23:27:31 +0800

    Add template for enrol_requests/new action; Add spec for enrol_requests_controller.

--- spec/controllers/course/enrol_requests_controller_spec.rb
+++ spec/controllers/course/enrol_requests_controller_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Course::EnrolRequestsController, :type => :controller do
+  with_tenant(:instance) do
+    let!(:user) { create(:user) }
+    let!(:instance_user) do
+      result = InstanceUser.create(instance: instance, user: user)
+      instance.reload
+      user.reload
+      result
+    end
+    let!(:course) { create(:course) }
+
+    describe '#new' do
+      subject { get new_course_enrol_request_path(course, role: 'student') }
+      before do
+        sign_in(user)
+      end
+
+      it 'should create a new request after first attempt' do
+        subject
+        expect(response).to render_template(:new)
+        expect(assigns(:enrol_request)).to be_a_new(EnrolRequest)
+      end
+
+      it 'should not create a new request after subsequent attempts' do
+        subject
+        expect(response).to render_template(:new)
+        expect(assigns(:enrol_request)).not_to be_a_new(EnrolRequest)
+      end
+    end
+  end
 
 end
