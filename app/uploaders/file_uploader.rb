2d9a985a8eb7f3ec161a28ed0a3b2c5ff1ac4a93
Wang Qiang <rovingbreeze@gmail.com>
2015-03-09 10:59:49 +0800

    Save original name in file uploader

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -8,6 +8,7 @@ class FileUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
+  before :cache, :save_original_filename
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -18,7 +19,8 @@ class FileUploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #   # ActionController::Base.helpers.
+  #       asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
@@ -47,4 +49,7 @@ class FileUploader < CarrierWave::Uploader::Base
   #   "something.jpg" if original_filename
   # end
 
+  def save_original_filename(file)
+    model.name = file.original_filename if file.respond_to?(:original_filename)
+  end
 end
