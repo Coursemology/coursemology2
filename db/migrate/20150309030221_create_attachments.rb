1bd40ff0025224fa56e474727054aa09f92d0fda
Wang Qiang <rovingbreeze@gmail.com>
2015-03-09 11:07:33 +0800

    Add an attachment model and related db migrations

---
+++ db/migrate/20150309030221_create_attachments.rb
@@ -0,0 +1,12 @@
+class CreateAttachments < ActiveRecord::Migration
+  def change
+    create_table :attachments do |t|
+      t.string :name, null: false
+      t.references :attachable, polymorphic: true, index: true
+      t.text :file_upload, null: false
+
+      t.userstamps null: false, foreign_key: { references: :users }
+      t.timestamps null: false
+    end
+  end
+end
