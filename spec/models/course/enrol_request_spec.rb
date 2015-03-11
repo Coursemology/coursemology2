b338ace94ddc1406a93c8b499d0e51dcb8d7c6dd
Han Lin <hanlin.dev@gmail.com>
2015-03-12 02:16:49 +0800

    Add Course::EnrolRequest model spec

--- spec/models/course/enrol_request_spec.rb
+++ spec/models/course/enrol_request_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Course::EnrolRequest, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:instance) { create(:instance) }
+  with_tenant(:instance) do
+    let!(:course) { create(:course) }
+    let!(:user) { create(:user) }
+    describe 'creating enrol request of the same user to the same course twice' do
+      subject { create(:course_enrol_request, course: course, user: user, role: :student) }
+      before do
+        create(:course_enrol_request, course: course, user: user, role: :student)
+      end
+      it 'fails' do
+        expect{ subject }.to change(Course::EnrolRequest, :count).by(0).and raise_error
+      end
+    end
+  end
 end
