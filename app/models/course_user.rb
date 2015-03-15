80fbe4e8f07e77f715ba17ec943f2d0bbea77017
Han Lin <hanlin.dev@gmail.com>
2015-03-15 21:07:43 +0800

    Add course user specs.

--- app/models/course_user.rb
+++ app/models/course_user.rb
@@ -2,9 +2,9 @@ class CourseUser < ActiveRecord::Base
   belongs_to :user, inverse_of: :course_users
   belongs_to :course, inverse_of: :course_users
   enum role: { student: 0, teaching_assistant: 1, manager: 2, owner: 3 }
-  scope :student, -> { where(role: :student) }
-  scope :teaching_assistant, -> { where(role: :teaching_assistant) }
-  scope :manager, -> { where(role: :manager) }
-  scope :owner, -> { where(role: :owner) }
-  scope :staff, -> { where(role: :staff) }
+  scope :student, -> { where(role: roles[:student]) }
+  scope :teaching_assistant, -> { where(role: roles[:teaching_assistant]) }
+  scope :manager, -> { where(role: roles[:manager]) }
+  scope :owner, -> { where(role: roles[:owner]) }
+  scope :staff, -> { where(role: [roles[:teaching_assistant], roles[:manager], roles[:owner]]) }
 end
