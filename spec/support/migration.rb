1e843cc965599b4ad3e1ef07b8939dbc79dd330e
Joel Low <joel@joelsplace.sg>
2015-03-16 12:40:19 +0800

    Refactor the code needed to create and drop temporary tables used in model tests.

---
+++ spec/support/migration.rb
@@ -0,0 +1,48 @@
+# Test group helpers for creating tables.
+module ActiveRecord::Migration::TestGroupHelpers
+  # Defines a temporary table that is instantiated when needed, within a `with_temporary_table`
+  # block.
+  #
+  # @param table_name Symbol The name of the table to define.
+  # @param proc Proc The table definition, same as that of a block given to
+  #                  `ActiveRecord::Migration::create_table`
+  def temporary_table(table_name, &proc)
+    define_method(table_name) do
+      proc
+    end
+  end
+
+  # Using the temporary table defined previously, run the examples in this group.
+  #
+  # @param table_name Symbol The name of the table to use.
+  # @param proc Proc The examples requiring the use of the temporary table.
+  def with_temporary_table(table_name, &proc)
+    context "with temporary table #{table_name}" do |*params|
+      before(:context) do
+        ActiveRecord::Migration::TestGroupHelpers.before_context(table_name, send(table_name))
+      end
+
+      after(:context) do
+        ActiveRecord::Migration::TestGroupHelpers.after_context(table_name)
+      end
+
+      instance_exec(*params, &proc)
+    end
+  end
+
+  def self.before_context(table_name, table_definition)
+    ActiveRecord::Migration.suppress_messages do
+      ActiveRecord::Migration.create_table(table_name, &table_definition)
+    end
+  end
+
+  def self.after_context(table_name)
+    ActiveRecord::Migration.suppress_messages do
+      ActiveRecord::Migration.drop_table(table_name)
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.extend ActiveRecord::Migration::TestGroupHelpers, type: :model
+end
