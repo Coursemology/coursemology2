7a31724f6da20ecf633bfe32a61edfc17498ac4e
Wang Qiang <rovingbreeze@gmail.com>
2015-03-22 23:28:49 +0800

    `rake rails:update`

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,10 @@ class Application < Rails::Application
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
   # config.i18n.default_locale = :de
+
+  # Do not swallow errors in after_commit/after_rollback callbacks.
+  config.active_record.raise_in_transactional_callbacks = true
+
   config.eager_load_paths << "#{Rails.root}/lib/autoload"
   config.eager_load_paths << "#{Rails.root}/app/models/modules"
   config.eager_load_paths << "#{Rails.root}/app/controllers/modules"
