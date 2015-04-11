57b27e5a4f0b81507440db7ccdc089814e5067da
WANG QIANG <rovingbreeze@gmail.com>
2015-04-11 20:12:07 +0800

    Test case for form_builder attachments helper

--- spec/libraries/acts_as_attachable_spec.rb
+++ spec/libraries/acts_as_attachable_spec.rb
@@ -33,10 +33,22 @@ RSpec.describe 'ActsAsAttachable' do
     class SampleView < ActionView::Base; end
     class SampleFormBuilder < ActionView::Helpers::FormBuilder; end
 
-    let(:template) { SampleView.new }
-    let(:resource) { SampleModel.new }
-    subject { SampleFormBuilder.new(:sample, resource, template, {}) }
+    let(:template) { SampleView.new(Rails.root.join('app', 'views')) }
+    let(:resource) do
+      model = SampleModel.new
+      model.attachments << build(:attachment)
+      model
+    end
+    let(:form_builder) { SampleFormBuilder.new(:sample, resource, template, {}) }
+    subject { form_builder }
 
     it { is_expected.to respond_to(:attachments) }
+
+    describe '#attachments' do
+      before { I18n.locale = I18n.default_locale }
+      subject { form_builder.attachments }
+
+      it { is_expected.to have_tag('div', text: 'Upload new file') }
+    end
   end
 end
