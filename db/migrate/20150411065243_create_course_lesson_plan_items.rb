e5c549ec5a21ea5d9a367f3ade929aa7f2420f7c
Brian Koh <kxmbrian@gmail.com>
2015-04-11 15:59:19 +0800

    Add lesson plan item model db migration

---
+++ db/migrate/20150411065243_create_course_lesson_plan_items.rb
@@ -0,0 +1,16 @@
+class CreateCourseLessonPlanItems < ActiveRecord::Migration
+  def change
+    create_table :course_lesson_plan_items do |t|
+      t.actable index: :unique
+
+      t.integer :base_exp, null: false
+      t.integer :time_bonus_exp, null: false
+      t.integer :extra_bonus_exp, null: false
+      t.timestamp :start_time
+      t.timestamp :bonus_end_time
+      t.timestamp :end_time
+      t.userstamps null: false, foreign_key: { references: :users }
+      t.timestamps null: false
+    end
+  end
+end
