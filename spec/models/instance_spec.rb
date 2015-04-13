2e69884473b2ce6f7f612f1c44741298455e0199
WANG QIANG <rovingbreeze@gmail.com>
2015-04-13 16:02:13 +0800

    Add specs for instance module settings

--- spec/models/instance_spec.rb
+++ spec/models/instance_spec.rb
@@ -64,4 +64,88 @@ RSpec.describe Instance, type: :model do
       expect(found_instance).to eq(first_instance)
     end
   end
+
+  describe 'module preferences' do
+    let(:instance) { create(:instance) }
+    let(:all_modules) { Course::ModuleHost.modules }
+
+    describe '#set_default_settings' do
+      it 'initializes the default values' do
+        all_modules.each do |m|
+          expect(instance.settings(m.key).enabled).to eq m.enabled_by_default?
+        end
+      end
+    end
+
+    describe '#enabled_modules' do
+      let(:enabled_modules) { instance.enabled_modules }
+
+      context 'without preference' do
+        it 'returns the default enabled modules' do
+          default_enabled_modules = all_modules.select(&:enabled_by_default?)
+          expect(enabled_modules.count).to eq default_enabled_modules.count
+          default_enabled_modules.each do |m|
+            expect(enabled_modules.include?(m)).to eq true
+          end
+        end
+      end
+
+      context 'with preference' do
+        let(:sample_module) { all_modules.first }
+        context 'disable a module' do
+          before do
+            instance.settings(sample_module.key).enabled = false
+            instance.save
+          end
+
+          it 'does not include the disabled module' do
+            expect(enabled_modules.include?(sample_module)).to eq false
+          end
+        end
+
+        context 'enable a module' do
+          before { instance.settings(sample_module.key).enabled = true }
+
+          it 'includes the disabled module' do
+            expect(enabled_modules.include?(sample_module)).to eq true
+          end
+        end
+      end
+    end
+
+    describe '#disabled_modules' do
+      let(:disabled_modules) { instance.disabled_modules }
+
+      context 'without preference' do
+        it 'returns the default disabled modules' do
+          default_disabled_modules = all_modules.select { |m| !m.enabled_by_default? }
+          expect(disabled_modules.count).to eq default_disabled_modules.count
+          default_disabled_modules.each do |m|
+            expect(disabled_modules.include?(m)).to eq true
+          end
+        end
+      end
+
+      context 'with preference' do
+        let(:sample_module) { all_modules.first }
+        context 'disable a module' do
+          before { instance.settings(sample_module.key).enabled = false }
+
+          it 'includes the disabled module' do
+            expect(disabled_modules.include?(sample_module)).to eq true
+          end
+        end
+
+        context 'enable a module' do
+          before do
+            instance.settings(sample_module.key).enabled = true
+          end
+
+          it 'does not include the enabled module' do
+            expect(disabled_modules.include?(sample_module)).to eq false
+          end
+        end
+      end
+    end
+  end
 end
