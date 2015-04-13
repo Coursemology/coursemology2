48a6bb5c15d6f27e020ca0ebafc39e89d1a775b3
WANG QIANG <rovingbreeze@gmail.com>
2015-04-13 16:33:44 +0800

    Move carrierwave configs from initializers to rspec/support

---
+++ spec/support/carrierwave.rb
@@ -0,0 +1,28 @@
+RSpec.configure do |config|
+  # Set file upload path in tests, using file storage
+  config.before(:suite) do
+    CarrierWave.configure do |carrier_wave_config|
+      carrier_wave_config.enable_processing = false
+    end
+
+    CarrierWave::Uploader::Base.descendants.each do |klass|
+      next if klass.anonymous?
+      klass.class_eval do
+        storage :file
+
+        def cache_dir
+          "#{Rails.root}/spec/support/uploads/tmp"
+        end
+
+        def store_dir
+          "#{Rails.root}/spec/support/uploads/#{model.class.to_s.underscore}/#{model.id}"
+        end
+      end
+    end
+  end
+
+  # Delete all the uploaded files after testing
+  config.after(:suite) do
+    FileUtils.rm_rf(Dir["#{Rails.root}/spec/support/uploads"])
+  end
+end
