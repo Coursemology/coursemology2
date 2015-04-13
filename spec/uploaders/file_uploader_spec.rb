c0ae1bbeb9403bb7d6733d5c36c5d4f8a3df4952
WANG QIANG <rovingbreeze@gmail.com>
2015-04-13 16:38:55 +0800

    Add missing require files

--- spec/uploaders/file_uploader_spec.rb
+++ spec/uploaders/file_uploader_spec.rb
@@ -1,3 +1,4 @@
+require 'rails_helper'
 require 'carrierwave/test/matchers'
 
 RSpec.describe FileUploader, type: :model do
--- spec/uploaders/image_uploader_spec.rb
+++ spec/uploaders/image_uploader_spec.rb
@@ -1,3 +1,4 @@
+require 'rails_helper'
 require 'carrierwave/test/matchers'
 
 RSpec.describe ImageUploader, type: :model do
