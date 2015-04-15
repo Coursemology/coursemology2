a39ca3b708a5529a517a6237e96f787d6e0ae58d
Brian Koh <kxmbrian@gmail.com>
2015-04-15 17:38:19 +0800

    Add .total_exp to LessonPlanItem model

--- app/models/course/lesson_plan_item.rb
+++ app/models/course/lesson_plan_item.rb
@@ -4,4 +4,12 @@ class Course::LessonPlanItem < ActiveRecord::Base
   validates :base_exp, numericality: { only_integer: true }
   validates :time_bonus_exp, numericality: { only_integer: true }
   validates :extra_bonus_exp, numericality: { only_integer: true }
+
+  # Gives the maximum number of EXP Points that an EXP-awarding item
+  # is allocated to give, which is the sum of base and bonus EXPs.
+  #
+  # @return [Integer] Maximum EXP awardable.
+  def total_exp
+    base_exp + time_bonus_exp + extra_bonus_exp
+  end
 end
