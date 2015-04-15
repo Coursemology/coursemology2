d352832af9c32e940db84a1792c12b448caf1ff9
Brian Koh <kxmbrian@gmail.com>
2015-04-15 14:11:13 +0800

    Add experience points record model tests

--- spec/models/course_user_spec.rb
+++ spec/models/course_user_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe CourseUser, type: :model do
   it { is_expected.to belong_to(:user).inverse_of(:course_users) }
   it { is_expected.to belong_to(:course).inverse_of(:course_users) }
   it { is_expected.to define_enum_for(:role) }
+  it do
+    is_expected.to have_many(:experience_points_records).
+      inverse_of(:course_user).
+      dependent(:destroy)
+  end
 
   context 'when course_user is created' do
     subject { CourseUser.new }
