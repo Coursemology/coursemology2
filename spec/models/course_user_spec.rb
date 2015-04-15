97816fec9ddbd941d774dba2357ed79ec616edae
Brian Koh <kxmbrian@gmail.com>
2015-04-15 17:01:20 +0800

    Add CourseUser .exp_points test

--- spec/models/course_user_spec.rb
+++ spec/models/course_user_spec.rb
@@ -12,4 +12,18 @@ RSpec.describe CourseUser, type: :model do
     it { is_expected.to be_student }
     it { is_expected.not_to be_phantom }
   end
+
+  let!(:instance) { create :instance }
+  with_tenant(:instance) do
+    describe '#exp_points' do
+      before do
+        exp_record_1 = create :manual_exp_record
+        @course_user = exp_record_1.course_user
+        exp_record_2 = create :manual_exp_record, course_user: @course_user
+        @exp_total = exp_record_1.exp_awarded + exp_record_2.exp_awarded
+      end
+      subject { @course_user.exp_points }
+      it { is_expected.to eq @exp_total }
+    end
+  end
 end
