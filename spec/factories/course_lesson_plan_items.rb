8b06e8750be9cc5d5b2f354371a5b21815c71f5d
Brian Koh <kxmbrian@gmail.com>
2015-04-15 17:40:14 +0800

    Add randomisation for exp in LessonPlanItem factory

--- spec/factories/course_lesson_plan_items.rb
+++ spec/factories/course_lesson_plan_items.rb
@@ -2,9 +2,9 @@ FactoryGirl.define do
   factory :course_lesson_plan_item, class: 'Course::LessonPlanItem' do
     creator
     updater
-    base_exp 100
-    time_bonus_exp 100
-    extra_bonus_exp 100
+    base_exp          { rand(1..10) * 100 }
+    time_bonus_exp    { rand(1..10) * 100 }
+    extra_bonus_exp   { rand(1..10) * 100 }
     start_time 1.days.from_now
     bonus_cutoff_time 2.days.from_now
     end_time 3.days.from_now
