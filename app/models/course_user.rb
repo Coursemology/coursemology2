55ed76c13426b5a43a43b99d1a1ee9a3d6ffb548
Brian Koh <kxmbrian@gmail.com>
2015-04-15 12:46:19 +0800

    Add ExpRecord-CourseUser association

--- app/models/course_user.rb
+++ app/models/course_user.rb
@@ -1,5 +1,8 @@
 class CourseUser < ActiveRecord::Base
   belongs_to :user, inverse_of: :course_users
   belongs_to :course, inverse_of: :course_users
+  has_many :exp_records, class_name: Course::ExpRecord.name,
+                         inverse_of: :course_user, dependent: :destroy
+
   enum role: { student: 0, teaching_assistant: 1, manager: 2, owner: 3 }
 end
