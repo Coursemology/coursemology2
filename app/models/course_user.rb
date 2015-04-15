7c12e50e1a009a21b8d445e56170ed8f1e750784
Brian Koh <kxmbrian@gmail.com>
2015-04-15 17:00:50 +0800

    Add .exp_points to CourseUser

--- app/models/course_user.rb
+++ app/models/course_user.rb
@@ -5,4 +5,11 @@ class CourseUser < ActiveRecord::Base
                          inverse_of: :course_user, dependent: :destroy
 
   enum role: { student: 0, teaching_assistant: 1, manager: 2, owner: 3 }
+
+  # Number of Experience (EXP) Points a course_user has gained
+  #
+  # @return [Integer] Number of EXP Points
+  def exp_points
+    exp_records.map(&:exp_awarded).inject(:+)
+  end
 end
