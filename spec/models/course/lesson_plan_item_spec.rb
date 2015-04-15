fe39158e86535c6155d355be97471961561277c3
Brian Koh <kxmbrian@gmail.com>
2015-04-15 17:39:38 +0800

    Add test for LessonPlanItem .total_exp

--- spec/models/course/lesson_plan_item_spec.rb
+++ spec/models/course/lesson_plan_item_spec.rb
@@ -4,4 +4,17 @@ RSpec.describe Course::LessonPlanItem, type: :model do
   it { is_expected.to validate_numericality_of(:base_exp).only_integer }
   it { is_expected.to validate_numericality_of(:time_bonus_exp).only_integer }
   it { is_expected.to validate_numericality_of(:extra_bonus_exp).only_integer }
+
+  let!(:instance) { create(:instance) }
+  with_tenant(:instance) do
+    let(:lesson_plan_item) { FactoryGirl.create :course_lesson_plan_item }
+    describe '#total_exp' do
+      it 'equals base exp plus bonuses' do
+        sum = lesson_plan_item.base_exp +
+              lesson_plan_item.time_bonus_exp +
+              lesson_plan_item.extra_bonus_exp
+        expect(lesson_plan_item.total_exp).to eq sum
+      end
+    end
+  end
 end
