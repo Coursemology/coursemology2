0b89b81d9c847dc4bb34551a545a2ecd5bcbb516
Brian Koh <kxmbrian@gmail.com>
2015-04-16 19:04:21 +0800

    Add workaround to resolve deadlock: New exp-awarding item (EA) cannot be saved unless ExpRecord is valid. ExpRecord will not be valid until EA is saved.

--- app/models/course/exp_record.rb
+++ app/models/course/exp_record.rb
@@ -4,11 +4,15 @@ class Course::ExpRecord < ActiveRecord::Base
 
   belongs_to :course_user, inverse_of: :exp_records
 
-  validates :course_user, presence: true
   validates :exp_awarded, numericality: { only_integer: true }
   validates :reason, presence: true, if: :manual_exp?
 
+  # True if EXP record is manually given.
+  # Before saving of source item, e.g. submission,
+  # #specific is nil. The conditional is a workaround.
+  #
+  # @return [Boolean]
   def manual_exp?
-    specific.nil?
+    new_record? ? actable_type.nil? : specific.nil?
   end
 end
