af4a32e85cc13ddf056856833dcb0592de22bcf4
Joel Low <joel@joelsplace.sg>
2015-04-25 11:10:48 +0800

    Set null constraints on the timestamp columns.

---
+++ db/migrate/20150425030128_set_timestamps_nullity.rb
@@ -0,0 +1,12 @@
+class SetTimestampsNullity < ActiveRecord::Migration
+  def change
+    change_column_null :users, :created_at, false
+    change_column_null :users, :updated_at, false
+    change_column_null :instance_users, :created_at, false
+    change_column_null :instance_users, :updated_at, false
+    change_column_null :courses, :created_at, false
+    change_column_null :courses, :updated_at, false
+    change_column_null :course_users, :created_at, false
+    change_column_null :course_users, :updated_at, false
+  end
+end
