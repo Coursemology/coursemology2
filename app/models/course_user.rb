17243e28352e767f7c11127c34e8a1584740943b
Joel Low <joel@joelsplace.sg>
2015-05-12 13:51:36 +0800

    Implement a collection method in the database instead of reducing on the application

--- app/models/course_user.rb
+++ app/models/course_user.rb
@@ -2,14 +2,13 @@ class CourseUser < ActiveRecord::Base
   belongs_to :user, inverse_of: :course_users
   belongs_to :course, inverse_of: :course_users
   has_many :experience_points_records, class_name: Course::ExperiencePointsRecord.name,
-                                       inverse_of: :course_user, dependent: :destroy
+                                       inverse_of: :course_user, dependent: :destroy do
+    def experience_points
+      sum(:points_awarded)
+    end
+  end
 
   enum role: { student: 0, teaching_assistant: 1, manager: 2, owner: 3 }
 
-  # Number of Experience (EXP) Points a course_user has gained
-  #
-  # @return [Integer] Number of EXP Points
-  def exp_points
-    exp_records.map(&:exp_awarded).inject(:+)
-  end
+  delegate :experience_points, to: :experience_points_records
 end
