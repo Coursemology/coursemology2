9768a4e8dc36505a7d7797b8ad8a350425802997
WANG QIANG <rovingbreeze@gmail.com>
2015-05-16 15:57:51 +0800

    Validate user add to the group are in the same course with group

--- app/models/course/group_user.rb
+++ app/models/course/group_user.rb
@@ -5,6 +5,8 @@ class Course::GroupUser < ActiveRecord::Base
 
   enum role: { normal: 0, manager: 1 }
 
+  validate :user_and_group_in_same_course
+
   belongs_to :user, inverse_of: :course_group_users
   belongs_to :course_group, class_name: Course::Group.name, inverse_of: :group_users
 
@@ -15,5 +17,10 @@ class Course::GroupUser < ActiveRecord::Base
   # Set default values
   def set_defaults
     self.role ||= :normal
+  end
+
+  def user_and_group_in_same_course #:nodoc:
+    return if user.courses.include?(group.course)
+    errors.add(:user, I18n.t('activerecord.errors.models.course_group_user.not_enrolled'))
   end
 end
--- spec/factories/course_group_users.rb
+++ spec/factories/course_group_users.rb
@@ -6,6 +6,12 @@ FactoryGirl.define do
     creator
     updater
 
+    after(:build) do |group_user|
+      course = group_user.course_group.course
+      user = group_user.user
+      create(:course_user, course: course, user: user) unless user.courses.include?(course)
+    end
+
     factory :course_group_student
     factory :course_group_manager do
       role :manager
