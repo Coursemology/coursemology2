75d5eb62618f6e13b03a585d4883f799a4d19b5c
Zihuan Xu <xzhflying@163.com>
2015-05-19 20:43:59 +0800

    Add Notification Model functions to manage notifications and activties

--- app/models/notification_style.rb
+++ app/models/notification_style.rb
@@ -1,3 +1,12 @@
 class NotificationStyle < ActiveRecord::Base
   belongs_to :activity, inverse_of: :notification_styles, dependent: :destroy
+
+  acts_as_readable on: :updated_at
+
+  def template_path
+    partial_root = ''
+    path = activity.key.split('.')
+    path.unshift partial_root
+    path.join('/') + "/#{notification_type}"
+  end
 end
