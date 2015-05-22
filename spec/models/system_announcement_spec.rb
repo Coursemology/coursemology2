3dda169cf823daf4cc1f9dc3547bb527ccb32a16
Wang Qiang <rovingbreeze@gmail.com>
2015-05-22 14:14:18 +0800

    Add specs

--- spec/models/system_announcement_spec.rb
+++ spec/models/system_announcement_spec.rb
@@ -6,4 +6,14 @@ RSpec.describe SystemAnnouncement, type: :model do
 
     it { is_expected.not_to be_valid }
   end
+
+  describe '.default_scope' do
+    before { create_list(:system_announcement, 3) }
+
+    it 'orders by descending valid_from' do
+      dates = SystemAnnouncement.all.map(&:valid_from)
+      sorted_dates = dates.sort { |a, b| b <=> a }
+      expect(dates).to eq(sorted_dates)
+    end
+  end
 end
