ecca11e485475f533c8fb956055a67d67c2ea7ce
WANG QIANG <rovingbreeze@gmail.com>
2015-05-26 18:01:41 +0800

    Add controller/feature specs for course: components

---
+++ spec/controllers/course/settings_controller_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe Course::SettingsController, type: :controller do
+  let(:instance) { create(:instance) }
+  with_tenant(:instance) do
+    let(:user) { create(:user) }
+    let(:course) { create(:course, creator: user) }
+    before { sign_in(user) }
+
+    describe '#components' do
+      subject { get :components, course_id: course }
+      it { is_expected.to render_template(:components) }
+    end
+
+    describe '#update_components' do
+      let(:ids_to_enable) do
+        allow(controller).to receive(:current_course).and_return(course)
+        all_components = controller.current_component_host.instance_components
+        all_component_ids = all_components.map { |c| c.key.to_s }
+        all_component_ids.sample(1 + rand(all_component_ids.count))
+      end
+      let(:components_params) { { enabled_component_ids: ids_to_enable } }
+      let(:settings) { Course::Settings.new(course.reload) }
+
+      subject do
+        post :update_components, settings_effective: components_params, course_id: course
+      end
+
+      it 'enables the specified component and disables all other components' do
+        subject
+        expect(settings.enabled_component_ids).to eq(ids_to_enable)
+      end
+
+      context 'when course cannot be saved' do
+        before do
+          allow(course).to receive(:save).and_return(false)
+          controller.instance_variable_set(:@course, course)
+          subject
+        end
+
+        it { is_expected.to render_template(:components) }
+      end
+    end
+  end
+end
---
+++ spec/features/course/components_settings_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.feature 'Course: Components' do
+  let!(:instance) { create(:instance) }
+
+  with_tenant(:instance) do
+    let(:user) { create(:user) }
+    let(:course) { create(:course, creator: user) }
+    let(:components)  { Course::ComponentHost.components }
+    let(:sample_component_id) do
+      "settings_effective_enabled_component_ids_#{components.sample.key}"
+    end
+
+    context 'As Course Owner' do
+      before { login_as(user, scope: :user) }
+
+      scenario 'I can view the list of enabled/disabled components' do
+        visit course_components_path(course)
+
+        components.each do |component|
+          expect(page).to have_selector('th', text: component.name)
+          enabled = course.reload.settings(:components, component.key).enabled
+          enabled = enabled.nil? ? component.enabled_by_default? : enabled
+          checkbox = find("#settings_effective_enabled_component_ids_#{component.key}")
+          if enabled
+            expect(checkbox).to be_checked
+          else
+            expect(checkbox).not_to be_checked
+          end
+        end
+      end
+
+      scenario 'I can enable a component' do
+        visit course_components_path(course)
+
+        check(sample_component_id)
+        click_button('submit')
+        expect(page).to have_checked_field(sample_component_id)
+      end
+
+      scenario 'I can disable a component' do
+        visit course_components_path(course)
+
+        uncheck(sample_component_id)
+        click_button('submit')
+        expect(page).to have_unchecked_field(sample_component_id)
+      end
+    end
+  end
+end
