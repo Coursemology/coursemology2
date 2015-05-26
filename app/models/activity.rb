de0c39dce478130eccf3c4e9b13c042f845e031b
Zihuan Xu <xzhflying@163.com>
2015-05-26 10:26:14 +0800

    Add function in activity model to translate key to path

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,11 +2,10 @@
 class Activity < PublicActivity::Activity
   has_many :notification_styles, inverse_of: :activity, dependent: :destroy
 
-  # overwrite public_activity gem's method to set template path for us
-  def template_path(key, partial_root)
+  def template_path
+    partial_root = ''
     path = key.split('.')
-    path.delete_at(0) if path[0] == 'activity'
     path.unshift partial_root
-    path.join('/') + 'activity_feed'
+    path.join('/') + '/activity_feed'
   end
 end
