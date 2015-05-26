57de09abf7f25bc3c1384df3f76fe6d21e801ea3
Minh Tu Le <minhtule05@gmail.com>
2015-05-26 09:27:58 +0700

    Add helper to override route helpers

---
+++ app/helpers/route_overrides_helper.rb
@@ -0,0 +1,39 @@
+module RouteOverridesHelper
+  class << self
+    private
+
+    def mapping_for(from, to)
+      {
+        from.to_s.singularize => to.to_s.singularize,
+        from.to_s.pluralize => to.to_s.pluralize
+      }
+    end
+
+    def map_route_helpers_with(mapping)
+      ['_path', '_url'].each do |suffix|
+        ['', 'new_', 'edit_'].each do |prefix|
+          mapping.each do |from, to|
+            define_method(prefix + from + suffix) do |*forwarded_args|
+              send(prefix + to + suffix, *forwarded_args)
+            end
+          end
+        end
+      end
+    end
+
+    # Override route helper methods e.g. to remove the namespacing in the model class.
+    #
+    # @param from [Symbol, String] The route helper to be overridden. This helper could be generated
+    #        by a form helper link_to but is not actually created from the route setup.
+    # @param to [Symbol, String] The correct route to be used which is created by the route setup.
+    def map_route(from, to:)
+      mapping = mapping_for(from, to)
+      map_route_helpers_with(mapping)
+    end
+  end
+
+  map_route :course_course_achievement_condition_course_condition_achievement,
+            to: :course_achievement_condition_achievement
+  map_route :course_course_achievement_condition_course_condition_level,
+            to: :course_achievement_condition_level
+end
