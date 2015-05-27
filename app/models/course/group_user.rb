a23f868063e8835482bd8d4cf7b30ed6d9145ff5
WANG QIANG <rovingbreeze@gmail.com>
2015-05-28 00:12:06 +0800

    Set default role of group_user to normal

--- app/models/course/group_user.rb
+++ app/models/course/group_user.rb
@@ -1,10 +1,19 @@
 class Course::GroupUser < ActiveRecord::Base
   stampable
 
+  after_initialize :set_defaults, if: :new_record?
+
   enum role: { normal: 0, manager: 1 }
 
   belongs_to :user, inverse_of: :course_group_users
   belongs_to :course_group, class_name: Course::Group.name, inverse_of: :group_users
 
   alias_method :group, :course_group
+
+  private
+
+  # Set default values
+  def set_defaults
+    self.role ||= :normal
+  end
 end
