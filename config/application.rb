bae57ddce42a8ac91792b5e52b1405f30dae81e5
Joel Low <joel@joelsplace.sg>
2015-05-27 10:02:05 +0800

    Restructure all the localisation files so we do not end up with one huge .yml

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,8 @@ class Application < Rails::Application
   # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
   # config.time_zone = 'Central Time (US & Canada)'
 
-  # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-  # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+  # The default locale is :en
+  config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
   # config.i18n.default_locale = :de
 
   # Do not swallow errors in after_commit/after_rollback callbacks.
