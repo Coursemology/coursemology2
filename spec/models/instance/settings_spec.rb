8ba885e75b53a689980d5f2059cc65db8743099a
WANG QIANG <rovingbreeze@gmail.com>
2015-05-28 13:39:56 +0800

    Fixing broken specs

--- spec/models/instance/settings_spec.rb
+++ spec/models/instance/settings_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Instance::Settings, type: :model do
-  ALL_COMPONENTS = [:A, :B, :C, :D, :E, :F]
-  ENABLED_COMPONENTS = [:A, :C, :E]
+  class Component
+    attr_accessor :key
+
+    def initialize(key)
+      @key = key
+    end
+  end
+
+  ALL_COMPONENTS = [:A, :B, :C, :D, :E, :F].map { |key| Component.new(key) }
+  ENABLED_COMPONENTS = [:A, :C, :E].map { |key| Component.new(key) }
 
   let(:settings) do
     class MockSettings
@@ -15,13 +23,13 @@ RSpec.describe Instance::Settings, type: :model do
 
     component_settings = Struct.new(:enabled)
     components_settings = MockSettings.new
-    component_settings_store = {}
+    component_settings_store = HashWithIndifferentAccess.new
     components_settings.stub_method(:settings) do |symbol|
-      enabled = ENABLED_COMPONENTS.include?(symbol)
+      enabled = ENABLED_COMPONENTS.map(&:key).include?(symbol.to_sym)
       component_settings_store[symbol] ||= component_settings.new(enabled)
     end
     components_settings.stub_method(:map) do |&block|
-      ALL_COMPONENTS.map { |symbol| [symbol, settings(symbol)] }.map(&block)
+      ALL_COMPONENTS.map { |component| [component.key, settings(symbol.key)] }.map(&block)
     end
     instance_settings = MockSettings.new
     instance_settings.stub_method(:settings) do |_|
@@ -31,7 +39,7 @@ RSpec.describe Instance::Settings, type: :model do
     instance_settings
   end
 
-  subject { Instance::Settings.new(settings) }
+  subject { Instance::Settings.new(settings, ALL_COMPONENTS) }
 
   describe 'mocks' do
     it 'has many components' do
@@ -42,22 +50,24 @@ RSpec.describe Instance::Settings, type: :model do
 
   describe '#enabled_components' do
     it 'retrieves only enabled components' do
-      enabled_component_ids = subject.enabled_components.map(&:id)
-      expect(enabled_component_ids).to contain_exactly(*ENABLED_COMPONENTS)
+      enabled_component_ids = subject.enabled_components.map { |c| c.id.to_sym }
+      expect(enabled_component_ids).to contain_exactly(*(ENABLED_COMPONENTS.map(&:key)))
     end
   end
 
   describe '#enabled_component_ids' do
     it 'retrieves only enabled components' do
-      expect(subject.enabled_component_ids).to contain_exactly(*ENABLED_COMPONENTS)
+      expected_ids = ENABLED_COMPONENTS.map { |c| c.key.to_s }
+      expect(subject.enabled_component_ids).to contain_exactly(*expected_ids)
     end
   end
 
   describe '#enabled_component_ids=' do
     it 'sets the enabled components' do
-      new_enabled_components = ENABLED_COMPONENTS + [:B]
-      subject.enabled_component_ids = new_enabled_components
-      expect(subject.enabled_component_ids).to contain_exactly(*new_enabled_components)
+      new_enabled_components = ENABLED_COMPONENTS + [Component.new(:B)]
+      expected_ids = new_enabled_components.map { |c| c.key.to_s }
+      subject.enabled_component_ids = new_enabled_components.map { |c| c.key.to_s }
+      expect(subject.enabled_component_ids).to contain_exactly(*expected_ids)
     end
   end
 end
