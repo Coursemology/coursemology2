311b8f123a826e12369631c2bc6113835786e070
WANG QIANG <rovingbreeze@gmail.com>
2015-06-02 13:37:40 +0800

    Implement new/edit Course::Groups

--- app/controllers/course/groups_controller.rb
+++ app/controllers/course/groups_controller.rb
@@ -3,4 +3,33 @@ class Course::GroupsController < Course::ComponentController
 
   def index #:nodoc:
   end
+
+  def new #:nodoc:
+  end
+
+  def create #:nodoc:
+    if @group.save
+      redirect_to(edit_course_group_path(current_course, @group),
+                  success: t('.success', name: @group.name))
+    else
+      render 'new'
+    end
+  end
+
+  def edit #:nodoc:
+  end
+
+  def update #:nodoc:
+    if @group.update_attributes(group_params)
+      redirect_to(course_groups_path(current_course), success: t('.success', title: @group.name))
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def group_params #:nodoc:
+    params.require(:group).permit(:name, user_ids: [])
+  end
 end
