eb35159c0ac151e121684e6d01d9e972ccc5bc3d
Zihuan Xu <xzhflying@163.com>
2015-06-04 20:14:08 +0800

    Add Activity Model
    * Add Activity Model and Migration
    * Add relation between activity and notification model
    * Add #template_path method in activity model to translate key to path

---
+++ app/models/activity.rb
@@ -0,0 +1,16 @@
+# Activity model for customisation & custom methods
+class Activity < PublicActivity::Activity
+  belongs_to :notification, inverse_of: :activity, dependent: :destroy
+
+  # Translate the key of activity to path with a given type of notification
+  #
+  # @example template_path(:popup) => /ClassName/notifications/ActivityType/popup
+  #
+  # @param notification_type [Symbol] the type of notification
+  # @return [String]
+  def template_path(notification_type)
+    return unless [:activity_feed, :email, :popup].include?(notification_type)
+    path = key.split('.')
+    format("/%s/#{notification_type}", path.join('/'))
+  end
+end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,5 @@
 class Notification < ActiveRecord::Base
   acts_as_readable on: :updated_at
+
+  has_one :activity, inverse_of: :notification, dependent: :destroy
 end
