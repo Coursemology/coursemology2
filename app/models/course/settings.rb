41c2566ac41cf10dea1ea67603fda06ceaad4906
WANG QIANG <rovingbreeze@gmail.com>
2015-06-06 10:00:20 +0800

    Add course settings adapters

---
+++ app/models/course/settings.rb
@@ -0,0 +1,23 @@
+# Provides an adapter for configuration settings as a resource for use with controllers and views.
+class Course::Settings
+  include SettingsConcern
+
+  # Initialises the settings adapter
+  #
+  # @param [#settings] settings The settings object provided by the settings_on_rails gem.
+  def initialize(settings)
+    @settings = settings
+  end
+
+  # Update settings with the hash attributes
+  #
+  # @param [Hash] attributes The hash who stores the new settings
+  def update(attributes)
+    attributes.each { |k, v| send("#{k}=", v) }
+    true
+  end
+
+  def persisted? #:nodoc:
+    true
+  end
+end
