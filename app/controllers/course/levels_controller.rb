3b346d8d23338c4975de1d55bdf9f5fc991c2937
Brian Koh <kxmbrian@gmail.com>
2015-06-07 23:26:31 +0800

    Add levels controller #index

--- app/controllers/course/levels_controller.rb
+++ app/controllers/course/levels_controller.rb
@@ -1,2 +1,15 @@
-class Course::LevelsController < ApplicationController
+class Course::LevelsController < Course::ComponentController
+  before_action :number_levels, only: [:index]
+  load_and_authorize_resource
+
+  def index #:nodoc:
+  end
+
+  private
+
+  # This methods ensures that the Course::Levels are numbered
+  # for use by the controller.
+  def number_levels
+    @levels = @course.numbered_levels
+  end
 end
