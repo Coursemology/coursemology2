ebb9f8406d0b6e7e3d6b3c8342fa4aae4677a045
Brian Koh <kxmbrian@gmail.com>
2015-06-08 18:34:40 +0800

    Add level #create feature

--- app/controllers/course/levels_controller.rb
+++ app/controllers/course/levels_controller.rb
@@ -9,6 +9,12 @@ class Course::LevelsController < Course::ComponentController
   end
 
   def create #:nodoc:
+    if @level.save
+      redirect_to(course_levels_path(current_course),
+                  success: t('.success', threshold: @level.experience_points_threshold))
+    else
+      render 'new'
+    end
   end
 
   private
@@ -17,5 +23,9 @@ class Course::LevelsController < Course::ComponentController
   # for use by the controller.
   def number_levels
     @levels = @course.numbered_levels
+  end
+
+  def level_params #:nodoc:
+    params.require(:course_level).permit(:experience_points_threshold)
   end
 end
