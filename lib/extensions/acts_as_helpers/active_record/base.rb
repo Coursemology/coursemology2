6d8ac29d317b78eba6dc98e404441edd82489555
Joel Low <joel@joelsplace.sg>
2015-06-11 14:59:50 +0800

    Remove unnecessary class_eval.

--- lib/extensions/acts_as_helpers/active_record/base.rb
+++ lib/extensions/acts_as_helpers/active_record/base.rb
@@ -3,17 +3,19 @@ module Extensions::ActsAsHelpers::ActiveRecord::Base
     # Decorator for items that give course_users EXP Points
     def acts_as_experience_points_record
       acts_as :experience_points_record, class_name: Course::ExperiencePointsRecord.name
-      class_eval do
-        def manual_exp?
-          false
-        end
-      end
+      include ExperiencePointsInstanceMethods
     end
 
-    # Decorator for abstractions with concreate occurrences which have
+    # Decorator for abstractions with concrete occurrences which have
     # the potential to award course_users EXP Points
     def acts_as_lesson_plan_item
       acts_as :lesson_plan_item, class_name: Course::LessonPlanItem.name
+    end
+  end
+
+  module ExperiencePointsInstanceMethods
+    def manual_exp?
+      false
     end
   end
 end
