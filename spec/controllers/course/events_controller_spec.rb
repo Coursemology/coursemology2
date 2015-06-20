0a5c54ba65137edf4dbe01f100287c09f434429a
Brian Koh <kxmbrian@gmail.com>
2015-06-20 15:45:45 +0800

    Add Course::Event controller and view specs

---
+++ spec/controllers/course/events_controller_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+RSpec.describe Course::EventsController, type: :controller do
+  let!(:instance) { create(:instance) }
+
+  with_tenant(:instance) do
+    let!(:user) { create(:administrator) }
+    let!(:course) { create(:course) }
+    let!(:event_immutable_stub) do
+      stub = create(:course_event, course: course)
+      allow(stub).to receive(:save).and_return(false)
+      allow(stub).to receive(:update).and_return(false)
+      allow(stub).to receive(:destroy).and_return(false)
+      stub
+    end
+
+    before { sign_in(user) }
+
+    describe '#save' do
+      context 'when save fails' do
+        subject { post :create, course_id: course, event: event_immutable_stub }
+        before do
+          controller.instance_variable_set(:@event, event_immutable_stub)
+          subject
+        end
+
+        it { is_expected.to render_template('new') }
+      end
+    end
+
+    describe '#update' do
+      subject do
+        patch :update, course_id: course,
+                       id: event_immutable_stub,
+                       event: { id: event_immutable_stub.id }
+      end
+
+      context 'when update fails' do
+        before do
+          controller.instance_variable_set(:@event, event_immutable_stub)
+          subject
+        end
+
+        it { is_expected.to render_template('edit') }
+      end
+    end
+
+    describe '#destroy' do
+      subject { delete :destroy, course_id: course, id: event_immutable_stub }
+
+      context 'when destroy fails' do
+        before do
+          controller.instance_variable_set(:@event, event_immutable_stub)
+          subject
+        end
+
+        it { is_expected.to redirect_to(course_lesson_plan_path(course)) }
+      end
+    end
+  end
+end
---
+++ spec/features/course/course_event_management_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.feature 'Course: Events' do
+  let!(:instance) { create(:instance) }
+
+  with_tenant(:instance) do
+    let!(:user) { create(:administrator) }
+    let!(:course) { create(:course) }
+    let(:event) { create(:course_event, course: course) }
+    let(:new_event_title) { 'Modified event title' }
+
+    context 'As a Course Administrator' do
+      before do
+        login_as(user, scope: :user)
+      end
+
+      scenario 'I can create a course event' do
+        visit course_lesson_plan_path(course)
+        find_link(nil, href: new_course_event_path(course)).click
+
+        fill_in 'title', with: 'Title'
+        fill_in 'start_time', with: 1.days.from_now
+
+        expect do
+          click_button I18n.t('helpers.submit.event.create')
+        end.to change(course.events, :count).by(1)
+      end
+
+      scenario 'I can edit a course event' do
+        event
+        visit course_lesson_plan_path(course)
+
+        find_link(nil, href: edit_course_event_path(course, event)).click
+        fill_in 'title', with: new_event_title
+        click_button I18n.t('helpers.submit.event.update')
+
+        expect(event.reload.title).to eq(new_event_title)
+      end
+
+      scenario 'I can delete a course event' do
+        event
+        visit course_lesson_plan_path(course)
+
+        expect do
+          find_link(nil, href: course_event_path(course, event)).click
+        end.to change(course.events, :count).by(-1)
+      end
+    end
+  end
+end
