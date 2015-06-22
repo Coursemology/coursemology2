51cee09c46a8d8a23b035c6c07a423aaff2623cd
fonglh <fonglh@gmail.com>
2015-06-22 18:45:25 +0800

    Add tests for duplication of ComplexObjects with children
    Rename descriptions.
    Remove unnecessary tests of Ruby's clone function
    Add ComplexObject stub class with duplicate function
    Add tests to duplicate ComplexObject and ensure children are only
    duplicated once even when referenced by multiple objects.

--- spec/libraries/duplicator_spec.rb
+++ spec/libraries/duplicator_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Test Duplicator Core Class' do
+RSpec.describe Duplicator do
   class SimpleObject
     def initialize(id)
       @id = id
@@ -21,33 +21,19 @@ RSpec.describe 'Test Duplicator Core Class' do
     end
   end
 
-  context 'Without the Duplicator framework' do
-    before :each do
-      @obj_a = SimpleObject.new(1)
-      @obj_b = @obj_a.duplicate(Duplicator.new)
-    end
+  context 'when SimpleObject is duplicated' do
 
-    it 'tests duplicated objects have the same contents' do
-      expect(@obj_a).to eq(@obj_b)
-    end
-
-    it 'tests duplicated objects are not the same object' do
-      expect(@obj_a).to_not be(@obj_b)
-    end
-  end
-
-  context 'With the Duplicator framework' do
     before :each do
       @duplicator = Duplicator.new
       @obj_a = SimpleObject.new(2)
     end
 
-    it 'tests objects not in list are not duplicated' do
+    it 'is not duplicated if not in set' do
       duplicated_object = @duplicator.duplicate_object(@obj_a)
       expect(duplicated_object).to be_nil
     end
 
-    it 'tests that objects in list are duplicated' do
+    it 'is duplicated if in set' do
       @duplicator.duplicate([@obj_a])
       duplicate_of_a = @duplicator.duplicate_of(@obj_a)
 
@@ -56,13 +42,107 @@ RSpec.describe 'Test Duplicator Core Class' do
       expect(duplicate_of_a).to eq(@obj_a)
     end
 
-    it 'ensures objects are only duplicated once' do
+    it 'is duplicated once' do
       @duplicator.duplicate([@obj_a])
       duplicate_of_a = @duplicator.duplicate_of(@obj_a)
       duplicate = @duplicator.duplicate_object(@obj_a)
 
       expect(duplicate).to_not be_nil
       expect(duplicate).to be(duplicate_of_a)
+    end
+  end
+
+  context 'when ComplexObject is duplicated' do
+    # ComplexObject has children
+    class ComplexObject
+      attr_reader :children
+
+      def initialize(id, children)
+        @id = id
+        @children = children
+      end
+
+      def duplicate(duplicator)
+        myself = self.clone
+        new_children = Array.new
+
+        @children.each do |child|
+          new_child = duplicator.duplicate_object(child)
+          new_children << new_child unless new_child.nil?
+        end
+
+        # example if object has no accessor for the children
+        myself.instance_variable_set(:@children, new_children)
+        myself
+      end
+
+      def ==(other)
+        self.class == other.class && self.state == other.state
+      end
+
+      protected
+
+      def state
+        [@id, @children]
+      end
+    end
+
+    before :each do
+      @duplicator = Duplicator.new
+
+      @s1 = SimpleObject.new(1)
+      @s2 = SimpleObject.new(2)
+
+      # setup 2 objects with overlapping children
+      @c1 = ComplexObject.new(11, [@s1, @s2])
+      @c2 = ComplexObject.new(12, [@s1])
+
+      # setup an even more complicated object
+      @c3 = ComplexObject.new(13, [@c1, @c2])
+    end
+
+    it 'duplicates objects in given list (no children)' do
+      @duplicator.duplicate([@c1])
+      dup_c1 = @duplicator.duplicate_of(@c1)
+
+      expect(dup_c1.children).to be_empty
+    end
+
+    it 'duplicates objects in given list (all children)' do
+      @duplicator.duplicate([@c1, @s1, @s2])
+      dup_c1 = @duplicator.duplicate_of(@c1)
+
+      orig_children = @c1.children
+      dup_children = dup_c1.children
+
+      # both children should be duplicated, same values, different objects
+      expect(dup_children.length).to be(2)
+      expect(orig_children).to eq(dup_children)
+      expect(orig_children).to_not be(dup_children)
+    end
+
+    it 'duplicates objects referenced in 2 places once' do
+      @duplicator.duplicate([@c1, @c2, @s1, @s2])
+      dup_c1 = @duplicator.duplicate_of(@c1)
+      dup_c2 = @duplicator.duplicate_of(@c2)
+
+      expect(dup_c1.children[0]).to be(dup_c2.children[0])
+    end
+
+    it 'duplicates objects with ComplexObject children' do
+      @duplicator.duplicate([@c3, @c1, @c2, @s1, @s2])
+      dup_c3 = @duplicator.duplicate_of(@c3)
+
+      expect(dup_c3.children.length).to be(2)
+      expect(dup_c3.children[0]).to eq(@c1)
+      expect(dup_c3.children[1]).to eq(@c2)
+      expect(dup_c3.children[0]).to_not be(@c1)
+      expect(dup_c3.children[1]).to_not be(@c2)
+    end
+
+    it 'partially duplicates objects when not all children are in list' do
+      pending('implementation')
+      fail
     end
   end
 end
