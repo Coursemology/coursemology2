03cfb1f3d14d43177c440eb45415abb888472628
WANG QIANG <rovingbreeze@gmail.com>
2015-06-23 17:13:04 +0800

    Namespace admin helpers to system/admin

--- spec/helpers/system/admin/announcements_helper_spec.rb
+++ spec/helpers/system/admin/announcements_helper_spec.rb
@@ -10,5 +10,5 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe Admin::AnnouncementsHelper, type: :helper do
+RSpec.describe System::Admin::AnnouncementsHelper, type: :helper do
 end
