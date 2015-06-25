ad5b3ec6431805a9d2d23aa2239cbe9be8e1ff1a
Joel Low <joel@joelsplace.sg>
2015-06-25 11:42:50 +0800

    Implement enabling/disabling course registration codes.

--- app/controllers/course/user_invitations_controller.rb
+++ app/controllers/course/user_invitations_controller.rb
@@ -7,7 +7,7 @@ class Course::UserInvitationsController < Course::ComponentController
 
   def create # :nodoc:
     if invite
-      redirect_to course_users_invitations_path(current_course), success: t('.success')
+      redirect_to create_redirect_path, success: t('.success')
     else
       render 'new'
     end
@@ -23,22 +23,49 @@ class Course::UserInvitationsController < Course::ComponentController
   def course_user_invitation_params # :nodoc:
     invitations_attributes = { course_user: [:name], user_email: [:email] }
     @invite_params ||= params.require(:course).
-                       permit(:invitations_file, invitations_attributes: invitations_attributes)
+                       permit(:invitations_file, :registration_key,
+                              invitations_attributes: invitations_attributes)
   end
 
   # Determines the parameters to be passed to the invitation service object.
   #
-  # @return [Hash]
+  # @return [Tempfile|Hash|String|nil]
   def invitation_params
-    course_user_invitation_params[:invitations_file].try(:tempfile) ||
-      course_user_invitation_params[:invitations_attributes]
+    @invitation_params ||= course_user_invitation_params[:invitations_file].try(:tempfile) ||
+                           course_user_invitation_params[:invitations_attributes] ||
+                           course_user_invitation_params[:registration_key]
+  end
+
+  # Determines if the user uploaded a file.
+  #
+  # @return [bool]
+  def invite_by_file?
+    invitation_params.is_a?(Tempfile)
+  end
+
+  # Determines if the user keyed in entries manually.
+  #
+  # @return [bool]
+  def invite_by_entry?
+    invitation_params.is_a?(Hash)
+  end
+
+  # Determines if the user is changing the registration code enabled state.
+  #
+  # @return [bool]
+  def invite_by_registration_code?
+    invitation_params.nil? || invitation_params.is_a?(String)
   end
 
   # Invites the users via the service object.
   #
   # @return [bool] True if the invitation was successful.
   def invite
-    invitation_service.invite(invitation_params)
+    if invite_by_file? || invite_by_entry?
+      invitation_service.invite(invitation_params)
+    elsif invite_by_registration_code?
+      invitation_service.enable_registration_code(invitation_params == 'checked'.freeze)
+    end
   rescue CSV::MalformedCSVError => e
     current_course.errors[:users_file] = e.message
     return false
@@ -49,5 +76,16 @@ class Course::UserInvitationsController < Course::ComponentController
   # @return [Course::UserInvitationService]
   def invitation_service
     @invitation_service ||= Course::UserInvitationService.new(current_course)
+  end
+
+  # The path to redirect to after the {#create} action.
+  #
+  # @return [String]
+  def create_redirect_path
+    if invite_by_file? || invite_by_entry?
+      course_users_invitations_path(current_course)
+    elsif invite_by_registration_code?
+      invite_course_users_path(current_course, anchor: 'registration_code')
+    end
   end
 end
