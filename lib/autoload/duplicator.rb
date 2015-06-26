1dc7077109fe8f20be82e9fb345590e5cecf56aa
fonglh <fonglh@gmail.com>
2015-06-26 18:14:23 +0800

    Redefine Duplicator API and tests
    Switch to exclusion list instead of inclusion, update tests accordingly.
    Use dup instead of clone so frozen objects won't remain frozen after
    duplication.
    Use Duplicator to call dup instead of delegating it to the object's
    duplicate method. Object will have to implement a duplicate method
    similar to ComplexObject#duplicate.
    Add tests for duplicating cyclic graphs.

--- lib/autoload/duplicator.rb
+++ lib/autoload/duplicator.rb
@@ -1,45 +1,26 @@
-require 'set'
-
 class Duplicator
-  def initialize
+  # Create an instance of Duplicator to track duplicated objects.
+  #
+  # @param [Array] excluded_objects A list of objects to be excluded from duplication
+  def initialize(excluded_objects=[])
     @duplicated_objects = {}  # hash to check what has been duplicated
-    @to_dup_objects = Set.new # hash to check what should be duplicated
+    @exclusion_set = excluded_objects.to_set  # set to check what should be excluded
   end
 
-  # Check the duplicated_objects hash to see if source_object has already been duplicated.
-  # If it has, return it.
-  # Else check if it should be duplicated. If yes, call its duplicate method.
-  # Else save nil into the hash and return nil.
+  # Deep copy source_object and its children. source_object must provide a duplicate
+  # method which duplicates its children.
   #
-  # @param source_object [Object] The object to be duplicated.
+  # @param [Object#duplicate] source_object The object to be duplicated.
   # @return duplicated_object A reference to the duplicated object.
   def duplicate_object(source_object)
     if @duplicated_objects.has_key?(source_object)
       @duplicated_objects[source_object]
-    elsif @to_dup_objects.include?(source_object)
-      @duplicated_objects[source_object] = source_object.duplicate(self)
+    elsif not @exclusion_set.include?(source_object)
+      @duplicated_objects[source_object] = source_object.dup
+      source_object.duplicate(self)
+      @duplicated_objects[source_object]
     else
       @duplicated_objects[source_object] = nil
     end
-  end
-
-  # Take in a list of checked objects from the DuplicationView and duplicate them all.
-  # Also convert the list to a hash for faster lookup.
-  #
-  # @param to_duplicate_list [Array] The list of objects selected for duplication.
-  # @return [void]
-  def duplicate(to_duplicate_list)
-    @to_dup_objects = to_duplicate_list.to_set
-    to_duplicate_list.each do |item|
-      duplicate_object(item)
-    end
-  end
-
-  # Returns the duplicate of an object if it exists, nil otherwise.
-  #
-  # @param [Object] object
-  # @return [Object, nil]
-  def duplicate_of(object)
-    @duplicated_objects[object]
   end
 end
--- spec/libraries/duplicator_spec.rb
+++ spec/libraries/duplicator_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe Duplicator do
     end
 
     def duplicate(duplicator)
-      self.clone
     end
 
     def ==(other)
@@ -21,21 +20,87 @@ RSpec.describe Duplicator do
     end
   end
 
-  context 'when SimpleObject is duplicated' do
+  # ComplexObject has children
+  class ComplexObject
+    attr_reader :children
 
+    def initialize(id, children)
+      @id = id
+      @children = children
+    end
+
+    def duplicate(duplicator)
+      new_children = Array.new
+
+      @children.each do |child|
+        new_child = duplicator.duplicate_object(child)
+        new_children << new_child unless new_child.nil?
+      end
+
+      # example if object has no accessor for the children
+      # self is already duplicated by Duplicator. Find the duplicated version
+      duplicator.duplicate_object(self).instance_variable_set(:@children, new_children)
+    end
+
+    def ==(other)
+      self.class == other.class && self.state == other.state
+    end
+
+    protected
+
+    def state
+      [@id, @children]
+    end
+  end
+
+  def create_complex_objects
+    @s1 = SimpleObject.new(1)
+    @s2 = SimpleObject.new(2)
+    @s3 = SimpleObject.new(3)
+
+    # setup 2 objects with overlapping children
+    @c1 = ComplexObject.new(11, [@s1, @s2])
+    @c2 = ComplexObject.new(12, [@s1])
+
+    # setup an even more complicated object
+    @c3 = ComplexObject.new(13, [@c1, @c2, @s3])
+  end
+
+  def create_cyclic_graph
+    #
+    #       ------> c3 ---
+    #       |       ^    |
+    # c1 -> c2      |    -> c5
+    #       |       |       |
+    #       ------> c4 <-----
+    #               |
+    #               -----> s1
+    #
+    @s1 = SimpleObject.new(1)
+    @c4 = ComplexObject.new(14, [])      # assign children later
+    @c5 = ComplexObject.new(15, [@c4])
+    @c3 = ComplexObject.new(13, [@c5])
+    @c2 = ComplexObject.new(12, [@c3, @c4])
+    @c1 = ComplexObject.new(11, [@c2])
+
+    @c4.instance_variable_set(:@children, [@s1, @c3])   # create cycle
+  end
+
+  context 'when SimpleObject is duplicated' do
     before :each do
-      @duplicator = Duplicator.new
       @obj_a = SimpleObject.new(2)
     end
 
-    it 'is not duplicated if not in set' do
+    it 'is not duplicated if excluded' do
+      @duplicator = Duplicator.new([@obj_a])
       duplicated_object = @duplicator.duplicate_object(@obj_a)
+
       expect(duplicated_object).to be_nil
     end
 
-    it 'is duplicated if in set' do
-      @duplicator.duplicate([@obj_a])
-      duplicate_of_a = @duplicator.duplicate_of(@obj_a)
+    it 'is duplicated by default' do
+      @duplicator= Duplicator.new
+      duplicate_of_a = @duplicator.duplicate_object(@obj_a)
 
       expect(duplicate_of_a).to_not be_nil
       expect(duplicate_of_a).to_not be(@obj_a)
@@ -43,106 +108,160 @@ RSpec.describe Duplicator do
     end
 
     it 'is duplicated once' do
-      @duplicator.duplicate([@obj_a])
-      duplicate_of_a = @duplicator.duplicate_of(@obj_a)
-      duplicate = @duplicator.duplicate_object(@obj_a)
+      @duplicator= Duplicator.new
+      duplicate_of_a = @duplicator.duplicate_object(@obj_a)
+      duplicate_of_a_2 = @duplicator.duplicate_object(@obj_a)
 
-      expect(duplicate).to_not be_nil
-      expect(duplicate).to be(duplicate_of_a)
+      expect(duplicate_of_a).to_not be_nil
+      expect(duplicate_of_a).to be(duplicate_of_a_2)
     end
   end
 
-  context 'when ComplexObject is duplicated' do
-    # ComplexObject has children
-    class ComplexObject
-      attr_reader :children
-
-      def initialize(id, children)
-        @id = id
-        @children = children
-      end
-
-      def duplicate(duplicator)
-        myself = self.clone
-        new_children = Array.new
-
-        @children.each do |child|
-          new_child = duplicator.duplicate_object(child)
-          new_children << new_child unless new_child.nil?
-        end
-
-        # example if object has no accessor for the children
-        myself.instance_variable_set(:@children, new_children)
-        myself
-      end
-
-      def ==(other)
-        self.class == other.class && self.state == other.state
-      end
-
-      protected
-
-      def state
-        [@id, @children]
-      end
-    end
-
+  context 'when ComplexObject is duplicated without exclusions' do
     before :each do
+      create_complex_objects
       @duplicator = Duplicator.new
-
-      @s1 = SimpleObject.new(1)
-      @s2 = SimpleObject.new(2)
-
-      # setup 2 objects with overlapping children
-      @c1 = ComplexObject.new(11, [@s1, @s2])
-      @c2 = ComplexObject.new(12, [@s1])
-
-      # setup an even more complicated object
-      @c3 = ComplexObject.new(13, [@c1, @c2])
     end
 
-    it 'duplicates objects in given list (no children)' do
-      @duplicator.duplicate([@c1])
-      dup_c1 = @duplicator.duplicate_of(@c1)
-
-      expect(dup_c1.children).to be_empty
-    end
-
-    it 'duplicates objects in given list (all children)' do
-      @duplicator.duplicate([@c1, @s1, @s2])
-      dup_c1 = @duplicator.duplicate_of(@c1)
+    it 'duplicates object' do
+      dup_c1 = @duplicator.duplicate_object(@c1)
 
       orig_children = @c1.children
       dup_children = dup_c1.children
 
+      expect(dup_c1).to eq(@c1)
+      expect(dup_c1).to_not be(@c1)
+
       # both children should be duplicated, same values, different objects
       expect(dup_children.length).to be(2)
-      expect(orig_children).to eq(dup_children)
-      expect(orig_children).to_not be(dup_children)
+      (0..1).each do |i|
+        expect(orig_children[i]).to eq(dup_children[i])
+        expect(orig_children[i]).to_not be(dup_children[i])
+      end
     end
 
     it 'duplicates objects referenced in 2 places once' do
-      @duplicator.duplicate([@c1, @c2, @s1, @s2])
-      dup_c1 = @duplicator.duplicate_of(@c1)
-      dup_c2 = @duplicator.duplicate_of(@c2)
+      dup_c1 = @duplicator.duplicate_object(@c1)
+      dup_c2 = @duplicator.duplicate_object(@c2)
 
+      # dup_s1 should be the same object
       expect(dup_c1.children[0]).to be(dup_c2.children[0])
+
+      # test that @c1 and @c2 have the same values but are not the same object
+      expect(dup_c1).to eq(@c1)
+      expect(dup_c2).to eq(@c2)
+      expect(dup_c1).to_not be(@c1)
+      expect(dup_c2).to_not be(@c2)
     end
 
     it 'duplicates objects with ComplexObject children' do
-      @duplicator.duplicate([@c3, @c1, @c2, @s1, @s2])
-      dup_c3 = @duplicator.duplicate_of(@c3)
+      dup_c3 = @duplicator.duplicate_object(@c3)
 
-      expect(dup_c3.children.length).to be(2)
+      expect(dup_c3.children.length).to be(3)
       expect(dup_c3.children[0]).to eq(@c1)
       expect(dup_c3.children[1]).to eq(@c2)
       expect(dup_c3.children[0]).to_not be(@c1)
       expect(dup_c3.children[1]).to_not be(@c2)
     end
+  end
 
-    it 'partially duplicates objects when not all children are in list' do
-      pending('implementation')
-      fail
+  context 'when objects are excluded' do
+    before :each do
+      create_complex_objects
+    end
+
+    it 'duplicates ComplexObject but not excluded children' do
+      duplicator = Duplicator.new([@s1, @s2])
+      dup_c1 = duplicator.duplicate_object(@c1)
+
+      expect(dup_c1.children).to be_empty
+      expect(dup_c1).to_not be(@c1)
+    end
+
+    it 'partially duplicates objects when some children are excluded' do
+      duplicator = Duplicator.new([@s2, @c2])
+      dup_c3 = duplicator.duplicate_object(@c3)
+
+      dup_c1_children = dup_c3.children[0].children
+
+      expect(dup_c3.children.length).to be(2)
+      expect(dup_c1_children.length).to be(1)
+      expect(dup_c1_children[0]).to eq(@s1)
+      expect(dup_c1_children[0]).to_not be(@s1)
+    end
+  end
+
+  context 'when object graph has cycles' do
+    before :each do
+      create_cyclic_graph
+    end
+
+    it 'duplicates cyclic two object graph' do
+      c1 = ComplexObject.new(11, [])
+      c2 = ComplexObject.new(12, [c1])
+      c1.instance_variable_set(:@children, [c2])
+      duplicator = Duplicator.new
+      dup_c1 = duplicator.duplicate_object(c1)
+
+      expect(dup_c1).to eq(c1)
+      expect(dup_c1).to_not be(c1)
+    end
+
+    it 'duplicates cyclic graph' do
+      duplicator = Duplicator.new
+      dup_c1 = duplicator.duplicate_object(@c1)
+
+      expect(dup_c1).to eq(@c1)
+      expect(dup_c1).to_not be(@c1)
+    end
+
+    it 'duplicates cyclic graph without excluded tail' do
+      duplicator = Duplicator.new([@s1])
+      dup_c1 = duplicator.duplicate_object(@c1)
+
+      # get original and duplicated node c4
+      c4 = @c1.children[0].children[1]
+      dup_c4 = dup_c1.children[0].children[1]
+
+      expect(dup_c1).to_not eq(@c1)
+      expect(dup_c4.children.length).to_not eq(c4.children.length)
+    end
+
+    it 'duplicates cyclic graph without c5' do
+      duplicator = Duplicator.new([@c5])
+      dup_c1 = duplicator.duplicate_object(@c1)
+
+      dup_c3 = dup_c1.children[0].children[0]
+
+      expect(dup_c1).to_not be(@c1)
+      expect(dup_c3.children).to be_empty
+    end
+
+    it 'duplicates cyclic graph without c4' do
+      duplicator = Duplicator.new([@c4])
+      dup_c1 = duplicator.duplicate_object(@c1)
+
+      # should be left with c1 -> c2 -> c3 -> c5
+      dup_c2 = dup_c1.children[0]
+      dup_c3 = dup_c2.children[0]
+      dup_c5 = dup_c3.children[0]
+
+      expect(dup_c1.children.length).to be(1)
+      expect(dup_c2.children.length).to be(1)
+      expect(dup_c3.children.length).to be(1)
+      expect(dup_c5.children.length).to be(0)
+    end
+
+    it 'duplicates sub-graph from c3' do
+      duplicator = Duplicator.new
+      dup_c3 = duplicator.duplicate_object(@c3)
+
+      dup_c4 = dup_c3.children[0].children[0]
+
+      expect(dup_c3).to_not be(@c3)
+      expect(dup_c4.children[0]).to eq(@s1)
+      # check cycle
+      expect(dup_c4.children[1]).to be(dup_c3)
     end
   end
 end
