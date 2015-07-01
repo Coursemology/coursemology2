4a27e836cfac655d9d7c8947cce2585860fdb461
WANG QIANG <rovingbreeze@gmail.com>
2015-07-01 14:07:17 +0800

    Add active_models to handle sidebar settings

---
+++ app/models/course/settings/sidebar.rb
@@ -0,0 +1,45 @@
+class Course::Settings::Sidebar
+  include ActiveModel::Model
+  include ActiveModel::Conversion
+
+  attr_reader :sidebar_items
+
+  # @param [#settings] settings The settings object provided by the settings_on_rails gem.
+  # @param [Array<Hash>] sidebar_items The sidebar items.
+  def initialize(settings, sidebar_items)
+    @settings = settings
+    @sidebar_items = begin
+      sidebar_items = sidebar_items.map do |item|
+        Course::Settings::SidebarItem.new(settings, item)
+      end
+      sidebar_items.sort_by(&:weight)
+    end
+  end
+
+  # Update settings with the hash attributes
+  #
+  # @param [Hash] attributes The hash who stores the new settings
+  def update(attributes)
+    attributes.each { |k, v| send("#{k}=", v) }
+    valid?
+  end
+
+  # Read order from attributes and change the order of sidebar items.
+  #
+  # @param [Array<Hash>] attributes the attributes which indicates the new order.
+  def sidebar_items_attributes=(attributes)
+    attributes.values.each do |attribute|
+      key = attribute[:id]
+      new_weight = attribute[:weight].to_i
+      @settings.settings(key).weight = new_weight
+    end
+  end
+
+  def persisted? #:nodoc
+    true
+  end
+
+  def valid? #:nodoc
+    sidebar_items.all?(&:valid?)
+  end
+end
