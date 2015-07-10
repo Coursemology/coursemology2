ad2f342a34e9ed4dbbc7943c732d25004e43b0c8
Zihuan Xu <xzhflying@163.com>
2015-07-10 14:17:52 +0800

    Add Notifier::Base and ActivityWrapper to manage notifications
    * Add Notifier::Base and ActivityWrapper
    * Add related tests

---
+++ lib/autoload/notifier/base/activity_wrapper.rb
@@ -0,0 +1,25 @@
+class Notifier::Base::ActivityWrapper < SimpleDelegator
+  # Send notifications according to input type and recipient
+  #
+  # @param [Object] recipient The recipient of the notification
+  # @param [Symbol] type The type of notification
+  def notify(recipient, type)
+    super(recipient, type).tap do |notification|
+      @notifier.send(:notify, recipient, notification)
+    end
+
+    self
+  end
+
+  # Save activity and send out pending emails
+  def save(*)
+    super.tap do |result|
+      @notifier.send(:send_pending_emails) if result
+    end
+  end
+
+  def initialize(notifier, activity) #:nodoc:
+    super(activity)
+    @notifier = notifier
+  end
+end
