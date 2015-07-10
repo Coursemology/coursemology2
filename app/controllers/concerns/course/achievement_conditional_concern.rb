e45294e6b6a4f258a5394868795afe994715c702
Darius Foo <darius.foo.tw@gmail.com>
2015-07-10 19:38:56 +0800

    Standardised condition new and edit pages
    Added breadcrumbs, removed non-standard back link, made buttons look
    consistent

--- app/controllers/concerns/course/achievement_conditional_concern.rb
+++ app/controllers/concerns/course/achievement_conditional_concern.rb
@@ -1,9 +1,22 @@
 module Course::AchievementConditionalConcern
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :add_conditional_breadcrumbs
+  end
+
   def return_to_path
     edit_course_achievement_path(current_course, @conditional)
   end
 
   def set_conditional
     @conditional = Course::Achievement.find(params[:achievement_id])
+  end
+
+  private
+
+  def add_conditional_breadcrumbs
+    add_breadcrumb :index, :course_achievements_path
+    add_breadcrumb @conditional.title, edit_course_achievement_path(current_course, @conditional)
   end
 end
