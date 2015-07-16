3f056815991e05071f53a08543b5d939a4089b81
Joel Low <joel@joelsplace.sg>
2015-07-16 15:28:40 +0800

    Add missing default course component specification.

---
+++ spec/components/course/component_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Course::ComponentHost::Component do
+  class self::DummyComponent
+    include Course::ComponentHost::Component
+
+    def initialize(*)
+    end
+  end
+
+  context 'when a class first includes the module' do
+    subject { self.class::DummyComponent.new }
+    it 'has an empty sidebar ' do
+      expect(subject.sidebar_items).to eq([])
+    end
+
+    it 'is enabled' do
+      expect(subject.enabled_by_default?).to be(true)
+    end
+
+    it 'has a generated key' do
+      expect(subject.key).to be_a(Symbol)
+      expect(subject.key.to_s).to match(/dummy_component$/)
+    end
+  end
+end
