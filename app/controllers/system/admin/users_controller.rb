283f2defeb46d3374a7689de2ac93b036e8ebc0e
WANG QIANG <rovingbreeze@gmail.com>
2015-07-20 12:49:48 +0800

    Implement Update and destroy users

--- app/controllers/system/admin/users_controller.rb
+++ app/controllers/system/admin/users_controller.rb
@@ -5,4 +5,26 @@ class System::Admin::UsersController < System::Admin::Controller
   def index
     @users = @users.ordered_by_name.includes(:emails).page(params[:page])
   end
+
+  def update
+    if @user.update_attributes(user_params)
+      redirect_to admin_users_path, success: t('.success', user: @user.name)
+    else
+      redirect_to admin_users_path, danger: @user.errors.full_messages.to_sentence
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      redirect_to admin_users_path, success: t('.success', user: @user.name)
+    else
+      redirect_to admin_users_path, danger: @user.errors.full_messages.to_sentence
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:role)
+  end
 end
