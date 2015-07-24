7dcadc12b07182aa97e3a6a2039547d4f2d077ce
Joel Low <joel@joelsplace.sg>
2015-07-24 11:27:26 +0800

    Use factory inheritance instead of building the factory from scratch.

--- spec/factories/course_assessment_assessments.rb
+++ spec/factories/course_assessment_assessments.rb
@@ -1,20 +1,8 @@
 FactoryGirl.define do
   sequence(:course_assessment_assessment_name) { |n| "Assessment #{n}" }
-  factory :course_assessment_assessment, class: Course::Assessment, aliases: [:assessment] do
-    transient do
-      course nil
-    end
-
-    lesson_plan_item do
-      options = {}
-      options[:course] = course if course
-      build(:course_lesson_plan_item, options)
-    end
-    tab do
-      options = {}
-      options[:course] = course if course
-      build(:course_assessment_tab, options)
-    end
+  factory :course_assessment_assessment, class: Course::Assessment, aliases: [:assessment],
+          parent: :course_lesson_plan_item do
+    tab { build(:course_assessment_tab, course: course) }
     title { generate(:course_assessment_assessment_name) }
     base_exp 1000
   end
