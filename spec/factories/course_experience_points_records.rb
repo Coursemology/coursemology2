b39d530888368c68743b5b418fe0ae3b980f0a22
Joel Low <joel@joelsplace.sg>
2015-07-27 15:59:56 +0800

    Use factory inheritance instead of reimplementing the factory.

--- spec/factories/course_experience_points_records.rb
+++ spec/factories/course_experience_points_records.rb
@@ -1,6 +1,14 @@
 FactoryGirl.define do
   factory :course_experience_points_record, class: Course::ExperiencePointsRecord.name do
-    course_user
+    transient do
+      course { build(:course) }
+      user { build(:user) }
+    end
+
+    course_user do
+      course.course_users.find_by(user: user) ||
+        build(:course_user, course: course, user: user)
+    end
     points_awarded { rand(1..20) * 100 }
     reason 'EXP for some event'
 
