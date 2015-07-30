2cf1e52edbacd5da6bb1066f881cd99bfcfbf83e
Darius Foo <darius.foo.tw@gmail.com>
2015-07-30 14:03:52 +0800

    dependent: :destroy for categories and tabs

--- app/models/course/assessment/category.rb
+++ app/models/course/assessment/category.rb
@@ -1,6 +1,7 @@
 # Represents a category of assessments. This is typically 'Mission' and 'Training'.
 class Course::Assessment::Category < ActiveRecord::Base
   belongs_to :course, inverse_of: :assessment_categories
-  has_many :tabs, class_name: Course::Assessment::Tab.name, inverse_of: :category
+  has_many :tabs, class_name: Course::Assessment::Tab.name, inverse_of: :category,
+           dependent: :destroy
   has_many :assessments, through: :tabs
 end
