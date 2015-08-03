317dca65919ffa5be1a79c7e49f7a46133efa39a
Joel Low <joel@joelsplace.sg>
2015-08-03 16:43:31 +0800

    Implement an interface for attempting questions.
    This also defines the workflow transitions for submissions and answers.

--- app/models/course/assessment/answer.rb
+++ app/models/course/assessment/answer.rb
@@ -1,5 +1,17 @@
 class Course::Assessment::Answer < ActiveRecord::Base
+  include Workflow
   actable
+
+  workflow do
+    state :attempting do
+      event :submit, transitions_to: :submitted
+    end
+    state :submitted do
+      event :unsubmit, transitions_to: :attempting
+      event :grade, transitions_to: :graded
+    end
+    state :graded
+  end
 
   belongs_to :submission, inverse_of: :answers
   belongs_to :question, inverse_of: nil
--- app/models/course/assessment/question.rb
+++ app/models/course/assessment/question.rb
@@ -5,4 +5,17 @@ class Course::Assessment::Question < ActiveRecord::Base
   has_and_belongs_to_many :tags
 
   delegate :to_partial_path, to: :actable
+
+  # Attempts the given question in the submission. This builds a new answer for the current
+  # question.
+  #
+  # @param [Course::Assessment::Submission] submission The submission which the answer should
+  #   belong to.
+  # @return [Course::Assessment::Answer] The answer corresponding to the question. It is required
+  #   that the {Course::Assessment::Answer#question} property be the same as +self+. The result
+  #   should not be persisted.
+  def attempt(submission)
+    return actable.attempt(submission) if actable
+    fail NotImplementedError, 'Questions must implement the #attempt method for submissions.'
+  end
 end
