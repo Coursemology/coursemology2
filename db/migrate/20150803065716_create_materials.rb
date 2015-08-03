e5f6e6846c6b442fa7f237d0db9d0b8398c7017d
WANG QIANG <rovingbreeze@gmail.com>
2015-08-03 15:56:18 +0800

    Declare material models and specs

---
+++ db/migrate/20150803065716_create_materials.rb
@@ -0,0 +1,24 @@
+class CreateMaterials < ActiveRecord::Migration
+  def change
+    create_table :course_material_folders do |t|
+      t.references :parent_folder, foreign_key: { references: :course_material_folders }
+      t.references :owner, polymorphic: true
+      t.string :name, null: false
+      t.text :description
+      t.datetime :start_at, null: false
+      t.datetime :end_at
+
+      t.userstamps null: false, foreign_key: { references: :users }
+      t.timestamps null: false
+    end
+
+    create_table :course_materials do |t|
+      t.references :folder, null: false, foreign_key: { references: :course_material_folders }
+      t.string :name, null: false
+      t.text :description
+
+      t.userstamps null: false, foreign_key: { references: :users }
+      t.timestamps null: false
+    end
+  end
+end
