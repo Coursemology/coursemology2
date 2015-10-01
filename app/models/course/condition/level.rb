f9b5a3645100ba05fff9642378cbf44e06d70cde
Minh-Tu Le <minhtule05@gmail.com>
2015-10-02 01:49:06 +0800

    Add verifier to level condition

--- app/models/course/condition/level.rb
+++ app/models/course/condition/level.rb
@@ -5,4 +5,13 @@ class Course::Condition::Level < ActiveRecord::Base
   def title
     self.class.human_attribute_name('title.title', value: minimum_level)
   end
+
+  # Checks if the user satisfies the minimum level condition.
+  #
+  # @param [CourseUser] course_user The user that the level condition is being checked on. The user
+  #   must respond to `level_number` message and return the user's current level as an Fixnum.
+  # @return [Boolean] true if the user is above or equal the minimum level and false otherwise.
+  def satisfied_by?(course_user)
+    course_user.level_number >= minimum_level
+  end
 end
--- spec/models/course/condition/level_spec.rb
+++ spec/models/course/condition/level_spec.rb
@@ -10,4 +10,26 @@ RSpec.describe Course::Condition::Level, type: :model do
         to eq(Course::Condition::Level.human_attribute_name('title.title', value: 10))
     end
   end
+
+  describe '#satisfied_by?' do
+    let(:course_user) do
+      course_user = double
+      allow(course_user).to receive(:level_number).and_return(10)
+      course_user
+    end
+
+    context "when the user's level is above or equal to the minimum level" do
+      it 'returns true' do
+        subject.minimum_level = 9
+        expect(subject.satisfied_by?(course_user)).to be_truthy
+      end
+    end
+
+    context "when the user's level is below the minimum level" do
+      it 'returns false' do
+        subject.minimum_level = 11
+        expect(subject.satisfied_by?(course_user)).to be_falsey
+      end
+    end
+  end
 end
