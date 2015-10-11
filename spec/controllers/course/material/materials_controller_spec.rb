30c0b123df8b7f9c0f68fde6a7a4d5a594ed9d01
WANG QIANG <rovingbreeze@gmail.com>
2015-10-11 12:22:16 +0800

    Add specs for materials management

---
+++ spec/controllers/course/material/materials_controller_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe Course::Material::MaterialsController, type: :controller do
+  let(:instance) { create(:instance) }
+
+  with_tenant(:instance) do
+    let(:user) { create(:administrator) }
+    let(:material_stub) do
+      stub = create(:material)
+      allow(stub).to receive(:destroy).and_return(false)
+      stub
+    end
+    let(:folder) { material_stub.folder }
+    let(:course) { folder.course }
+
+    before { sign_in(user) }
+
+    describe '#update' do
+      let(:file) { fixture_file_upload('files/picture.jpg', 'image/jpeg') }
+      let(:attributes) { attributes_for(:material, file: file) }
+      subject do
+        patch :update, course_id: course, folder_id: folder,
+                       id: material_stub, material: attributes
+      end
+
+      context 'when a different file is given' do
+        it 'changes the file' do
+          old_file_url = material_stub.attachment.file_upload.url
+          subject
+          expect(material_stub.attachment.reload.file_upload.url).not_to eq(old_file_url)
+        end
+      end
+    end
+
+    describe '#destroy' do
+      subject { delete :destroy, course_id: course, folder_id: folder, id: material_stub }
+
+      context 'when material cannot be destroyed' do
+        before do
+          controller.instance_variable_set(:@material, material_stub)
+          subject
+        end
+
+        it { is_expected.to redirect_to(course_material_folder_path(course, folder)) }
+
+        it 'shows a flash message' do
+          expect(flash[:danger]).to eq(I18n.t('course.material.materials.destroy.failure'))
+        end
+      end
+    end
+  end
+end
---
+++ spec/factories/course_materials.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :course_material, class: Course::Material.name, aliases: [:material] do
+    folder
+    sequence(:name) { |n| "Material #{n}" }
+    sequence(:description) { |n| "Material Description #{n}" }
+    attachment
+  end
+end
