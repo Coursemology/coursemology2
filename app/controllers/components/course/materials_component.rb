81e407996eab329adaaa4ca0b3eb6ca7237368ba
WANG QIANG <rovingbreeze@gmail.com>
2015-10-13 19:50:59 +0800

    Implement materials settings & declare settings sidebar item

--- app/controllers/components/course/materials_component.rb
+++ app/controllers/components/course/materials_component.rb
@@ -2,13 +2,34 @@ class Course::MaterialsComponent < SimpleDelegator
   include Course::ControllerComponentHost::Component
 
   def sidebar_items
+    main_sidebar_items + settings_sidebar_items
+  end
+
+  def settings
+    @settings ||= Course::MaterialSettings.new(current_course.settings(:material))
+  end
+
+  private
+
+  def main_sidebar_items
     [
       {
         key: :materials,
-        title: I18n.t('course.material.sidebar_title'),
+        title: settings.title || t('course.material.sidebar_title'),
         weight: 4,
         path: course_material_folder_path(current_course, current_course.root_folder),
         unread: 0
+      }
+    ]
+  end
+
+  def settings_sidebar_items
+    [
+      {
+        title: t('course.material.sidebar_title'),
+        type: :settings,
+        weight: 4,
+        path: course_admin_materials_path(current_course)
       }
     ]
   end
---
+++ app/controllers/course/admin/material_settings_controller.rb
@@ -0,0 +1,26 @@
+class Course::Admin::MaterialSettingsController < Course::Admin::Controller
+  add_breadcrumb :edit, :course_admin_materials_path
+  before_action :load_settings
+
+  def edit
+  end
+
+  def update
+    if @settings.update(material_settings_params) && current_course.save
+      redirect_to course_admin_materials_path(current_course), success: t('.success')
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  # Load our settings adapter to handle material settings
+  def load_settings
+    @settings ||= Course::MaterialSettings.new(current_course.settings(:material))
+  end
+
+  def material_settings_params #:nodoc:
+    params.require(:material_settings).permit(:title)
+  end
+end
