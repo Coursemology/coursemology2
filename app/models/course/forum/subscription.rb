2f0134e054ee6f7666fc5d6d2ceff9f1423d146b
Zihuan Xu <xzhflying@163.com>
2015-10-19 15:04:17 +0800

    Fix 'inverse of problem' in Subscription related models

--- app/models/course/forum/subscription.rb
+++ app/models/course/forum/subscription.rb
@@ -1,4 +1,4 @@
 class Course::Forum::Subscription < ActiveRecord::Base
   belongs_to :forum, inverse_of: :subscriptions
-  belongs_to :user, inverse_of: :forum_subscriptions
+  belongs_to :user, inverse_of: nil
 end
--- spec/models/course/forum/subscription_spec.rb
+++ spec/models/course/forum/subscription_spec.rb
@@ -2,5 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Course::Forum::Subscription, type: :model do
   it { is_expected.to belong_to(:forum).inverse_of(:subscriptions) }
-  it { is_expected.to belong_to(:user).inverse_of(:forum_subscriptions) }
+  it { is_expected.to belong_to(:user) }
 end
