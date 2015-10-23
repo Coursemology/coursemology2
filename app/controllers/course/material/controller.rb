4f28b9ad2d80b435eccfc67c20b225b4632f6b73
WANG QIANG <rovingbreeze@gmail.com>
2015-10-23 15:18:21 +0800

    Implement breadcrumbs for materails

--- app/controllers/course/material/controller.rb
+++ app/controllers/course/material/controller.rb
@@ -3,6 +3,7 @@ class Course::Material::Controller < Course::ComponentController
                                        class: Course::Material::Folder.name
 
   before_action :check_component
+  before_action :add_folder_breadcrumb
 
   private
 
@@ -17,5 +18,16 @@ class Course::Material::Controller < Course::ComponentController
   # @raise [Coursemology::ComponentNotFoundError] When the component is disabled.
   def check_component
     fail ComponentNotFoundError unless component
+  end
+
+  def add_folder_breadcrumb
+    folders_chain = @folder.ancestors.reverse << @folder
+    root_folder = folders_chain.shift
+
+    add_breadcrumb component.settings.title || t('course.material.sidebar_title'),
+                   course_material_folder_path(current_course, root_folder)
+    folders_chain.each do |folder|
+      add_breadcrumb folder.name, course_material_folder_path(current_course, folder)
+    end
   end
 end
