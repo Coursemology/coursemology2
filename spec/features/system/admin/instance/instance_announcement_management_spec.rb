ccbc220e28fbd33249a351a800eae831ea40c692
Joel Low <joel@joelsplace.sg>
2015-10-24 11:57:07 +0800

    Rewrite instance announcement specs to use the new user story style.

--- spec/features/system/admin/instance/instance_announcement_management_spec.rb
+++ spec/features/system/admin/instance/instance_announcement_management_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'System: Administration: InstanceAnnouncements', type: :feature do
+RSpec.feature 'System: Administration: Instance Announcements' do
   subject { page }
 
   let!(:instance) { create(:instance) }
@@ -12,154 +12,75 @@ RSpec.describe 'System: Administration: InstanceAnnouncements', type: :feature d
       login_as(user, scope: :user)
     end
 
-    describe 'announcement creation' do
-      before { visit new_admin_instance_announcement_path }
+    context 'As a system administrator' do
+      scenario 'I can create instance announcements' do
+        visit new_admin_instance_announcement_path
 
-      context 'with invalid information' do
-        before do
+        click_button I18n.t('helpers.submit.announcement.create')
+        expect(page).to have_button(I18n.t('helpers.submit.announcement.create'))
+        expect(page).to have_css('div.has-error')
+
+        announcement = build_stubbed(:instance_announcement, instance: instance)
+        fill_in 'announcement[title]',    with: announcement.title
+        fill_in 'announcement[content]',  with: announcement.content
+        fill_in 'announcement[start_at]', with: announcement.start_at
+        fill_in 'announcement[end_at]',   with: announcement.end_at
+        expect do
           click_button I18n.t('helpers.submit.announcement.create')
-        end
+        end.to change { Instance::Announcement.count }.by(1)
 
-        it 'stays on the same page' do
-          expect(page).to have_button(I18n.t('helpers.submit.announcement.create'))
-        end
-
-        it 'shows errors' do
-          expect(page).to have_css('div.has-error')
-        end
+        expect(page).to have_selector('div.alert.alert-success')
+        expect(current_path).to eq(admin_instance_announcements_path)
       end
 
-      context 'with valid information' do
-        let(:announcement) { build(:instance_announcement, instance: instance) }
+      scenario 'I can edit instance announcements' do
+        announcement = create(:instance_announcement, instance: instance)
+        visit edit_admin_instance_announcement_path(announcement)
 
-        before do
-          fill_in 'announcement[title]',    with: announcement.title
-          fill_in 'announcement[content]',  with: announcement.content
-          fill_in 'announcement[start_at]', with: announcement.start_at
-          fill_in 'announcement[end_at]',   with: announcement.end_at
-        end
+        expect(page).to have_field('announcement[title]', with: announcement.title)
+        expect(page).to have_field('announcement[content]', with: announcement.content)
+        expect(page).to have_field('announcement[start_at]', with: announcement.start_at)
+        expect(page).to have_field('announcement[end_at]', with: announcement.end_at)
 
-        it 'creates an announcement' do
-          expect { click_button I18n.t('helpers.submit.announcement.create') }.to change(
-            Instance::Announcement, :count).by(1)
-        end
+        fill_in 'announcement[title]', with: ''
+        click_button I18n.t('helpers.submit.announcement.update')
+        expect(announcement.reload.title).not_to be_empty
+        expect(page).to have_button(I18n.t('helpers.submit.announcement.update'))
+        expect(page).to have_css('div.has-error')
 
-        context 'after creation' do
-          before { click_button I18n.t('helpers.submit.announcement.create') }
+        new_title = 'New Title'
+        new_content = 'New content'
 
-          it 'shows the success message' do
-            expect(page).
-              to have_selector('div',
-                               text: I18n.t('system.admin.instance.announcements.create.success'))
-          end
+        fill_in 'announcement[title]',        with: new_title
+        fill_in 'announcement[content]',      with: new_content
+        click_button I18n.t('helpers.submit.announcement.update')
 
-          it 'redirects the user to the index page' do
-            expect(current_path).to eq(admin_instance_announcements_path)
-          end
-        end
-      end
-    end
-
-    describe 'announcement editing' do
-      let!(:announcement) { create(:instance_announcement, instance: instance) }
-
-      before { visit edit_admin_instance_announcement_path(announcement) }
-
-      context 'page rendering' do
-        it { is_expected.to have_field('announcement[title]', with: announcement.title) }
-
-        it do
-          is_expected.to have_field('announcement[content]', with: announcement.content)
-        end
-
-        it do
-          is_expected.to have_field('announcement[start_at]',
-                                    with: announcement.start_at)
-        end
-
-        it do
-          is_expected.to have_field('announcement[end_at]', with: announcement.end_at)
-        end
+        expect(current_path).to eq admin_instance_announcements_path
+        expect(page).to have_selector('div.alert.alert-success')
+        expect(announcement.reload.title).to eq(new_title)
+        expect(announcement.reload.content).to eq(new_content)
       end
 
-      context 'with invalid information' do
-        before do
-          fill_in 'announcement[title]', with: ''
-          click_button I18n.t('helpers.submit.announcement.update')
-        end
+      scenario 'I can see all instance announcements' do
+        announcements = create_list(:instance_announcement, 2, instance: instance)
+        visit admin_instance_announcements_path(page: Instance::Announcement.page.total_pages)
 
-        it 'stays on the same page' do
-          expect(page).to have_button(I18n.t('helpers.submit.announcement.update'))
-        end
-
-        it 'shows errors' do
-          expect(page).to have_css('div.has-error')
-        end
-      end
-
-      context 'with valid information' do
-        let(:new_title)  { 'New Title' }
-        let(:new_content) { 'New content' }
-
-        before do
-          fill_in 'announcement[title]',        with: new_title
-          fill_in 'announcement[content]',      with: new_content
-          click_button I18n.t('helpers.submit.announcement.update')
-        end
-
-        it 'redirects the user to index page' do
-          expect(current_path).to eq admin_instance_announcements_path
-        end
-
-        it 'shows the success message' do
-          expect(page).
-            to have_selector('div',
-                             text: I18n.t('system.admin.instance.announcements.update.success'))
-        end
-
-        it 'changes the attributes' do
-          expect(announcement.reload.title).to eq(new_title)
-          expect(announcement.reload.content).to eq(new_content)
-        end
-      end
-    end
-
-    describe 'index' do
-      let!(:announcements) do
-        create_list(:instance_announcement, 10, instance: instance)
-      end
-
-      before do
-        visit admin_instance_announcements_path
-      end
-
-      context 'management buttons' do
-        it { is_expected.to have_link(nil, href: new_admin_instance_announcement_path) }
-      end
-
-      it 'shows all announcements' do
+        expect(page).to have_link(nil, href: new_admin_instance_announcement_path)
         announcements.each do |announcement|
-          expect(page).to have_selector('div', text: announcement.title)
-          expect(page).to have_selector('div', text: announcement.content)
-        end
-      end
-
-      it 'shows all management buttons' do
-        announcements.each do |announcement|
+          expect(page).to have_content_tag_for(announcement)
           expect(page).to have_link(nil, href: edit_admin_instance_announcement_path(announcement))
           expect(page).to have_link(nil, href: admin_instance_announcement_path(announcement))
         end
       end
-    end
 
-    describe 'announcement deletion' do
-      let!(:announcement) { create(:instance_announcement, instance: instance) }
-      let(:announcement_path) { admin_instance_announcement_path(announcement) }
-      before { visit admin_instance_announcements_path }
+      scenario 'I can delete announcements' do
+        announcement = create(:instance_announcement, instance: instance)
+        visit admin_instance_announcements_path(page: Instance::Announcement.page.total_pages)
 
-      it 'deletes the announcement' do
-        find_link(nil, href: announcement_path).click
-        it { is_expected.not_to have_link(nil, href: announcement_path) }
+        find_link(nil, href: admin_instance_announcement_path(announcement)).click
+        expect(instance.announcements.exists?(announcement.id)).to be(false)
+        visit admin_instance_announcements_path(page: Instance::Announcement.page.total_pages)
+        expect(page).not_to have_link(nil, href: admin_instance_announcement_path(announcement))
       end
     end
   end
