c1926fade9df2c0cba41898842e2503486c12395
Joel Low <joel@joelsplace.sg>
2015-10-24 15:03:13 +0800

    Rewrite the user registration specs to use the new user story style.
    Also fixed false negatives when nesting it blocks.

--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -3,29 +3,26 @@ require 'rails_helper'
 RSpec.feature 'Users: Sign Up' do
   let(:instance) { create(:instance) }
   with_tenant(:instance) do
-    before { visit new_user_registration_path }
+    context 'As an unregistered user' do
+      scenario 'I can register for an account' do
+        visit new_user_registration_path
 
-    context 'with invalid information' do
-      subject { click_button I18n.t('user.registrations.new.sign_up') }
-      it 'does not create a user' do
-        expect { subject }.not_to change(User, :count)
-      end
-    end
+        expect do
+          click_button I18n.t('user.registrations.new.sign_up')
+        end.not_to change(User, :count)
+        expect(page).to have_selector('div.has-error')
 
-    context 'with valid information' do
-      let(:valid_user) { build(:user) }
-      subject { click_button I18n.t('user.registrations.new.sign_up') }
+        valid_user = attributes_for(:user).reverse_merge(email: generate(:email))
+        fill_in 'user_name', with: valid_user[:name]
+        fill_in 'user_email', with: valid_user[:email]
+        fill_in 'user_password', with: valid_user[:password]
+        fill_in 'user_password_confirmation', with: valid_user[:password]
 
-      before do
-        fill_in 'user_name', with: valid_user.name
-        fill_in 'user_email', with: valid_user.email
-        fill_in 'user_password', with: valid_user.password
-        fill_in 'user_password_confirmation', with: valid_user.password
-      end
-
-      it 'creates a user' do
-        it { is_expected.to change(User, :count).by(1) }
-        it { is_expected.to change(instance.instance_users, :count).by(1) }
+        expect do
+          click_button I18n.t('user.registrations.new.sign_up')
+        end.to change(User, :count).by(1)
+        user = User::Email.find_by!(email: valid_user[:email]).user
+        expect(instance.users.exists?(user)).to be_truthy
       end
     end
   end
