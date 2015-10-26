cfddc3c44c78d6382d28156f61181d22488c4c45
fonglh <fonglh@gmail.com>
2015-10-26 15:42:44 +0800

    Setup UI and actions for duplication.
    Generate controller for Duplication.
    Add Duplicate Course entry to sidebar.
    Create a simple duplication form.
    Create Duplication service which will do the actual duplication call.

---
+++ app/controllers/course/duplications_controller.rb
@@ -0,0 +1,32 @@
+class Course::DuplicationsController < Course::ComponentController
+  def show
+  end
+
+  def create
+    if duplicate
+      redirect_to course_duplication_path(current_course), success: t('.duplicating')
+    else
+      redirect_to course_duplication_path(current_course), danger: t('.failed')
+    end
+  end
+
+  private
+
+  def create_duplication_params # :nodoc
+    params.require(:duplication).require(:new_course_start_time)
+  end
+
+  # Duplicates the course via the service object
+  #
+  # @return [Boolean] True if the duplication was successful.
+  def duplicate
+    duplication_service.duplicate(create_duplication_params)
+  end
+
+  # Create a duplication service object for this object.
+  #
+  # @return [Course::DuplicationService]
+  def duplication_service
+    @duplication_service ||= Course::DuplicationService.new(current_course)
+  end
+end
