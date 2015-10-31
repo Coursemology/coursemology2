2fd604bc2e763c1f52eb5c6192bee007b434efd9
Joel Low <joel@joelsplace.sg>
2015-10-31 14:49:31 +0800

    Implement the skeleton for the auto grading service.

---
+++ app/services/course/assessment/answer/auto_grading_service.rb
@@ -0,0 +1,37 @@
+class Course::Assessment::Answer::AutoGradingService
+  class << self
+    # Picks the grader for the given grading, then grades into the given
+    # +Course::Assessment::Answer::AutoGrading+ object.
+    #
+    # @param [Course::Assessment::Answer::AutoGrading] auto_grading The object to store grading
+    #   results in.
+    def grade(auto_grading)
+      pick_grader(auto_grading.answer.question).grade(auto_grading)
+    end
+
+    private
+
+    # Picks the grader to use for the given question.
+    #
+    # @param [Course::Assessment::Question] question The question that the needs to be graded.
+    # @return [Course::Assessment::Answer::AnswerAutoGraderService] The service object that can
+    #   grade this question.
+    def pick_grader(_question)
+      fail
+    end
+  end
+
+  # Grades into the given +Course::Assessment::Answer::AutoGrading+ object. This does not do
+  # anything, except mark the grading as having been graded.
+  #
+  # Subclasses should call this implementation after they are done to persist the changes to the
+  # database.
+  #
+  # @param [Course::Assessment::Answer::AutoGrading] auto_grading The object to store grading
+  #   results in.
+  # @return [Boolean] True if the grading could be saved.
+  def grade(auto_grading)
+    auto_grading.status = :graded if auto_grading.submitted?
+    auto_grading.save
+  end
+end
