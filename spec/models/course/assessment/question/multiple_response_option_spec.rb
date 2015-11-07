0b37edccceec8e4067c3b311bd8521be057e205a
Joel Low <joel@joelsplace.sg>
2015-11-07 12:34:18 +0800

    Implement a `correct` scope for MRQ options.

--- spec/models/course/assessment/question/multiple_response_option_spec.rb
+++ spec/models/course/assessment/question/multiple_response_option_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Course::Assessment::Question::MultipleResponseOption do
-  it 'belongs to question' do
+  it 'belongs to a question' do
     expect(subject).to belong_to(:question).
       class_name(Course::Assessment::Question::MultipleResponse.name)
+  end
+
+  let(:instance) { create(:instance) }
+  with_tenant(:instance) do
+    describe '.correct' do
+      let(:question) { create(:course_assessment_question_multiple_response) }
+      subject { question.options.correct }
+
+      it 'only includes correct answers' do
+        expect(subject.all?(&:correct?)).to eq(true)
+      end
+    end
   end
 end
