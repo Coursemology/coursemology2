e36fc27da0766006ecf3ce15a55fdd86e85b8ba9
WANG QIANG <rovingbreeze@gmail.com>
2015-11-09 16:09:19 +0800

    Implement material download job

--- app/jobs/course/material/zip_download_job.rb
+++ app/jobs/course/material/zip_download_job.rb
@@ -1,18 +1,18 @@
 class Course::Material::ZipDownloadJob < ApplicationJob
   include TrackableJob
-  include FilenameSanitizer
 
   protected
 
   # Performs the download service.
   #
-  # @param [String] folder The folder containing the materials.
+  # @param [Course::Material::Folder] folder The folder containing the materials.
   # @param [Array<Course::Material>] materials The materials to be downloaded.
-  # @param [String] filename The name of the zip file, default set to the folder's name. This is
-  #   useful when you don't want to use folder's name as the zip filename(such as root folder).
+  # @param [String] filename The name of the zip file. This defaults to the name of the folder. This
+  #   is useful when you don't want to use the name of the folder as the zip filename (such as the
+  #   root folder).
   def perform_tracked(folder, materials, filename = folder.name)
-    zip_file = Course::Material::ZipDownloadService.download_and_zip(materials)
-    redirect_to publish_file(zip_file, sanitize(filename) + '.zip')
+    zip_file = Course::Material::ZipDownloadService.download_and_zip(folder, materials)
+    redirect_to publish_file(zip_file, Pathname.normalize_filename(filename) + '.zip')
   end
 
   # Makes the given file publicly accessible.
@@ -22,11 +22,13 @@ class Course::Material::ZipDownloadJob < ApplicationJob
   # @return [String] The url to the public visible file
   def publish_file(file, name)
     # TODO: Support remote path and publish the file on the load balancing server.
-    public_dir = File.join(Rails.public_path, 'downloads/', job.id)
+    downloads_dir = 'downloads/'
+    public_dir = File.join(Rails.public_path, downloads_dir, job.id)
     FileUtils.mkdir_p(public_dir) unless File.exist?(public_dir)
 
     public_file = File.join(public_dir, name)
     FileUtils.cp(file, public_file)
-    public_file.sub(Rails.public_path.to_s, '')
+
+    File.join(downloads_dir, job.id, File.basename(public_file))
   end
 end
