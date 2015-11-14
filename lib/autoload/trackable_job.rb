65fdbd390aaca8704906362c8e31c09161a93f08
Joel Low <joel@joelsplace.sg>
2015-11-14 14:06:18 +0800

    Implement Trackable Jobs.
    This is a mix-in which allows jobs to be tracked by their UUID. These jobs can redirect the client to another page after the job is completed.

---
+++ lib/autoload/trackable_job.rb
@@ -0,0 +1,69 @@
+# This is a mix-in that allows jobs to be trackable. Trackable jobs can be queried at /jobs/<id>.
+# A client requesting HTML would see a progress bar; a client requesting JSON will get a status
+# message. When the job is complete, the trackable job can specify a path to redirect the user to.
+#
+# To use this mix-in, implement +perform_tracked+ instead of +perform+, with the same arguments.
+# If you intend to use +rescue_from+ and you wish for the job to fail, call +rescue_tracked+ with
+# the exception.
+#
+# Jobs are ephemeral; do not expect jobs to remain after a long period of time. Also since jobs
+# are uniquely identified only by ID and do not need authentication, do not leak anything
+# sensitive from the job.
+module TrackableJob
+  extend ActiveSupport::Concern
+
+  class Job < ActiveRecord::Base
+    enum status: [:submitted, :completed, :errored]
+
+    validates :redirect_to, absence: true, if: :submitted?
+    validates :error, absence: true, unless: :errored?
+  end
+
+  included do
+    rescue_from(StandardError) do |exception|
+      rescue_tracked(exception)
+    end
+  end
+
+  # @!attribute [r] job
+  #   The Job object which tracks the status of this job.
+  attr_reader :job
+
+  # Implements +initialize+, creating the job in the database.
+  def initialize(*args)
+    super
+
+    @job = Job.create!(id: job_id)
+  end
+
+  def perform(*args)
+    perform_tracked(*args)
+    @job.status = :completed
+    @job.save!
+  end
+
+  protected
+
+  def perform_tracked(*)
+    fail NotImplementedError
+  end
+
+  def rescue_tracked(exception)
+    @job.status = :errored
+    @job.error = { message: exception.to_s }
+    @job.save!
+  end
+
+  private
+
+  def deserialize_arguments(serialized_arguments)
+    super
+
+    @job = Job.find(job_id)
+  end
+
+  # Specifies that the job should redirect to the given path.
+  def redirect_to(path)
+    @job.redirect_to = path
+  end
+end
---
+++ spec/libraries/trackable_job_spec.rb
@@ -0,0 +1,106 @@
+require 'rails_helper'
+
+RSpec.describe TrackableJob do
+  class self::NoOpJob < ActiveJob::Base
+    include TrackableJob
+  end
+
+  class self::ExampleJob < self::NoOpJob
+    protected
+
+    def perform_tracked
+    end
+  end
+
+  describe TrackableJob::Job, type: :model do
+    subject { TrackableJob::Job.new }
+
+    def self.validate_absence_of_error
+      it 'validates absence of :error' do
+        subject.error = { message: '' }
+        expect(subject.valid?).to be(false)
+        expect(subject.errors[:error]).not_to be_empty
+      end
+    end
+
+    it { is_expected.to validate_absence_of(:redirect_to) }
+    validate_absence_of_error
+
+    context 'when the job is completed' do
+      before { subject.status = :completed }
+
+      it { is_expected.not_to validate_absence_of(:redirect_to) }
+      validate_absence_of_error
+    end
+
+    context 'when the job is errored' do
+      before { subject.status = :errored }
+
+      it { is_expected.not_to validate_absence_of(:redirect_to) }
+      it 'does not validate absence of :error' do
+        subject.error = { message: '' }
+        expect(subject.valid?).to be(true)
+      end
+    end
+  end
+
+  subject { self.class::ExampleJob.perform_later }
+
+  context 'when a new job is created' do
+    it 'has a submitted state' do
+      expect(subject.job.status).to eq('submitted')
+    end
+
+    it 'is persisted to the database' do
+      expect(TrackableJob::Job.find(subject.job_id)).to eq(subject.job)
+    end
+  end
+
+  context 'when the job is completed' do
+    before { subject.perform_now }
+    it 'transitions to the completed state' do
+      expect(subject.job.status).to eq('completed')
+    end
+  end
+
+  context 'when the job has an error' do
+    before do
+      def subject.perform_tracked
+        fail
+      end
+
+      subject.perform_now
+    end
+
+    it 'transitions to the errored state' do
+      expect(subject.job.status).to eq('errored')
+    end
+
+    it 'has the error' do
+      expect(subject.job.error).to be_present
+    end
+  end
+
+  describe '#perform_tracked' do
+    subject { self.class::NoOpJob.perform_later }
+
+    it 'fails with NotImplementedError' do
+      expect { subject.perform_tracked }.to raise_error(NotImplementedError)
+    end
+  end
+
+  describe '#deserialize_arguments' do
+    it 'fetches the job' do
+      subject.send(:deserialize_arguments, [])
+      expect(subject.job.id).to eq(subject.job_id)
+    end
+  end
+
+  describe '#redirect_to' do
+    let(:redirect_to_path) { '/' }
+    it 'sets the #redirect_to attribute of the job' do
+      subject.send(:redirect_to, redirect_to_path)
+      expect(subject.job.redirect_to).to eq(redirect_to_path)
+    end
+  end
+end
