fe0b14f0d7c9136f58577fc5bc46caec7019f488
Joel Low <joel@joelsplace.sg>
2015-11-15 20:55:56 +0800

    Allow specs to switch out the ActiveJob queue adapter.

---
+++ spec/support/active_job.rb
@@ -0,0 +1,31 @@
+module ActiveJob::TestGroupHelpers
+  def self.with_active_job_queue_adapter_method(adapter = :test)
+    proc { |example| ActiveJob::TestGroupHelpers.with_active_job_queue_adapter(example, adapter) }
+  end
+
+  def self.with_active_job_queue_adapter(example, adapter)
+    old_adapter = ActiveJob::Base.queue_adapter
+    ActiveJob::Base.queue_adapter = adapter
+    example.call
+  ensure
+    ActiveJob::Base.queue_adapter = old_adapter
+  end
+
+  def with_active_job_queue_adapter(adapter, &proc)
+    context "with #{adapter} ActiveJob queue adapter" do |*params|
+      around(:each, &ActiveJob::TestGroupHelpers.with_active_job_queue_adapter_method(adapter))
+
+      module_exec(*params, &proc)
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.extend ActiveJob::TestGroupHelpers
+
+  # TODO: Remove when upgrading to RSpec >= 3.4
+  config.around(:each, type: :job,
+                &ActiveJob::TestGroupHelpers.with_active_job_queue_adapter_method)
+
+  config.backtrace_exclusion_patterns << /\/spec\/support\/active_job\.rb/
+end
