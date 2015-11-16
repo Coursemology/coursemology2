bfecd5801f4ac7bd5b35d8383a366dcde4727162
WANG QIANG <rovingbreeze@gmail.com>
2015-11-16 16:38:23 +0800

    Make #link_to_user polymorphic for CourseUsers Fix #549

--- app/helpers/course/controller_helper.rb
+++ app/helpers/course/controller_helper.rb
@@ -27,4 +27,23 @@ module Course::ControllerHelper
       end
     end
   end
+
+  # Links to the given User or CourseUser. If a User is given, the CourseUser under
+  # current_course of the given user will be displayed.
+  #
+  # @param [CourseUser|User] user The CourseUser/User to display.
+  # @param [Hash] options The options to pass to +link_to+
+  # @param [Proc] block The block to use for displaying the user.
+  # @return [String] The user-visible string, including embedded HTML which will display the
+  #   string within a link to bring to the User page.
+  def link_to_user(user, options = {}, &block)
+    return link_to_course_user(user, options, &block) if user.is_a?(CourseUser)
+
+    course_user = user.course_users.find_by(course: controller.current_course)
+    if course_user
+      link_to_course_user(course_user, options, &block)
+    else
+      super(user, options, &block)
+    end
+  end
 end
