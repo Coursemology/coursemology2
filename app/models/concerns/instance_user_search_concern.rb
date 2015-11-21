da15071c6528f8ffc100cc865c2e83d044a36785
WANG QIANG <rovingbreeze@gmail.com>
2015-11-21 19:22:16 +0800

    Allow to search for instance_users

---
+++ app/models/concerns/instance_user_search_concern.rb
@@ -0,0 +1,19 @@
+module InstanceUserSearchConcern
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    # Search and filter users by their names or emails.
+    #
+    # @param [String] keywords The keywords for filtering users.
+    # @return [Array<User>] The users which match the keyword. All users will be returned if
+    #   keyword is blank.
+    def search(keyword)
+      return all if keyword.blank?
+
+      condition = "%#{keyword}%"
+      joins { user.emails.outer }.
+        where { (user.name =~ condition) | (user.emails.email =~ condition) }.
+        group { instance_users.id }
+    end
+  end
+end
--- app/models/instance_user.rb
+++ app/models/instance_user.rb
@@ -1,4 +1,5 @@
 class InstanceUser < ActiveRecord::Base
+  include InstanceUserSearchConcern
   acts_as_tenant :instance, inverse_of: :instance_users
 
   enum role: { normal: 0, instructor: 1, administrator: 2 }
