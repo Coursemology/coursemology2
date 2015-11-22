6522605a5deac9441a24643a14c6a0b4c775ca0b
WANG QIANG <rovingbreeze@gmail.com>
2015-11-22 21:44:11 +0800

    Add pathname helpers extension

---
+++ lib/extensions/pathname_helpers/pathname.rb
@@ -0,0 +1,28 @@
+module Extensions::PathnameHelpers::Pathname
+  module ClassMethods
+    # Normalizes the filename.
+    #   Replace following characters with whitespace: / \ ? * : | " < >
+    #   Remove leading and trailing whitespaces.
+    #   Collapse intra-string whitespaces into single whitespace.
+    #   Remove trailing dots.
+    #
+    # @param [String] filename
+    # @return [String] The normalized filename.
+    def normalize_filename(filename)
+      filename.tr('/"?*:|<>\\', ' ').squish.gsub(/\.+$/, '')
+    end
+
+    # Normalizes the path.
+    #   Replace '\' with '/'.
+    #   Collapse adjacent slashes('/') into single slash.
+    #   Adopt +normalize_filename+ to each part of the path.
+    #
+    # @param [String] path
+    # @return [String] The normalized path.
+    def normalize_path(path)
+      path.tr('\\', '/').
+        gsub(/\/+/, '/').
+        split('/').map { |name| normalize_filename(name) }.join('/')
+    end
+  end
+end
---
+++ spec/libraries/pathname_helpers_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe 'Extension: Pathname' do
+  describe '.normalize_filename' do
+    subject { Pathname.normalize_filename(filename) }
+    context 'when the filename has illegal characters' do
+      let(:filename) { 'lol"|/\?*<>:lol' }
+      it { is_expected.to eq('lol lol') }
+    end
+
+    context 'when the filename has trailing dots' do
+      let(:filename) { 'lol..' }
+      it { is_expected.to eq('lol') }
+    end
+
+    context 'when the filename has extra whitespaces' do
+      let(:filename) { 'lol  lol  ' }
+      it { is_expected.to eq('lol lol') }
+    end
+  end
+
+  describe '.normalize_path' do
+    subject { Pathname.normalize_path(path) }
+
+    context 'when the path has back slashes' do
+      let(:path) { 'lol\lol/lol' }
+      it { is_expected.to eq('lol/lol/lol') }
+    end
+
+    context 'when the path has adjacent slashes' do
+      let(:path) { 'lol//lol' }
+      it { is_expected.to eq('lol/lol') }
+    end
+
+    context 'when the path has illegal characters' do
+      let(:path) { 'lol*?/lol.' }
+      it { is_expected.to eq('lol/lol') }
+    end
+  end
+end
