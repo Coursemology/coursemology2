e98e185da45944007b1f24442356d36f6e1abbc2
weiqingtoh <weiqingtoh@gmail.com>
2015-11-24 19:14:25 +0800

    Fixed assessment submission bug and added specs - When student submits an assessment without an end_time (which is not necessary), the subsequent page will crash. - Commit fixes bug and adjusts the necessary specs

--- spec/factories/course_lesson_plan_items.rb
+++ spec/factories/course_lesson_plan_items.rb
@@ -5,8 +5,16 @@ FactoryGirl.define do
     time_bonus_exp    { rand(1..10) * 100 }
     extra_bonus_exp   { rand(1..10) * 100 }
     start_at { 1.day.ago }
-    bonus_end_at { 2.days.from_now }
-    end_at { 3.days.from_now }
+    bonus_end_at { nil }
+    end_at { nil }
     sequence(:title) { |n| "Example Lesson Plan Item #{n}" }
+
+    trait :with_bonus_end_time do
+      bonus_end_at { 2.days.from_now }
+    end
+
+    trait :with_end_time do
+      end_at { 3.days.from_now }
+    end
   end
 end
