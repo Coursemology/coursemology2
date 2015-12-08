b371132446bf4298d26c37fabf4c23b88036c043
Minqi Chen <cnchenminqi@gmail.com>
2015-12-08 15:19:35 +0800

    Text response answer text sanitization
    Defaults to empty string and strip whitespaces

--- app/models/course/assessment/question/text_response_solution.rb
+++ app/models/course/assessment/question/text_response_solution.rb
@@ -1,12 +1,17 @@
 class Course::Assessment::Question::TextResponseSolution < ActiveRecord::Base
   enum solution_type: [:exact_match, :keyword]
 
+  before_validation :strip_whitespace
   validate :validate_grade
 
   belongs_to :question, class_name: Course::Assessment::Question::TextResponse.name,
                         inverse_of: :solutions
 
   private
+
+  def strip_whitespace
+    solution.strip! unless solution.nil?
+  end
 
   def validate_grade
     errors.add(:grade, :invalid_grade) if grade > question.maximum_grade
