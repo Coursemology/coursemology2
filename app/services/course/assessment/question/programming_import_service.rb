7f0e7046e691f8506d39c2d6ac6dcf59c03f74ef
Joel Low <joel@joelsplace.sg>
2015-12-09 10:13:28 +0800

    Implement a service for importing a programming package's contents into the question.

---
+++ app/services/course/assessment/question/programming_import_service.rb
@@ -0,0 +1,105 @@
+# Imports the provided programming package into the question. This evaluates the package to
+# obtain the set of tests, as well as extracts the templates from the package to be stored
+# together with the question.
+class Course::Assessment::Question::ProgrammingImportService
+  class << self
+    # Imports the programming package into the question.
+    #
+    # @param [Course::Assessment::Question::Programming] question The programming question for
+    #   import.
+    # @param [Course::Assessment::ProgrammingPackage] package The package containing the
+    #   tests and template files.
+    #
+    # @overload import(question, package)
+    #   @param [Course::Assessment::Question::Programming] question The programming question for
+    #     import.
+    #   @param [String] package The path to the package containing the tests and template files.
+    def import(question, package)
+      package = Course::Assessment::ProgrammingPackage.new(package) unless \
+        package.is_a?(Course::Assessment::ProgrammingPackage)
+      new(question, package).send(:import)
+    end
+  end
+
+  private
+
+  # Creates a new service import object.
+  #
+  # @param [Course::Assessment::Question::Programming] question The programming question for import.
+  # @param [Course::Assessment::ProgrammingPackage] package The package containing the tests and
+  #   template files.
+  def initialize(question, package)
+    @question = question
+    @package = package
+  end
+
+  # Imports the templates and tests found in the package.
+  def import
+    template_files = nil
+    template_import_thread = Thread.new { template_files = import_template_files }
+    evaluation_result = evaluate_package
+    template_import_thread.join
+
+    save(template_files, evaluation_result)
+  end
+
+  # Extracts the templates from the package.
+  #
+  # @return [Hash<Pathname, String>] The templates found in the package.
+  def import_template_files
+    @package.submission_files
+  end
+
+  # Evaluates the package to obtain the set of tests.
+  #
+  # @return [Course::Assessment::ProgrammingEvaluationService::Result]
+  def evaluate_package
+    Course::Assessment::ProgrammingEvaluationService.
+      execute(@question.assessment.course, @question.language, @question.memory_limit,
+              @question.time_limit, @package.path)
+  end
+
+  # Saves the templates and tests to the question.
+  #
+  # @param [Hash<Pathname, String>] template_files The templates found in the package.
+  # @param [Course::Assessment::ProgrammingEvaluationService::Result] evaluation_result The
+  #   result of evaluating the package.
+  def save(template_files, evaluation_result)
+    @question.template_files = build_template_file_records(template_files)
+    @question.test_cases = build_test_case_records(evaluation_result.test_report)
+
+    @question.save
+  end
+
+  # Builds the template file records from the templates loaded from the package.
+  #
+  # @param [Hash<Pathname, String>] template_files The templates found in the package.
+  # @return [Array<Course::Assessment::Question::ProgrammingTemplateFile>]
+  def build_template_file_records(template_files)
+    template_files.to_a.map do |(filename, content)|
+      Course::Assessment::Question::ProgrammingTemplateFile.new(filename: filename.to_s,
+                                                                content: content)
+    end
+  end
+
+  # Builds the test case records from the test report.
+  #
+  # @param [String] test_report The test case report from evaluating the package.
+  # @return [Array<Course::Assessment::Question::ProgrammingTestCase>]
+  def build_test_case_records(test_report)
+    test_cases = parse_test_report(test_report)
+    test_cases.map do |test_case|
+      @question.test_cases.build(identifier: test_case.identifier,
+                                 public: !(test_case.name =~ /public/i).nil?,
+                                 description: test_case.name)
+    end
+  end
+
+  # Parses the test report for test cases and statuses.
+  #
+  # @param [String] test_report The test case report from evaluating the package.
+  # @return [Array<>]
+  def parse_test_report(test_report)
+    Course::Assessment::ProgrammingTestCaseReport.new(test_report).test_cases
+  end
+end
---
+++ spec/services/course/assessment/question/programming_import_service_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe Course::Assessment::Question::ProgrammingImportService do
+  let(:instance) { create(:instance) }
+  with_tenant(:instance) do
+    let(:question) { create(:course_assessment_question_programming, template_file_count: 0) }
+    let(:package_path) do
+      File.join(Rails.root, 'spec/fixtures/course/programming_question_template.zip')
+    end
+    let(:package) { Course::Assessment::ProgrammingPackage.new(package_path) }
+    subject { Course::Assessment::Question::ProgrammingImportService.new(question, package) }
+
+    describe '.import' do
+      subject { Course::Assessment::Question::ProgrammingImportService }
+      it 'accepts package file paths' do
+        expect(subject).to receive(:new).
+          with(question, instance_of(Course::Assessment::ProgrammingPackage)).
+          and_call_original
+        subject.import(question, package_path)
+      end
+    end
+
+    describe '#import' do
+      it 'imports the test cases' do
+        subject.send(:import)
+        expect(question.test_cases).not_to be_empty
+      end
+
+      it 'imports the template files' do
+        subject.send(:import)
+        expect(question.template_files).not_to be_empty
+        expect(question.template_files.map(&:filename)).to contain_exactly('__init__.py')
+      end
+    end
+  end
+end
