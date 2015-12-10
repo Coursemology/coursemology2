3dfcdb94b7da98c0693db84e06164bfc7d5f16bb
WANG QIANG <rovingbreeze@gmail.com>
2015-12-10 14:55:00 +0800

    Define abilities for instance announcements

---
+++ app/models/components/system/admin/instance_announcements_ability_component.rb
@@ -0,0 +1,23 @@
+module System::Admin::InstanceAnnouncementsAbilityComponent
+  include AbilityHost::Component
+
+  def define_permissions
+    if user
+      allow_instance_users_show_announcements
+      allow_instance_admin_manage_announcements
+    end
+
+    super
+  end
+
+  private
+
+  def allow_instance_users_show_announcements
+    can :read, Instance::Announcement,
+        instance_all_instance_users_hash.reverse_merge(already_started_hash)
+  end
+
+  def allow_instance_admin_manage_announcements
+    can :manage, Instance::Announcement, instance_instance_user_hash(:administrator)
+  end
+end
