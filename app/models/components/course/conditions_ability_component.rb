7c96d1d9b7c8e0ba31bb7870d14eaa55618e6973
WANG QIANG <rovingbreeze@gmail.com>
2015-12-12 02:24:09 +0800

    Allow staff to manage conditions

---
+++ app/models/components/course/conditions_ability_component.rb
@@ -0,0 +1,17 @@
+module Course::ConditionsAbilityComponent
+  include AbilityHost::Component
+
+  def define_permissions
+    allow_staff_manage_conditions if user
+
+    super
+  end
+
+  private
+
+  def allow_staff_manage_conditions
+    can :manage, Course::Condition, course_staff_hash
+    can :manage, Course::Condition::Achievement, condition: course_staff_hash
+    can :manage, Course::Condition::Level, condition: course_staff_hash
+  end
+end
