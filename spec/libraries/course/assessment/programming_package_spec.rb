0cfcdba41c02836c8ca4ece7c4d2ae27fbf42621
Joel Low <joel@joelsplace.sg>
2015-12-12 08:04:16 +0800

    Allow passing an IO object to the package constructor.
    This allows reuse of open file objects, so that a new file handle need not be opened. This can help with sharing violations in temporary files.

--- spec/libraries/course/assessment/programming_package_spec.rb
+++ spec/libraries/course/assessment/programming_package_spec.rb
@@ -6,9 +6,13 @@ RSpec.describe Course::Assessment::ProgrammingPackage do
   self::TEMP_PACKAGE_PATH = File.join(Rails.root, 'tmp/spec')
 
   def temp_package_path
+    temp_package_stream.tap(&:close).path
+  end
+
+  def temp_package_stream
     package_path = self.class::TEMP_PACKAGE_PATH
     Dir.mkdir(package_path) unless Dir.exist?(package_path)
-    Tempfile.create('programming_package', package_path).tap(&:close).path
+    Tempfile.create('programming_package', package_path)
   end
 
   def open_package(path)
@@ -17,6 +21,34 @@ RSpec.describe Course::Assessment::ProgrammingPackage do
 
   let(:package_path) { self.class::PACKAGE_PATH }
   subject { open_package(package_path) }
+
+  describe '#initialize' do
+    context 'when a file path is specified' do
+      it 'opens the file' do
+        expect(subject.submission_files).not_to be_empty
+      end
+    end
+
+    context 'when a file stream is specified' do
+      let(:package_stream) { temp_package_stream }
+      subject { open_package(package_stream) }
+      before do
+        IO.copy_stream(self.class::PACKAGE_PATH, package_stream)
+        package_stream.seek(0)
+      end
+
+      it 'opens the file' do
+        expect(subject.submission_files).not_to be_empty
+      end
+    end
+
+    context 'when anything else is specified' do
+      subject { open_package(3) }
+      it 'fails with ArgumentError' do
+        expect { subject }.to raise_error(ArgumentError)
+      end
+    end
+  end
 
   describe '#path' do
     it 'returns the path to the package' do
@@ -48,6 +80,15 @@ RSpec.describe Course::Assessment::ProgrammingPackage do
       replacement = { Pathname.new('test.py') => 'test!' }
       expect(subject.submission_files = replacement).to eq(replacement)
       expect(subject.submission_files).to eq(replacement)
+    end
+  end
+
+  describe '#ensure_file_open!' do
+    context 'when the file cannot be opened' do
+      it 'raises an IllegalStateError' do
+        subject.instance_variable_set(:@path, nil)
+        expect { subject.submission_files }.to raise_error(IllegalStateError)
+      end
     end
   end
 end
