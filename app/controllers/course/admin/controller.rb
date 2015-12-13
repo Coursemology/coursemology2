c8ab27777326e81d0220d8ec9001408095da17c8
WANG QIANG <rovingbreeze@gmail.com>
2015-12-13 15:40:44 +0800

    Authorize manage course in course admin

--- app/controllers/course/admin/controller.rb
+++ app/controllers/course/admin/controller.rb
@@ -1,4 +1,12 @@
 class Course::Admin::Controller < Course::ComponentController
   add_breadcrumb :admin, :course_admin_path
   layout 'course_admin'
+
+  before_action :authorize_admin
+
+  private
+
+  def authorize_admin
+    authorize!(:manage, current_course)
+  end
 end
---
+++ spec/controllers/course/admin/admin_controller_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+RSpec.describe Course::Admin::AdminController do
+  let(:instance) { create(:instance) }
+  with_tenant(:instance) do
+    let(:course) { create(:course) }
+    before { sign_in(user) }
+
+    describe '#index' do
+      subject { get :index, course_id: course }
+
+      context 'when the user is a Course Manager' do
+        let(:user) { create(:course_manager, :approved, course: course).user }
+
+        it { is_expected.to render_template(:index) }
+      end
+
+      context 'when the user is a Teaching Assistant' do
+        let(:user) { create(:course_teaching_assistant, :approved, course: course).user }
+
+        it { expect { subject }.to raise_exception(CanCan::AccessDenied) }
+      end
+
+      context 'when the user is a Course Student' do
+        let(:user) { create(:course_student, :approved, course: course).user }
+
+        it { expect { subject }.to raise_exception(CanCan::AccessDenied) }
+      end
+    end
+
+    describe '#update' do
+      let(:title) { 'New Title' }
+      subject { patch :update, course_id: course, course: { title: title } }
+
+      context 'when the user is a Course Manager' do
+        let(:user) { create(:course_manager, :approved, course: course).user }
+
+        it { is_expected.to redirect_to(course_admin_path(course)) }
+
+        it 'changes the title' do
+          subject
+
+          expect(course.reload.title).to eq(title)
+        end
+      end
+
+      context 'when the user is a Teaching Assistant' do
+        let(:user) { create(:course_teaching_assistant, :approved, course: course).user }
+
+        it { expect { subject }.to raise_exception(CanCan::AccessDenied) }
+      end
+
+      context 'when the user is a Course Student' do
+        let(:user) { create(:course_student, :approved, course: course).user }
+
+        it { expect { subject }.to raise_exception(CanCan::AccessDenied) }
+      end
+    end
+  end
+end
