52d764267ea00826756ccc19d8e2a29ae1bc9d3c
WANG QIANG <rovingbreeze@gmail.com>
2015-12-14 14:00:12 +0800

    Filter sidebar items by abilities

--- app/controllers/components/course/groups_component.rb
+++ app/controllers/components/course/groups_component.rb
@@ -2,6 +2,8 @@ class Course::GroupsComponent < SimpleDelegator
   include Course::ControllerComponentHost::Component
 
   def sidebar_items
+    return [] unless can_manage_group?
+
     [
       {
         key: :groups,
@@ -11,5 +13,15 @@ class Course::GroupsComponent < SimpleDelegator
         path: course_groups_path(current_course)
       }
     ]
+  end
+
+  private
+
+  def can_manage_group?
+    can?(:manage, Course::Group.new(course: current_course)) || manageable_groups.any?
+  end
+
+  def manageable_groups
+    current_course.groups.accessible_by(current_ability, :manage)
   end
 end
