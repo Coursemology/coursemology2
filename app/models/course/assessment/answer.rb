20afc2e1f8183cc528e2c8735049d4ddb38f5eb5
WANG QIANG <rovingbreeze@gmail.com>
2016-01-20 16:30:56 +0800

    Make single answer auto gradable

--- app/models/course/assessment/answer.rb
+++ app/models/course/assessment/answer.rb
@@ -33,15 +33,18 @@ class Course::Assessment::Answer < ActiveRecord::Base
 
   # Creates an Auto Grading job for this answer. This saves the answer if there are pending changes.
   #
+  # @param [String|nil] redirect_to_path The path to be redirected after auto grading job was
+  #   finished.
   # @return [Course::Assessment::Answer::AutoGradingJob] The job instance.
   # @raise [ArgumentError] When the question cannot be auto graded.
   # @raise [IllegalStateError] When the answer has not been submitted.
-  def auto_grade!
+  def auto_grade!(redirect_to_path = nil)
     raise ArgumentError unless question.auto_gradable?
     raise IllegalStateError if attempting?
 
     ensure_auto_grading!
-    Course::Assessment::Answer::AutoGradingJob.perform_later(auto_grading).tap do |job|
+    Course::Assessment::Answer::AutoGradingJob.
+      perform_later(auto_grading, redirect_to_path).tap do |job|
       auto_grading.job_id = job.job_id
       save!
     end
