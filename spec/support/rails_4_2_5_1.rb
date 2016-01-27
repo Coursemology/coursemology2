4a8312ff00b0925add5a0fc595f5da824050c40c
Joel Low <joel@joelsplace.sg>
2016-01-27 09:40:03 +0800

    Revert "Patch RSpec to support rails 4.2.5.1"
    This reverts commit cf0f2f45b2971c3297687beecff551844f6ac1a6.

--- spec/support/rails_4_2_5_1.rb
+++
@@ -1,4 +0,0 @@
-# This file should be removed when rspec/rspec-rails#1533 is merged.
-RSpec::Rails::ViewRendering::EmptyTemplatePathSetDecorator.class_eval do
-  alias_method :find_all_anywhere, :find_all
-end
