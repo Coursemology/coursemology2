6b24106bf8c062f817b58e99e2fc8053f6f1d2f1
weiqingtoh <weiqingtoh@gmail.com>
2016-01-27 16:43:19 +0800

    Add badge for achievements in model and helpers
    Added helper method to display badge and default achievement badges

---
+++ spec/helpers/course/achievements_helper_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Course::AchievementsHelper do
+  let!(:instance) { create(:instance) }
+  with_tenant(:instance) do
+    let(:achievement) { create(:course_achievement) }
+
+    describe '#display_achievement_badge' do
+      context 'when an achievement badge is uploaded' do
+        let(:icon) { File.join(Rails.root, '/spec/fixtures/files/picture.jpg') }
+        before do
+          file = File.open(icon, 'rb')
+          achievement.badge = file
+          file.close
+        end
+
+        it 'displays the achievement badge' do
+          expect(helper.display_achievement_badge(achievement)).
+            to include(achievement.badge.medium.url)
+        end
+      end
+
+      context 'when an achievement badge is not uploaded' do
+        it 'displays the default achievement badge' do
+          expect(helper.display_achievement_badge(achievement)).
+            to have_tag('img', with: { :'src^' => '/assets/achievement_blank-' })
+        end
+      end
+    end
+  end
+end
