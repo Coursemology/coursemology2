1e3af69ae8dba6c19aec544b7c07acd2f5d95cef
Joel Low <joel@joelsplace.sg>
2016-01-28 16:08:04 +0800

    Run all specs which trigger a job within the test ActiveJob Queue Adapter.
    Here we are not testing whether the jobs run, so they should not run so that the jobs do not exhaust the available set of database connections.

--- spec/models/course/assessment/submission_spec.rb
+++ spec/models/course/assessment/submission_spec.rb
@@ -52,12 +52,14 @@ RSpec.describe Course::Assessment::Submission do
     end
 
     describe '#grade' do
-      let(:assessment_traits) { [:with_all_question_types] }
-      let(:submission1_traits) { :submitted }
-      let(:submission) { submission1 }
+      with_active_job_queue_adapter(:test) do
+        let(:assessment_traits) { [:with_all_question_types] }
+        let(:submission1_traits) { :submitted }
+        let(:submission) { submission1 }
 
-      it 'sums the grade of all answers' do
-        expect(submission.grade).to eq(submission.answers.map(&:grade).reduce(0, :+))
+        it 'sums the grade of all answers' do
+          expect(submission.grade).to eq(submission.answers.map(&:grade).reduce(0, :+))
+        end
       end
     end
 
@@ -96,43 +98,45 @@ RSpec.describe Course::Assessment::Submission do
     end
 
     describe '#publish!' do
-      let(:assessment_traits) { [:with_all_question_types] }
-      let(:submission) { submission1 }
-      let(:submission1_traits) { :submitted }
-
-      it 'propagates the graded state to its answers' do
-        expect(submission.answers.all?(&:submitted?)).to be(true)
-        submission.publish!
-        expect(submission.answers.all?(&:graded?)).to be(true)
-      end
-
-      context 'when some of the answers are already graded' do
-        before do
-          submission.answers.sample.tap do |answer|
-            answer.publish!
-            answer.save!
-          end
-
-          expect(submission.answers.any?(&:graded?)).to be(true)
-        end
+      with_active_job_queue_adapter(:test) do
+        let(:assessment_traits) { [:with_all_question_types] }
+        let(:submission) { submission1 }
+        let(:submission1_traits) { :submitted }
 
         it 'propagates the graded state to its answers' do
+          expect(submission.answers.all?(&:submitted?)).to be(true)
           submission.publish!
           expect(submission.answers.all?(&:graded?)).to be(true)
+        end
+
+        context 'when some of the answers are already graded' do
+          before do
+            submission.answers.sample.tap do |answer|
+              answer.publish!
+              answer.save!
+            end
+
+            expect(submission.answers.any?(&:graded?)).to be(true)
+          end
+
+          it 'propagates the graded state to its answers' do
+            submission.publish!
+            expect(submission.answers.all?(&:graded?)).to be(true)
+          end
         end
       end
     end
 
     describe '#auto_grade!' do
-      let(:assessment_traits) { [:with_all_question_types] }
-      let(:submission1_traits) { :submitted }
-      let(:submission) { submission1 }
-
-      it 'returns an ActiveJob' do
-        expect(submission.auto_grade!).to be_a(ActiveJob::Base)
-      end
-
       with_active_job_queue_adapter(:test) do
+        let(:assessment_traits) { [:with_all_question_types] }
+        let(:submission1_traits) { :submitted }
+        let(:submission) { submission1 }
+
+        it 'returns an ActiveJob' do
+          expect(submission.auto_grade!).to be_a(ActiveJob::Base)
+        end
+
         it 'queues the job' do
           submission
           expect { submission.auto_grade! }.to \
