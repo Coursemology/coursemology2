c6e7fa7c4903ba19914c20d6ab6c0fc6a005f072
WANG QIANG <rovingbreeze@gmail.com>
2016-01-29 17:57:43 +0800

    Adopt services in submissions controller

--- app/services/course/assessment/submission/guided_service.rb
+++ app/services/course/assessment/submission/guided_service.rb
@@ -1,3 +1,12 @@
 # frozen_string_literal: true
 class Course::Assessment::Submission::GuidedService < Course::Assessment::SubmissionService
+  private
+
+  def questions_to_attempt
+    @questions_to_attempt ||= @submission.assessment.questions.step(@submission, step_param)
+  end
+
+  def step_param
+    params.permit(:step)[:step]
+  end
 end
