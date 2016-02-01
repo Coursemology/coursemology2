8a27e38660d117724be3f868073d56640a6c793d
weiqingtoh <weiqingtoh@gmail.com>
2016-02-01 14:28:53 +0800

    Achievement views to support locked or obtained achievements

--- app/helpers/course/achievements_helper.rb
+++ app/helpers/course/achievements_helper.rb
@@ -19,4 +19,21 @@ module Course::AchievementsHelper
       image_tag('achievement_locked.svg')
     end
   end
+
+  # A helper to add a CSS class for each achievement, based on whether the course_user
+  # is an admin, course staff, or student. For students, the method also checks whether
+  # the course_user has obtained the achievement.
+  #
+  # @param [Course::Achievement] achievement The actual achievement.
+  # @param [Course::User] current_course_user The current_course_user.
+  # @return [Array<String>] CSS class to be added to the achievement tag.
+  def achievement_status_class(achievement, current_course_user)
+    if current_course_user.nil? || current_course_user.staff?
+      []
+    elsif achievement.course_users.include?(current_course_user)
+      ['granted']
+    else
+      ['locked']
+    end
+  end
 end
