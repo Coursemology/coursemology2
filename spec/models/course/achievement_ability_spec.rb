9814e32b381919242064f37d90219374680086a4
weiqingtoh <weiqingtoh@gmail.com>
2016-02-05 14:28:09 +0800

    Add ability to display badge for achievements for course students

--- spec/models/course/achievement_ability_spec.rb
+++ spec/models/course/achievement_ability_spec.rb
@@ -10,13 +10,23 @@ RSpec.describe Course::Achievement do
     let!(:draft_achievement) { create(:course_achievement, course: course, draft: true) }
 
     context 'when the user is a Course Student' do
-      let(:user) { create(:course_student, :approved, course: course).user }
+      let(:course_user) { create(:course_student, :approved, course: course) }
+      let(:user) { course_user.user }
 
       it { is_expected.to be_able_to(:show, achievement) }
       it { is_expected.not_to be_able_to(:show, draft_achievement) }
+      it { is_expected.not_to be_able_to(:display_badge, achievement) }
 
       it 'sees the published achievements' do
         expect(course.achievements.accessible_by(subject)).to contain_exactly(achievement)
+      end
+
+      context 'when the user obtains the achievement' do
+        before do
+          create(:course_user_achievement, course_user: course_user, achievement: achievement)
+        end
+
+        it { is_expected.to be_able_to(:display_badge, achievement) }
       end
     end
 
