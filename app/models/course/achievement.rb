f84cce261a2d199dcd2642ffe3fb8ba23649d6e3
weiqingtoh <weiqingtoh@gmail.com>
2016-02-05 14:14:27 +0800

    Correct wrong association for achievement, added specs

--- app/models/course/achievement.rb
+++ app/models/course/achievement.rb
@@ -8,7 +8,7 @@ class Course::Achievement < ActiveRecord::Base
   belongs_to :course, inverse_of: :achievements
   has_many :course_user_achievements, class_name: Course::UserAchievement.name,
                                       inverse_of: :achievement, dependent: :destroy
-  has_many :course_users, through: :user_achievements, class_name: CourseUser.name
+  has_many :course_users, through: :course_user_achievements, class_name: CourseUser.name
 
   default_scope { order(weight: :asc) }
 
--- spec/models/course/achievement_spec.rb
+++ spec/models/course/achievement_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe Course::Achievement, type: :model do
   it { is_expected.to have_many(:course_user_achievements).inverse_of(:achievement) }
-  it { is_expected.to have_many(:course_users) }
+  it { is_expected.to have_many(:course_users).through(:course_user_achievements) }
   it { is_expected.to have_many :conditions }
   it { is_expected.to validate_presence_of :title }
   it { is_expected.to belong_to(:course).inverse_of :achievements }
