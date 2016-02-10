ded43299e6987b6afe02333bd6a527895fa14835
Joel Low <joel@joelsplace.sg>
2016-02-10 17:01:12 +0800

    Fix the Programming Auto Grading model's reference to its test results.

--- app/models/course/assessment/answer/programming_auto_grading_test_result.rb
+++ app/models/course/assessment/answer/programming_auto_grading_test_result.rb
@@ -2,4 +2,6 @@
 class Course::Assessment::Answer::ProgrammingAutoGradingTestResult < ActiveRecord::Base
   belongs_to :auto_grading, class_name: Course::Assessment::Answer::ProgrammingAutoGrading.name,
                             inverse_of: :test_results
+  belongs_to :test_case, class_name: Course::Assessment::Question::ProgrammingTestCase.name,
+                         inverse_of: nil
 end
