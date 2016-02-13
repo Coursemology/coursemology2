3accbd1f0b573ceea6d6e4230a569e0adca5b1e2
Joel Low <joel@joelsplace.sg>
2016-02-13 08:20:20 +0800

    Decorate the deepest container in the layout hierarchy with the page class.
    This prevents elements of outer layouts from being affected by the page being rendered.

--- app/helpers/application_theming_helper.rb
+++ app/helpers/application_theming_helper.rb
@@ -3,4 +3,12 @@ module ApplicationThemingHelper
   def application_resources
     include_jquery
   end
+
+  def page_class
+    return nil if content_for?(:page_class_specified)
+
+    page_class = super
+    content_for(:page_class_specified) { page_class }
+    page_class
+  end
 end
