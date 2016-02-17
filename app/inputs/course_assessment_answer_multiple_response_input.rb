fe0e0ea2ff2800d9f4a286f0a2bf2da7254719ba
Joel Low <joel@joelsplace.sg>
2016-02-17 20:55:38 +0800

    Highlight the correct answer for MRQ questions if the user can grade the submission.

---
+++ app/inputs/course_assessment_answer_multiple_response_input.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+class CourseAssessmentAnswerMultipleResponseInput < SimpleForm::Inputs::CollectionRadioButtonsInput
+  def input_type
+    check_boxes? ? :check_boxes : :radio_buttons
+  end
+
+  protected
+
+  def check_boxes?
+    true
+  end
+
+  def has_required?
+    !check_boxes?
+  end
+
+  def build_nested_boolean_style_item_tag(collection_builder)
+    collection_builder.public_send(builder_input_type) +
+      content_tag(:span, collection_builder.text, class: [correct_class(collection_builder.object)])
+  end
+
+  # Gets the CSS class for the answer's correct state.
+  #
+  # @param [Course::Assessment::Question::MultipleResponseOption] option The answer to get the
+  #   correct class of.
+  # @return [nil] When no solution is supposed to be displayed, or the option is incorrect.
+  # @return [String] When the answer is correct.
+  def correct_class(option)
+    return nil unless options[:display_solution]
+    option.correct? ? 'correct' : nil
+  end
+
+  def builder_input_type
+    check_boxes? ? :check_box : :radio_button
+  end
+
+  def item_wrapper_class
+    check_boxes? ? 'checkbox' : 'radio'
+  end
+end
--- spec/features/course/assessment/answer/multiple_response_answer_spec.rb
+++ spec/features/course/assessment/answer/multiple_response_answer_spec.rb
@@ -9,17 +9,17 @@ RSpec.describe 'Course: Assessments: Submissions: Multiple Response Answers' do
     let(:assessment) { create(:course_assessment_assessment, :with_mcq_question, course: course) }
     before { login_as(user, scope: :user) }
 
+    let(:submission) do
+      create(:course_assessment_submission, *submission_traits, assessment: assessment, user: user)
+    end
+    let(:submission_traits) { nil }
+    let(:options) { assessment.questions.first.specific.options }
+    let(:correct_option) { options.find(&:correct?).option }
+
     context 'As a Course Student' do
       let(:user) { create(:course_user, :approved, course: course).user }
-      let(:submission) do
-        submission = create(:course_assessment_submission, assessment: assessment, user: user)
-        assessment.questions.attempt(submission)
-        submission
-      end
-      let(:correct_option) { assessment.questions.first.specific.options.find(&:correct?).option }
 
       scenario 'I can save my submission' do
-        submission
         visit edit_course_assessment_submission_path(course, assessment, submission)
 
         check correct_option
@@ -32,7 +32,6 @@ RSpec.describe 'Course: Assessments: Submissions: Multiple Response Answers' do
       end
 
       scenario 'I cannot update my submission after finalising' do
-        submission
         visit edit_course_assessment_submission_path(course, assessment, submission)
 
         click_button I18n.t('course.assessment.submissions.worksheet.finalise')
@@ -40,6 +39,19 @@ RSpec.describe 'Course: Assessments: Submissions: Multiple Response Answers' do
         within find(content_tag_selector(submission.answers.first)) do
           expect(all(:field, disabled: true)).not_to be_empty
           all(:field, disabled: true).each { |input| expect(input.disabled?).to be_truthy }
+        end
+      end
+    end
+
+    context 'As Course Staff' do
+      let(:user) { create(:course_teaching_assistant, :approved, course: course).user }
+      let(:submission_traits) { :submitted }
+
+      scenario 'I can view the correct answer' do
+        visit edit_course_assessment_submission_path(course, assessment, submission)
+
+        within find(content_tag_selector(submission.answers.first)) do
+          expect(all('.correct').length).to eq(options.to_a.count(&:correct?))
         end
       end
     end
