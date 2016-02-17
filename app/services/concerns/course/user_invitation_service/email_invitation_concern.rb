e337cad78089e1cc9553a9122a9e9bf020dcbdf8
Joel Low <joel@joelsplace.sg>
2016-02-17 17:55:09 +0800

    Fix circular dependency in production.

--- app/services/concerns/course/user_invitation_service/email_invitation_concern.rb
+++ app/services/concerns/course/user_invitation_service/email_invitation_concern.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 require 'csv'
 
+class Course::UserInvitationService; end
 module Course::UserInvitationService::EmailInvitationConcern
   extend ActiveSupport::Autoload
 
