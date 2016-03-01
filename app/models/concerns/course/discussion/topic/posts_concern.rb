d53d7adab56148791c1dd25be8bd64f43903f30e
Joel Low <joel@joelsplace.sg>
2016-03-01 09:10:43 +0800

    Memoise the topological ordering of the posts associated with a topic.

---
+++ app/models/concerns/course/discussion/topic/posts_concern.rb
@@ -0,0 +1,14 @@
+module Course::Discussion::Topic::PostsConcern
+  # Reloads the association.
+  def reload
+    remove_instance_variable(:@ordered_topologically) if defined?(@ordered_topologically)
+    super
+  end
+
+  # Retrieves the topological ordering of the posts associated with this topic.
+  #
+  # Call +reload+ to reset the ordering.
+  def ordered_topologically
+    @ordered_topologically ||= super
+  end
+end
--- app/models/course/discussion/topic.rb
+++ app/models/course/discussion/topic.rb
@@ -2,7 +2,9 @@
 class Course::Discussion::Topic < ActiveRecord::Base
   actable
 
-  has_many :posts, dependent: :destroy, inverse_of: :topic
+  has_many :posts, dependent: :destroy, inverse_of: :topic do
+    include Course::Discussion::Topic::PostsConcern
+  end
   has_many :subscriptions, dependent: :destroy, inverse_of: :topic
 
   accepts_nested_attributes_for :posts
