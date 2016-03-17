ed63100c1a380ab2009bcaf2b079a90cc56214ef
xuanyi <xuanyi.custode@gmail.com>
2016-03-17 22:04:51 +0800

    Implement satisfiability graph build service.

---
+++ app/services/course/conditional/satisfiability_graph_build_service.rb
@@ -0,0 +1,19 @@
+class Course::Conditional::SatisfiabilityGraphBuildService
+  class << self
+    # Build and cache the satisfiability graph for the given course.
+    #
+    # @param [Course] course The course to build the satsifiability graph
+    def build(course) # rubocop:disable Delegate
+      # TODO: Cache the satisfiability graph
+      new.build(course)
+    end
+  end
+
+  # Build the satisfiability graph for the given course.
+  #
+  # @param [Course] course The course to build the satsifiability graph
+  # @return [Course::Conditional::UserSatisfiabilityGraph] The satisfiability graph for the course
+  def build(course)
+    Course::Conditional::UserSatisfiabilityGraph.new(Course::Condition.conditionals_for(course))
+  end
+end
