47831ba0327692d512b01c64a3911b8b10c364fb
weiqingtoh <weiqingtoh@gmail.com>
2016-03-22 16:24:24 +0800

    Add component check for achievements if component is disabled

--- app/controllers/course/achievement/controller.rb
+++ app/controllers/course/achievement/controller.rb
@@ -1,7 +1,23 @@
 # frozen_string_literal: true
 class Course::Achievement::Controller < Course::ComponentController
+  before_action :check_component
   load_and_authorize_resource :achievement, through: :course, class: Course::Achievement.name
   add_breadcrumb :index, :course_achievements_path
 
   helper name
+
+  private
+
+  # Ensure that the component is enabled.
+  #
+  # @raise [Coursemology::ComponentNotFoundError] When the component is disabled.
+  def check_component
+    raise ComponentNotFoundError unless component
+  end
+
+  # @return [Course::AchievementsComponent] The forum component.
+  # @return [nil] If component is disabled.
+  def component
+    current_component_host[:course_achievements_component]
+  end
 end
