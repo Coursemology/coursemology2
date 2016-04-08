a57c13da1ad7a113c858d92269f2f9689e63b385
Joel Low <joel@joelsplace.sg>
2016-04-08 21:34:37 +0800

    Implement model methods for votes.
     - Implement scopes for selecting upvotes and downvotes.
     - Implement post vote count computation.
     - Implement model methods for casting votes and counting votes.
     - Implement preloading posts with the votes of a given user.
     - Implement retrieving the vote for a given user.

--- spec/models/course/discussion/post/vote_spec.rb
+++ spec/models/course/discussion/post/vote_spec.rb
@@ -3,4 +3,26 @@ require 'rails_helper'
 
 RSpec.describe Course::Discussion::Post::Vote do
   it { is_expected.to belong_to(:post).inverse_of(:votes) }
+
+  let(:instance) { create(:instance) }
+  with_tenant(:instance) do
+    let(:user) { create(:user) }
+    let(:post) { create(:course_discussion_post) }
+
+    describe '.upvotes' do
+      let!(:vote) { post.votes.create(vote_flag: true, creator: user) }
+      it 'retrieves all upvotes' do
+        expect(post.votes.upvotes).not_to be_empty
+        expect(post.votes.upvotes.all?(&:vote_flag)).to be(true)
+      end
+    end
+
+    describe '.downvotes' do
+      let!(:vote) { post.votes.create(vote_flag: false, creator: user) }
+      it 'retrieves all upvotes' do
+        expect(post.votes.downvotes).not_to be_empty
+        expect(post.votes.downvotes.any?(&:vote_flag)).to be(false)
+      end
+    end
+  end
 end
