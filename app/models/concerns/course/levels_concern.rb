9ad701b4b3f4918d25486ff009cea98d07bbd0ff
weiqingtoh <weiqingtoh@gmail.com>
2016-04-11 10:38:12 +0800

    Refactor course level methods for optimisation purposes
    - Shifted methods into levels association
    - Method allows the use of ruby or SQL to compute current level

--- app/models/concerns/course/levels_concern.rb
+++ app/models/concerns/course/levels_concern.rb
@@ -2,23 +2,27 @@
 module Course::LevelsConcern
   extend ActiveSupport::Concern
 
-  # Returns Course::Level object corresponding to
-  # the level that a course participant would have attained if
-  # s/he had experience_points number of experience points.
+  # Returns the Course::Level object corresponding to the experience points provided.
+  # To use ruby to obtain the required level, ensure that course.levels is already loaded.
+  # Otherwise, an SQL call is fired for each method call.
   #
-  # If experience_points <= 0, the level is assumed to be the
-  # default level (the 0th level) with 0 experience_points threshold.
+  # If experience_points <= 0, the level is assumed to be the default level
+  # (the 0th level) with 0 experience_points threshold.
   #
   # @param [Fixnum] experience_points Number of Experience Points
   # @return [Course::Level] A Course::Level instance.
   def level_for(experience_points)
-    return levels.first if experience_points < 0
-    levels.reverse_order.find_by('experience_points_threshold <= ?', experience_points)
+    return first if experience_points < 0
+    if loaded?
+      reverse.find { |level| level.experience_points_threshold <= experience_points }
+    else
+      reverse_order.find_by('experience_points_threshold <= ?', experience_points)
+    end
   end
 
   # Test if the course has a default level.
   # @return [Boolean] True if there is a default level, otherwise false.
   def default_level?
-    levels.any?(&:default_level?)
+    any?(&:default_level?)
   end
 end
