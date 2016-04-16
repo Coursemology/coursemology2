51a1b33a1cf78a578fcb5d4063116ee0f9ee113e
deppbot <deppbot@jollygoodcode.com>
2016-04-16 17:30:54 +0800

    Bundle Update on 2016-04-16

--- lib/extensions/high_voltage_page_action_class/action_view/base.rb
+++ lib/extensions/high_voltage_page_action_class/action_view/base.rb
@@ -2,7 +2,8 @@
 module Extensions::HighVoltagePageActionClass::ActionView::Base
   def page_action_class
     if controller.is_a?(HighVoltage::PagesController)
-      current_page = controller.current_page
+      # TODO: Depends on thoughtbot/high_voltage#235
+      current_page = controller.send(:current_page)
       current_page.sub(/^pages\//, '')
     else
       super
