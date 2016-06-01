a526fec280dcd124ba3caf907d963f27d2c95922
WANG QIANG <rovingbreeze@gmail.com>
2016-06-01 15:54:10 +0800

    Implement achievement notifier

--- app/models/course/user_achievement.rb
+++ app/models/course/user_achievement.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class Course::UserAchievement < ActiveRecord::Base
   after_initialize :set_defaults, if: :new_record?
+  after_create :send_notification
 
   belongs_to :course_user, inverse_of: :course_user_achievements
   belongs_to :achievement, class_name: Course::Achievement.name,
@@ -11,5 +12,9 @@ class Course::UserAchievement < ActiveRecord::Base
   # Set default values
   def set_defaults
     self.obtained_at ||= Time.zone.now
+  end
+
+  def send_notification
+    Course::AchievementNotifier.achievement_gained(course_user.user, achievement)
   end
 end
---
+++ spec/notifiers/course/achievement_notifier.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe Course::AchievementNotifier, type: :notifier do
+  let!(:instance) { create(:instance) }
+
+  with_tenant(:instance) do
+    describe '#achievement_gained' do
+      let(:course) { create(:course) }
+      let(:achievement) { create(:course_achievement, course: course) }
+      let(:user) { create(:course_user, course: course).user }
+
+      subject { Course::AchievementNotifier.achievement_gained(user, achievement) }
+
+      it 'sends a course notification' do
+        expect { subject }.to change(course.notifications, :count).by(1)
+      end
+
+      it 'sends a user notification' do
+        expect { subject }.to change(user.notifications, :count).by(1)
+      end
+    end
+  end
+end
