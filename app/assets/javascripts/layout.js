cecad8057799259d39564ab07eeb6a546b1943aa
Brian Koh <kxmbrian@gmail.com>
2016-07-12 17:51:00 +0800

    Make toolbar less obtrusive for comment replying interface

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -3,12 +3,21 @@
 
 (function($) {
   'use strict';
+
+  function initializeSummernote(element) {
+    var airmodeOptions = $.extend(true, { airMode: true },
+                                        { popover: $.summernote.options.popover });
+    airmodeOptions.popover.air.unshift(['style', ['style']]);
+    $('textarea.text.airmode', element).summernote(airmodeOptions);
+    $('textarea.text', element).summernote();
+  }
+
   function initializeComponents(element) {
     $('[data-toggle="popover"]', element).popover();
     $('[title]', element).tooltip();
     $('input.toggle-all[type="checkbox"]', element).checkboxToggleAll();
-    $('textarea.text', element).summernote();
     $('textarea.code', element).ace();
+    initializeSummernote(element);
   }
 
   // Queue component initialisation until the script has completely loaded.
