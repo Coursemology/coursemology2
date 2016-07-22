95255574ac868a512b70b6ed30cd8477a5bef889
WANG QIANG <rovingbreeze@gmail.com>
2016-07-22 11:33:50 +0800

    Allow users to change their time_zones

--- spec/features/user/profile_spec.rb
+++ spec/features/user/profile_spec.rb
@@ -15,18 +15,21 @@ RSpec.feature 'User: Profile' do
       scenario 'I can change my profile' do
         new_name = 'New Name'
         empty_name = ''
+        time_zone = 'Singapore'
 
         fill_in :user_name, with: empty_name
         click_button 'submit'
         expect(page).to have_selector('div.alert-danger')
 
         fill_in :user_name, with: new_name
+        find('#user_time_zone').find(:xpath, "option[@value='#{time_zone}']").select_option
         attach_file :user_profile_photo, File.join(Rails.root, '/spec/fixtures/files/picture.jpg')
         click_button 'submit'
 
         expect(page).to have_selector('div', text: I18n.t('user.profiles.update.success'))
         expect(page).to have_field('user_name', with: new_name)
         expect(user.reload.profile_photo.url).to be_present
+        expect(user.reload.time_zone).to eq(time_zone)
       end
 
       scenario 'I can link my account to facebook' do
