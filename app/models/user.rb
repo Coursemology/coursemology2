cfca7bb58310e654f300dde237f4361221bc044f
WANG QIANG <rovingbreeze@gmail.com>
2016-07-22 10:24:26 +0800

    Add time_zone to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,6 +38,7 @@ class User < ActiveRecord::Base
 
   validates :email, :encrypted_password, :authentication_token, absence: true, if: :built_in?
   schema_validations except: [:encrypted_password]
+  validates :time_zone, inclusion: { in: ActiveSupport::TimeZone.zones_map.keys }, allow_nil: true
 
   has_many :emails, -> { order('primary' => :desc) }, class_name: User::Email.name,
                                                       inverse_of: :user, dependent: :destroy
