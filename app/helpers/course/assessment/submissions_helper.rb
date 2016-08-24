c73cbfb26067539520623550c7a9d25eac559028
weiqingtoh <weiqingtoh@gmail.com>
2016-08-24 14:19:07 +0800

    Modify helper to only count student submissions and not staff submissions

--- app/helpers/course/assessment/submissions_helper.rb
+++ app/helpers/course/assessment/submissions_helper.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 module Course::Assessment::SubmissionsHelper
-  # Returns the count of submissions in a course that are pending grading
+  # Returns the count of student submissions in a course that are pending grading.
   #
   # @return [Fixnum] The required count
   def pending_submissions_count
-    @pending_submissions_count ||=
-      Course::Assessment::Submission.from_course(current_course).with_submitted_state.count
+    @pending_submissions_count ||= begin
+      student_ids = current_course.course_users.students.select(:user_id)
+      Course::Assessment::Submission.from_course(current_course).by_users(student_ids).
+        with_submitted_state.count
+    end
   end
 
   # Returns the count of submissions of my students in a course that are pending grading
