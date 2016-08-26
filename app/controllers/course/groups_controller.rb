1076768f46c4eae54f583c36dc919731cf40b501
weiqingtoh <weiqingtoh@gmail.com>
2016-08-26 23:51:55 +0800

    Fix bug to show group_managers of each group instead of course_managers
    - Also removed list styling as it is really ugly.

--- app/controllers/course/groups_controller.rb
+++ app/controllers/course/groups_controller.rb
@@ -4,6 +4,7 @@ class Course::GroupsController < Course::ComponentController
   add_breadcrumb :index, :course_groups_path
 
   def index #:nodoc:
+    @groups = @groups.includes(group_users: { course_user: :course })
   end
 
   def new #:nodoc:
