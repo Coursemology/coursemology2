7acc65846d6e2d05fabb416bd331a1a556d92dce
xuanyi <xuanyi.custode@gmail.com>
2016-08-27 15:46:30 +0800

    Email notification for new topic in forum.

--- spec/notifiers/course/forum/topic_notifier.rb
+++ spec/notifiers/course/forum/topic_notifier.rb
@@ -9,12 +9,20 @@ RSpec.describe Course::Forum::TopicNotifier, type: :notifier do
       let(:course) { create(:course) }
       let(:forum) { create(:forum, course: course) }
       let!(:topic) { create(:forum_topic, forum: forum) }
-      let(:user) { create(:course_user, course: course).user }
+      let(:user) do
+        user = create(:course_user, course: course).user
+        forum.subscriptions.create(user: user)
+        user
+      end
 
       subject { Course::Forum::TopicNotifier.topic_created(user, topic) }
 
       it 'sends a course notification' do
         expect { subject }.to change(course.notifications, :count).by(1)
+      end
+
+      it 'sends an email notification' do
+        expect { subject }.to change { ActionMailer::Base.deliveries.count }.by(1)
       end
     end
   end
