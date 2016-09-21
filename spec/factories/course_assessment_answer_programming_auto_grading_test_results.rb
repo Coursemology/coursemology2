0a6b9caf91bc5533b0f8523164679fa5c97706b5
fonglh <fonglh@gmail.com>
2016-09-21 15:03:01 +0800

    Add specs for get_output.
    Add traits to test_result factory to populate various messages.

--- spec/factories/course_assessment_answer_programming_auto_grading_test_results.rb
+++ spec/factories/course_assessment_answer_programming_auto_grading_test_results.rb
@@ -4,5 +4,29 @@ FactoryGirl.define do
           class: Course::Assessment::Answer::ProgrammingAutoGradingTestResult do
     auto_grading { build(:course_assessment_answer_programming_auto_grading) }
     passed true
+
+    trait :failed do
+      passed false
+      messages { { 'failure': 'simulated failure message' } }
+    end
+
+    trait :errored do
+      passed false
+      messages { { 'error': 'simulated error message' } }
+    end
+
+    trait :output do
+      messages { { 'output': 'simulated test function output' } }
+    end
+
+    trait :failed_with_output do
+      passed false
+      messages do
+        {
+          'failure': 'simulated failure message',
+          'output': 'simulated failed output'
+        }
+      end
+    end
   end
 end
