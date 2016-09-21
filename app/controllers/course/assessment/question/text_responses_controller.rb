528dca9e35af0fd00363ca8d36b8322ac51b7901
weiqingtoh <weiqingtoh@gmail.com>
2016-09-21 17:17:41 +0800

    Modify assessment question text_response controller to support file_upload questions

--- app/controllers/course/assessment/question/text_responses_controller.rb
+++ app/controllers/course/assessment/question/text_responses_controller.rb
@@ -6,12 +6,16 @@ class Course::Assessment::Question::TextResponsesController < \
                               through: :assessment, parent: false
 
   def new
+    if params[:file_upload] == 'true'
+      @text_response_question.hide_text = true
+      @text_response_question.allow_attachment = true
+    end
   end
 
   def create
     if @text_response_question.save
       redirect_to course_assessment_path(current_course, @assessment),
-                  success: t('.success')
+                  success: t('.success', name: question_type)
     else
       render 'new'
     end
@@ -23,20 +27,21 @@ class Course::Assessment::Question::TextResponsesController < \
   def update
     if @text_response_question.update_attributes(text_response_question_params)
       redirect_to course_assessment_path(current_course, @assessment),
-                  success: t('.success')
+                  success: t('.success', name: question_type)
     else
       render 'edit'
     end
   end
 
   def destroy
+    title = question_type
     if @text_response_question.destroy
       redirect_to course_assessment_path(current_course, @assessment),
-                  success: t('.success')
+                  success: t('.success', name: title)
     else
       error = @text_response_question.errors.full_messages.to_sentence
       redirect_to course_assessment_path(current_course, @assessment),
-                  danger: t('.failure', error: error)
+                  danger: t('.failure', name: title, error: error)
     end
   end
 
@@ -45,8 +50,13 @@ class Course::Assessment::Question::TextResponsesController < \
   def text_response_question_params
     params.require(:question_text_response).permit(
       :title, :description, :staff_only_comments, :maximum_grade, :weight, :allow_attachment,
+      :hide_text,
       skill_ids: [],
       solutions_attributes: [:_destroy, :id, :solution_type, :solution, :grade, :explanation]
     )
+  end
+
+  def question_type
+    @text_response_question.question_type
   end
 end
