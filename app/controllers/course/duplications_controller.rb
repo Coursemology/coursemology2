386cd9f0bd1cc87965d3bb298d0066051ad39c03
fonglh <fonglh@gmail.com>
2016-10-05 16:23:12 +0800

    Create a simple duplication form.
    Choose new start date and have it redirect back to the page with a
    success message.

--- app/controllers/course/duplications_controller.rb
+++ app/controllers/course/duplications_controller.rb
@@ -1,4 +1,8 @@
 class Course::DuplicationsController < Course::ComponentController
   def show
   end
+
+  def create
+    redirect_to course_duplication_path(current_course), success: t('.duplicating')
+  end
 end
