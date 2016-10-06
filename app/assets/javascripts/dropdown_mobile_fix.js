71a4dc550efa82b0711141f3f95d17ade53a0c3e
weiqingtoh <weiqingtoh@gmail.com>
2016-10-06 17:14:54 +0800

    Fix bug where dropdown menu does not work on iOS devices

---
+++ app/assets/javascripts/dropdown_mobile_fix.js
@@ -0,0 +1,17 @@
+// Dropdown does not work on iOS devices as only click delegation for a and input are supported
+// Event delegation for iOS http://www.quirksmode.org/blog/archives/2010/09/click_event_del.html
+// Proposed solution is modified from http://stackoverflow.com/a/22318440
+
+(function($) {
+  'use strict';
+
+  function initializeDropdownEventListener() {
+    $('[data-toggle=dropdown]').each(function() {
+      this.addEventListener('click', function() {}, false);
+    });
+  }
+
+  $(document).on('turbolinks:load', function() {
+    initializeDropdownEventListener();
+  });
+})(jQuery);
