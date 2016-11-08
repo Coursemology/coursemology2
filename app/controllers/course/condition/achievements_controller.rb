c3ef9361d9729364ea5438433f67c01e78b6ec3a
weiqingtoh <weiqingtoh@gmail.com>
2016-11-08 14:01:45 +0800

    Add check to disable creation/editing/deleting of conditions if component is disabled for levels, achievement and assessment

--- app/controllers/course/condition/achievements_controller.rb
+++ app/controllers/course/condition/achievements_controller.rb
@@ -49,4 +49,12 @@ class Course::Condition::AchievementsController < Course::ConditionsController
   def set_course
     @achievement_condition.course = current_course
   end
+
+  # Define achievement component for the check whether the component is defined.
+  #
+  # @return [Course::AchievementsComponent] The achievements component.
+  # @return [nil] If component is disabled.
+  def component
+    current_component_host[:course_achievements_component]
+  end
 end
