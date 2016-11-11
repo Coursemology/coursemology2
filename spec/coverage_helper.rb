37f756f69b5736e1afe5504c9af12aa688c157f6
weiqingtoh <weiqingtoh@gmail.com>
2016-11-11 09:47:01 +0800

    Modify configuration due to breaking changes in codeclimate-test-reporter
    See https://github.com/codeclimate/ruby-test-reporter#upgrading-from-pre-10-versions

--- spec/coverage_helper.rb
+++ spec/coverage_helper.rb
@@ -50,11 +50,6 @@ if ENV['CI']
     Coveralls.wear!('rails')
   end
 
-  # Code Climate
-  CoverageHelper.load('codeclimate-test-reporter') do
-    CodeClimate::TestReporter.start
-  end
-
   # Code coverage exclusions
   SimpleCov.start do
     # SimpleCov configuration
