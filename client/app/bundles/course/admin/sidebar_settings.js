8b76b83402d8389dbd5bb39f84f5211e3b9b7fe6
WANG QIANG <rovingbreeze@gmail.com>
2016-11-28 19:22:19 +0800

    Fix an issue that table row shrinks when dragging

--- client/app/bundles/course/admin/sidebar_settings.js
+++ client/app/bundles/course/admin/sidebar_settings.js
@@ -6,6 +6,12 @@ $(document).ready(() => {
 
   $('.sidebar-items tbody').sortable({
     axis: 'y',
+    helper(e, ui) {
+      ui.children().each(() => {
+        $(this).width($(this).width());
+      });
+      return ui;
+    },
     stop() {
       $('.weight input').val(index => index + 1);
     },
