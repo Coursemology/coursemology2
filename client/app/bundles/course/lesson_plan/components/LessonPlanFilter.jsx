725e4ab6e88c2b1832ea65a5b181c6730878be10
Brian Koh <kxmbrian@gmail.com>
2016-12-14 20:41:36 +0800

    Use MaterialUI for LessonPlanFilter

--- client/app/bundles/course/lesson_plan/components/LessonPlanFilter.jsx
+++ client/app/bundles/course/lesson_plan/components/LessonPlanFilter.jsx
@@ -1,6 +1,12 @@
 import React, { PropTypes } from 'react';
 import Immutable from 'immutable';
-import { DropdownButton, MenuItem, Glyphicon } from 'react-bootstrap';
+import { injectIntl, defineMessages } from 'react-intl';
+import RaisedButton from 'material-ui/RaisedButton';
+import Popover from 'material-ui/Popover';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import Done from 'material-ui/svg-icons/action/done';
+import KeyboardArrowDown from 'material-ui/svg-icons/hardware/keyboard-arrow-down';
 import styles from './LessonPlanFilter.scss';
 
 const propTypes = {
@@ -8,37 +14,87 @@ const propTypes = {
   toggleItemTypeVisibility: PropTypes.func.isRequired,
   hiddenItemTypes: PropTypes.instanceOf(Immutable.List).isRequired,
   items: PropTypes.instanceOf(Immutable.List).isRequired,
+  intl: PropTypes.shape({
+    formatMessage: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
+const translations = defineMessages({
+  filter: {
+    id: 'course.lessonPlan.lessonPlanFilter.filter',
+    defaultMessage: 'Filter',
+  },
+});
+
 class LessonPlanFilter extends React.Component {
+  constructor(props) {
+    super(props);
 
-  renderTypeMenuItem(itemType) {
-    const { lessonPlanItemTypeKey, hiddenItemTypes } = this.props;
-    const itemTypeKey = lessonPlanItemTypeKey(itemType);
+    this.state = {
+      open: false,
+    };
 
-    return (
-      <MenuItem eventKey={itemTypeKey} key={itemTypeKey}>
-        <Glyphicon glyph="ok" className={hiddenItemTypes.includes(itemTypeKey) ? styles.hidden : ''} />
-        { ' ' }
-        { itemType.join(': ') }
-      </MenuItem>
-    );
+    this.handleTouchTap = this.handleTouchTap.bind(this);
+    this.handleRequestClose = this.handleRequestClose.bind(this);
+  }
+
+  handleTouchTap(event) {
+    // This prevents ghost click.
+    event.preventDefault();
+
+    this.setState({
+      open: true,
+      anchorEl: event.currentTarget,
+    });
+  }
+
+  handleRequestClose() {
+    this.setState({
+      open: false,
+    });
   }
 
   render() {
-    const { toggleItemTypeVisibility, items } = this.props;
+    const {
+      toggleItemTypeVisibility,
+      lessonPlanItemTypeKey,
+      hiddenItemTypes,
+      items,
+      intl,
+    } = this.props;
     const lessonPlanItemTypes = items.map(item => item.get('lesson_plan_item_type')).toSet().toList().sort();
 
     return (
       <div className={styles.filterContainer}>
-        <DropdownButton
-          id="lesson-plan-filter-dropdown-button"
-          pullRight
-          title="Filter"
-          onSelect={itemType => toggleItemTypeVisibility(itemType)}
+        <RaisedButton
+          onTouchTap={this.handleTouchTap}
+          label={intl.formatMessage(translations.filter)}
+          labelPosition="before"
+          icon={<KeyboardArrowDown />}
+        />
+        <Popover
+          open={this.state.open}
+          anchorEl={this.state.anchorEl}
+          anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+          targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+          onRequestClose={this.handleRequestClose}
         >
-          { lessonPlanItemTypes.map(type => this.renderTypeMenuItem(type)) }
-        </DropdownButton>
+          <Menu>
+            {
+              lessonPlanItemTypes.map((type) => {
+                const itemTypeKey = lessonPlanItemTypeKey(type);
+                return (
+                  <MenuItem
+                    key={itemTypeKey}
+                    primaryText={type.join(': ')}
+                    rightIcon={hiddenItemTypes.includes(itemTypeKey) ? '' : <Done />}
+                    onTouchTap={() => toggleItemTypeVisibility(itemTypeKey)}
+                  />
+                );
+              })
+            }
+          </Menu>
+        </Popover>
       </div>
     );
   }
@@ -46,4 +102,4 @@ class LessonPlanFilter extends React.Component {
 
 LessonPlanFilter.propTypes = propTypes;
 
-export default LessonPlanFilter;
+export default injectIntl(LessonPlanFilter);
