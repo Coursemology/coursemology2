4e8aa250b31f9be2c5711c202195d5ec1efb39f8
WANG QIANG <rovingbreeze@gmail.com>
2016-12-27 23:21:32 +0800

    Delete TodoItems after course_user is destroyed

--- app/models/concerns/course_user/todo_concern.rb
+++ app/models/concerns/course_user/todo_concern.rb
@@ -4,6 +4,7 @@ module CourseUser::TodoConcern
 
   included do
     after_create :create_todos_for_course_user
+    after_destroy :delete_todos
   end
 
   # Overrides #accept method, which is part of the workflow event transition handler
@@ -21,5 +22,12 @@ module CourseUser::TodoConcern
     items =
       Course::LessonPlan::Item.where(course_id: course_id).includes(:actable).select(&:has_todo?)
     Course::LessonPlan::Todo.create_for!(items, self)
+  end
+
+  # Delete all todos of the user in current course.
+  def delete_todos
+    items_in_current_course =
+      Course::LessonPlan::Item.where(course_id: course_id).select(:id)
+    Course::LessonPlan::Todo.where(user_id: user_id, item_id: items_in_current_course).delete_all
   end
 end
