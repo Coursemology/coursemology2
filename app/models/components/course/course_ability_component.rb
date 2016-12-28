f2210e97acf414b4e9c0fb820c70e1f6edf88bd4
WANG QIANG <rovingbreeze@gmail.com>
2016-12-28 10:41:27 +0800

    Define abilities for enrol_requests

--- app/models/components/course/course_ability_component.rb
+++ app/models/components/course/course_ability_component.rb
@@ -23,11 +23,12 @@ module Course::CourseAbilityComponent
 
   def allow_showing_open_courses
     # TODO: Replace with just the symbols when Rails 5 is released.
-    can [:read, :register], Course, status: [Course.statuses[:published], Course.statuses[:opened]]
+    can [:read, :register], Course, published_or_opened_course_hash
   end
 
   def allow_unregistered_users_registering_open_courses
-    can [:create, :register], CourseUser, user_id: user.id, workflow_state: 'pending'
+    can :create, Course::EnrolRequest, course: published_or_opened_course_hash, user: user
+    can :destroy, Course::EnrolRequest, user: user
   end
 
   def allow_registered_users_showing_course
@@ -37,9 +38,14 @@ module Course::CourseAbilityComponent
   def allow_owners_managing_course
     can :manage, Course, course_user_hash(*CourseUser::MANAGER_ROLES.to_a)
     can :manage, CourseUser, course_managers_hash
+    can :manage, Course::EnrolRequest, course_managers_hash
   end
 
   def allow_staff_manage_users
     can [:show_users, :manage_users], Course, course_user_hash(*CourseUser::STAFF_ROLES.to_a)
+  end
+
+  def published_or_opened_course_hash
+    { status: [Course.statuses[:published], Course.statuses[:opened]] }
   end
 end
