edca20417834995f1eefb4222cf02ff141213241
WANG QIANG <rovingbreeze@gmail.com>
2016-12-29 15:16:52 +0800

    Auto fill-in the user info from invitation

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class User::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
+  before_action :load_invitation, only: [:new, :create]
   layout :select_layout
 
   # GET /resource/sign_up
@@ -68,5 +69,15 @@ class User::RegistrationsController < Devise::RegistrationsController
   # @return [nil]
   def select_layout
     'user_admin' if ['edit', 'update'].include?(params['action'])
+  end
+
+  def load_invitation
+    return unless invitation_param.present?
+
+    @invitation = Course::UserInvitation.unconfirmed.find_by(invitation_key: invitation_param)
+  end
+
+  def invitation_param
+    params.permit(:invitation)[:invitation]
   end
 end
