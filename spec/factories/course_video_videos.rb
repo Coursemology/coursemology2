874743500db1fb311e7a04228de4fa51ba3500c8
weiqingtoh <weiqingtoh@gmail.com>
2017-01-05 21:39:39 +0800

    Add description to video factory

--- spec/factories/course_video_videos.rb
+++ spec/factories/course_video_videos.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 FactoryGirl.define do
   sequence(:course_video_title) { |n| "Video #{n}" }
+  sequence(:course_video_description) { |n| "Video Description #{n}" }
   factory :course_video, class: Course::Video.name, aliases: [:video],
                          parent: :course_lesson_plan_item do
     course
     title { generate(:course_video_title) }
+    description { generate(:course_video_description) }
     url 'https://www.youtube.com/watch?v=i_YiovUyMds'
     published false
 
