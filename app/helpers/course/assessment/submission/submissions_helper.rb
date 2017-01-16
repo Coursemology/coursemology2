7131d04652bf60224c2c47cce68210f8f192c9ec
Brian Koh <kxmbrian@gmail.com>
2017-01-16 19:52:21 +0800

    Remove new_comments_post since comments are now linear

--- app/helpers/course/assessment/submission/submissions_helper.rb
+++ app/helpers/course/assessment/submission/submissions_helper.rb
@@ -12,17 +12,6 @@ module Course::Assessment::Submission::SubmissionsHelper
     "course_assessment_submission_answer_#{answer.id}_comments"
   end
 
-  # Finds the comment being created/edited, or constructs a new one in reply to the latest post.
-  #
-  # @param [Course::Discussion::Topic] topic The topic being replied to.
-  # @return [Course::Discussion::Post]
-  def new_comments_post(topic)
-    new_post = topic.posts.find(&:new_record?)
-    return new_post if new_post
-
-    topic.posts.build
-  end
-
   # Return the CSS class of the explanation based on the correctness of the answer.
   #
   # @return [String]
