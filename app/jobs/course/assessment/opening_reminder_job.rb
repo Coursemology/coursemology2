3e1c3bda02becb33b0eaa7985174b72a591f5004
WANG QIANG <rovingbreeze@gmail.com>
2017-01-17 16:53:34 +0800

    Don't use trackable job for email jobs

--- app/jobs/course/assessment/opening_reminder_job.rb
+++ app/jobs/course/assessment/opening_reminder_job.rb
@@ -1,13 +1,11 @@
 class Course::Assessment::OpeningReminderJob < ApplicationJob
-  include TrackableJob
-
   rescue_from(ActiveJob::DeserializationError) do |_|
     # Prevent the job from retrying due to deleted records
   end
 
   protected
 
-  def perform_tracked(user, assessment, token)
+  def perform(user, assessment, token)
     instance = Course.unscoped { assessment.course.instance }
     ActsAsTenant.with_tenant(instance) do
       Course::Assessment::ReminderService.opening_reminder(user, assessment, token)
