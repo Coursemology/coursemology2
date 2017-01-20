603f29461f53de30ad3c286f1e4dbdf7594860ad
weiqingtoh <weiqingtoh@gmail.com>
2017-01-20 17:09:19 +0800

    Add validation for enrol_request to prevent duplicates

--- app/models/course/enrol_request.rb
+++ app/models/course/enrol_request.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 class Course::EnrolRequest < ActiveRecord::Base
+  validate :validate_user_not_in_course, on: :create
+
   belongs_to :course, inverse_of: :enrol_requests
   belongs_to :user, inverse_of: :course_enrol_requests
+
+  private
+
+  # Ensure that there are no enrol requests by users in the course.
+  def validate_user_not_in_course
+    errors.add(:base, :user_in_course) unless course.course_users.where(user: user).blank?
+  end
 end
---
+++ spec/models/course/enrol_request_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe Course::EnrolRequest, type: :model do
+  it { is_expected.to belong_to(:course).inverse_of(:enrol_requests) }
+  it { is_expected.to belong_to(:user).inverse_of(:course_enrol_requests) }
+
+  let!(:instance) { create :instance }
+  with_tenant(:instance) do
+    let(:user) { create(:user) }
+    let(:course) { create(:course) }
+
+    describe 'validations' do
+      context 'when enrolled user is an existing course_user' do
+        let!(:student) { create(:course_student, course: course) }
+        subject { build(:course_enrol_request, course: course, user: student.user) }
+
+        it 'is not valid' do
+          expect(subject.valid?).to be_falsey
+          expect(subject.errors[:base]).to include(
+            I18n.t('activerecord.errors.models.course/enrol_request.user_in_course')
+          )
+        end
+      end
+    end
+  end
+end
