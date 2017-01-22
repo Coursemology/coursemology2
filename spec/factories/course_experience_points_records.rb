c5a9faf281d1af7eb637345935af07a9b26374ec
weiqingtoh <weiqingtoh@gmail.com>
2017-01-23 00:52:23 +0800

    Modify experience points record where default record is has awarded attributes

--- spec/factories/course_experience_points_records.rb
+++ spec/factories/course_experience_points_records.rb
@@ -14,12 +14,19 @@ FactoryGirl.define do
     end
     points_awarded { rand(1..20) * 100 }
     draft_points_awarded nil
-    awarded_at nil
-    awarder nil
+    awarded_at { 1.day.ago }
+    awarder { creator }
     reason { 'Reason for manually-awarded experience points' if manually_awarded? }
 
     trait :inactive do
       points_awarded nil
+    end
+
+    trait :draft do
+      inactive
+      draft_points_awarded { rand(1..20) * 100 }
+      awarded_at nil
+      awarder nil
     end
   end
 end
