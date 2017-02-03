09ad3d3aa1739e28f1d5736806d08a92e6a282f1
Brian Koh <kxmbrian@gmail.com>
2017-02-03 15:45:30 +0800

    Add image Thumbnail component

---
+++ client/app/bundles/course/survey/components/Thumbnail.jsx
@@ -0,0 +1,112 @@
+import React, { PropTypes } from 'react';
+import { injectIntl } from 'react-intl';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import formTranslations from 'lib/translations/form';
+
+const styles = {
+  dialogContent: {
+    width: '90%',
+    maxWidth: 'none',
+  },
+  dialogBody: {
+    display: 'flex',
+    justifyContent: 'space-around',
+  },
+};
+
+class Thumbnail extends React.Component {
+  constructor(props) {
+    super(props);
+    const { src, file } = props;
+    const isFromFile = !src && file;
+
+    this.state = {
+      src: null,
+      alt: isFromFile ? file.name : null,
+      open: false,
+    };
+
+    if (isFromFile) {
+      const reader = new FileReader();
+      reader.onload = () => this.setState({ src: reader.result });
+      reader.readAsDataURL(file);
+    }
+  }
+
+  render() {
+    // eslint-disable-next-line no-unused-vars
+    const { intl, src, alt, file, onTouchTap, style, ...props } = this.props;
+    const source = src || this.state.src;
+    const altText = alt || this.state.alt || src;
+
+    const thumbnailStyle = {
+      ...style,
+      cursor: 'zoom-in',
+    };
+
+    const expandedImageStyle = {
+      ...style,
+      width: 'auto',
+      height: 'auto',
+      maxWidth: '100%',
+      maxHeight: '100%',
+      objectFit: 'contain',
+    };
+
+    const onThumbnailTouchTap = onTouchTap && typeof (onTouchTap) === 'function' ?
+      (event) => {
+        onTouchTap(event);
+        this.setState({ open: true });
+      } :
+      () => this.setState({ open: true });
+
+    const actions = [
+      <FlatButton
+        label={intl.formatMessage(formTranslations.close)}
+        primary
+        onTouchTap={() => this.setState({ open: false })}
+      />,
+    ];
+
+    return (
+      <div>
+        <img
+          src={source}
+          alt={altText}
+          onTouchTap={onThumbnailTouchTap}
+          style={thumbnailStyle}
+          {...props}
+        />
+        <Dialog
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={() => this.setState({ open: false })}
+          bodyStyle={styles.dialogBody}
+          contentStyle={styles.dialogContent}
+        >
+          <img
+            src={source}
+            alt={altText}
+            style={expandedImageStyle}
+            {...props}
+          />
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+Thumbnail.propTypes = {
+  intl: PropTypes.shape({
+    formatMessage: PropTypes.func.isRequired,
+  }).isRequired,
+  src: PropTypes.string,
+  alt: PropTypes.string,
+  file: PropTypes.instanceOf(File),
+  onTouchTap: PropTypes.func,
+  style: PropTypes.shape(),
+};
+
+export default injectIntl(Thumbnail);
