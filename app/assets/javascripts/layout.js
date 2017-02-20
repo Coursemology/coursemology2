ed7816b4ba5e4e7f53515d6627169151b1af4cae
Jeremy Yap <jeremy.yapjl@gmail.com>
2017-02-20 15:25:24 +0800

    Resize uploaded images if file size is too big
    Maximum width and height is 1920/1080

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -5,6 +5,43 @@
 (function($, EVENT_HELPERS) {
   'use strict';
 
+  function compressImage(image, onImageCompressed) {
+    // Maximum image size, images larger than this will be compressed
+    var IMAGE_MAX_WIDTH = 1920;
+    var IMAGE_MAX_HEIGHT = 1080;
+
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      var img = document.createElement('img');
+      var canvas = document.createElement('canvas');
+
+      img.src = e.target.result;
+      var width = img.width;
+      var height = img.height;
+
+      if (width <= IMAGE_MAX_WIDTH && height <= IMAGE_MAX_HEIGHT ) {
+       onImageCompressed(e.target.result);
+       return;
+      }
+      if (width > IMAGE_MAX_WIDTH) {
+        height *= IMAGE_MAX_WIDTH / width;
+        width = IMAGE_MAX_WIDTH;
+      }
+      if (height > IMAGE_MAX_HEIGHT) {
+        width *= IMAGE_MAX_HEIGHT / height;
+        height = IMAGE_MAX_HEIGHT;
+      }
+
+      canvas.width = width;
+      canvas.height = height;
+      var ctx = canvas.getContext('2d');
+      ctx.drawImage(img, 0, 0, width, height);
+
+      onImageCompressed(canvas.toDataURL('image/jpeg'));
+    };
+    reader.readAsDataURL(image);
+  }
+
   // Initialises Summernote
   //   Function applies config options to summernote based on CSS classes applied in 'textarea'.
   //   Currently supported options include `airmode` and `focus`.
@@ -14,6 +51,13 @@
     airmodeOptions.popover.air.unshift(['style', ['style']]);
 
     $('textarea.text').not('.summernote-initialised').each(function(){
+      var $summernote = $(this);
+      function onImageCompressed(dataUrl) {
+        var img = document.createElement('img');
+        img.src = dataUrl;
+        $summernote.summernote('insertNode', img);
+      }
+
       var options = {
         toolbar: [
           ['paragraph-style', ['style']],
@@ -26,7 +70,15 @@
           ['insert', ['link', 'picture', 'video']],
           ['misc', ['fullscreen', 'codeview', 'help']],
         ],
+        callbacks: {
+          onImageUpload: function(files) {
+            for (var i = 0; i < files.length; i++) {
+              compressImage(files[i], onImageCompressed);
+            }
+          }
+        }
       };
+
       if ($(this).hasClass('airmode')) {
         options = $.extend(true, options, airmodeOptions);
       }
--- client/app/lib/components/MaterialSummernote.jsx
+++ client/app/lib/components/MaterialSummernote.jsx
@@ -34,6 +34,53 @@ class MaterialSummernote extends React.Component {
     }
   };
 
+  onImageUpload = (files) => {
+    for (let i = 0; i < files.length; i += 1) {
+      this.compressImage(files[i], (dataUrl) => {
+        const img = document.createElement('img');
+        img.src = dataUrl;
+        ReactSummernote.insertNode(img);
+      });
+    }
+  }
+
+  compressImage = (image, onImageCompressed) => {
+    // Maximum image size, images larger than this will be compressed
+    const IMAGE_MAX_WIDTH = 1920;
+    const IMAGE_MAX_HEIGHT = 1080;
+
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const img = document.createElement('img');
+      const canvas = document.createElement('canvas');
+
+      img.src = e.target.result;
+      let width = img.width;
+      let height = img.height;
+
+      if (width <= IMAGE_MAX_WIDTH && height <= IMAGE_MAX_HEIGHT) {
+        onImageCompressed(e.target.result);
+        return;
+      }
+      if (width > IMAGE_MAX_WIDTH) {
+        height *= IMAGE_MAX_WIDTH / width;
+        width = IMAGE_MAX_WIDTH;
+      }
+      if (height > IMAGE_MAX_HEIGHT) {
+        width *= IMAGE_MAX_HEIGHT / height;
+        height = IMAGE_MAX_HEIGHT;
+      }
+
+      canvas.width = width;
+      canvas.height = height;
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(img, 0, 0, width, height);
+
+      onImageCompressed(canvas.toDataURL('image/jpeg'));
+    };
+    reader.readAsDataURL(image);
+  };
+
   render() {
     const {
       baseTheme,
@@ -103,6 +150,7 @@ class MaterialSummernote extends React.Component {
             onChange={this.props.onChange}
             onFocus={() => { this.setState({ isFocused: true }); }}
             onBlur={() => { this.setState({ isFocused: false }); }}
+            onImageUpload={this.onImageUpload}
           />
         </div>
       </div>
