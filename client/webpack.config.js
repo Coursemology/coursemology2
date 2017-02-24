914b021aa9e1550e6b0edaf9dfa6230734b1cba0
Allen Wang <rovingbreeze@gmail.com>
2017-02-25 02:30:07 +0800

    Setup webpack dev server

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,12 @@ const path = require('path');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const StatsPlugin = require('stats-webpack-plugin');
 
-const devBuild = process.env.NODE_ENV !== 'production';
+const env = process.env.NODE_ENV || 'development';
+const production = env === 'production';
+const development = env === 'development';
+
+// must match config.webpack.dev_server.port
+const devServerPort = 8080;
 
 const config = {
   entry: {
@@ -28,9 +33,9 @@ const config = {
   },
 
   output: {
-    filename: '[name]-[chunkhash].js',
-    chunkFilename: '[name]-[chunkhash].js',
-    path: '../public/webpack',
+    filename: production ? '[name]-[chunkhash].js' : '[name].js',
+    chunkFilename: production ? '[name]-[chunkhash].js' : '[name].js',
+    path: path.join(__dirname, '..', 'public', 'webpack'),
     publicPath: '/webpack/',
   },
 
@@ -102,11 +107,16 @@ const config = {
   },
 };
 
-module.exports = config;
-
-if (devBuild) {
-  console.log('Webpack dev build for Rails'); // eslint-disable-line no-console
-  module.exports.devtool = 'eval-source-map';
+if (development) {
+  config.devServer = {
+    compress: true,
+    port: devServerPort,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+  };
+  config.output.publicPath = `//localhost:${devServerPort}/webpack/`;
+  config.devtool = 'cheap-module-eval-source-map';
 } else {
-  console.log('Webpack production build for Rails'); // eslint-disable-line no-console
+  console.log(`\nWebpack ${env} build for Rails...`); // eslint-disable-line no-console
 }
+
+module.exports = config;
