cb51db05cebebc9aa774f2776921aa74810c95d8
Jeremy Yap <jeremy.yapjl@gmail.com>
2017-02-28 00:12:20 +0800

    Fix Summernote image upload resize failing randomly
    When the image is cached, the image loads instantly and height/width is
    non-zero. However, when image is not cached, the code sees the height
    and width as 0 before the image is loaded, and decides that the image
    does not need to be resized.
    
    This commit fixes the bug by waiting for the image onload event before
    checking the width/height.

--- client/app/lib/components/MaterialSummernote.jsx
+++ client/app/lib/components/MaterialSummernote.jsx
@@ -52,17 +52,19 @@ class MaterialSummernote extends React.Component {
     const IMAGE_MAX_WIDTH = 1920;
     const IMAGE_MAX_HEIGHT = 1080;
 
+    const img = document.createElement('img');
+    const canvas = document.createElement('canvas');
+
     const reader = new FileReader();
     reader.onload = function onload(e) {
-      const img = document.createElement('img');
-      const canvas = document.createElement('canvas');
-
       img.src = e.target.result;
+    };
+    img.onload = function onload() {
       let width = img.width;
       let height = img.height;
 
       if (width <= IMAGE_MAX_WIDTH && height <= IMAGE_MAX_HEIGHT) {
-        onImageCompressed(e.target.result);
+        onImageCompressed(img.src);
         return;
       }
       if (width > IMAGE_MAX_WIDTH) {
