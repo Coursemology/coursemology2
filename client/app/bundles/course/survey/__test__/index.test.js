9bc21ad079901d921fb9308b53658489a4f1254e
Brian Koh <kxmbrian@gmail.com>
2017-03-30 15:58:58 +0800

    Show loading indicator for SurveyIndex page

--- client/app/bundles/course/survey/__test__/index.test.js
+++ client/app/bundles/course/survey/__test__/index.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Router, createMemoryHistory } from 'react-router';
 import { mount } from 'enzyme';
 import MockAdapter from 'axios-mock-adapter';
-import ReactTestUtils from 'react-addons-test-utils';
 import ProviderWrapper from 'lib/components/ProviderWrapper';
 import CourseAPI from 'api/course';
 import storeCreator from 'course/survey/store';
@@ -21,7 +19,16 @@ describe('Surveys', () => {
   it('renders the index page and survey form', async () => {
     mock.onGet(`/courses/${courseId}/surveys`)
       .reply(200, {
-        surveys: [],
+        surveys: [{
+          id: 1,
+          base_exp: 20,
+          canViewResults: true,
+          title: 'First Survey',
+          isStarted: false,
+          published: true,
+          start_at: '2017-02-27T00:00:00.000+08:00',
+          end_at: '2017-03-12T23:59:00.000+08:00',
+        }],
         canCreate: true,
       });
     // Set the route to index page
@@ -40,10 +47,6 @@ describe('Surveys', () => {
     await sleep(1);
     expect(spyIndex).toHaveBeenCalled();
     expect(indexPage.find('AddButton').length).toBe(1);
-    expect(indexPage.find('Subheader').find('span').text()).toBe('No surveys have been created.');
-
-    const newSurveyBtn = ReactDOM.findDOMNode(indexPage.find('button').node);
-    ReactTestUtils.Simulate.touchTap(newSurveyBtn);
-    expect(indexPage.find('SurveyFormDialogue').first().props().visible).toBe(true);
+    expect(indexPage.find('Table')).toHaveLength(1);
   });
 });
--- client/app/bundles/course/survey/reducers/surveysFlags.js
+++ client/app/bundles/course/survey/reducers/surveysFlags.js
@@ -1,6 +1,7 @@
 import actionTypes from '../constants';
 
 const initialState = {
+  isLoadingSurveys: false,
   canCreate: false,
   isQuestionMoved: false,
 };
@@ -9,8 +10,14 @@ export default function (state = initialState, action) {
   const { type } = action;
 
   switch (type) {
+    case actionTypes.LOAD_SURVEYS_REQUEST: {
+      return { ...state, isLoadingSurveys: true };
+    }
     case actionTypes.LOAD_SURVEYS_SUCCESS: {
-      return { ...state, canCreate: action.canCreate };
+      return { ...state, isLoadingSurveys: false, canCreate: action.canCreate };
+    }
+    case actionTypes.LOAD_SURVEYS_FAILURE: {
+      return { ...state, isLoadingSurveys: false };
     }
     case actionTypes.REORDER_QUESTION:
     case actionTypes.CHANGE_QUESTION_SECTION: {
