e55ccf26db3fb9e51ef4a09406bb3c49dccafa63
Brian Koh <kxmbrian@gmail.com>
2017-03-31 11:52:18 +0800

    Add loading indicator and response count to SurveyResults page

--- client/app/bundles/course/survey/pages/SurveyResults/index.jsx
+++ client/app/bundles/course/survey/pages/SurveyResults/index.jsx
@@ -10,6 +10,7 @@ import { Card, CardText } from 'material-ui/Card';
 import ArrowBack from 'material-ui/svg-icons/navigation/arrow-back';
 import surveyTranslations from 'course/survey/translations';
 import { fetchResults } from 'course/survey/actions/surveys';
+import LoadingIndicator from 'course/survey/components/LoadingIndicator';
 import { surveyShape, sectionShape } from 'course/survey/propTypes';
 import ResultsSection from './ResultsSection';
 
@@ -18,11 +19,20 @@ const translations = defineMessages({
     id: 'course.surveys.SurveyResults.includePhantoms',
     defaultMessage: 'Include Phantom Students',
   },
+  responsesCount: {
+    id: 'course.surveys.SurveyResults.responsesCount',
+    defaultMessage: 'Number of Responses: {count}',
+  },
+  noSections: {
+    id: 'course.surveys.SurveyResults.noSections',
+    defaultMessage: 'This survey does not have any questions yet.',
+  },
 });
 
 class SurveyResults extends React.Component {
   static propTypes = {
     dispatch: PropTypes.func.isRequired,
+    isLoading: PropTypes.bool.isRequired,
     params: PropTypes.shape({
       courseId: PropTypes.string.isRequired,
       surveyId: PropTypes.string.isRequired,
@@ -44,9 +54,66 @@ class SurveyResults extends React.Component {
     dispatch(fetchResults(surveyId));
   }
 
-  renderResults() {
+  getRespondentsCount() {
+    const { sections } = this.props;
+    if (
+      (sections && sections.length) < 1 ||
+      (sections[0].questions && sections[0].questions.length < 1) ||
+      (sections[0].questions[0].answers && sections[0].questions[0].answers.length < 1)
+    ) {
+      return { totalStudents: 0, realStudents: 0 };
+    }
+
+    const totalStudents = sections[0].questions[0].answers.length;
+    const realStudents =
+      sections[0].questions[0].answers.filter(answer => !answer.phantom).length;
+    return { totalStudents, realStudents };
+  }
+
+  renderIncludePhantomToggle() {
+    return (
+      <Card>
+        <CardText>
+          <Toggle
+            label={<FormattedMessage {...translations.includePhantoms} />}
+            labelPosition="right"
+            onToggle={(_, value) => this.setState({ includePhantoms: value })}
+          />
+        </CardText>
+      </Card>
+    );
+  }
+
+  renderBody() {
+    const { sections } = this.props;
+    const noSections = sections && sections.length < 1;
+    if (noSections) {
+      return <Subheader><FormattedMessage {...translations.noSections} /></Subheader>;
+    }
+
+    const { totalStudents, realStudents } = this.getRespondentsCount();
+    const responsesCount = this.state.includePhantoms ? totalStudents : realStudents;
     return (
       <div>
+        <Card>
+          <CardText>
+            <h4>
+              <FormattedMessage
+                {...translations.responsesCount}
+                values={{ count: responsesCount.toString() }}
+              />
+            </h4>
+            {
+              totalStudents === realStudents ? null :
+              <Toggle
+                label={<FormattedMessage {...translations.includePhantoms} />}
+                labelPosition="right"
+                onToggle={(_, value) => this.setState({ includePhantoms: value })}
+              />
+            }
+          </CardText>
+        </Card>
+        <Subheader><FormattedMessage {...surveyTranslations.questions} /></Subheader>
         {
           this.props.sections.map((section, index) =>
             <ResultsSection
@@ -61,7 +128,8 @@ class SurveyResults extends React.Component {
   }
 
   render() {
-    const { survey, params: { courseId } } = this.props;
+    const { survey, isLoading, params: { courseId } } = this.props;
+    if (isLoading) { return <LoadingIndicator />; }
     return (
       <div>
         <TitleBar
@@ -69,17 +137,7 @@ class SurveyResults extends React.Component {
           iconElementLeft={<IconButton><ArrowBack /></IconButton>}
           onLeftIconButtonTouchTap={() => browserHistory.push(`/courses/${courseId}/surveys`)}
         />
-        <Card>
-          <CardText>
-            <Toggle
-              label={<FormattedMessage {...translations.includePhantoms} />}
-              labelPosition="right"
-              onToggle={(_, value) => this.setState({ includePhantoms: value })}
-            />
-          </CardText>
-        </Card>
-        <Subheader><FormattedMessage {...surveyTranslations.questions} /></Subheader>
-        {this.renderResults()}
+        { this.renderBody() }
       </div>
     );
   }
