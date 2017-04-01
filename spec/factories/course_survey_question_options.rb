221f1a9c59e7376ab952acc086ce150b9ded96d2
Jeremy Yap <jeremy.yapjl@gmail.com>
2017-04-02 01:54:27 +0800

    Expand factories for surveys

---
+++ spec/factories/course_survey_question_options.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+FactoryGirl.define do
+  sequence(:course_survey_question_option_text) { |n| "Option #{n}" }
+  factory :course_survey_question_option, class: Course::Survey::QuestionOption.name do
+    transient do
+      last_weight { question.options.maximum(:weight) }
+    end
+
+    association :question, factory: :survey_question
+    option { generate(:course_survey_question_option_text) }
+    weight { last_weight ? last_weight + 1 : 0 }
+
+    trait :with_attachment do
+      attachment_reference
+    end
+  end
+end
--- spec/factories/course_surveys.rb
+++ spec/factories/course_surveys.rb
@@ -3,44 +3,56 @@ FactoryGirl.define do
   sequence(:course_survey_name) { |n| "Survey #{n}" }
   factory :course_survey, class: Course::Survey.name,
                           aliases: [:survey], parent: :course_lesson_plan_item do
+    transient do
+      section_count 1
+      section_traits []
+    end
+
     title { generate(:course_survey_name) }
     anonymous false
     allow_response_after_end false
     allow_modify_after_submit false
-  end
 
-  trait :published do
-    published true
-  end
+    trait :published do
+      published true
+    end
 
-  trait :unpublished do
-    published false
-  end
+    trait :unpublished do
+      published false
+    end
 
-  trait :anonymous do
-    anonymous true
-  end
+    trait :anonymous do
+      anonymous true
+    end
 
-  trait :allow_response_after_end do
-    allow_response_after_end true
-  end
+    trait :allow_response_after_end do
+      allow_response_after_end true
+    end
 
-  trait :allow_modify_after_submit do
-    allow_modify_after_submit true
-  end
+    trait :allow_modify_after_submit do
+      allow_modify_after_submit true
+    end
 
-  trait :not_started do
-    start_at { 1.day.from_now }
-    end_at { 3.days.from_now }
-  end
+    trait :not_started do
+      start_at { 1.day.from_now }
+      end_at { 3.days.from_now }
+    end
 
-  trait :expired do
-    start_at { 3.days.ago }
-    end_at { 1.day.ago }
-  end
+    trait :expired do
+      start_at { 3.days.ago }
+      end_at { 1.day.ago }
+    end
 
-  trait :currently_active do
-    start_at { 2.days.ago }
-    end_at { 2.days.from_now }
+    trait :currently_active do
+      start_at { 2.days.ago }
+      end_at { 2.days.from_now }
+    end
+
+    after(:build) do |survey, evaluator|
+      evaluator.section_count.downto(1).each do
+        section = build(:section, *evaluator.section_traits, survey: survey)
+        survey.sections << section
+      end
+    end
   end
 end
