ceb14ce3437b624c6cb817cc75a78e5be1cf5991
Brian Koh <kxmbrian@gmail.com>
2017-04-18 14:33:45 +0800

    Add tests for survey response controller

--- spec/controllers/course/survey/responses_controller_spec.rb
+++ spec/controllers/course/survey/responses_controller_spec.rb
@@ -7,40 +7,159 @@ RSpec.describe Course::Survey::ResponsesController do
   with_tenant(:instance) do
     let(:admin) { create(:administrator) }
     let!(:course) { create(:course, creator: admin) }
+    let(:manager) { create(:course_manager, course: course) }
     let!(:student) { create(:course_student, course: course) }
-    let!(:survey) { create(:survey, course: course) }
+    let!(:survey) { create(:survey, *survey_traits, course: course) }
     let!(:survey_question) do
       section = create(:course_survey_section, survey: survey)
       create(:course_survey_question, question_type: :text, section: section)
     end
-    let(:response) { create(:response, *response_traits, survey: survey, creator: student.user) }
+    let(:new_question) do
+      section = create(:course_survey_section, survey: survey)
+      create(:course_survey_question, question_type: :text, section: section)
+    end
+    let(:survey_response) do
+      create(:response, *response_traits,
+             survey: survey, creator: student.user, course_user: student)
+    end
+    let(:survey_traits) { nil }
     let(:response_traits) { nil }
 
     before { sign_in(user) }
+
+    describe '#create' do
+      let(:create_response_request) do
+        post :create, format: :json, course_id: course.id, survey_id: survey.id
+      end
+
+      context 'when user is a student' do
+        let(:user) { student.user }
+        let(:survey_traits) { [:published, :currently_active] }
+
+        context 'when response has not already been created' do
+          it 'creates a response with answers populated' do
+            expect { create_response_request }.to change { survey.responses.count }.by(1)
+            response_answer_count =
+              survey.responses.find_by(course_user_id: student.id).answers.count
+            expect(response_answer_count).to eq(survey.questions.count)
+          end
+        end
+
+        context 'when response has already been created' do
+          before { survey_response }
+          it 'responds with the id of the existing survey response' do
+            expect { create_response_request }.to change { survey.responses.count }.by(0)
+            expect(response.body).to eq({ responseId: survey_response.id }.to_json)
+          end
+        end
+      end
+
+      context 'when user is not enrolled in the course' do
+        let(:user) { create(:administrator) }
+        before { create_response_request }
+        it { is_expected.to respond_with :bad_request }
+      end
+    end
+
+    describe '#show' do
+      subject do
+        get :show, format: :json,
+                   course_id: course.id, survey_id: survey.id, id: survey_response.id
+      end
+
+      context 'when a new question is created after response was last edited' do
+        let(:user) { manager.user }
+
+        before do
+          survey_response
+          new_question
+          subject
+        end
+
+        it 'does not create answers for new questions' do
+          response_answer_count = survey.responses.find(survey_response.id).answers.count
+          expect(response_answer_count).not_to eq(survey.questions.count)
+        end
+      end
+    end
+
+    describe '#edit' do
+      subject do
+        get :edit, format: :json,
+                   course_id: course.id, survey_id: survey.id, id: survey_response.id
+      end
+
+      context 'when a new question is created after response was last edited' do
+        let(:user) { student.user }
+        let(:survey_traits) { [:published, :currently_active] }
+
+        before do
+          survey_response
+          new_question
+          subject
+        end
+
+        it 'creates answers for all new question' do
+          response_answer_count = survey.responses.find(survey_response.id).answers.count
+          expect(response_answer_count).to eq(survey.questions.count)
+        end
+      end
+    end
+
+    describe '#update' do
+      subject do
+        patch :update, format: :json, response: response_params,
+                       course_id: course.id, survey_id: survey.id, id: survey_response.id
+      end
+
+      context 'when the response is being submitted' do
+        let(:user) { student.user }
+        let(:response_params) { { submit: true } }
+
+        context 'when the response is on time' do
+          let(:survey_traits) { [:published, :currently_active] }
+
+          it 'sets submitted_at and awards full points' do
+            expect { subject }.to change { student.experience_points }.
+              by(survey.base_exp + survey.time_bonus_exp)
+            expect(survey_response.reload.submitted_at).not_to be_nil
+          end
+        end
+
+        context 'when the response is late' do
+          let(:survey_traits) { [:published, :expired, :allow_response_after_end] }
+
+          it 'sets submitted_at and awards base points only' do
+            expect { subject }.to change { student.experience_points }.by(survey.base_exp)
+            expect(survey_response.reload.submitted_at).not_to be_nil
+          end
+        end
+      end
+    end
 
     describe '#unsubmit' do
       let(:user) { admin }
       let(:response_traits) { :submitted }
       let!(:response_answer) do
-        create(:course_survey_answer, question: survey_question, response: response)
+        create(:course_survey_answer, question: survey_question, response: survey_response)
       end
       let(:new_response_text) { response_answer.text_response + 'New Stuff' }
       let(:response_params) do
         { answer_attributes: { id: response_answer.id, text_response: new_response_text } }
       end
       subject do
-        post :unsubmit, format: :json, course_id: course.id, survey_id: survey.id, id: response.id,
-                        response: response_params
+        post :unsubmit, format: :json, response: response_params,
+                        course_id: course.id, survey_id: survey.id, id: survey_response.id
       end
 
       it 'unsubmits the survey response' do
         subject
-        expect(response.reload.submitted?).to be_falsey
+        expect(survey_response.reload.submitted?).to be_falsey
       end
 
       it 'does not update the answer attributes' do
         subject
-        expect(response.reload.answers.first.text_response).not_to eq(new_response_text)
+        expect(survey_response.reload.answers.first.text_response).not_to eq(new_response_text)
       end
 
       context 'when user is a student' do
