d46261cf9603cd607fa9325103bc0e8f0822adb6
weiqingtoh <weiqingtoh@gmail.com>
2017-05-04 14:05:56 +0800

    Modify controller action to only return status 400
    - Using render results in rails trying to render the 'create' partial after the action has completed execution.
    - This causes the app to try to render the entire programming annotation with an invalid post, causing errors as the created_at is nil.

--- app/controllers/course/assessment/submission/answer/programming/annotations_controller.rb
+++ app/controllers/course/assessment/submission/answer/programming/annotations_controller.rb
@@ -10,7 +10,7 @@ class Course::Assessment::Submission::Answer::Programming::AnnotationsController
       if super && @annotation.save
         send_created_notification(@post)
       else
-        render status: :bad_request
+        head :bad_request
       end
     end
   end
