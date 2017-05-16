8b5c6edcea67924e325a96c6aeed3d70150aafa5
Allen Wang <rovingbreeze@gmail.com>
2017-05-16 20:01:51 +0800

    Only mark the announcements as read after the action * Also fixed a bug that the announcements in the next page get marked as unread

--- spec/features/course/announcement_pagination_spec.rb
+++ spec/features/course/announcement_pagination_spec.rb
@@ -8,10 +8,11 @@ RSpec.describe 'Course: Announcements', type: :feature do
     let!(:instance) { Instance.default }
 
     with_tenant(:instance) do
+      let(:announcement_count) { 30 }
       let(:course) { create(:course) }
-      let(:user) { create(:course_manager, course: course).user }
+      let!(:user) { create(:course_manager, course: course).user }
       let!(:announcements) do
-        create_list(:course_announcement, 30, course: course)
+        create_list(:course_announcement, announcement_count, course: course)
       end
 
       before do
@@ -22,10 +23,21 @@ RSpec.describe 'Course: Announcements', type: :feature do
         visit course_announcements_path(course)
 
         expect(page).to have_selector('nav.pagination')
-        course.announcements.sorted_by_sticky.sorted_by_date.page(1).each do |announcement|
-          expect(page).to have_selector('div', text: announcement.title)
-          expect(page).to have_selector('div', text: announcement.content)
+        announcements = course.announcements.sorted_by_sticky.sorted_by_date.page(1)
+        announcements.each do |announcement|
+          expect(page).to have_selector('div.unread', text: announcement.title)
+          expect(page).to have_selector('div.unread', text: announcement.content)
         end
+
+        # In total there are two pages, the test is to ensure that only the announcements in the
+        # current page get marked as unread.
+        expect(course.announcements.unread_by(user).count).
+          to eq(announcement_count - announcements.size)
+
+        # Visit the page again should not reduce the unread count.
+        visit course_announcements_path(course)
+        expect(course.announcements.unread_by(user).count).
+          to eq(announcement_count - announcements.size)
       end
 
       context 'after clicked second page' do
