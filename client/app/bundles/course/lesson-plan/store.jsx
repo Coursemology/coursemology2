fc49dfd83cf4f385cf6e03b361e5057f9cbc4af2
Brian Koh <kxmbrian@gmail.com>
2017-06-03 11:12:56 +0800

    Refactor lesson plan store

--- client/app/bundles/course/lesson-plan/store.jsx
+++ client/app/bundles/course/lesson-plan/store.jsx
@@ -1,12 +1,12 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import rootReducer, { initialStates as defaultInitialStates } from './reducers';
+import rootReducer from './reducers';
 
-export default (props) => {
-  const initialStates = defaultInitialStates.mergeDeep({ lessonPlan: props });
-  const storeCreator = compose(
-    applyMiddleware(thunkMiddleware)
-  )(createStore);
+export default (initialStates) => {
+  const storeCreator = (process.env.NODE_ENV === 'development') ?
+    // eslint-disable-next-line global-require
+    compose(applyMiddleware(thunkMiddleware, require('redux-logger').logger))(createStore) :
+    compose(applyMiddleware(thunkMiddleware))(createStore);
 
   return storeCreator(rootReducer, initialStates);
 };
