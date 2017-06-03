274c28a8a42d78f5f670c0d45534711c3e4f7c60
Brian Koh <kxmbrian@gmail.com>
2017-06-03 11:13:28 +0800

    Update LessonPlanLayout

--- client/app/bundles/course/lesson-plan/items.jsx
+++ client/app/bundles/course/lesson-plan/items.jsx
@@ -1,5 +1,23 @@
-import initializeComponent from 'lib/helpers/initializeComponent';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router } from 'react-router-dom';
+import history from 'lib/history';
+import ProviderWrapper from 'lib/components/ProviderWrapper';
+import LessonPlanLayout from 'course/lesson-plan/containers/LessonPlanLayout';
 import storeCreator from './store';
-import LessonPlan from './containers/LessonPlan';
 
-initializeComponent(LessonPlan, 'lesson-plan-items', storeCreator);
+$(document).ready(() => {
+  const mountNode = document.getElementById('lesson-plan-items');
+
+  if (mountNode) {
+    const store = storeCreator();
+
+    render(
+      <ProviderWrapper {...{ store }}>
+        <Router history={history}>
+          <LessonPlanLayout />
+        </Router>
+      </ProviderWrapper>
+    , mountNode);
+  }
+});
