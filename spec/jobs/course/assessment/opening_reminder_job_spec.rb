7e6f1ad7a3b09307af8bf5e56083825086a2ed51
weiqingtoh <weiqingtoh@gmail.com>
2017-07-08 23:41:56 +0800

    Add check to send opening reminder emails only if start_at is at a future date

--- spec/jobs/course/assessment/opening_reminder_job_spec.rb
+++ spec/jobs/course/assessment/opening_reminder_job_spec.rb
@@ -4,13 +4,36 @@ require 'rails_helper'
 RSpec.describe Course::Assessment::OpeningReminderJob do
   let(:instance) { Instance.default }
   with_tenant(:instance) do
-    let(:assessment) { create(:assessment) }
+    let(:assessment) { create(:assessment, start_at: old_start_at) }
+    let(:time_now) { Time.zone.now }
+    before { assessment.start_at = new_start_at }
+    subject { assessment.save }
 
-    context 'when start_at of the assessment is changed' do
-      it 'creates a opening reminder job' do
-        assessment.start_at = Time.zone.now
+    context 'when old start_at is in the past' do
+      let(:old_start_at) { time_now - 2.days }
 
-        expect { assessment.save }.to have_enqueued_job(Course::Assessment::OpeningReminderJob)
+      context 'when new start_at is in the future' do
+        let(:new_start_at) { time_now + 3.days }
+        it { expect { subject }.to have_enqueued_job(Course::Assessment::OpeningReminderJob) }
+      end
+
+      context 'when new start_at is in the past' do
+        let(:new_start_at) { time_now - 3.days }
+        it { expect { subject }.not_to have_enqueued_job(Course::Assessment::OpeningReminderJob) }
+      end
+    end
+
+    context 'when old start_at is in the future' do
+      let(:old_start_at) { time_now + 2.days }
+
+      context 'when new start_at is in the future' do
+        let(:new_start_at) { time_now + 3.days }
+        it { expect { subject }.to have_enqueued_job(Course::Assessment::OpeningReminderJob) }
+      end
+
+      context 'when new start_at is in the past' do
+        let(:new_start_at) { time_now - 2.days }
+        it { expect { subject }.to have_enqueued_job(Course::Assessment::OpeningReminderJob) }
       end
     end
   end
