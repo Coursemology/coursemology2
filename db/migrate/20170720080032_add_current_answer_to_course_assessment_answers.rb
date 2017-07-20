8afdc24a45c7d29f519607d4292a4bbe8f660a71
fonglh <fonglh@gmail.com>
2017-07-20 16:27:18 +0800

    Add current_answer flag to course_assessment_answers.
    When migration is run, assume that the latest answer (max created_at)
    is the current answer.

---
+++ db/migrate/20170720080032_add_current_answer_to_course_assessment_answers.rb
@@ -0,0 +1,28 @@
+class AddCurrentAnswerToCourseAssessmentAnswers < ActiveRecord::Migration
+  def change
+    add_column :course_assessment_answers, :current_answer, :boolean, null: false, default: false
+
+    reversible do |dir|
+      dir.up { set_current_answers }
+      dir.down {}
+    end
+  end
+
+  # Take the latest created_at answer as the current_answer.
+  def set_current_answers
+    execute <<-SQL
+      WITH current_answers AS (
+        WITH max_created_table AS (
+          SELECT MAX(created_at) AS max_created_at, submission_id, question_id FROM course_assessment_answers
+          GROUP BY submission_id, question_id
+        )
+        SELECT id FROM course_assessment_answers, max_created_table
+          WHERE course_assessment_answers.created_at = max_created_table.max_created_at
+          AND course_assessment_answers.submission_id = max_created_table.submission_id
+          AND course_assessment_answers.question_id = max_created_table.question_id
+      )
+      UPDATE course_assessment_answers SET current_answer = true
+        FROM current_answers WHERE course_assessment_answers.id = current_answers.id
+    SQL
+  end
+end
