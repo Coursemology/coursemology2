d96ce93a1f21a4f941b73a04af6a2bfaca579586
Allen Wang <rovingbreeze@gmail.com>
2017-07-25 17:06:00 +0800

    Implement specs for user role requests

---
+++ spec/factories/instance_user_role_requests.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+FactoryGirl.define do
+  factory :instance_user_role_request, aliases: [:role_request], class: Instance::UserRoleRequest do
+    instance
+    user { build(:instance_user, instance: instance).user }
+    role :instructor
+    organization 'NUS'
+    designation 'Lecturer'
+    reason 'I like coursemology'
+  end
+end
