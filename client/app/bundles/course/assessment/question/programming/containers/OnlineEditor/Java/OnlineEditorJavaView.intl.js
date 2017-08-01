e47060d9135f31e0722fcf9ce1bd1e7912ff1355
ZhengWei <tanzhengwei143@gmail.com>
2017-08-01 15:41:28 +0800

    Refactor OnlineEditor translations into their own language-specific translations

---
+++ client/app/bundles/course/assessment/question/programming/containers/OnlineEditor/Java/OnlineEditorJavaView.intl.js
@@ -0,0 +1,80 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  prependSubtitle: {
+    id: 'course.assessment.question.programming.onlineEditorJavaView.prependSubtitle',
+    defaultMessage: 'For any library imports (optional, hidden, inserted before student\'s code)',
+    description: 'Subtitle for prepend code block.',
+  },
+  appendSubtitle: {
+    id: 'course.assessment.question.programming.onlineEditorJavaView.appendSubtitle',
+    defaultMessage: 'Within the scope for the test class: Declare any methods or instance variables here',
+    description: 'Subtitle for append code block.',
+  },
+  submitAsFileToggle: {
+    id: 'course.assessment.question.programming.onlineEditorJavaView.submitAsFileToggle',
+    defaultMessage: 'File Submission',
+  },
+  submitAsFileToggleDisabled: {
+    id: 'course.assessment.question.programming.onlineEditorJavaView.submitAsFileToggleDisabled',
+    defaultMessage: 'File Submission (This option cannot be switched as some answers have already been submitted)',
+  },
+  addSubmissionFileButton: {
+    id: 'course.assessment.question.programming.onlineEditorJavaView.addSubmissionFileButton',
+    defaultMessage: 'Add Submission File',
+  },
+  addSolutionFileButton: {
+    id: 'course.assessment.question.programming.onlineEditorJavaView.addSolutionFileButton',
+    defaultMessage: 'Add Solution File',
+  },
+  submissionFilesHeader: {
+    id: 'course.assessment.question.programming.onlineEditorJavaView.submissionFilesHeader',
+    defaultMessage: 'Submission Files (Required)',
+    description: 'Header for the submission files of the online editor.',
+  },
+  solutionFilesHeader: {
+    id: 'course.assessment.question.programming.onlineEditorJavaView.solutionFilesHeader',
+    defaultMessage: 'Solution Files (Required)',
+    description: 'Header for the solution files of the online editor.',
+  },
+  newSubmissionFilesHeader: {
+    id: 'course.assessment.question.programming.onlineEditorJavaView.newSubmissionFilesHeader',
+    defaultMessage: 'New Submission Files',
+    description: 'Header for the new submission files panel.',
+  },
+  newSolutionFilesHeader: {
+    id: 'course.assessment.question.programming.onlineEditorJavaView.newSolutionFilesHeader',
+    defaultMessage: 'New Solution Files',
+    description: 'Header for the new solution files panel.',
+  },
+  currentSubmissionFilesHeader: {
+    id: 'course.assessment.question.programming.onlineEditorJavaView.currentSubmissionFilesHeader',
+    defaultMessage: 'Current Submission Files',
+    description: 'Header for the current submission files panel.',
+  },
+  currentSolutionFilesHeader: {
+    id: 'course.assessment.question.programming.onlineEditorJavaView.currentSolutionFilesHeader',
+    defaultMessage: 'Current Solution Files',
+    description: 'Header for the current solution files panel.',
+  },
+  testCaseDescriptionCode: {
+    id: 'course.assessment.question.programming.onlineEditorJavaView.testCaseDescriptionCode',
+    defaultMessage: 'CODE',
+    description: 'Value of the code key that is passed into the test case description.',
+  },
+  testCaseDescriptionEditor: {
+    id: 'course.assessment.question.programming.onlineEditorJavaView.testCaseDescriptionEditor',
+    defaultMessage: 'CODE EDITOR',
+    description: 'Value of the code key that is passed into the test case description.',
+  },
+  expectEquals: {
+    id: 'course.assessment.question.programming.onlineEditorJavaView.expectEquals',
+    defaultMessage: 'void expectEquals(expression, expected)',
+    description: 'Function template for expectEquals',
+  },
+  fileSubmissionDescriptionNote: {
+    id: 'course.assessment.question.programming.onlineEditorJavaView.fileSubmissionDescriptionNote',
+    defaultMessage: 'File Submission',
+    description: 'Value of the key that is passed into the description of the file submission toggle.',
+  },
+});
