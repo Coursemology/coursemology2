610364c59a77b9793c0692711392037d56c7fb1a
Jeremy Yap <jeremy.yapjl@gmail.com>
2017-08-04 23:55:14 +0800

    Hide explanation panel for evaluation test case failures for manually graded assessment

--- client/app/bundles/course/assessment/submission/pages/SubmissionEditIndex/SubmissionEditForm.jsx
+++ client/app/bundles/course/assessment/submission/pages/SubmissionEditIndex/SubmissionEditForm.jsx
@@ -10,8 +10,7 @@ import { Card, CardHeader, CardText } from 'material-ui/Card';
 import CircularProgress from 'material-ui/CircularProgress';
 import RaisedButton from 'material-ui/RaisedButton';
 import Paper from 'material-ui/Paper';
-import { red100, red200, red900, yellow900, grey100, blue500,
-         green200, green900, white } from 'material-ui/styles/colors';
+import { red100, red200, red900, yellow900, grey100, blue500, white } from 'material-ui/styles/colors';
 
 /* eslint-disable import/extensions, import/no-extraneous-dependencies, import/no-unresolved */
 import Dialog from 'material-ui/Dialog';
@@ -130,7 +129,7 @@ class SubmissionEditForm extends Component {
       } else if (explanation.failureType === 'private_test') {
         title = intl.formatMessage(translations.privateTestCaseFailure);
       } else {
-        title = intl.formatMessage(translations.wrong);
+        return null;
       }
 
       return (
@@ -138,10 +137,10 @@ class SubmissionEditForm extends Component {
           <CardHeader
             style={{
               ...styles.explanationHeader,
-              backgroundColor: explanation.correct ? green200 : red200,
+              backgroundColor: red200,
             }}
             title={title}
-            titleColor={explanation.correct ? green900 : red900}
+            titleColor={red900}
           />
           <CardText>
             {explanation.explanations.map(exp => <div dangerouslySetInnerHTML={{ __html: exp }} />)}
