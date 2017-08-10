712aa843c574775708877e0f9b60f21b350ca9a6
Allen Wang <rovingbreeze@gmail.com>
2017-08-10 14:06:09 +0800

    Handle the hidden fields in programming management spec

--- spec/features/course/assessment/question/programming_management_spec.rb
+++ spec/features/course/assessment/question/programming_management_spec.rb
@@ -29,13 +29,15 @@ RSpec.describe 'Course: Assessments: Questions: Programming Management' do
                                  question_attributes[:description]
         fill_in_react_summernote 'textarea#question_programming_staff_only_comments',
                                  question_attributes[:staff_only_comments]
-
         fill_in 'question_programming[maximum_grade]', with: question_attributes[:maximum_grade]
-        within find_field('question_programming[skill_ids][]', visible: false) do
-          select skill.title, visible: false
+
+        page.execute_script("$('select[name=\"question_programming[skill_ids][]\"]').show()")
+        within find_field('question_programming[skill_ids][]') do
+          select skill.title
         end
-        select question_attributes[:language].name,
-               from: 'question_programming[language_id]', visible: false
+
+        page.execute_script("$('select[name=\"question_programming[language_id]\"]').show()")
+        select question_attributes[:language].name, from: 'question_programming[language_id]'
         page.find('#programmming-question-form-submit').click
 
         expect(page).to_not have_xpath('//form//*[contains(@class, \'fa-spinner\')]')
@@ -57,6 +59,7 @@ RSpec.describe 'Course: Assessments: Questions: Programming Management' do
         expect(page).to have_xpath('//form[@id=\'programmming-question-form\']')
 
         page.find('#question_programming_file', visible: false).click
+
         attach_file 'question_programming[file]',
                     File.join(ActionController::TestCase.fixture_path,
                               'course/empty_programming_question_template.zip'),
@@ -67,8 +70,7 @@ RSpec.describe 'Course: Assessments: Questions: Programming Management' do
 
         page.find('#question_programming_file', visible: false).click
         attach_file 'question_programming[file]',
-                    File.join(ActionController::TestCase.fixture_path,
-                              'course/programming_question_template.zip'),
+                    File.join(ActionController::TestCase.fixture_path, 'course/programming_question_template.zip'),
                     visible: false
         page.find('#programmming-question-form-submit').click
         wait_for_job
@@ -78,10 +80,9 @@ RSpec.describe 'Course: Assessments: Questions: Programming Management' do
         visit edit_course_assessment_question_programming_path(course, assessment, question)
 
         expect(page).to have_selector('div.alert.alert-success')
+
         question.template_files.reload.each do |template|
           expect(page).to have_selector('.template-tab', text: template.filename)
-          expect(page).to have_selector('div.template-content',
-                                        text: template.content)
         end
 
         question.test_cases.reload.each do |test_case|
@@ -121,19 +122,14 @@ RSpec.describe 'Course: Assessments: Questions: Programming Management' do
       end
 
       scenario 'I can create a new question and upload the template package', js: true do
-        skill = create(:course_assessment_skill, course: course)
         visit new_course_assessment_question_programming_path(course, assessment)
+
         expect(page).to have_xpath('//form[@id=\'programmming-question-form\']')
+
         question_attributes = attributes_for(:course_assessment_question_programming)
-        fill_in 'question_programming[title]', with: question_attributes[:title]
-        fill_in_react_summernote 'textarea#question_programming_description',
-                                 question_attributes[:description]
         fill_in 'question_programming[maximum_grade]', with: question_attributes[:maximum_grade]
-        within find_field('question_programming[skill_ids][]', visible: false) do
-          select skill.title, visible: false
-        end
-        select question_attributes[:language].name,
-               from: 'question_programming[language_id]', visible: false
+        page.execute_script("$('select[name=\"question_programming[language_id]\"]').show()")
+        select question_attributes[:language].name, from: 'question_programming[language_id]'
         page.check('question_programming[autograded]', visible: false)
         fill_in 'question_programming[memory_limit]', with: question_attributes[:memory_limit]
         # For some reasons we have to clear the old field first then can fill in the value, otherwise
@@ -145,10 +141,8 @@ RSpec.describe 'Course: Assessments: Questions: Programming Management' do
         page.find('#upload-package-tab').click
         page.find('#question_programming_file', visible: false).click
         attach_file 'question_programming[file]',
-                    File.join(ActionController::TestCase.fixture_path,
-                              'course/programming_question_template.zip'),
+                    File.join(ActionController::TestCase.fixture_path, 'course/programming_question_template.zip'),
                     visible: false
-
         page.find('#programmming-question-form-submit').click
         wait_for_job
 
