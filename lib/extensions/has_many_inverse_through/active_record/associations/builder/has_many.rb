31f4b944ee9a5430ca2a5ba38ecd3bd2b50caa9f
Jeremy Yap <jeremy.yapjl@gmail.com>
2017-09-20 23:11:33 +0800

    Change ActiveRecord::Associations::Builder::HasMany.valid_options to class method

--- lib/extensions/has_many_inverse_through/active_record/associations/builder/has_many.rb
+++ lib/extensions/has_many_inverse_through/active_record/associations/builder/has_many.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 module Extensions::HasManyInverseThrough::ActiveRecord::Associations::Builder::HasMany
-  module PrependMethods
-    def valid_options
+  module ClassMethods
+    def valid_options(options)
       [:inverse_through] + super
     end
   end
