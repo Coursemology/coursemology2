d010ec52b28f7cbac76f84790660cd4e91be4142
Jeremy Yap <jeremy.yapjl@gmail.com>
2017-09-24 13:40:50 +0800

    Upgrade activerecord-userstamp and simple_form

--- lib/extensions/association_inverse_suppression/active_record/base.rb
+++ lib/extensions/association_inverse_suppression/active_record/base.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 module Extensions::AssociationInverseSuppression::ActiveRecord::Base
   module ClassMethods
+    # Bring forward the userstamp association definitions
+    # TODO: Remove after lowjoel/activerecord-userstamp#27 is closed
+    def inherited(klass)
+      super
+
+      klass.class_eval do
+        add_userstamp_associations({})
+      end
+    end
+
     def add_userstamp_associations(options)
       options.reverse_merge!(inverse_of: false)
       super(options)
