a4e80e2c1f52cfc4ff2fb486f746f34d4ae62ca9
fonglh <fonglh@gmail.com>
2017-10-04 14:29:38 +0800

    Database migration to rekey sidebar ordering for assessment categories.
    Also add a rollback to go from assessments_#{category_id} back to
    assessments.

---
+++ db/migrate/20171004053203_rekey_sidebar_settings.rb
@@ -0,0 +1,32 @@
+class RekeySidebarSettings < ActiveRecord::Migration
+  def up
+    ActsAsTenant.without_tenant do
+      Course.all.each do |course|
+        weight = course.settings(:sidebar, :assessments).weight
+        next if !weight
+
+        course.assessment_categories.each do |c|
+          course.settings(:sidebar).settings("assessments_#{c.id}").weight = weight
+        end
+        course.settings(:sidebar).assessments = nil
+        course.save!
+      end
+    end
+  end
+
+  def down
+    ActsAsTenant.without_tenant do
+      Course.all.each do |course|
+        weights = course.assessment_categories.map do |c|
+          weight = course.settings(:sidebar).settings("assessments_#{c.id}").weight
+          course.settings(:sidebar).send("assessments_#{c.id}=", nil)
+          weight
+        end
+        next if weights.empty?
+        weight = weights.first
+        course.settings(:sidebar, :assessments).weight = weight
+        course.save!
+      end
+    end
+  end
+end
