0a0a691d6786f8b3cdd3b4323a9c53ef4f0c28dc
ShaoWei Teo <teo.shaowei@comp.nus.edu.sg>
2017-10-10 15:49:35 +0800

    Hide built-in users

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  let(:instance) { Instance.default }
+
+  with_tenant(:instance) do
+    let!(:user) { create(:user) }
+
+    describe '#show' do
+      before { sign_in(user) }
+      subject { get :show, id: user_id }
+
+      context 'When user is system user' do
+        let(:user_id) { User::SYSTEM_USER_ID }
+        it 'returns the 404 page' do
+          subject
+
+          expect(response.status).to eq(404)
+        end
+      end
+
+      context 'When user is deleted user' do
+        let(:user_id) { User::DELETED_USER_ID }
+        it 'returns the 404 page' do
+          subject
+
+          expect(response.status).to eq(404)
+        end
+      end
+    end
+  end
+end
