dc2b9e76d1926daacbf77cb1df6c9e07508f4147
Shen Yichen <2007.yichen@gmail.com>
2017-10-16 21:14:49 +0800

    Add email settings to Videos Component
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- app/models/course/settings/videos_component.rb
+++ app/models/course/settings/videos_component.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 class Course::Settings::VideosComponent < Course::Settings::Component
   include ActiveModel::Conversion
+  include Course::Settings::EmailSettingsConcern
+
+  def self.email_setting_items
+    {
+      video_opening: { enabled_by_default: true },
+      video_closing: { enabled_by_default: true }
+    }
+  end
+
+  def self.component_class
+    Course::VideosComponent
+  end
 
   # Returns the title of video component
   #
