e99b70466ed9cd5cd9f7a80dde2e0545519db042
fonglh <fonglh@gmail.com>
2017-10-16 15:24:49 +0800

    Modify Python test package generation to run test types independently.
    Add public, private, evaluation Makefile targets.
    Update test script generation to produce a different class for each test
    type.

--- app/services/course/assessment/question/programming/python/python_autograde_pre.py
+++ app/services/course/assessment/question/programming/python/python_autograde_pre.py
@@ -3,9 +3,3 @@ import unittest
 import xmlrunner
 import sys
 
-class AutoGrader(unittest.TestCase):
-    def setUp(self):
-        # clears the dictionary containing meta data for each test
-        self.meta = { 'expression': '', 'expected': '', 'hint': '' }
-
-# Do not modify above this line, unless you know what you are doing
