b0eea17c687100d2e39ddbb446504e2ed4f62a60
Shen Yichen <2007.yichen@gmail.com>
2017-10-23 23:45:55 +0800

    Change controller to nest comments
    Since comments are no longer grouped under a single topic when they are
    made at the same time, we can assign a new post to the last topological
    post.
    
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- app/controllers/course/video/topics_controller.rb
+++ app/controllers/course/video/topics_controller.rb
@@ -15,6 +15,7 @@ class Course::Video::TopicsController < Course::Video::Controller
   def create
     result = @topic.class.transaction do
       @post.title = @video.title
+      @post.parent = last_post_from(@topic)
       raise ActiveRecord::Rollback unless @post.save && create_topic_subscription && update_topic_pending_status
       raise ActiveRecord::Rollback unless @topic.save
       true
@@ -32,8 +33,8 @@ class Course::Video::TopicsController < Course::Video::Controller
     params.permit(:timestamp, :video_id)
   end
 
-  def parent_id_param
-    params.try(:[], :discussion_post).try(:[], :parent_id)
+  def reply_topic_id_params
+    params.try(:[], :discussion_post).try(:[], :topic_id)
   end
 
   def discussion_topic
@@ -44,9 +45,15 @@ class Course::Video::TopicsController < Course::Video::Controller
     @topic.ensure_subscribed_by(current_user)
   end
 
+  # TODO: Refactor this properly together with the method
+  # in Course::Assessment::SubmissionQuestion::CommentsController
+  def last_post_from(topic)
+    # @post is in topic.posts, so we filter out @post, which has no id yet.
+    topic.posts.ordered_topologically.flatten.select(&:id).last
+  end
+
   def load_existing_topic
-    return unless parent_id_param
-    topic = Course::Discussion::Post.find(parent_id_param).topic.specific
-    @topic = topic unless topic.nil?
+    return if reply_topic_id_params.blank?
+    @topic = Course::Video::Topic.find(reply_topic_id_params)
   end
 end
