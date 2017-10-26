928267b507049abb5b38e9f72fb9e3f99c2372d6
Shen Yichen <2007.yichen@gmail.com>
2017-10-26 23:15:48 +0800

    Add specs for topic model and topic management
    Also altered the factory class to reflect userstamps, as well as create
    submissions if necessary.
    
    The randomized choices in the topic management spec are altered to pick
    from a list of three procs. Only the selected proc will be invoked.
    
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- spec/factories/course_video_topics.rb
+++ spec/factories/course_video_topics.rb
@@ -3,13 +3,19 @@ FactoryGirl.define do
   factory :course_video_topic, class: Course::Video::Topic.name,
                                parent: :course_discussion_topic,
                                aliases: [:video_topic] do
-    transient do
-      creator { build(:course_student, course: course).user }
-    end
 
     course { create(:course) }
     video { build(:video, course: course) }
+    creator { build(:course_student, course: course).user }
+    updater { creator }
     timestamp 5
-    posts { [build(:course_discussion_post, creator: creator, updater: creator)] }
+    posts { [build(:course_discussion_post, creator: creator, updater: updater)] }
+
+    trait :with_submission do
+      after(:build) do |topic|
+        topic.video.submissions =
+          [build(:course_video_submission, video: topic.video, creator: topic.creator)]
+      end
+    end
   end
 end
