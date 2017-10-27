6120607b92449e6ff2a3460911a8bf782a5b1667
Shen Yichen <2007.yichen@gmail.com>
2017-10-27 13:15:34 +0800

    Remove dead code from TopicsController
    The post creation from the #create route will no longer be used since we
    are using the main discussion controller for new posts in video.
    
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- app/controllers/course/video/topics_controller.rb
+++ app/controllers/course/video/topics_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 class Course::Video::TopicsController < Course::Video::Controller
   load_and_authorize_resource :topic, through: :video, class: Course::Video::Topic.name
-  before_action :load_existing_topic, only: :create
 
   include Course::Discussion::PostsConcern
   skip_load_and_authorize_resource :post, except: :create
@@ -14,8 +13,6 @@ class Course::Video::TopicsController < Course::Video::Controller
 
   def create
     result = @topic.class.transaction do
-      @post.title = @video.title
-      @post.parent = last_post_from(@topic)
       raise ActiveRecord::Rollback unless @post.save && create_topic_subscription && update_topic_pending_status
       raise ActiveRecord::Rollback unless @topic.save
       true
@@ -33,27 +30,11 @@ class Course::Video::TopicsController < Course::Video::Controller
     params.permit(:timestamp, :video_id)
   end
 
-  def reply_topic_id_params
-    params.try(:[], :discussion_post).try(:[], :topic_id)
-  end
-
   def discussion_topic
     @topic.try(:discussion_topic)
   end
 
   def create_topic_subscription
     @topic.ensure_subscribed_by(current_user)
-  end
-
-  # TODO: Refactor this properly together with the method
-  # in Course::Assessment::SubmissionQuestion::CommentsController
-  def last_post_from(topic)
-    # @post is in topic.posts, so we filter out @post, which has no id yet.
-    topic.posts.ordered_topologically.flatten.select(&:id).last
-  end
-
-  def load_existing_topic
-    return if reply_topic_id_params.blank?
-    @topic = Course::Video::Topic.find(reply_topic_id_params)
   end
 end
