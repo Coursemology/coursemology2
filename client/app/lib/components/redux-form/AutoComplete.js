abcafc93541c35b2054ebc60b9acd288be0157ab
Jeremy Yap <jeremy.yapjl@gmail.com>
2017-10-28 16:51:25 +0800

    Fix AutoComplete redux-form component

--- client/app/lib/components/redux-form/AutoComplete.js
+++ client/app/lib/components/redux-form/AutoComplete.js
@@ -7,6 +7,7 @@ const mapProps = props => ({
   floatingLabelFixed: true,
   searchText: props.input.value,
   onNewRequest: props.input.onChange,
+  onUpdateInput: props.input.onChange,
 });
 
 export default createComponent(AutoComplete, mapProps);
