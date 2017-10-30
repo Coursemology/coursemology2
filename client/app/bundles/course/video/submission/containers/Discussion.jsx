0c936461a3e56f4fb7ccb9f67e82c24ed8d223db
Shen Yichen <2007.yichen@gmail.com>
2017-10-30 20:21:01 +0800

    Hook up components for scrolling
    Change the discussion component so it responds to changes in the
    scrolling states.
    
    The component scrolls to a topic element if set, but unsets the scroll
    topic if scrolled by the user.
    
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- client/app/bundles/course/video/submission/containers/Discussion.jsx
+++ client/app/bundles/course/video/submission/containers/Discussion.jsx
@@ -8,15 +8,19 @@ import styles from './Discussion.scss';
 import NewPostContainer from './DiscussionElements/NewPostContainer';
 import Topic from './DiscussionElements/Topic';
 import Controls from './DiscussionElements/Controls';
+import { unsetScrollTopic } from '../actions/discussion';
 
 const propTypes = {
   topicIds: PropTypes.arrayOf(PropTypes.string),
   autoScroll: PropTypes.bool,
+  scrollTopicId: PropTypes.string,
+  onScroll: PropTypes.func,
 };
 
 const defaultProps = {
   topicIds: [],
   autoScroll: false,
+  scrollTopicId: null,
 };
 
 class Discussion extends React.Component {
@@ -36,6 +40,9 @@ class Discussion extends React.Component {
       this.topicPane.scrollTop = this.savedAutoScrollPos;
     } else if (this.props.autoScroll) {
       this.topicPane.scrollTop = this.topicPane.scrollHeight;
+    } else if (this.props.scrollTopicId !== null) {
+      const topicElem = document.getElementById(`discussion-topic-${this.props.scrollTopicId}`);
+      this.topicPane.scrollTop = topicElem.offsetTop;
     }
   }
 
@@ -46,18 +53,19 @@ class Discussion extends React.Component {
   render() {
     return (
       <Paper zDepth={2} className={styles.rootContainer}>
-        <div
-          ref={this.setRef}
-          className={styles.topicsContainer}
-          style={{ overflowY: this.props.autoScroll ? 'hidden' : 'scroll' }}
-        >
-          {this.props.topicIds.map(id => <Topic key={id.toString()} topicId={id} />)}
-        </div>
-        <Divider />
         <div className={styles.newCommentEditor}>
           <NewPostContainer>
             <Controls />
           </NewPostContainer>
+        </div>
+        <Divider />
+        <div
+          ref={this.setRef}
+          className={styles.topicsContainer}
+          style={{ overflowY: this.props.autoScroll ? 'hidden' : 'scroll' }}
+          onScroll={this.props.onScroll}
+        >
+          {this.props.topicIds.map(id => <Topic key={id.toString()} topicId={id} />)}
         </div>
       </Paper>
     );
@@ -69,19 +77,34 @@ Discussion.defaultProps = defaultProps;
 
 function mapStateToProps(state) {
   // TODO: Use reselect
-  const autoScroll = state.discussion.scrolling.autoScroll;
+  const scrolling = state.discussion.scrolling;
   const currentTime = state.video.playerProgress;
 
   const sortedKeys = state.discussion.topics
     .filter(topic => topic.topLevelPostIds.length > 0)
-    .filter(topic => !autoScroll || topic.timestamp <= currentTime)
+    .filter(topic => !scrolling.autoScroll || topic.timestamp <= currentTime)
     .sort((topic1, topic2) => topic1.timestamp - topic2.timestamp)
     .keySeq()
     .toArray();
   return {
     topicIds: sortedKeys,
-    autoScroll,
+    autoScroll: scrolling.autoScroll,
+    scrollTopicId: scrolling.topicId,
   };
 }
 
-export default connect(mapStateToProps)(Discussion);
+function mapDispatchToProps(dispatch) {
+  return {
+    onScroll: () => dispatch(unsetScrollTopic()),
+  };
+}
+
+function mergeProps(stateProps, dispatchProps) {
+  if (stateProps.scrollTopicId === null) {
+    return Object.assign({}, stateProps, dispatchProps, { onScroll: null });
+  }
+
+  return Object.assign({}, stateProps, dispatchProps);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(Discussion);
