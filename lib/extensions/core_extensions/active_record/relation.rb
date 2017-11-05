a3389294604d13c1a5cb7dd31d0f187cde47d8a1
Jeremy Yap <jeremy.yapjl@gmail.com>
2017-11-05 22:54:29 +0800

    Update ActiveRecord::Relation#select_for_count monkeypatching documentation

--- lib/extensions/core_extensions/active_record/relation.rb
+++ lib/extensions/core_extensions/active_record/relation.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 module Extensions::CoreExtensions::ActiveRecord::Relation
   # Fix for count queries involving calculated attributes
-  # To remove after upgrading to Rails 5
+  # To remove if Rails ever fixes this, see rails/rails#15138 and rails/rails#24809
   # https://github.com/aha-app/calculated_attributes#known-issues
   def select_for_count
     :all
