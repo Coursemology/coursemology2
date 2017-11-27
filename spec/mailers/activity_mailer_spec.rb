deaecccd8cb6b653bf69e55cd7259853e46eb267
fonglh <fonglh@gmail.com>
2017-11-27 16:01:16 +0800

    Test that Post notifications do not have salutation and sign off.
    Ensure that no_greeting_mailer layout is used if the object is a
    Course::Discussion::Post.

--- spec/mailers/activity_mailer_spec.rb
+++ spec/mailers/activity_mailer_spec.rb
@@ -15,29 +15,31 @@ RSpec.describe ActivityMailer, type: :mailer do
     let(:text) { mail.body.parts.find { |part| part.content_type.start_with?('text/plain') }.to_s }
     let(:html) { mail.body.parts.find { |part| part.content_type.start_with?('text/html') }.to_s }
 
-    it 'sets the correct headers' do
-      expect(mail.subject).to eq('test mailer')
-      expect(mail.to).to include(user.email)
-    end
+    context 'with default mailer layout' do
+      it 'sets the correct headers' do
+        expect(mail.subject).to eq('test mailer')
+        expect(mail.to).to include(user.email)
+      end
 
-    it 'renders the layout' do
-      expect(text).to include(I18n.t('layouts.mailer.greeting', user: user.name))
-      expect(text).to include(I18n.t('layouts.mailer.complimentary_close'))
-      expect(text).to include(I18n.t('layouts.mailer.sign_off'))
+      it 'renders the layout' do
+        expect(text).to include(I18n.t('layouts.mailer.greeting', user: user.name))
+        expect(text).to include(I18n.t('layouts.mailer.complimentary_close'))
+        expect(text).to include(I18n.t('layouts.mailer.sign_off'))
 
-      expect(html).to include(I18n.t('layouts.mailer.greeting', user: user.name))
-      expect(html).to include(I18n.t('layouts.mailer.complimentary_close'))
-      expect(html).to include(I18n.t('layouts.mailer.sign_off'))
-    end
+        expect(html).to include(I18n.t('layouts.mailer.greeting', user: user.name))
+        expect(html).to include(I18n.t('layouts.mailer.complimentary_close'))
+        expect(html).to include(I18n.t('layouts.mailer.sign_off'))
+      end
 
-    it 'sends a multipart email' do
-      expect(html).to include('HTML test')
-      expect(text).to include('Plain text test')
-    end
+      it 'sends a multipart email' do
+        expect(html).to include('HTML test')
+        expect(text).to include('Plain text test')
+      end
 
-    it 'provides views with the given object' do
-      expect(html).to include(user.email)
-      expect(text).to include(user.email)
+      it 'provides views with the given object' do
+        expect(html).to include(user.id.to_s)
+        expect(text).to include(user.id.to_s)
+      end
     end
 
     context 'with no greeting mailer layout' do
@@ -51,5 +53,6 @@ RSpec.describe ActivityMailer, type: :mailer do
         expect(text).not_to include(I18n.t('layouts.mailer.complimentary_close'))
         expect(text).not_to include(I18n.t('layouts.mailer.sign_off'))
       end
+    end
   end
 end
