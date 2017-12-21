e83a083a9b9c4be0468e193504cca8d4ccd87703
Shen Yichen <2007.yichen@gmail.com>
2017-12-22 00:07:06 +0900

    Add reducer logic to record events
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- client/app/bundles/course/video/submission/reducers/video.js
+++ client/app/bundles/course/video/submission/reducers/video.js
@@ -1,8 +1,8 @@
+import { List as makeImmutableList } from 'immutable';
 import { playerStates, videoActionTypes, videoDefaults } from 'lib/constants/videoConstants';
 import { isPlayingState, timeIsPastRestricted } from 'lib/helpers/videoHelpers';
 
 export const initialState = {
-  videoId: null,
   videoUrl: null,
   playerState: playerStates.UNSTARTED,
   playerProgress: 0,
@@ -12,6 +12,9 @@ export const initialState = {
   playbackRate: 1,
   restrictContentAfter: null,
   forceSeek: false,
+  sessionId: null,
+  sessionSequenceNum: 0,
+  sessionEvents: makeImmutableList(),
 };
 
 /**
@@ -82,7 +85,15 @@ function generateStateTransformer(state) {
   return changes => Object.assign({}, state, { forceSeek: false }, changes);
 }
 
-export default function (state = initialState, action) {
+/**
+ * The reducer to transform the video state excluding session data.
+ *
+ * This reducer changes the video's UI states, such as playback rate, volume, and player progress.
+ * @param state The original state object.
+ * @param action The action the reducer is to process
+ * @return {*} The transformed state.
+ */
+function videoStateReducer(state = initialState, action) {
   // Only forceSeek if explicitly specified
   const transformState = generateStateTransformer(state);
 
@@ -104,4 +115,98 @@ export default function (state = initialState, action) {
     default:
       return state;
   }
+}
+
+/**
+ * Generates a session event based on the event type.
+ *
+ * This function produces a base event, recording the player progress and playback rate, as well as assign a sequence
+ * number based on the value stored in the state. These parameters can be extended or overwritten with the params
+ * argument.
+ * @param state The original state object.
+ * @param type The event type.
+ * @param params The parameters to overwrite or extend the base evnt with.
+ * @return {*} The event object to record.
+ */
+function generateEvent(state, type, params = {}) {
+  const baseEvent = {
+    sequence_num: state.sessionSequenceNum,
+    event_type: type,
+    video_time: Math.round(state.playerProgress),
+    playback_rate: state.playbackRate,
+    event_time: new Date(),
+  };
+  return Object.assign(baseEvent, params);
+}
+
+/**
+ * Handles the session state change for a player state change.
+ *
+ * This function produces a new state with a new session event included on a player state change. If the player state
+ * will not change, or if the new player state does not need recording, the original state is returned.
+ * @param state The original state object.
+ * @param action The action to process.
+ * @return {*} The transformed state object.
+ */
+function handleSessionChangeState(state, action) {
+  let stateChange = null;
+  if (state.playerState === action.playerState) {
+    return state;
+  } else if (action.playerState === playerStates.PLAYING) {
+    stateChange = 'play';
+  } else if (action.playerState === playerStates.PAUSED) {
+    stateChange = 'pause';
+  } else if (action.playerState === playerStates.BUFFERING && isPlayingState(state.playerState)) {
+    stateChange = 'buffer';
+  } else if (action.playerState === playerStates.ENDED) {
+    stateChange = 'end';
+  } else {
+    return state;
+  }
+
+  return Object.assign({}, state, {
+    sessionSequenceNum: state.sessionSequenceNum + 1,
+    sessionEvents: state.sessionEvents.push(generateEvent(state, stateChange)),
+  });
+}
+
+/**
+ * The reducer to transform the session data.
+ *
+ * This reducer listens to actions that we want to record and creates a new event in state.sessionEvents. The exception
+ * is the REMOVE_EVENTS action, where specified events will be removed.
+ * @param state The original state object.
+ * @param action The action the reducer is to process
+ * @return {*} The transformed state.
+ */
+function videoSessionReducer(state = initialState, action) {
+  if (!state.sessionId) {
+    return state;
+  }
+  const events = state.sessionEvents;
+  switch (action.type) {
+    case videoActionTypes.CHANGE_PLAYBACK_RATE:
+      return Object.assign({}, state, {
+        sessionSequenceNum: state.sessionSequenceNum + 1,
+        sessionEvents: events.push(generateEvent(state, 'speed_change', { playback_rate: action.playbackRate })),
+      });
+    case videoActionTypes.CHANGE_PLAYER_STATE:
+      return handleSessionChangeState(state, action);
+    case videoActionTypes.SEEK_START:
+      return Object.assign({}, state, {
+        sessionSequenceNum: state.sessionSequenceNum + 1,
+        sessionEvents: events.push(generateEvent(state, 'seek_start')),
+      });
+    case videoActionTypes.SEEK_END:
+      return Object.assign({}, state, {
+        sessionSequenceNum: state.sessionSequenceNum + 1,
+        sessionEvents: events.push(generateEvent(state, 'seek_end')),
+      });
+    default:
+      return state;
+  }
+}
+
+export default function (state = initialState, action) {
+  return videoStateReducer(videoSessionReducer(state, action), action);
 }
