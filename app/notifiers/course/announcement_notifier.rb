efafb93805d9a169fcdf7f938a2c199d57868a56
fonglh <fonglh@gmail.com>
2018-01-08 09:43:01 +0800

    Hide greeting for announcement notification email.

--- app/notifiers/course/announcement_notifier.rb
+++ app/notifiers/course/announcement_notifier.rb
@@ -8,4 +8,19 @@ class Course::AnnouncementNotifier < Notifier::Base
       notify(announcement.course, :email).
       save if email_enabled
   end
+
+  private
+
+  # Create an email for the users of a course based on a given course notification record.
+  # Overrides email_course in Notifier::Base to pass a custom layout for this notifier.
+  #
+  # @param [CourseNotification] notification The notification which is used to generate emails
+  def email_course(notification)
+    notification.course.users.each do |user|
+      @pending_emails << ActivityMailer.email(recipient: user,
+                                              notification: notification,
+                                              view_path: notification_view_path(notification),
+                                              layout_path: 'no_greeting_mailer')
+    end
+  end
 end
