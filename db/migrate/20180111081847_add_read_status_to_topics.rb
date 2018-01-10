79c419890cb8bc82dece13f26de728e3f3056ef7
weiqingtoh <weiqingtoh@gmail.com>
2018-01-10 17:15:27 +0800

    Migration to initialise the readmarks for topics

---
+++ db/migrate/20180111081847_add_read_status_to_topics.rb
@@ -0,0 +1,14 @@
+# This migration initialises the read/unread models for Course::Discussion::Topic.
+class AddReadStatusToTopics < ActiveRecord::Migration[5.1]
+  def up
+    # Declare all Course::Discussion::Topic read for each user.
+    #
+    # This does not affect Forums because ReadMarks are declared on those
+    # models rather than on Course::Discussion::Topic.
+    User.find_each { |user| Course::Discussion::Topic.mark_as_read! :all, for: user }
+  end
+
+  def down
+    ReadMark.where(readable_id: nil, readable_type: Course::Discussion::Topic.name).delete_all
+  end
+end
