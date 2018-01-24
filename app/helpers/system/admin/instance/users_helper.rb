c8e3796cb136ed02b58295b68f3fb753cdac59fe
Brian Koh <kxmbrian@gmail.com>
2018-01-24 22:53:42 +0800

    Allow global admins to see who are admins in an instance

---
+++ app/helpers/system/admin/instance/users_helper.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+module System::Admin::Instance::UsersHelper
+  include System::Admin::UsersHelper
+end
---
+++ app/helpers/system/admin/users_helper.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+module System::Admin::UsersHelper
+  # Wraps the user count in a link unless the the linked page is the current page,
+  # or the user count is nil or zero.
+  def user_count_link(page_params, user_count, link_params)
+    return 0 unless user_count
+    link_active = page_params[:active] == link_params[:active] && page_params[:role] == link_params[:role]
+    link_active ? user_count : link_to(user_count, link_params)
+  end
+end
