f0e30a753e236e2b4428b7dbc2237dde74a38871
weiqingtoh <weiqingtoh@gmail.com>
2018-02-21 16:13:29 +0800

    Delete forum topic if only post is deleted
    Solves #1457

--- spec/controllers/course/forum/posts_controller_spec.rb
+++ spec/controllers/course/forum/posts_controller_spec.rb
@@ -8,30 +8,45 @@ RSpec.describe Course::Forum::PostsController, type: :controller do
     let(:user) { create(:administrator) }
     let(:course) { create(:course) }
     let(:forum) { create(:forum, course: course) }
-    let(:topic) { create(:forum_topic, forum: forum) }
-    let!(:post_stub) do
-      stub = build_stubbed(:course_discussion_post, topic: topic.acting_as)
-      allow(stub).to receive(:destroy).and_return(false)
-      stub
-    end
 
     before { sign_in(user) }
 
     describe '#destroy' do
       subject do
-        delete :destroy, params: { course_id: course, forum_id: forum, topic_id: topic, id: post_stub }
+        delete :destroy,
+               params: { course_id: course, forum_id: forum, topic_id: topic, id: post.id }
       end
 
       context 'when destroy fails' do
+        let(:topic) do
+          topic = create(:forum_topic, forum: forum)
+          create(:course_discussion_post, topic: topic.acting_as)
+          topic
+        end
+        let!(:post) do
+          stub = build_stubbed(:course_discussion_post, topic: topic.acting_as)
+          allow(stub).to receive(:destroy).and_return(false)
+          stub
+        end
         before do
-          controller.instance_variable_set(:@post, post_stub)
+          controller.instance_variable_set(:@post, post)
           subject
         end
 
         it { is_expected.to redirect_to(course_forum_topic_path(course, forum, topic)) }
         it 'sets an error flash message' do
           expect(flash[:danger]).to eq(I18n.t('course.discussion.posts.destroy.failure',
-                                              error: post_stub.errors.full_messages.to_sentence))
+                                              error: post.errors.full_messages.to_sentence))
+        end
+      end
+
+      context 'when the only post in the topic is deleted' do
+        let(:topic) { create(:forum_topic, forum: forum) }
+        let(:post) { topic.posts.first }
+        before { subject }
+
+        it 'destroys the topic' do
+          expect(forum.topics).to be_empty
         end
       end
     end
