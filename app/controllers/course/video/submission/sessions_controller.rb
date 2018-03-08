956d0040037d2fec56b2b5f7828f2b6d98687eda
Shen Yichen <2007.yichen@gmail.com>
2018-03-08 18:16:48 +0800

    Add create action to video sessions
    We want to be able to create sessions in a POST request instead of in
    submission#edit so that the browser doesn't cache the session id when
    the back button is pressed.
    
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- app/controllers/course/video/submission/sessions_controller.rb
+++ app/controllers/course/video/submission/sessions_controller.rb
@@ -1,15 +1,23 @@
 # frozen_string_literal: true
 class Course::Video::Submission::SessionsController < Course::Video::Submission::Controller
+  load_and_authorize_resource :session, class: Course::Video::Session.name, through: :submission
+
+  def create
+    time_now = Time.zone.now
+    @session.session_start = time_now
+    @session.session_end = time_now
+    head :bad_request unless @session.save
+  end
 
   def update
     # We received a message from client, so time is updated regardless of how event records turn out
     if params[:is_old_session]
-      @session.update_attributes!(last_video_time: session_params[:last_video_time])
+      @session.update_attributes!(last_video_time: update_params[:last_video_time])
     else
       @session.update_attributes!(session_end: Time.zone.now,
-                                  last_video_time: session_params[:last_video_time])
+                                  last_video_time: update_params[:last_video_time])
     end
-    @session.merge_in_events!(session_params[:events])
+    @session.merge_in_events!(update_params[:events])
   rescue ArgumentError => _
     head :bad_request
   rescue ActiveRecord::RecordInvalid => _
@@ -22,7 +30,7 @@ class Course::Video::Submission::SessionsController < Course::Video::Submission:
     @video.tab
   end
 
-  def session_params
+  def update_params
     params.require(:session).permit(:last_video_time,
                                     events: [[:sequence_num, :event_type, :video_time,
                                               :playback_rate, :event_time]])
