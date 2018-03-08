dfb23bc03819326c39dcc76c70e82c28b09e623e
Shen Yichen <2007.yichen@gmail.com>
2018-03-08 18:59:33 +0800

    Read and request for sessionId
    Change frontend to request for a sessionId on demand.
    
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- client/app/bundles/course/video/submission/submissions.jsx
+++ client/app/bundles/course/video/submission/submissions.jsx
@@ -1,21 +1,46 @@
 import React from 'react';
 import { render } from 'react-dom';
 import ProviderWrapper from 'lib/components/ProviderWrapper';
+import LoadingIndicator from 'lib/components/LoadingIndicator';
+import CourseAPI from 'api/course';
 import Submission from './containers/Submission';
 import storeCreator from './store';
+
+function renderSubmission(state, node) {
+  render(
+    <ProviderWrapper {...storeCreator(state)}>
+      <Submission />
+    </ProviderWrapper>
+    , node
+  );
+}
 
 $(document).ready(() => {
   const mountNode = document.getElementById('video-component');
 
   if (mountNode) {
-    const data = mountNode.getAttribute('data');
-    const initialState = JSON.parse(data);
+    const submissionData = mountNode.getAttribute('data');
+    const initialState = JSON.parse(submissionData);
+    const { enableMonitoring } = initialState;
 
-    render(
-      <ProviderWrapper {...storeCreator(initialState)}>
-        <Submission />
-      </ProviderWrapper>
-      , mountNode
-    );
+    if (enableMonitoring) {
+      render(
+        <ProviderWrapper>
+          <LoadingIndicator />
+        </ProviderWrapper>
+        , mountNode
+      );
+
+      CourseAPI.video.sessions.create()
+        .then(({ data }) => {
+          initialState.video.sessionId = data.id;
+          renderSubmission(initialState, mountNode);
+        })
+        .catch(() => {
+          renderSubmission(initialState, mountNode);
+        });
+    } else {
+      renderSubmission(initialState, mountNode);
+    }
   }
 });
