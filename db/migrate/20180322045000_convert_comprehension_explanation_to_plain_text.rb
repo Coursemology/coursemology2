a343ad27b6002cb40b5ac0bc62b1333e2f4fde69
Mak Qi En <makqien@users.noreply.github.com>
2018-03-22 13:41:21 +0800

    Convert TextResponseComprehensionSolution `explanation` to plaintext

---
+++ db/migrate/20180322045000_convert_comprehension_explanation_to_plain_text.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+class ConvertComprehensionExplanationToPlainText < ActiveRecord::Migration[5.1]
+  def up
+    Course::Assessment::Question::TextResponseComprehensionSolution.
+      find_each do |s|
+      unless s.explanation.nil? then
+        # replace all HTML tags with ' '
+        s_explanation = s.explanation.
+                        gsub(/\<(.*?)\>/, ' ').
+                        strip
+        s.update_column(:explanation, s_explanation)
+      end
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
