d2a705704c040554cee308302fa9d98bd46fd70c
weiqingtoh <weiqingtoh@gmail.com>
2018-03-22 09:33:18 +0800

    Fix bug where only first errors is surfaced for invitation
    Solves one issue from #2744.
    
    It is found that with the current code, only the first
    invalid record has errors on it. This means not all invalid
    records are surfaced. Calling #valid? on it runs validations
    on the objects and accurately surfaces out the issues.

--- spec/controllers/course/user_invitations_controller_spec.rb
+++ spec/controllers/course/user_invitations_controller_spec.rb
@@ -39,6 +39,14 @@ RSpec.describe Course::UserInvitationsController, type: :controller do
 
         it { is_expected.to redirect_to(course_user_invitations_path(course)) }
 
+        context 'when no users are manually specified for invitations' do
+          subject { post :create, params: { course_id: course } }
+
+          it 'redirects to the invitaions path without an error' do
+            expect(subject).to redirect_to(course_user_invitations_path(course))
+          end
+        end
+
         context 'when the invitations do not get created successfully' do
           before do
             stubbed_invitation_service = Course::UserInvitationService.new(user, course)
@@ -56,17 +64,13 @@ RSpec.describe Course::UserInvitationsController, type: :controller do
           let(:invite_params) do
             { invitations_file: fixture_file_upload('course/invalid_invitation.csv') }
           end
+
           it { is_expected.to render_template(:new) }
           it 'sets the course errors property' do
             subject
             expect(controller.current_course.errors.count).not_to eq(0)
             expect(controller.current_course.errors[:invitations_file].length).not_to eq(0)
           end
-        end
-
-        context 'when no users are manually specified for invitations' do
-          subject { post :create, params: { course_id: course } }
-          it { is_expected.to redirect_to(course_user_invitations_path(course)) }
         end
       end
 
@@ -95,6 +99,25 @@ RSpec.describe Course::UserInvitationsController, type: :controller do
           subject
           current_course = controller.current_course
           expect(current_course.errors[:invitations_file]).not_to be_empty
+        end
+      end
+    end
+
+    describe '#aggregate_errors' do
+      let!(:course_lecturer) { create(:course_manager, course: course, user: user) }
+      before { sign_in(user) }
+
+      subject { post :create, params: { course_id: course, course: invite_params } }
+
+      context 'when the CSV has invalid emails' do
+        let(:invite_params) do
+          { invitations_file: fixture_file_upload('course/invalid_email_invitation.csv') }
+        end
+
+        it 'propogates the course errors accurately' do
+          subject
+          # Expect 2 errors from user invitations
+          expect(controller.send(:aggregate_errors).count).to eq(2)
         end
       end
     end
