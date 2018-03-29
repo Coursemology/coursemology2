84540608557935970066cb5fdc0ce2623f595f72
weiqingtoh <weiqingtoh@gmail.com>
2018-03-29 17:37:42 +0800

    Reject invitations only if there are unconfirmed invitations
    Solves #2595.
    
    Currently all users are added through Course::UserInvitation.
    There is a uniqueness constraint as part of validation, to ensure
    that no invitation with the same course_id and email exists. However
    this does not account for course_users who accepted the invitation
    and left, and rejoin in the future.
    
    This commit restricts the uniqueness check to unconfirmed invitations,
    for us to track past confirmed invitations. This is preferred to
    deleting the Course::UserInvitation object if the course_user leaves.

--- spec/models/course/user_invitation_spec.rb
+++ spec/models/course/user_invitation_spec.rb
@@ -7,4 +7,31 @@ RSpec.describe Course::UserInvitation, type: :model do
       expect(subject.invitation_key).to start_with('I')
     end
   end
+
+  let(:instance) { Instance.default }
+  with_tenant(:instance) do
+    let(:course) { create(:course) }
+
+    describe 'validations' do
+      context 'when there is a previous invitation with the same email' do
+        let(:email) { generate(:email) }
+        let!(:previous_invitation) do
+          create(:course_user_invitation, *invitation_traits, course: course, email: email)
+        end
+        subject { build(:course_user_invitation, course: course, email: email) }
+
+        context 'if the previous invitation has been confirmed' do
+          let(:invitation_traits) { [:confirmed] }
+
+          it { is_expected.to be_valid }
+        end
+
+        context 'if the previous invitation has not been confirmed' do
+          let(:invitation_traits) { [] }
+
+          it { is_expected.not_to be_valid }
+        end
+      end
+    end
+  end
 end
