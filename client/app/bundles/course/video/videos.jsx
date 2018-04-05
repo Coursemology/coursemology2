828f621e13d04c0d77bb2ff669e82d000aede32a
Shen Yichen <2007.yichen@gmail.com>
2018-04-05 22:14:18 +0800

    Add frontend for video overall stats
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

---
+++ client/app/bundles/course/video/videos.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from 'react-dom';
+import ProviderWrapper from 'lib/components/ProviderWrapper';
+import HeatMap from './submission/containers/Charts/HeatMap';
+
+$(document).ready(() => {
+  const mountNode = document.getElementById('video-overall-stats');
+
+  if (!mountNode) { return; }
+
+  const submissionData = mountNode.getAttribute('data');
+  const initialState = JSON.parse(submissionData);
+
+  render(
+    <ProviderWrapper>
+      <HeatMap {...initialState} />
+    </ProviderWrapper>
+    , mountNode
+  );
+});
