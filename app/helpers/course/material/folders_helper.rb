aae9cc8a32adafdd42ea114192c6bf694573a4f0
fonglh <fonglh@gmail.com>
2018-04-26 11:19:11 +0800

    Display an icon beside folder start at if it's within SDL period.
    Show an eye icon beside the folder start at if it's visible to students
    due to the self directed learning setting.
    
    Add folder helper spec.

--- app/helpers/course/material/folders_helper.rb
+++ app/helpers/course/material/folders_helper.rb
@@ -8,4 +8,13 @@ module Course::Material::FoldersHelper
   def display_folder(folder)
     folder.root? ? component.settings.title || t('course.material.sidebar_title') : folder.name
   end
+
+  # Display an icon when the folder's start_at is in the future, but the course's advance_start_at
+  # option already makes it visible to students.
+  #
+  # @param [Course::Material::Folder] folder The folder to be tested.
+  # @return [Boolean] Whether the icon should be displayed.
+  def show_sdl_warning?(folder)
+    folder.effective_start_at < Time.zone.now && folder.start_at > Time.zone.now
+  end
 end
