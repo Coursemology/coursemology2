56734f969a1ab06cf94d2893696a34d6fc4a3251
weiqingtoh <weiqingtoh@gmail.com>
2018-05-08 11:53:23 +0800

    Modify conditions ability to only allow teaching staff

--- app/models/components/course/conditions_ability_component.rb
+++ app/models/components/course/conditions_ability_component.rb
@@ -3,17 +3,17 @@ module Course::ConditionsAbilityComponent
   include AbilityHost::Component
 
   def define_permissions
-    allow_staff_manage_conditions if user
+    allow_teaching_staff_manage_conditions if user
 
     super
   end
 
   private
 
-  def allow_staff_manage_conditions
-    can :manage, Course::Condition, course_staff_hash
-    can :manage, Course::Condition::Achievement, condition: course_staff_hash
-    can :manage, Course::Condition::Assessment, condition: course_staff_hash
-    can :manage, Course::Condition::Level, condition: course_staff_hash
+  def allow_teaching_staff_manage_conditions
+    can :manage, Course::Condition, course_teaching_staff_hash
+    can :manage, Course::Condition::Achievement, condition: course_teaching_staff_hash
+    can :manage, Course::Condition::Assessment, condition: course_teaching_staff_hash
+    can :manage, Course::Condition::Level, condition: course_teaching_staff_hash
   end
 end
