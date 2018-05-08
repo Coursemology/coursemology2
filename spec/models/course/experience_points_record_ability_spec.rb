f65c377617485bf8018c798d2d249a035c2a0d8f
weiqingtoh <weiqingtoh@gmail.com>
2018-05-08 15:01:28 +0800

    Implement observer permissions for exp records
    - Observer cannot disburse exp points.
    - Observer can only read exp records, no create/update/delete.

--- spec/models/course/experience_points_record_ability_spec.rb
+++ spec/models/course/experience_points_record_ability_spec.rb
@@ -42,8 +42,8 @@ RSpec.describe Course::ExperiencePointsRecord do
       end
     end
 
-    context 'when the user is a Course Staff' do
-      let(:course_user) { create(:course_manager, course: course) }
+    context 'when the user is a Course Teaching Staff' do
+      let(:course_user) { create(:course_teaching_assistant, course: course) }
       let(:user) { course_user.user }
       let(:foreign_points_record) { create(:course_experience_points_record) }
 
@@ -66,6 +66,22 @@ RSpec.describe Course::ExperiencePointsRecord do
         it "cannot access the student's experience points history" do
           records = foreign_points_record.course_user.experience_points_records
           expect(records.accessible_by(subject)).to be_empty
+        end
+      end
+    end
+
+    context 'when the user is a Course Observer' do
+      let(:course_user) { create(:course_observer, course: course) }
+      let(:user) { course_user.user }
+
+      context 'when record belongs to a student from the same course' do
+        it { is_expected.to be_able_to(:read, points_record) }
+        it { is_expected.not_to be_able_to(:create, points_record) }
+        it { is_expected.not_to be_able_to(:update, points_record) }
+        it { is_expected.not_to be_able_to(:destroy, points_record) }
+        it "can access the student's experience points history" do
+          expect(course_student.experience_points_records.accessible_by(subject)).
+            to contain_exactly(points_record)
         end
       end
     end
