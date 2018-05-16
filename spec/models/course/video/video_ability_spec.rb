2760801fad0da7a5f5686233395ad0bee1128062
weiqingtoh <weiqingtoh@gmail.com>
2018-05-16 18:34:04 +0800

    Implement observer permissions for videos
    - Observers can see all videos and student submissions.
    - Observers cannot update/modify videos or submissions.

--- spec/models/course/video/video_ability_spec.rb
+++ spec/models/course/video/video_ability_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Course::Video do
     let(:published_video) { create(:video, :published, course: course) }
     let(:published_video_not_started) { create(:video, :not_started, :published, course: course) }
     let(:video_submission) do
-      create(:video_submission, video: published_video, creator: course_user.user)
+      create(:video_submission, video: published_video, creator: user)
     end
     let(:other_video_submission) do
       create(:video_submission, course: course, video: published_video)
@@ -38,16 +38,41 @@ RSpec.describe Course::Video do
       it { is_expected.to be_able_to(:update, video_submission) }
     end
 
-    context 'when the user is a Course Staff' do
+    context 'when the user is a Course Teaching Staff' do
       let(:user) { create(:course_teaching_assistant, course: course).user }
 
       # Course Video
       it { is_expected.to be_able_to(:manage, draft_video) }
       it { is_expected.to be_able_to(:manage, published_video) }
+
+      # Course Video Submissions
       it { is_expected.to be_able_to(:update, other_video_submission) }
       it { is_expected.to be_able_to(:read, other_video_submission) }
       it { is_expected.to be_able_to(:update, video_submission) }
       it { is_expected.to be_able_to(:read, video_submission) }
+    end
+
+    context 'when the user is a Course Observer' do
+      let(:user) { create(:course_observer, course: course).user }
+
+      # Course Video
+      it { is_expected.to be_able_to(:read, draft_video) }
+      it { is_expected.to be_able_to(:read, published_video) }
+      it { is_expected.to be_able_to(:read, published_video_not_started) }
+      it { is_expected.to be_able_to(:attempt, draft_video) }
+      it { is_expected.to be_able_to(:attempt, published_video) }
+      it { is_expected.to be_able_to(:attempt, published_video_not_started) }
+      it { is_expected.not_to be_able_to(:manage, draft_video) }
+      it { is_expected.not_to be_able_to(:manage, published_video) }
+      it { is_expected.not_to be_able_to(:manage, published_video_not_started) }
+
+      # Course Video Submissions
+      it { is_expected.to be_able_to(:create, video_submission) }
+      it { is_expected.to be_able_to(:read, video_submission) }
+      it { is_expected.to be_able_to(:update, video_submission) }
+      it { is_expected.not_to be_able_to(:create, other_video_submission) }
+      it { is_expected.to be_able_to(:read, other_video_submission) }
+      it { is_expected.not_to be_able_to(:update, other_video_submission) }
     end
   end
 end
