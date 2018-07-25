ebf931200eca321cd7b59effcc2e77cb87023aa7
fonglh <fonglh@gmail.com>
2018-07-25 14:23:41 +0800

    Do not maintain frozen status of child objects.

--- app/helpers/application_html_formatters_helper.rb
+++ app/helpers/application_html_formatters_helper.rb
@@ -60,7 +60,7 @@ module ApplicationHTMLFormattersHelper
   # SanitizationFilter Custom Options
   # Link: https://github.com/jch/html-pipeline#2-how-do-i-customize-a-whitelist-for-sanitizationfilters
   SANITIZATION_FILTER_WHITELIST = begin
-    list = HTML::Pipeline::SanitizationFilter::WHITELIST.dup
+    list = HTML::Pipeline::SanitizationFilter::WHITELIST.deep_dup
     list[:elements] |= ['span', 'font', 'u']
     list[:attributes][:all] |= ['style']
     list[:attributes]['font'] = ['face']
