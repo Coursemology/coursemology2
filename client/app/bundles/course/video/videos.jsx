a74bbda8c65b343019e463f7e6d8024bbd4941e1
Shen Yichen <2007.yichen@gmail.com>
2018-08-27 00:05:09 -0700

    Fix data for video heatmap
    JSON data was not corrected after video player was added.
    
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- client/app/bundles/course/video/videos.jsx
+++ client/app/bundles/course/video/videos.jsx
@@ -2,18 +2,27 @@ import React from 'react';
 import { render } from 'react-dom';
 import ProviderWrapper from 'lib/components/ProviderWrapper';
 import HeatMap from './submission/containers/Charts/HeatMap';
+import storeCreator from './submission/store';
+import styles from './submission/containers/Statistics.scss';
+import VideoPlayer from './submission/containers/VideoPlayer';
 
 $(document).ready(() => {
   const mountNode = document.getElementById('video-overall-stats');
 
   if (!mountNode) { return; }
 
-  const submissionData = mountNode.getAttribute('data');
-  const initialState = JSON.parse(submissionData);
+  const data = mountNode.getAttribute('data');
+  const { statistics, video } = JSON.parse(data);
 
   render(
-    <ProviderWrapper>
-      <HeatMap {...initialState} />
+    <ProviderWrapper {...storeCreator({ video })}>
+      <div>
+        <div className={styles.statisticsVideoView}>
+          <VideoPlayer />
+        </div>
+      </div>
+      <hr />
+      <HeatMap {...statistics} />
     </ProviderWrapper>,
     mountNode
   );
