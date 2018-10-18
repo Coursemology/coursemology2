1a6fe2f69d7e37c56c8f7f3705ea0df97b63b979
fonglh <fonglh@users.noreply.github.com>
2018-10-18 11:12:50 +0800

    Update app/models/course/assessment/question.rb

--- app/models/course/assessment/question.rb
+++ app/models/course/assessment/question.rb
@@ -8,7 +8,7 @@ class Course::Assessment::Question < ApplicationRecord
   validates :maximum_grade, numericality: { greater_than: -1000, less_than: 1000 }, presence: true
   validates :creator, presence: true
   validates :updater, presence: true
-  validates :actable_type, uniqueness: { scope: [:actable_id], allow_nil: true,
+  validates :actable_type, uniqueness: { scope: [:actable_id],
                                          if: -> { actable_id? && actable_type_changed? } }
   validates :actable_id, uniqueness: { scope: [:actable_type], allow_nil: true,
                                        if: -> { actable_type? && actable_id_changed? } }
