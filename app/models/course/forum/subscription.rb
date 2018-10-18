2385a5862b0e68bd7c487de80a2bbe9be6ec09a9
fonglh <fonglh@users.noreply.github.com>
2018-10-18 11:13:55 +0800

    Update app/models/course/forum/subscription.rb

--- app/models/course/forum/subscription.rb
+++ app/models/course/forum/subscription.rb
@@ -2,7 +2,7 @@
 class Course::Forum::Subscription < ApplicationRecord
   validates :forum, presence: true
   validates :user, presence: true
-  validates :forum_id, uniqueness: { scope: [:user_id], allow_nil: true,
+  validates :forum_id, uniqueness: { scope: [:user_id],
                                      if: -> { user_id? && forum_id_changed? } }
   validates :user_id, uniqueness: { scope: [:forum_id], allow_nil: true,
                                     if: -> { forum_id? && user_id_changed? } }
