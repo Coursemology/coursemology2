fac7956213fc79a60012c278d64528d44991136b
trang mai <trangmei@gmail.com>
2018-11-20 11:50:00 +0800

    Simplify video events query

--- app/models/concerns/course/video/interval_query_concern.rb
+++ app/models/concerns/course/video/interval_query_concern.rb
@@ -77,5 +77,16 @@ module Course::Video::IntervalQueryConcern
             ')',
               end_types: type_sym_to_id(END_TYPES))
     }
+
+    # @!method self.all_start_and_end_events
+    #   Returns all events of start-types or end_types,
+    #   sorted first by session then by sequence number inside the same session
+    scope :all_start_and_end_events, lambda {
+      where(event_type: type_sym_to_id(START_TYPES + END_TYPES)).
+        unscope(:order).
+        order(:session_id, :sequence_num).
+        includes(:session).
+        references(:session)
+    }
   end
 end
