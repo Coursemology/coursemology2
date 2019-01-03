b2d8871744aaab91d90e927f431408f68e76ec84
Louie Tan <louietyj@gmail.com>
2019-01-03 09:52:05 +0800

    Refactor Course.upcoming_lesson_plan_items_exist?
    Fixes #3243

--- app/notifiers/course/consolidated_opening_reminder_notifier.rb
+++ app/notifiers/course/consolidated_opening_reminder_notifier.rb
@@ -2,7 +2,7 @@
 class Course::ConsolidatedOpeningReminderNotifier < Notifier::Base
   # Create an opening reminder activity if there are upcoming items for the course.
   def opening_reminder(course)
-    return unless Course::LessonPlan::Item.upcoming_for_exists?(course)
+    return unless course.upcoming_lesson_plan_items_exist?
 
     create_activity(actor: User.system, object: course, event: :opening_reminder).
       notify(course, :email).save
