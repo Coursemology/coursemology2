85570d26a6982b2eb47928e887a12165d9afab18
trang mai <trangmei@gmail.com>
2019-01-16 20:42:20 +0800

    Change video default placeHolderDuration from 600 to 0

--- client/app/lib/constants/videoConstants.js
+++ client/app/lib/constants/videoConstants.js
@@ -13,7 +13,7 @@ export const youtubeOpts = {
 export const videoDefaults = {
   volume: 0.8,
   availablePlaybackRates: [0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 2.0, 2.5],
-  placeHolderDuration: 600,
+  placeHolderDuration: 0,
   progressUpdateFrequencyMs: 500,
 };
 
