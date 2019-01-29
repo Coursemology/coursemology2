70753d4a3539e71b725a96335a60c0dd47fd344f
trang mai <trangmei@gmail.com>
2019-01-29 13:14:35 +0800

    Add missing end statement

--- db/migrate/20190129044142_add_cached_to_course_video_submission_statistics.rb
+++ db/migrate/20190129044142_add_cached_to_course_video_submission_statistics.rb
@@ -2,4 +2,5 @@ class AddCachedToCourseVideoSubmissionStatistics < ActiveRecord::Migration[5.2]
   def change
     add_column :course_video_submission_statistics, :cached, :boolean,
                 default: false, null: false
+  end
 end
