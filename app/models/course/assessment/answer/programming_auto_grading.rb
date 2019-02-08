0088a18feb70a120bcb3f4bcf2baefed6f258b4f
trang mai <trangmei@gmail.com>
2019-02-08 13:28:21 +0800

    Strip null byte from stdout and stderr before save

--- app/models/course/assessment/answer/programming_auto_grading.rb
+++ app/models/course/assessment/answer/programming_auto_grading.rb
@@ -3,6 +3,8 @@ class Course::Assessment::Answer::ProgrammingAutoGrading < ApplicationRecord
   acts_as :auto_grading, class_name: Course::Assessment::Answer::AutoGrading.name,
                          inverse_of: :actable
 
+  before_save :strip_null_byte
+
   validates :exit_code, numericality: { only_integer: true }, allow_nil: true
 
   has_one :programming_answer, through: :answer,
@@ -12,4 +14,13 @@ class Course::Assessment::Answer::ProgrammingAutoGrading < ApplicationRecord
            class_name: Course::Assessment::Answer::ProgrammingAutoGradingTestResult.name,
            foreign_key: :auto_grading_id, inverse_of: :auto_grading,
            dependent: :destroy
+
+  private
+
+  # Remove null bytes from stdout and stderr to avoid psql error:
+  # ArgumentError Exception: string contains null byte
+  def strip_null_byte
+    self.stdout = stdout.delete("\000") if stdout
+    self.stderr = stderr.delete("\000") if stderr
+  end
 end
