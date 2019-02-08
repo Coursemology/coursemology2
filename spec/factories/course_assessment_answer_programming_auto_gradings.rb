484d240052bbe8d0ca1cff29371010ee8f63ce3f
trang mai <trangmei@gmail.com>
2019-02-08 13:28:21 +0800

    Add spec for when stderr and stdout constains null byte

--- spec/factories/course_assessment_answer_programming_auto_gradings.rb
+++ spec/factories/course_assessment_answer_programming_auto_gradings.rb
@@ -3,5 +3,10 @@ FactoryBot.define do
   factory :course_assessment_answer_programming_auto_grading,
           class: Course::Assessment::Answer::ProgrammingAutoGrading,
           parent: :course_assessment_answer_auto_grading do
+
+    trait :with_null_byte do
+      stderr { "Makefile:6: recipe for target 'test' failed\000" }
+      stdout { "\000SyntaxError: invalid syntax" }
+    end
   end
 end
--- spec/models/course/assessment/answer/programming_auto_grading_spec.rb
+++ spec/models/course/assessment/answer/programming_auto_grading_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe Course::Assessment::Answer::ProgrammingAutoGrading do
       class_name(Course::Assessment::Answer::ProgrammingAutoGradingTestResult.name).
       with_foreign_key(:auto_grading_id)
   end
+
+  let(:instance) { Instance.default }
+  with_tenant(:instance) do
+    describe '#strip_null_byte' do
+      let(:auto_grading) do
+        build(:course_assessment_answer_programming_auto_grading, :with_null_byte)
+      end
+      it 'removes all null bytes from stderr and stdout' do
+        expect(auto_grading.save).to be_truthy
+      end
+    end
+  end
 end
