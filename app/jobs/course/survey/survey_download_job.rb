b49e8e5d9ca123a2883ba89da2bdb4efefe0b7c6
dexterleng <dexterlengcs@gmail.com>
2019-02-12 14:22:13 +0800

    Create survey download job

---
+++ app/jobs/course/survey/survey_download_job.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+class Course::Survey::SurveyDownloadJob < ApplicationJob
+  include TrackableJob
+  queue_as :lowest
+
+  protected
+
+  # Performs the download service.
+  #
+  # @param [Course::Survey] survey
+  def perform_tracked(survey)
+    csv_file = Course::Survey::SurveyDownloadService.download(survey)
+    redirect_to SendFile.send_file(csv_file, Pathname.normalize_filename(survey.title) + '.csv')
+  end
+end
