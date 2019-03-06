aa13216b8730762a8b8d113b32cf4d06586fd198
trang mai <trangmei@gmail.com>
2019-03-06 14:19:11 +0800

    Specify :optional on spec, following association definition in model

--- spec/models/attachment_reference_spec.rb
+++ spec/models/attachment_reference_spec.rb
@@ -2,7 +2,7 @@
 require 'rails_helper'
 
 RSpec.describe AttachmentReference do
-  it { is_expected.to belong_to(:attachable) }
+  it { is_expected.to belong_to(:attachable).optional }
   it { is_expected.to belong_to(:attachment) }
 
   describe '#file=' do
