aa65de5a2a46fc7cb030159e01168e5a3b76dde2
trang mai <trangmei@gmail.com>
2019-03-06 18:45:16 +0800

    Cascade destroy skill on branch#destroy

--- spec/models/course/assessment/skill_branch_spec.rb
+++ spec/models/course/assessment/skill_branch_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 
 RSpec.describe Course::Assessment::SkillBranch do
   it { is_expected.to belong_to(:course).inverse_of(:assessment_skill_branches) }
-  it { is_expected.to have_many(:skills) }
+  it { is_expected.to have_many(:skills).dependent(:destroy) }
 end
