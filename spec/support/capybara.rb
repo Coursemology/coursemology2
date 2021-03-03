0ecd7c57ffd35da81347a18e6123788749f2a808
Raymoond Tang <raymoondtang@gmail.com>
2021-03-03 12:33:32 +0800

    change ci testing parallelism to 1

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,13 +2,13 @@
 
 require 'selenium/webdriver'
 
-# How to make the screen bigger
-Capybara.register_driver :selenium_chrome_headless do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-      chromeOptions: { args: %w(headless disable-gpu window-size=2500,2500) }
-  )
-  Capybara::Selenium::Driver.new app, browser: :chrome, desired_capabilities: capabilities
-end
+# How to make the screen bigger,  window-size=2500,2500
+# Capybara.register_driver :selenium_chrome_headless do |app|
+#   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+#       chromeOptions: { args: %w(headless disable-gpu) }
+#   )
+#   Capybara::Selenium::Driver.new app, browser: :chrome, desired_capabilities: capabilities
+# end
 
 Capybara.javascript_driver = :selenium_chrome_headless
 
