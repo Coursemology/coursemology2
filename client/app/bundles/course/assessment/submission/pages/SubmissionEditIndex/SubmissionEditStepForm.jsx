e43addb66f3089fe2887f9f67e3bd68b124ea4e8
Eko Widianto <ekowidianto@outlook.com>
2021-08-16 16:54:57 +0800

    fix(submission page): Fix to enable steppers after a question with an incorrect answer in student view for published autograded submission.

--- client/app/bundles/course/assessment/submission/pages/SubmissionEditIndex/SubmissionEditStepForm.jsx
+++ client/app/bundles/course/assessment/submission/pages/SubmissionEditIndex/SubmissionEditStepForm.jsx
@@ -96,10 +96,10 @@ class SubmissionEditStepForm extends Component {
   }
 
   handleStepClick(index) {
-    const { skippable, graderView } = this.props;
+    const { published, skippable, graderView } = this.props;
     const { maxStep } = this.state;
 
-    if (skippable || graderView || index <= maxStep) {
+    if (published || skippable || graderView || index <= maxStep) {
       this.setState({
         stepIndex: index,
       });
@@ -376,7 +376,7 @@ class SubmissionEditStepForm extends Component {
 
   renderStepper() {
     const { maxStep, stepIndex } = this.state;
-    const { skippable, graderView, questionIds = [] } = this.props;
+    const { published, skippable, graderView, questionIds = [] } = this.props;
 
     if (questionIds.length <= 1) {
       return null;
@@ -390,7 +390,7 @@ class SubmissionEditStepForm extends Component {
         style={{ justifyContent: 'center', flexWrap: 'wrap' }}
       >
         {questionIds.map((questionId, index) => {
-          if (skippable || graderView || index <= maxStep) {
+          if (published || skippable || graderView || index <= maxStep) {
             return (
               <Step key={questionId} active={index <= maxStep}>
                 <StepButton
