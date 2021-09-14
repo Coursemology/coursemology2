ba7bc62cf030d29f7b89b680d6a01a49270ceb1c
Hanming Zhu <hanming@huawen.com>
2021-09-14 19:53:03 +0800

    Shift migration to latest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_21_030941) do
+ActiveRecord::Schema.define(version: 2021_09_14_114834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
