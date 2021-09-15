c4ca1879cff54c38af7662a6e563d3327407a67d
Hanming Zhu <hanming@huawen.com>
2021-09-16 01:51:22 +0800

    Update enzyme to use temporary React 17 adapter

--- client/app/__test__/setup.js
+++ client/app/__test__/setup.js
@@ -3,7 +3,7 @@ import { IntlProvider, intlShape } from 'react-intl';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 Enzyme.configure({ adapter: new Adapter() });
 
