8c1acc8ec3c7710d7863f3874e959ebe6851b826
Kevin Lim <lyskevin@gmail.com>
2021-09-20 01:39:11 +0800

    Fix failing tests in spec/libraries/course/conditional/user_satisfiability_graph_spec.rb

--- spec/libraries/course/conditional/user_satisfiability_graph_spec.rb
+++ spec/libraries/course/conditional/user_satisfiability_graph_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Course::Conditional::UserSatisfiabilityGraph do
   class DummyConditionalCondition < ApplicationRecord
     acts_as_conditional
     acts_as_condition
+    attr_accessor :satisfiability_type
     attr_accessor :conditions
     attr_accessor :satisfied
     attr_accessor :id
