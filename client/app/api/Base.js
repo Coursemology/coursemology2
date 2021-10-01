a751450e63df54a6dac8897f16e8e102c74f7eec
Hanming Zhu <hanming@huawen.com>
2021-10-01 22:41:42 +0800

    Revert wait_for_axios change

--- client/app/api/Base.js
+++ client/app/api/Base.js
@@ -1,26 +1,6 @@
 import axios from 'axios';
 import { csrfToken } from 'lib/helpers/server-context';
 
-let pendingRequestCount = 0;
-
-axios.interceptors.request.use((config) => {
-  pendingRequestCount += 1;
-  return config;
-}, Promise.reject);
-
-axios.interceptors.response.use(
-  (response) => {
-    pendingRequestCount -= 1;
-    return response;
-  },
-  (error) => {
-    pendingRequestCount -= 1;
-    return Promise.reject(error);
-  },
-);
-
-window.pendingRequestCount = pendingRequestCount;
-
 export default class BaseAPI {
   constructor() {
     this.client = null;
