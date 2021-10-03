c81bcc7516dbc2d15ca3a5a37074535e97f4c7ab
Eko Widianto <ekowidianto@outlook.com>
2021-10-04 00:51:14 +0800

    Update confirmation dialog, success and failure messages for question assessment

--- app/controllers/concerns/course/assessment/question/multiple_responses_concern.rb
+++ app/controllers/concerns/course/assessment/question/multiple_responses_concern.rb
@@ -5,13 +5,19 @@ module Course::Assessment::Question::MultipleResponsesConcern
   def switch_mcq_mrq_type(multiple_choice, unsubmit)
     case multiple_choice
     when 'true'
-      @multiple_response_question.grading_scheme = :any_correct
+      grading_scheme = :any_correct
     when 'false'
-      @multiple_response_question.grading_scheme = :all_correct
+      grading_scheme = :all_correct
     end
-    @multiple_response_question.save!
-    unsubmit_submissions unless unsubmit == 'false'
-    @multiple_response_question.question.answers.destroy_all unless unsubmit == 'false'
+
+    if @multiple_response_question.update(grading_scheme: grading_scheme)
+      unsubmit_submissions unless unsubmit == 'false'
+      @multiple_response_question.question.answers.destroy_all unless unsubmit == 'false'
+      flash.now[:success] = @message_success_switch
+    else
+      @multiple_response_question.reload
+      flash.now[:danger] = @message_failure_switch
+    end
   end
 
   def unsubmit_submissions
