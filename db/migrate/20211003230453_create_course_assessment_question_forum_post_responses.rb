be86770c09afae152fa51ae835a44c050c711885
Aylwin <aylwin.low.pub@gmail.com>
2021-10-11 22:26:59 +0800

    Fix minor formatting issues

--- db/migrate/20211003230453_create_course_assessment_question_forum_post_responses.rb
+++ db/migrate/20211003230453_create_course_assessment_question_forum_post_responses.rb
@@ -6,11 +6,11 @@ class CreateCourseAssessmentQuestionForumPostResponses < ActiveRecord::Migration
       t.integer :max_posts, limit: 2, null: false
     end
 
-    create_table:course_assessment_answer_forum_post_responses do |t|
+    create_table :course_assessment_answer_forum_post_responses do |t|
       t.string :answer_text, null: true
     end
 
-    create_table:course_assessment_answer_forum_posts do |t|
+    create_table :course_assessment_answer_forum_posts do |t|
       t.references :answer,
                    null: false,
                    foreign_key: {
