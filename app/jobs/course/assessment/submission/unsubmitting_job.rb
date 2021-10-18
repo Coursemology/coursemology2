12a5d45281d3e0509a36b52083d2cbf8b565b42c
Eko Widianto <ekowidianto@outlook.com>
2021-10-18 23:44:16 +0800

    docs(unsubmission job): Add documentation

--- app/jobs/course/assessment/submission/unsubmitting_job.rb
+++ app/jobs/course/assessment/submission/unsubmitting_job.rb
@@ -1,10 +1,20 @@
 # frozen_string_literal: true
+# This job comprises of 2 tasks: 1) unsubmitting submissions and 2) (Optional) deleting answers to a specific question
+# of the unsubmitted submissions
+
 class Course::Assessment::Submission::UnsubmittingJob < ApplicationJob
   include TrackableJob
   include Rails.application.routes.url_helpers
 
   protected
 
+  # Creates a job to unsubmit all submitted submissions for a given assessment
+  # and to optionally delete answers to a question.
+  # @param [User] unsubmitter User who creates the unsubmission job.
+  # @param [Array<Integer>] submission_ids Submission ids of the submissions that are to be unsubmitted.
+  # @param [Course::Assessment] assessment Assessment of the submissions.
+  # @param [Course::Assessment::Question] question Optional question that should have its answers deleted.
+  # @param [String] redirect_to_path Path to be redirected after the job is completed.
   def perform_tracked(unsubmitter, submission_ids, assessment, question = nil, redirect_to_path = nil)
     instance = Course.unscoped { assessment.course.instance }
     ActsAsTenant.with_tenant(instance) do
@@ -20,7 +30,7 @@ class Course::Assessment::Submission::UnsubmittingJob < ApplicationJob
   # Unsubmit all submitted submissions for a given assessment and delete answer to question.
   #
   # @param [Course::Submissions] submissions Submissions that are to be unsubmitted.
-  # @param [Course::Assessment::Question] question Question of which its answers are to be submitted.
+  # @param [Course::Assessment::Question] question Optional question that should have its answers deleted.
   # @param [User] unsubmitter The user object who would be unsubmitting the submission.
   def unsubmit_submission(submissions, question, unsubmitter)
     User.with_stamper(unsubmitter) do
