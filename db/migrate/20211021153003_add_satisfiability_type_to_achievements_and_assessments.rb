804bf56256d6c87db016db74331046c6f5070c15
Kevin Lim <lyskevin@gmail.com>
2021-10-21 23:49:51 +0800

    update migration file to use rails 6.0

--- db/migrate/20211021153003_add_satisfiability_type_to_achievements_and_assessments.rb
+++ db/migrate/20211021153003_add_satisfiability_type_to_achievements_and_assessments.rb
@@ -1,4 +1,4 @@
-class AddSatisfiabilityTypeToAchievementsAndAssessments < ActiveRecord::Migration[5.2]
+class AddSatisfiabilityTypeToAchievementsAndAssessments < ActiveRecord::Migration[6.0]
   def change
     add_column :course_achievements, :satisfiability_type, :integer, default: 0
     add_column :course_assessments, :satisfiability_type, :integer, default: 0
