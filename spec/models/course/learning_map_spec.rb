78e3add9b28506f4cbd1a75a4996785000a82a3c
Kevin Lim <lyskevin@gmail.com>
2021-10-22 13:16:33 +0800

    add tests for the learning map model

--- spec/models/course/learning_map_spec.rb
+++ spec/models/course/learning_map_spec.rb
@@ -1,5 +1,6 @@
+# frozen_string_literal: true
 require 'rails_helper'
 
 RSpec.describe Course::LearningMap, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:course).inverse_of(:learning_map) }
 end
