2a489ac86bb31d78eb83fbe35509780e8eea233b
Eko Widianto <ekowidianto@outlook.com>
2021-10-27 10:57:48 +0800

    refactor(course email setting): regenerate db migration files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_21_153003) do
+ActiveRecord::Schema.define(version: 2021_10_27_070704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
