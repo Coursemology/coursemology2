db0a60593941dcd7b4f965cbce6b0eb64b2326aa
Eko Widianto <ekowidianto@outlook.com>
2021-12-10 11:49:20 +0800

    refactor(role request controller and model): add approval and rejection workflow state to role request process

--- app/controllers/instance_user_role_requests_controller.rb
+++ app/controllers/instance_user_role_requests_controller.rb
@@ -5,6 +5,7 @@ class InstanceUserRoleRequestsController < ApplicationController
   def index
     add_breadcrumb current_tenant.name, :admin_instance_admin_path
     add_breadcrumb :index, :instance_user_role_requests_path
+    @user_role_requests = @user_role_requests.includes(:confirmer, :user)
     render layout: 'system_admin_instance'
   end
 
@@ -33,10 +34,10 @@ class InstanceUserRoleRequestsController < ApplicationController
   end
 
   def approve
-    @user_role_request.assign_attributes(user_role_request_params)
+    @user_role_request.update!(user_role_request_params)
 
-    @success, instance_user = @user_role_request.approve_and_destroy!
-    if @success
+    @success, instance_user = @user_role_request.approve!
+    if @success && @user_role_request.save!
       InstanceUserRoleRequestMailer.role_request_approved(instance_user).deliver_later
       flash.now[:success] = t('.success', user: instance_user.user.name, role: instance_user.role)
     else
@@ -44,13 +45,37 @@ class InstanceUserRoleRequestsController < ApplicationController
     end
   end
 
-  def destroy
-    redirect_to instance_user_role_requests_path, success: t('.success') if @user_role_request.destroy
+  def reject
+    if reject_role_request
+      send_rejection_email
+      success_message = if @user_role_request.rejection_message
+                          t('.success_with_email', user: @instance_user.user.name)
+                        else
+                          t('.success', user: @instance_user.user.name)
+                        end
+      redirect_to instance_user_role_requests_path, success: success_message
+    else
+      redirect_to instance_user_role_requests_path, danger: t('.failure')
+    end
   end
 
   private
 
   def user_role_request_params
     params.require(:user_role_request).permit(:role, :organization, :designation, :reason)
+  end
+
+  def user_role_request_rejection_params
+    params.fetch(:user_role_request, {}).permit(:rejection_message)
+  end
+
+  def reject_role_request
+    @user_role_request.update!(user_role_request_rejection_params) && @user_role_request.update!(reject: true)
+  end
+
+  def send_rejection_email
+    @instance_user = InstanceUser.find_by(user_id: @user_role_request.user_id)
+    InstanceUserRoleRequestMailer.role_request_rejected(@instance_user, @user_role_request.rejection_message).
+      deliver_later
   end
 end
--- app/models/instance/user_role_request.rb
+++ app/models/instance/user_role_request.rb
@@ -1,32 +1,37 @@
 # frozen_string_literal: true
 class Instance::UserRoleRequest < ApplicationRecord
+  include Workflow
   enum role: InstanceUser.roles.except(:normal)
 
   after_initialize :set_default_role, if: :new_record?
+
+  workflow do
+    state :pending do
+      event :approve, transitions_to: :approved
+      event :reject, transitions_to: :rejected
+    end
+    state :approved do
+    end
+    state :rejected do
+    end
+  end
 
   validates :role, presence: true
   validates :organization, length: { maximum: 255 }, allow_nil: true
   validates :designation, length: { maximum: 255 }, allow_nil: true
   validates :instance, presence: true
   validates :user, presence: true
+  validates :workflow_state, length: { maximum: 255 }, presence: true
+  validate :validate_no_duplicate_pending_request, on: :create
 
   belongs_to :instance, inverse_of: :user_role_requests
   belongs_to :user, inverse_of: nil
+  belongs_to :confirmer, class_name: User.name, inverse_of: nil, optional: true
 
-  # Set the corresponding instance user to the requested role and destroy the request.
-  #
-  # @return [Array(Boolean, InstanceUser)] returns success status and the updated instance user.
-  def approve_and_destroy!
-    instance_user = InstanceUser.find_or_initialize_by(user_id: user_id)
-    instance_user.role = role
-    success = self.class.transaction do
-      raise ActiveRecord::Rollback unless instance_user.save && destroy
+  alias_method :approve=, :approve!
+  alias_method :reject=, :reject!
 
-      true
-    end
-
-    [success, instance_user]
-  end
+  scope :pending, -> { where(workflow_state: :pending) }
 
   def send_new_request_email(instance)
     ActsAsTenant.without_tenant do
@@ -43,7 +48,32 @@ class Instance::UserRoleRequest < ApplicationRecord
 
   private
 
+  def validate_no_duplicate_pending_request
+    existing_request = Instance::UserRoleRequest.find_by(user_id: user_id, workflow_state: 'pending')
+    errors.add(:base, :existing_pending_request) if existing_request
+  end
+
   def set_default_role
     self.role ||= :instructor
+  end
+
+  def approve(_ = nil)
+    self.confirmed_at = Time.zone.now
+    self.confirmer = User.stamper
+
+    instance_user = InstanceUser.find_or_initialize_by(instance_id: instance_id, user_id: user_id)
+    instance_user.role = role
+
+    success = self.class.transaction do
+      raise ActiveRecord::Rollback unless instance_user.save
+
+      true
+    end
+    [success, instance_user]
+  end
+
+  def reject(_ = nil)
+    self.confirmed_at = Time.zone.now
+    self.confirmer = User.stamper
   end
 end
