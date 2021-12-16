5c592725f88ece2b08463e15ef094fccf6215db2
Eko Widianto <ekowidianto@outlook.com>
2021-12-16 19:11:07 +0800

    feat(submissions csv download): add service and job for submissions answers csv download

---
+++ app/jobs/course/assessment/submission/csv_download_job.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+class Course::Assessment::Submission::CsvDownloadJob < ApplicationJob
+  include TrackableJob
+  queue_as :lowest
+
+  protected
+
+  # Performs the submission download as csv service.
+  #
+  # @param [CourseUser] current_course_user The course user downloading the submissions.
+  # @param [Course::Assessment] assessment The assessments to download submissions for.
+  # @param [String|nil] course_users The subset of course users whose submissions to download.
+  def perform_tracked(current_course_user, assessment, course_users = nil)
+    csv_file = Course::Assessment::Submission::CsvDownloadService.
+               download(current_course_user, assessment, course_users)
+    redirect_to SendFile.send_file(csv_file, "#{Pathname.normalize_filename(assessment.title)}.csv")
+  end
+end
