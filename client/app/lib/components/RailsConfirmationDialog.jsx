0618aaffd3c5ebc8470c6d602dd2c0a4acb74b41
Hanming Zhu <hanming@huawen.com>
2021-12-24 15:02:05 +0800

    fix: bring back rails confirmation dialog

---
+++ client/app/lib/components/RailsConfirmationDialog.jsx
@@ -0,0 +1,52 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import ConfirmationDialog from 'lib/components/ConfirmationDialog';
+
+class RailsConfirmationDialog extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: true, disableButtons: false };
+    this.onConfirm = this.onConfirm.bind(this);
+    this.onConfirmSecondary = this.props.onConfirmSecondaryCallback
+      ? this.onConfirmSecondary.bind(this)
+      : null;
+  }
+
+  // Disable buttons once the confirm button is clicked, then do confirm callback.
+  onConfirm = () => {
+    this.setState({ disableButtons: true });
+    this.props.onConfirmCallback();
+  };
+
+  // Disable buttons once the secondary confirm button is clicked, then do confirm callback.
+  onConfirmSecondary() {
+    this.setState({ disableButtons: true });
+    this.props.onConfirmSecondaryCallback();
+  }
+
+  render() {
+    return (
+      <ConfirmationDialog
+        open={this.state.open}
+        disableCancelButton={this.state.disableButtons}
+        disableConfirmButton={this.state.disableButtons}
+        onCancel={() => this.setState({ open: false })}
+        onConfirm={this.onConfirm}
+        onConfirmSecondary={this.onConfirmSecondary}
+        message={this.props.message}
+        confirmButtonText={this.props.confirmButtonText}
+        confirmSecondaryButtonText={this.props.confirmSecondaryButtonText}
+      />
+    );
+  }
+}
+
+RailsConfirmationDialog.propTypes = {
+  onConfirmCallback: PropTypes.func,
+  onConfirmSecondaryCallback: PropTypes.func,
+  message: PropTypes.string,
+  confirmButtonText: PropTypes.string,
+  confirmSecondaryButtonText: PropTypes.string,
+};
+
+export default RailsConfirmationDialog;
