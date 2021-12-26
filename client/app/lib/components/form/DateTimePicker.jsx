4ab4c494537adb4bb70635aed83326c07a8ae6f9
Hanming Zhu <hanming@huawen.com>
2021-12-27 00:36:58 +0800

    fix: clear time error and add relevant comments

--- client/app/lib/components/form/DateTimePicker.jsx
+++ client/app/lib/components/form/DateTimePicker.jsx
@@ -88,6 +88,30 @@ const propTypes = {
   style: PropTypes.object,
 };
 
+/**
+ * Unfortunately, the behaviour of this component is not exactly ideal, as there
+ * are two fields - date and time, but they share the same Date object. What thus
+ * happens is that keyboard interactions, i.e. when users manually enter the date
+ * and time, are quite wonky.
+ *
+ * For example, if the user deletes the entry from the time field, we would ideally
+ * want to set it to null, i.e. no time specified, but doing so would also clear
+ * the date field, which is not what a user would expect. As such, we would have
+ * to instead set the time to 00:00, but doing so is also not exactly expected,
+ * since the user will see 00:00 pop up after deleting the data from the field.
+ *
+ * We face a similar issue when the user deletes the date field entry, where the
+ * time field will also be cleared since the date is set to null.
+ *
+ * We can't exactly treat all null values as invalid values as well, since there
+ * may be optional datetime inputs. As such, we need to be capable of storing
+ * null values for the datetime.
+ *
+ * TODO: To look into fixing the abovementioned solution. One possible way would
+ * be to simply prevent keyboard inputs, and only allow the usage of the pickers.
+ * Another would be to upgrade the picker dependencies and hope that the handling
+ * that comes shipped with the newer versions handle these better than we can.
+ */
 class DateTimePicker extends PureComponent {
   static displayState(dateTime) {
     return {
@@ -158,6 +182,7 @@ class DateTimePicker extends PureComponent {
 
   updateTime = (newTime) => {
     if (newTime === null) {
+      this.setState({ timeError: '' });
       // If there is already a date object, we don't want to just clear it, which
       // will be very disruptive if the user is also using the Date field, since
       // their date value will also just disappear.
@@ -188,6 +213,7 @@ class DateTimePicker extends PureComponent {
     const newDateTime = this.props.value
       ? moment(this.props.value).set({ hours, minutes })
       : moment({ hours, minutes });
+    this.setState({ timeError: '' });
     this.updateDateTime(newDateTime.toDate());
   };
 
@@ -232,7 +258,6 @@ class DateTimePicker extends PureComponent {
               style={styles.timeTextField}
               onChange={this.updateTime}
               clearable={clearable}
-              keyboard="true"
               keyboardIcon={<Schedule style={styles.pickerIcon} />}
               placeholder={intl.formatMessage(translations.timePlaceholder)}
               label="24-hr clock"
