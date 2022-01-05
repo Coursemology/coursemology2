6ffd5e39c5efe29a982b77a38db9ea6258bb0e95
Hanming Zhu <hanming@huawen.com>
2022-01-05 21:06:43 +0800

    tests: update statistics_controller_spec to cover download route

--- spec/controllers/course/statistics_controller_spec.rb
+++ spec/controllers/course/statistics_controller_spec.rb
@@ -36,9 +36,9 @@ RSpec.describe Course::StatisticsController, type: :controller do
       end
 
       context 'when a Course Observer visits the page' do
-        let(:user) { create(:course_student, course: course).user }
+        let(:user) { create(:course_observer, course: course).user }
         before { sign_in(user) }
-        it { expect { subject }.to raise_exception(CanCan::AccessDenied) }
+        it { expect(subject).to be_successful }
       end
     end
 
@@ -83,7 +83,7 @@ RSpec.describe Course::StatisticsController, type: :controller do
       end
 
       context 'when a Course Manager with students visits the page' do
-        let(:manager) { create(:course_teaching_assistant, course: course) }
+        let(:manager) { create(:course_manager, course: course) }
         let(:group) { create(:course_group, course: course) }
         let(:student) { create(:course_student, course: course) }
 
@@ -95,10 +95,71 @@ RSpec.describe Course::StatisticsController, type: :controller do
         it { expect(subject).to be_successful }
       end
 
-      context 'when a Course Observer visits the page' do
+      context 'when a Course Observer without students visits the page' do
+        let(:user) { create(:course_observer, course: course).user }
+        before { sign_in(user) }
+        it { expect(subject).to redirect_to(action: :all_students) }
+      end
+
+      context 'when a Course Observer with students visits the page' do
+        let(:observer) { create(:course_observer, course: course) }
+        let(:group) { create(:course_group, course: course) }
+        let(:student) { create(:course_student, course: course) }
+
+        before do
+          create(:course_group_manager, group: group, course_user: observer)
+          create(:course_group_user, group: group, course_user: student)
+          sign_in(observer.user)
+        end
+        it { expect(subject).to be_successful }
+      end
+    end
+
+    describe '#download' do
+      subject do
+        get :download, params: { course_id: course.id, user_id: course_user, only_my_students: false }
+      end
+
+      context 'when a Normal User tries to download' do
+        let(:user) { create(:user) }
+        before { sign_in(user) }
+        it { expect { subject }.to raise_exception(CanCan::AccessDenied) }
+      end
+
+      context 'when a Course Student tries to download' do
         let(:user) { create(:course_student, course: course).user }
         before { sign_in(user) }
         it { expect { subject }.to raise_exception(CanCan::AccessDenied) }
+      end
+
+      context 'when a Course Teaching Assistant tries to download' do
+        let(:user) { create(:course_teaching_assistant, course: course).user }
+        before { sign_in(user) }
+        it 'returns a html file' do
+          subject
+          expect(response.header['Content-Type']).to include('text/html')
+          expect(response.status).to eq(302)
+        end
+      end
+
+      context 'when a Course Manager tries to download' do
+        let(:user) { create(:course_manager, course: course).user }
+        before { sign_in(user) }
+        it 'returns a html file' do
+          subject
+          expect(response.header['Content-Type']).to include('text/html')
+          expect(response.status).to eq(302)
+        end
+      end
+
+      context 'when a Course Observer tries to download' do
+        let(:user) { create(:course_observer, course: course).user }
+        before { sign_in(user) }
+        it 'returns a html file' do
+          subject
+          expect(response.header['Content-Type']).to include('text/html')
+          expect(response.status).to eq(302)
+        end
       end
     end
   end
