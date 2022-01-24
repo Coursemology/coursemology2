0c1d32481300a7d11eaabee4b6cfe926488be862
Eko Widianto <ekowidianto@outlook.com>
2022-01-24 12:17:16 +0800

    perf(course homepage): remove n+1 due to repeated personal or reference time queries by preloading the assessment items with the time

--- app/controllers/course/courses_controller.rb
+++ app/controllers/course/courses_controller.rb
@@ -12,7 +12,9 @@ class Course::CoursesController < Course::Controller
   def show # :nodoc:
     @registration = Course::Registration.new
     @currently_active_announcements = current_course.announcements.currently_active.sorted_by_sticky.sorted_by_date
-    @activity_feeds = recent_activity_feeds.limit(20).preload(activity: [:object, :actor])
+    @activity_feeds = recent_activity_feeds.limit(20).preload(activity: [{ object: { topic: { actable: :forum } } },
+                                                                         :actor])
+    load_activity_course_users
     render layout: 'course'
   end
 
@@ -43,7 +45,7 @@ class Course::CoursesController < Course::Controller
       permit(:title, :description, :status, :start_at, :end_at, :logo)
   end
 
-  def load_todos
+  def load_todos # rubocop:disable Metrics/AbcSize
     return unless current_course_user&.student?
 
     todos = Course::LessonPlan::Todo.pending_for(current_course_user).
@@ -52,5 +54,27 @@ class Course::CoursesController < Course::Controller
     @video_todos = todos.select { |td| td.item.actable_type == Course::Video.name }
     @assessment_todos = todos.select { |td| td.item.actable_type == Course::Assessment.name }
     @survey_todos = todos.select { |td| td.item.actable_type == Course::Survey.name }
+    load_items_with_timeline
+  end
+
+  def load_items_with_timeline # rubocop:disable Metrics/AbcSize
+    # preload items with their timeline. Only the first 5 items are preloaded since
+    # we are only showing the first 5 items.
+    item_ids = [*@video_todos.map { |todo| todo.item.id }.first(5),
+                *@assessment_todos.map { |todo| todo.item.id }.first(5),
+                *@survey_todos.map { |todo| todo.item.id }.first(5)]
+    @todo_items_with_timeline_hash = @course.lesson_plan_items.where(id: item_ids).
+                                     with_reference_times_for(current_course_user).
+                                     with_personal_times_for(current_course_user).
+                                     to_h do |item|
+                                       [item.id, item]
+                                     end
+  end
+
+  def load_activity_course_users
+    activity_user_ids = @activity_feeds.map { |x| x.activity.actor_id }.uniq
+    @course_users_hash = current_course.course_users.where(user_id: activity_user_ids).to_h do |course_user|
+      [course_user.user_id, course_user]
+    end
   end
 end
