9d593e8faab64a8327ac2d6fd6711e4d2493dcf7
Hanming Zhu <hanming@huawen.com>
2022-02-09 09:38:13 +0800

    feat: add computation for a specific lesson plan item

--- app/controllers/concerns/course/lesson_plan/personalization_concern.rb
+++ app/controllers/concerns/course/lesson_plan/personalization_concern.rb
@@ -12,7 +12,13 @@ module Course::LessonPlan::PersonalizationConcern
   #   allows students to slow down their learning more effectively.
   # - We don't shift closing dates forward when the item has already opened for the student. This is to
   #   prevent students from being shocked that their deadlines have shifted forward suddenly.
-  def update_personalized_timeline_for_user(course_user, timeline_algorithm = nil)
+  #
+  # @param [CourseUser] course_user The user to update the personalized timeline for.
+  # @param [String|nil] timeline_algorithm The timeline algorithm to run. If not provided, the user's timeline algorithm
+  #   is used.
+  # @param [Set<Number>|nil] items_to_shift A set of lesson plan item IDs to shift. If not in this set, the item won't
+  #   be shifted.
+  def update_personalized_timeline_for_user(course_user, timeline_algorithm = nil, items_to_shift = nil)
     timeline_algorithm ||= course_user.timeline_algorithm
 
     strategy = case timeline_algorithm
@@ -28,6 +34,16 @@ module Course::LessonPlan::PersonalizationConcern
                end
 
     precomputed_data = strategy.precompute_data(course_user)
-    strategy.execute(course_user, precomputed_data)
+    strategy.execute(course_user, precomputed_data, items_to_shift)
+  end
+
+  # Updates the personalized timeline for all course users in the course of the given lesson plan item.
+  # Only the timing for the lesson plan item will be shifted. Generally, you should only call this if the timing of the
+  # lesson plan item has shifted, or other personalized timeline related changes have been made for a specific item.
+  def update_personalized_timeline_for_item(lesson_plan_item)
+    course = lesson_plan_item.course
+    course.course_users.each do |course_user|
+      update_personalized_timeline_for_user(course_user, nil, Set[lesson_plan_item.id])
+    end
   end
 end
