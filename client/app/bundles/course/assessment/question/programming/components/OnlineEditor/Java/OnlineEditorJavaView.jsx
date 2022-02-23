ff9ee521eb32c7e9c4a3e285c21dcfb33417e6fb
Eko Widianto <ekowidianto@outlook.com>
2022-02-23 15:16:22 +0800

    chore(mui v4 update): rename expansion panel components to accordion

--- client/app/bundles/course/assessment/question/programming/components/OnlineEditor/Java/OnlineEditorJavaView.jsx
+++ client/app/bundles/course/assessment/question/programming/components/OnlineEditor/Java/OnlineEditorJavaView.jsx
@@ -6,9 +6,9 @@ import AceEditor from 'react-ace';
 import { injectIntl, FormattedMessage, intlShape } from 'react-intl';
 import {
   Button,
-  ExpansionPanel,
-  ExpansionPanelDetails,
-  ExpansionPanelSummary,
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
   Fade,
   FormControlLabel,
   Table,
@@ -302,19 +302,19 @@ class OnlineEditorJavaView extends Component {
     };
 
     return (
-      <ExpansionPanel
+      <Accordion
         defaultExpanded
         style={{
           margin: 0,
         }}
       >
-        <ExpansionPanelSummary
+        <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           style={{ fontSize: 14, fontWeight: 'bold', margin: 0 }}
         >
           {header}
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails style={{ padding: 0 }}>
+        </AccordionSummary>
+        <AccordionDetails style={{ padding: 0 }}>
           <Table>
             <TableHead>
               <TableRow>
@@ -331,8 +331,8 @@ class OnlineEditorJavaView extends Component {
               {this.props.data.get(fileType).map(renderDataFile)}
             </TableBody>
           </Table>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
+        </AccordionDetails>
+      </Accordion>
     );
   }
 
@@ -361,24 +361,24 @@ class OnlineEditorJavaView extends Component {
       .map(renderNewFile);
 
     return (
-      <ExpansionPanel
+      <Accordion
         defaultExpanded
         style={{
           margin: 0,
         }}
       >
-        <ExpansionPanelSummary
+        <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           style={{ fontSize: 16, fontWeight: 'bold', margin: 0 }}
         >
           {header}
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails style={{ padding: 0 }}>
+        </AccordionSummary>
+        <AccordionDetails style={{ padding: 0 }}>
           <Table>
             <TableBody>{newPackageFilesRows}</TableBody>
           </Table>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
+        </AccordionDetails>
+      </Accordion>
     );
   }
 
@@ -464,19 +464,19 @@ class OnlineEditorJavaView extends Component {
     });
 
     return (
-      <ExpansionPanel
+      <Accordion
         defaultExpanded
         style={{
           margin: 0,
         }}
       >
-        <ExpansionPanelSummary
+        <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           style={{ fontSize: 16, fontWeight: 'bold', margin: 0 }}
         >
           {header}
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails style={{ padding: 0 }}>
+        </AccordionSummary>
+        <AccordionDetails style={{ padding: 0 }}>
           <Table>
             <TableHead>
               <TableRow>
@@ -507,8 +507,8 @@ class OnlineEditorJavaView extends Component {
               </TableRow>
             </TableFooter>
           </Table>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
+        </AccordionDetails>
+      </Accordion>
     );
   }
 
