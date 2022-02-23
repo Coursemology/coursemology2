c6e3c6306b8bb177998885b0059200027b0fc7c2
Kevin Lim <lyskevin@gmail.com>
2022-02-23 13:30:23 +0800

    test (tagging framework): add body block to course tag relationships factory

--- spec/factories/course_tag_relationships.rb
+++ spec/factories/course_tag_relationships.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
+# rubocop:disable Lint/EmptyBlock
 FactoryBot.define do
-  factory :course_tag_relationship, class: Course::Tag::Relationship.name
+  factory :course_tag_relationship, class: Course::Tag::Relationship.name do
+  end
 end
+# rubocop:enable Lint/EmptyBlock
