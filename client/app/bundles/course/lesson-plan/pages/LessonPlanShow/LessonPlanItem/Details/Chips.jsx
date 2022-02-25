227aaf21b69429c95c5c070835dafc3a618875d5
Eko Widianto <ekowidianto@outlook.com>
2022-02-25 14:46:21 +0800

    style(lesson plan chip): reduce icon size inside chip avatar

--- client/app/bundles/course/lesson-plan/pages/LessonPlanShow/LessonPlanItem/Details/Chips.jsx
+++ client/app/bundles/course/lesson-plan/pages/LessonPlanShow/LessonPlanItem/Details/Chips.jsx
@@ -23,6 +23,9 @@ const styles = {
   chip: {
     margin: 4,
   },
+  chipIcon: {
+    fontSize: '1.8rem',
+  },
   chipsWrapper: {
     display: 'flex',
     flexWrap: 'wrap',
@@ -102,7 +105,7 @@ class Chips extends React.Component {
       <Chip
         avatar={
           <Avatar style={styles.avatar}>
-            <DateRange />
+            <DateRange style={styles.chipIcon}/>
           </Avatar>
         }
         label={formatDateRange(startAt, endAt)}
