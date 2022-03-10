41feaf01d591608fbfc33d79a90f12bd01a301ab
Eko Widianto <ekowidianto@outlook.com>
2022-03-10 15:02:35 +0800

    chore(mui v5 upgrade): update mui radio component

--- client/app/bundles/course/assessment/submission/components/pastAnswers/PastMultipleResponseAnswer.jsx
+++ client/app/bundles/course/assessment/submission/components/pastAnswers/PastMultipleResponseAnswer.jsx
@@ -1,4 +1,5 @@
-import { Radio, FormControlLabel } from '@material-ui/core';
+import { FormControlLabel } from '@material-ui/core';
+import { Radio } from '@mui/material';
 import { green } from '@mui/material/colors';
 import { answerShape, questionShape } from '../../propTypes';
 
@@ -9,7 +10,7 @@ function MultipleResponse({ question, answer }) {
       {question.options.map((option) => (
         <FormControlLabel
           checked={selectedOptions.indexOf(option.id) !== -1}
-          control={<Radio color="primary" style={{ padding: '0 12px' }} />}
+          control={<Radio style={{ padding: '0 12px' }} />}
           disabled
           key={option.id}
           label={
