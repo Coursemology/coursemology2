c3971597da99d9473a32ce3dc1af4623f0e41e7e
Kevin Lim <lyskevin@gmail.com>
2022-03-27 15:04:50 +0800

    feat (learning map): add gate to node arrows view component

---
+++ client/app/bundles/course/learning-map/containers/GateToNodeArrows/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Xarrow from 'react-xarrows';
+
+const GateToNodeArrows = (props) => {
+  const {
+    arrowAnchorPositions,
+    arrowProperties,
+    getGateConnectionPointId,
+    nodes,
+  } = props;
+
+  return (
+    nodes.map(node => {
+      return (
+        <Xarrow
+          key={node.id}
+          start={getGateConnectionPointId(node.id)}
+          startAnchor={arrowAnchorPositions}
+          end={node.id}
+          endAnchor={arrowAnchorPositions}
+          color={arrowProperties.defaultColor}
+          divContainerStyle={{position: 'relative'}}
+          headSize={arrowProperties.headSize}
+          strokeWidth={arrowProperties.strokeWidth}
+        />
+      );
+    })
+  );
+};
+
+const mapStateToProps = (state) => ({
+  nodes: state.learningMap.nodes,
+});
+
+export default connect(mapStateToProps)(GateToNodeArrows);
