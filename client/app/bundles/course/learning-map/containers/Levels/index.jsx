534433f2be41ccbf5a4ba25946dc6e90e105f2bc
Kevin Lim <lyskevin@gmail.com>
2022-03-27 15:05:48 +0800

    feat (learning map): add levels view container

---
+++ client/app/bundles/course/learning-map/containers/Levels/index.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import Gate from '../../components/Gate';
+import Node from '../../components/Node';
+import { connect } from 'react-redux';
+
+const styles = {
+  level: {
+    alignItems: 'top',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    margin: '10px 30px',
+  },
+  nodeWithGate: {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  wrapper: {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'row',
+  },
+};
+
+const Levels = React.memo((props) => {
+  const {
+    gateInputSizeThreshold,
+    getGateConnectionPointId,
+    getGateId,
+    getGateInputId,
+    getNodeConnectionPointId,
+    nodes,
+  } = props;
+
+  const maxDepth = nodes.length > 0 ? nodes.reduce((prev, cur) => cur.depth > prev.depth ? cur : prev).depth : 0;
+  const levels = [...Array(maxDepth + 1)].map(index => new Array());
+  nodes.forEach(node => levels[node.depth].push(node));
+
+  return (
+    <div style={styles.wrapper}>
+      {
+        levels.map((level, index) =>
+        <div
+          key={index}
+          style={styles.level}
+        >
+          {
+            level.sort((node1, node2) => node1.id.localeCompare(node2.id)).map((node) =>
+            <div
+              key={node.id}
+              style={styles.nodeWithGate}
+            >
+              <>
+                <Gate
+                  gateInputSizeThreshold={gateInputSizeThreshold}
+                  getGateConnectionPointId={getGateConnectionPointId}
+                  getGateId={getGateId}
+                  getGateInputId={getGateInputId}
+                  key={getGateId(node.id)}
+                  node={node}
+                />
+                <Node
+                  key={node.id}
+                  node={node}
+                  getNodeConnectionPointId={getNodeConnectionPointId}
+                />
+              </>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+});
+
+const mapStateToProps = (state) => ({
+  nodes: state.learningMap.nodes,
+});
+
+export default connect(mapStateToProps)(Levels);
