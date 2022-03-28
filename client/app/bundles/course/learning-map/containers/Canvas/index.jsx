d5b68c16f6dd28306416d87fd78a417f688727d3
Kevin Lim <lyskevin@gmail.com>
2022-03-29 01:10:28 +0800

    feat (learning map): update learning map canvas to be able to zoom and pan

--- client/app/bundles/course/learning-map/containers/Canvas/index.jsx
+++ client/app/bundles/course/learning-map/containers/Canvas/index.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { resetSelection } from 'course/learning-map/actions';
+import React, { useRef, useState } from 'react';
 import Levels from '../Levels';
 import ArrowOverlay from '../ArrowOverlay';
 import CursorTracker from '../../components/CursorTracker';
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import { Xwrapper } from 'react-xarrows';
+import ZoomActionElements from '../../components/ZoomActionElements';
 
 const styles = {
   cursorPosition: {
@@ -13,18 +12,27 @@ const styles = {
   },
   wrapper: {
     outline: 'none',
+    overflow: 'hidden',
+    position: 'relative',
     width: '100%',
+    zIndex: 1,
   },
+  zoomAnimation: {
+    animationTime: 300,
+    animationType: 'easeOutCubic',
+  }
 };
 
 const arrowAnchorPositions = ['left', 'right'];
 
 const Canvas = (props) => {
-  const {
-    dispatch,
-  } = props;
   const gateInputSizeThreshold = 5;
   const cursorTrackerId = 'cursor-tracker';
+  const transformRef = useRef(null);
+  const [scale, setScale] = useState(1);
+  const zoomScale = 1.25;
+  const maxScale = 1.5;
+  const minScale = 0.25;
 
   const getGateInputId = (isSummaryGate, parentNodeId, childNodeId) => {
     return isSummaryGate ? `${childNodeId}-summary-gate` : `${parentNodeId}-to-${childNodeId}-gate-input`;
@@ -42,45 +50,64 @@ const Canvas = (props) => {
     return `${nodeId}-connection-point`;
   };
 
+  const onZoom = (setTransform, isZoomingIn) => {
+    const state = transformRef.current.state;
+    const newScale = isZoomingIn ? Math.min(state.scale * zoomScale, maxScale) : Math.max(state.scale / zoomScale, minScale);
+    setTransform(state.positionX, state.positionY, newScale);
+    setScale(newScale);
+  }
+
   return (
     <TransformWrapper
       doubleClick={{disabled: true}}
       limitToBounds={false}
       pinch={{disabled: true}}
       minScale={0.2}
+      maxScale={1.5}
       wheel={{disabled: true}}
+      zoomIn={styles.zoomAnimation}
+      zoomOut={styles.zoomAnimation}
+      ref={transformRef}
     >
-      <TransformComponent>
-        <div
-          onClick={() => dispatch(resetSelection())}
-          style={styles.wrapper}
-          tabIndex={0}
-        >
+      {({ setTransform }) => (
+        <>
           <Xwrapper>
-            <Levels
-              gateInputSizeThreshold={gateInputSizeThreshold}
-              getGateConnectionPointId={getGateConnectionPointId}
-              getGateId={getGateId}
-              getGateInputId={getGateInputId}
-              getNodeConnectionPointId={getNodeConnectionPointId}
-            /> 
-            <CursorTracker
-              arrowAnchorPositions={arrowAnchorPositions}
-              id={cursorTrackerId}
-              getNodeConnectionPointId={getNodeConnectionPointId}
+            <ZoomActionElements
+              zoomIn={() => { onZoom(setTransform, true) }}
+              zoomOut={() => { onZoom(setTransform, false) }}
             />
-            <ArrowOverlay
-              cursorTrackerId={cursorTrackerId}
-              gateInputSizeThreshold={gateInputSizeThreshold}
-              getGateConnectionPointId={getGateConnectionPointId}
-              getGateInputId={getGateInputId}
-              getNodeConnectionPointId={getNodeConnectionPointId}
-            />
+            <div
+              style={styles.wrapper}
+              tabIndex={0}
+            >
+              <TransformComponent>
+                <Levels
+                  gateInputSizeThreshold={gateInputSizeThreshold}
+                  getGateConnectionPointId={getGateConnectionPointId}
+                  getGateId={getGateId}
+                  getGateInputId={getGateInputId}
+                  getNodeConnectionPointId={getNodeConnectionPointId}
+                />
+                <CursorTracker
+                  arrowAnchorPositions={arrowAnchorPositions}
+                  id={cursorTrackerId}
+                  getNodeConnectionPointId={getNodeConnectionPointId}
+                />
+                <ArrowOverlay
+                  cursorTrackerId={cursorTrackerId}
+                  gateInputSizeThreshold={gateInputSizeThreshold}
+                  getGateConnectionPointId={getGateConnectionPointId}
+                  getGateInputId={getGateInputId}
+                  getNodeConnectionPointId={getNodeConnectionPointId}
+                  scale={scale}
+                />
+              </TransformComponent>
+            </div>
           </Xwrapper>
-        </div>
-      </TransformComponent>
+        </>
+      )}
     </TransformWrapper>
   );
 }
 
-export default connect()(Canvas);
+export default Canvas;
