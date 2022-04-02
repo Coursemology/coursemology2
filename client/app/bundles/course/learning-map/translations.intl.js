8f378f12bcf5490edc8388b22331143aa533488f
Kevin Lim <lyskevin@gmail.com>
2022-04-03 00:45:00 +0800

    chore (learning map): add react-intl translations file

--- client/app/bundles/course/learning-map/translations.intl.js
+++ client/app/bundles/course/learning-map/translations.intl.js
@@ -9,6 +9,50 @@ const translations = defineMessages({
     id: 'course.learningMap.zoomOut',
     defaultMessage: 'Zoom Out',
   },
+  addCondition: {
+    id: 'course.learningMap.addCondition',
+    defaultMessage: 'Add condition to:',
+  },
+  summaryGateContent: {
+    id: 'course.learningMap.summaryGateContent',
+    defaultMessage: '{numerator}/{denominator}',
+  },
+  unlockLevel: {
+    id: 'course.learningMap.unlockLevel',
+    defaultMessage: 'Level {unlockLevel}',
+  },
+  unlockRate: {
+    id: 'course.learningMap.unlockRate',
+    defaultMessage: '{unlockRate}%',
+  },
+  conditionDeletionConfirmation: {
+    id: 'course.learningMap.conditionDeletionConfirmation',
+    defaultMessage: 'Are you sure that you want to delete this condition?',
+  },
+  toggleSatisfiabilityType: {
+    id: 'course.learningMap.toggleSatisfiabilityType',
+    defaultMessage: 'Toggle satisfiability type to {satisfiabilityType}',
+  },
+  deleteCondition: {
+    id: 'course.learningMap.deleteCondition',
+    defaultMessage: 'Delete this condition',
+  },
+  defaultDashboardMessage: {
+    id: 'course.learningMap.defaultDashboardMessage',
+    defaultMessage: 'Learning Map',
+  },
+  selectedArrowDashboardMessage: {
+    id: 'course.learningMap.selectedArrowDashboardMessage',
+    defaultMessage: 'Selected condition: {fromNode} --> {toNode}',
+  },
+  selectedGateDashboardMessage: {
+    id: 'course.learningMap.selectedGateDashboardMessage',
+    defaultMessage: 'Selected gate for: {node}',
+  },
+  responseDashboardMessage: {
+    id: 'course.learningMap.responseDashboardMessage',
+    defaultMessage: '{responseMessage}',
+  },
 });
 
 export default translations;
