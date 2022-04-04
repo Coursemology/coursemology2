67b450bee0f843da9d47b229c5c408e319142b7d
Hanming Zhu <hanming@huawen.com>
2022-04-04 16:23:35 +0800

    feat(user statistics): add client-side API

--- client/app/api/course/Statistics/index.ts
+++ client/app/api/course/Statistics/index.ts
@@ -1,7 +1,9 @@
 import CourseStatisticsAPI from './CourseStatistics';
+import UserStatisticsAPI from './UserStatistics';
 
 const StatisticsAPI = {
   course: new CourseStatisticsAPI(),
+  user: new UserStatisticsAPI(),
 };
 
 Object.freeze(StatisticsAPI);
