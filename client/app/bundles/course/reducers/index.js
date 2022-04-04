79a79fd73a74c76345210a9af227a879dfaac37e
Hanming Zhu <hanming@huawen.com>
2022-04-04 16:23:53 +0800

    feat(user statistics): add reducer

--- client/app/bundles/course/reducers/index.js
+++ client/app/bundles/course/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import notificationPopup from 'lib/reducers/notificationPopup';
 import userEmailSubscriptions from './userEmailSubscriptions';
+import userStatistics from './userStatistics';
 
 export default combineReducers({
   notificationPopup,
   userEmailSubscriptions,
+  userStatistics,
 });
