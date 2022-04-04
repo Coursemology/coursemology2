66446af08546dae5ec70246a1cae592ce42acfa0
Hanming Zhu <hanming@huawen.com>
2022-04-05 00:13:39 +0800

    feat(personalized timeline): add react-side API

--- client/app/api/course/Admin/index.js
+++ client/app/api/course/Admin/index.js
@@ -1,9 +1,11 @@
 import NotificationsAPI from './Notifications';
 import LessonPlanAPI from './LessonPlan';
+import PersonalizedTimelineAPI from './PersonalizedTimeline';
 
 const AdminAPI = {
   notifications: new NotificationsAPI(),
   lessonPlan: new LessonPlanAPI(),
+  personalizedTimeline: new PersonalizedTimelineAPI(),
 };
 
 Object.freeze(AdminAPI);
