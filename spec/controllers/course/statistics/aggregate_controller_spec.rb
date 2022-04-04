659249cdae781c0372a1f34c32ac331d35e980e4
Hanming Zhu <hanming@huawen.com>
2022-04-04 12:04:21 +0800

    tests(statistics): fix tests related to statistics controllers

---
+++ spec/controllers/course/statistics/aggregate_controller_spec.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe Course::Statistics::AggregateController, type: :controller do
+  let(:instance) { Instance.default }
+
+  with_tenant(:instance) do
+    let(:course) { create(:course, :enrollable) }
+    let(:course_user) { create(:course_user, course: course) }
+
+    describe '#all_staff' do
+      subject { get :all_staff, format: :json, params: { course_id: course, user_id: course_user } }
+
+      context 'when a Normal User pings the endpoint' do
+        let(:user) { create(:user) }
+        before { sign_in(user) }
+        it { expect { subject }.to raise_exception(CanCan::AccessDenied) }
+      end
+
+      context 'when a Course Student pings the endpoint' do
+        let(:user) { create(:course_student, course: course).user }
+        before { sign_in(user) }
+        it { expect { subject }.to raise_exception(CanCan::AccessDenied) }
+      end
+
+      context 'when a Course Teaching Assistant pings the endpoint' do
+        let(:user) { create(:course_teaching_assistant, course: course).user }
+        before { sign_in(user) }
+        it { expect(subject).to be_successful }
+      end
+
+      context 'when a Course Manager pings the endpoint' do
+        let(:user) { create(:course_manager, course: course).user }
+        before { sign_in(user) }
+        it { expect(subject).to be_successful }
+      end
+
+      context 'when a Course Observer pings the endpoint' do
+        let(:user) { create(:course_observer, course: course).user }
+        before { sign_in(user) }
+        it { expect(subject).to be_successful }
+      end
+    end
+
+    describe '#all_students' do
+      subject { get :all_students, format: :json, params: { course_id: course, user_id: course_user } }
+
+      context 'when a Normal User pings the endpoint' do
+        let(:user) { create(:user) }
+        before { sign_in(user) }
+        it { expect { subject }.to raise_exception(CanCan::AccessDenied) }
+      end
+
+      context 'when a Course Student pings the endpoint' do
+        let(:user) { create(:course_student, course: course).user }
+        before { sign_in(user) }
+        it { expect { subject }.to raise_exception(CanCan::AccessDenied) }
+      end
+
+      context 'when a Course Teaching Assistant pings the endpoint' do
+        let(:user) { create(:course_teaching_assistant, course: course).user }
+        before { sign_in(user) }
+        it { expect(subject).to be_successful }
+      end
+
+      context 'when a Course Manager pings the endpoint' do
+        let(:user) { create(:course_manager, course: course).user }
+        before { sign_in(user) }
+        it { expect(subject).to be_successful }
+      end
+
+      context 'when a Course Observer pings the endpoint' do
+        let(:user) { create(:course_observer, course: course).user }
+        before { sign_in(user) }
+        it { expect(subject).to be_successful }
+      end
+    end
+  end
+end
---
+++ spec/controllers/course/statistics/statistics_controller_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe Course::Statistics::StatisticsController, type: :controller do
+  let(:instance) { Instance.default }
+
+  with_tenant(:instance) do
+    let(:course) { create(:course, :enrollable) }
+    let(:course_user) { create(:course_user, course: course) }
+
+    describe '#index' do
+      subject { get :index, params: { course_id: course, user_id: course_user } }
+
+      context 'when a Normal User visits the page' do
+        let(:user) { create(:user) }
+        before { sign_in(user) }
+        it { expect { subject }.to raise_exception(CanCan::AccessDenied) }
+      end
+
+      context 'when a Course Student visits the page' do
+        let(:user) { create(:course_student, course: course).user }
+        before { sign_in(user) }
+        it { expect { subject }.to raise_exception(CanCan::AccessDenied) }
+      end
+
+      context 'when a Course Teaching Assistant visits the page' do
+        let(:user) { create(:course_teaching_assistant, course: course).user }
+        before { sign_in(user) }
+        it { expect(subject).to be_successful }
+      end
+
+      context 'when a Course Manager visits the page' do
+        let(:user) { create(:course_manager, course: course).user }
+        before { sign_in(user) }
+        it { expect(subject).to be_successful }
+      end
+
+      context 'when a Course Observer visits the page' do
+        let(:user) { create(:course_observer, course: course).user }
+        before { sign_in(user) }
+        it { expect(subject).to be_successful }
+      end
+    end
+  end
+end
