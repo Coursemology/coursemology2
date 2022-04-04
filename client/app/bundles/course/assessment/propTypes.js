2ec44fc96546a673bed7f8c7da90c1297ebe4448
Hanming Zhu <hanming@huawen.com>
2022-04-04 15:45:11 +0800

    feat(assessment statistics): add prop types

---
+++ client/app/bundles/course/assessment/propTypes.js
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+
+export const assessmentShape = PropTypes.shape({
+  startAt: PropTypes.object.isRequired,
+  endAt: PropTypes.object,
+  maximumGrade: PropTypes.number,
+});
+
+export const ancestorShape = PropTypes.shape({
+  id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+  title: PropTypes.string.isRequired,
+  courseTitle: PropTypes.string.isRequired,
+});
+
+// Used in the courseUsers array
+export const courseUserShape = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  role: PropTypes.oneOf([
+    'owner',
+    'manager',
+    'student',
+    'teaching_assistant',
+    'observer',
+  ]).isRequired,
+  isPhantom: PropTypes.bool.isRequired,
+});
+
+export const submissionRecordsShape = PropTypes.shape({
+  courseUser: courseUserShape,
+  submittedAt: PropTypes.object,
+  endAt: PropTypes.string,
+  grade: PropTypes.number,
+  dayDifference: PropTypes.number,
+});
