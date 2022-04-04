4c9be4041e917ac8c16905fc2e1a1c772b857f08
Hanming Zhu <hanming@huawen.com>
2022-04-04 15:46:15 +0800

    feat(assessment statistics): add actions

--- client/app/bundles/course/assessment/actions.js
+++ client/app/bundles/course/assessment/actions.js
@@ -2,6 +2,12 @@ import { SubmissionError } from 'lib/redux-form';
 import CourseAPI from 'api/course';
 import { getCourseId } from 'lib/helpers/url-helpers';
 import actionTypes from './constants';
+import {
+  processSubmission,
+  processAssessment,
+  processAncestor,
+  processCourseUser,
+} from './utils/statisticsUtils';
 
 export function createAssessment(
   categoryId,
@@ -74,6 +80,70 @@ export function updateAssessment(
         if (error.response && error.response.data) {
           throw new SubmissionError(error.response.data.errors);
         }
+      });
+  };
+}
+
+export function fetchStatistics(assessmentId, failureMessage) {
+  return (dispatch) => {
+    dispatch({ type: actionTypes.FETCH_STATISTICS_REQUEST });
+    return CourseAPI.statistics.assessment
+      .fetchStatistics(assessmentId)
+      .then((response) => {
+        dispatch({
+          type: actionTypes.FETCH_STATISTICS_SUCCESS,
+          assessment: processAssessment(response.data.assessment),
+          submissions: response.data.submissions.map(processSubmission),
+          allStudents: response.data.allStudents.map(processCourseUser),
+        });
+      })
+      .catch(() => {
+        dispatch({
+          type: actionTypes.FETCH_STATISTICS_FAILURE,
+          message: failureMessage,
+        });
+      });
+  };
+}
+
+export function fetchAncestors(assessmentId, failureMessage) {
+  return (dispatch) => {
+    dispatch({ type: actionTypes.FETCH_ANCESTORS_REQUEST });
+    return CourseAPI.statistics.assessment
+      .fetchAncestors(assessmentId)
+      .then((response) => {
+        dispatch({
+          type: actionTypes.FETCH_ANCESTORS_SUCCESS,
+          ancestors: response.data.assessments.map(processAncestor),
+        });
+      })
+      .catch(() => {
+        dispatch({
+          type: actionTypes.FETCH_ANCESTORS_FAILURE,
+          message: failureMessage,
+        });
+      });
+  };
+}
+
+export function fetchAncestorStatistics(ancestorId, failureMessage) {
+  return (dispatch) => {
+    dispatch({ type: actionTypes.FETCH_ANCESTOR_STATISTICS_REQUEST });
+    return CourseAPI.statistics.assessment
+      .fetchStatistics(ancestorId)
+      .then((response) => {
+        dispatch({
+          type: actionTypes.FETCH_ANCESTOR_STATISTICS_SUCCESS,
+          assessment: processAssessment(response.data.assessment),
+          submissions: response.data.submissions.map(processSubmission),
+          allStudents: response.data.allStudents.map(processCourseUser),
+        });
+      })
+      .catch(() => {
+        dispatch({
+          type: actionTypes.FETCH_ANCESTOR_STATISTICS_FAILURE,
+          message: failureMessage,
+        });
       });
   };
 }
