4fce24f64413f83059e6f6780463af85e6db586c
Hanming Zhu <hanming@huawen.com>
2022-04-04 09:55:33 +0800

    feat(statistics): add response parsers

---
+++ client/app/bundles/course/statistics/utils/parseStudentsResponse.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/prefer-default-export
+export const processStudent = (student) => ({
+  ...student,
+  level: parseInt(student.level ?? 0, 10),
+  experiencePoints: parseInt(student.experiencePoints ?? 0, 10),
+  videoSubmissionCount: parseInt(student.videoSubmissionCount ?? 0, 10),
+  videoPercentWatched: parseFloat(student.videoPercentWatched ?? 0),
+});
