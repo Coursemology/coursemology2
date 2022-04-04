75bd954b76811c9227ed7c6be201f8d789d62c13
Hanming Zhu <hanming@huawen.com>
2022-04-05 00:14:23 +0800

    feat(personalized timeline): add reducers

--- client/app/bundles/course/admin/reducers/index.js
+++ client/app/bundles/course/admin/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import notificationPopup from 'lib/reducers/notificationPopup';
 import notificationSettings from './notificationSettings';
 import lessonPlanSettings from './lessonPlanSettings';
+import personalizedTimelineSettings from './personalizedTimelineSettings';
 
 export default combineReducers({
   notificationPopup,
   notificationSettings,
   lessonPlanSettings,
+  personalizedTimelineSettings,
 });
