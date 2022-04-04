ee0478584d56cd8c7044fa706b17191b48314193
Hanming Zhu <hanming@huawen.com>
2022-04-04 15:45:56 +0800

    feat(assessment statistics): add reducers

--- client/app/bundles/course/assessment/reducers/index.js
+++ client/app/bundles/course/assessment/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import editPage from './editPage';
 import formDialog from './formDialog';
+import statisticsPage from './statisticsPage';
 
 export default combineReducers({
   formDialog,
   editPage,
+  statisticsPage,
 });
