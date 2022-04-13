296cd077b3b77b4bf363273ff1f4933e6969b76b
Hanming Zhu <hanming@huawen.com>
2022-04-13 10:05:40 +0800

    tests(duplication history): add tests for course and assessment models

--- spec/models/course/assessment_spec.rb
+++ spec/models/course/assessment_spec.rb
@@ -13,6 +13,9 @@ RSpec.describe Course::Assessment do
   it { is_expected.to have_many(:submissions).dependent(:destroy) }
   it { is_expected.to have_many(:conditions) }
   it { is_expected.to have_many(:assessment_conditions).dependent(:destroy) }
+  it { is_expected.to have_one(:duplication_traceable).dependent(:destroy) }
+
+  it { should delegate_method(:source).to(:duplication_traceable).allow_nil }
 
   let(:instance) { Instance.default }
   with_tenant(:instance) do
