3d9751e3c3c7275b8430ed6cde692761be155dbf
Hanming Zhu <hanming@huawen.com>
2022-04-18 22:42:08 +0800

    tests(learning rate records): add factory for learning rate record

---
+++ spec/factories/course_learning_rate_records.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+FactoryBot.define do
+  factory :learning_rate_record, class: Course::LearningRateRecord.name do
+    course_user
+    learning_rate { 1.0 }
+    effective_min { 0.5 }
+    effective_max { 2.0 }
+  end
+end
