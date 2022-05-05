ca20ef111461ff51dc7fcdaa048fafadb66d6ef4
Eko Widianto <ekowidianto@outlook.com>
2022-05-05 10:08:47 +0800

    feat(achievement refactoring): remove old achievement pages and views

--- client/app/bundles/course/achievement/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import editPage from './editPage';
-import indexFormDialog from './indexFormDialog';
-
-export default combineReducers({
-  indexFormDialog,
-  editPage,
-});
