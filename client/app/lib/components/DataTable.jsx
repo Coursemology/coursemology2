2240387fe31e37616210230b2989f24e65a755c3
Eko Widianto <ekowidianto@outlook.com>
2022-05-05 10:11:14 +0800

    feat: add datatable component and set up toastify

---
+++ client/app/lib/components/DataTable.jsx
@@ -0,0 +1,89 @@
+import { createTheme, ThemeProvider } from '@mui/material';
+import MUIDataTable from 'mui-datatables';
+import { adaptedTheme } from 'lib/components/ProviderWrapper';
+
+const options = {
+  filter: true,
+  filterType: 'dropdown',
+  responsive: 'standard',
+  fixedSelectColumn: false,
+};
+
+const theme = createTheme({
+  ...adaptedTheme,
+  components: {
+    ...adaptedTheme.components,
+    MUIDataTableHeadCell: {
+      styleOverrides: {
+        fixedHeader: {
+          zIndex: 0,
+        },
+      },
+    },
+    MuiTablePagination: {
+      styleOverrides: {
+        selectLabel: {
+          marginBottom: 0,
+        },
+        displayedRows: {
+          marginBottom: 0,
+        },
+      },
+    },
+  },
+});
+
+const processColumns = (includeRowNumber, columns) => {
+  if (!columns) {
+    return columns;
+  }
+  if (includeRowNumber) {
+    columns.unshift({
+      name: 'S/N',
+      options: {
+        sort: false,
+        filter: false,
+        customBodyRender: (value, meta) => meta.rowIndex + 1,
+      },
+    });
+  }
+
+  return columns.map((c) => {
+    if (c.options?.alignCenter) {
+      return {
+        ...c,
+        options: {
+          ...c.options,
+          setCellHeaderProps: () => ({
+            align: 'center',
+            className: 'centered-table-head',
+          }),
+          setCellProps: () => ({
+            align: 'center',
+          }),
+        },
+      };
+    }
+    return c;
+  });
+};
+
+/**
+ * Props for this component are identical to MUIDataTable's.
+ *
+ * Refer to https://github.com/gregnb/mui-datatables for the documentation.
+ */
+const DataTable = (props) => (
+  <ThemeProvider theme={theme}>
+    <MUIDataTable
+      {...props}
+      columns={processColumns(props.includeRowNumber, props.columns)}
+      elevation={1}
+      options={{ ...options, ...(props.options ?? {}) }}
+    />
+  </ThemeProvider>
+);
+
+DataTable.propTypes = MUIDataTable.propTypes;
+
+export default DataTable;
