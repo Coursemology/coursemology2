fc360be6cf6ca68ea48805d83f69d4241ef0bdc0
Jefferson <9080974+qreoct@users.noreply.github.com>
2022-06-01 10:01:55 +0800

    more data flow for indiv user

--- app/views/course/users/_user_data.json.jbuilder
+++ app/views/course/users/_user_data.json.jbuilder
@@ -3,9 +3,25 @@
 json.partial! 'user_list_data', course_user: course_user
 
 json.email course_user.user.email
-json.role course_user.role
-json.achievments course_user.achievements
-json.achivementCount course_user.achievement_count
+json.role CourseUser.human_attribute_name(course_user.role)
 
-json.experiencePointsRecordsUrl course_user_experience_points_records_path(current_course, @course_user)
-json.manageEmailSubscriptionUrl course_user_manage_email_subscription_path(current_course, @course_user)
+can_read_progress = can?(:read, Course::ExperiencePointsRecord.new(course_user: course_user))
+if can_read_progress
+  json.experiencePointsRecordsUrl course_user_experience_points_records_path(current_course, @course_user)
+  json.level course_user.level_number
+  json.exp course_user.experience_points
+end
+
+if can?(:manage, Course::UserEmailUnsubscription.new(course_user: course_user))
+  json.manageEmailSubscriptionUrl course_user_manage_email_subscription_path(current_course, @course_user)
+end
+
+unless current_component_host[:course_achievements_component].nil?
+  json.achivementCount course_user.achievement_count if @course_user.student? && current_course.gamified?
+  json.achievments course_user.achievements if @course_user.student? && @course_user.achievements.present?
+end
+
+all_skill_branches = @skills_service.skill_branches
+json.skills all_skill_branches if can_read_progress && all_skill_branches.present?
+# json.skills all_skill_branches.each do |skill_branch|
+# json.skillBranchTitle skill_branch.title
--- client/app/bundles/course/users/pages/UserShow/index.tsx
+++ client/app/bundles/course/users/pages/UserShow/index.tsx
@@ -1,9 +1,8 @@
 import { FC, ReactElement, useEffect, useState } from 'react';
-import { defineMessages, injectIntl } from 'react-intl';
+import { injectIntl } from 'react-intl';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import { Grid } from '@mui/material';
+import { Avatar } from '@mui/material';
 import LoadingIndicator from 'lib/components/LoadingIndicator';
 import { AppDispatch, AppState } from 'types/store';
 import { getUserEntity } from '../../selectors';
@@ -14,37 +13,36 @@ interface Props {
   intl?: any;
 }
 
-// const styles = {
-//   courseUserImage: {
-//     height: 75,
-//     width: 75,
-//     marginTop: '1em',
-//   },
-//   courseUserName: {
-//     paddingTop: '2em',
-//   },
-//   courseUserMiniEntity: {
-//     textDecoration: 'none',
-//   },
-// };
-
-const translations = defineMessages({
-  fetchUserFailure: {
-    id: 'course.users.index.fetch.failure',
-    defaultMessage: 'Failed to retrieve course user.',
+const styles = {
+  courseUserImage: {
+    height: 75,
+    width: 75,
   },
-});
+  // courseUserName: {
+  //   paddingTop: '2em',
+  // },
+  // courseUserMiniEntity: {
+  //   textDecoration: 'none',
+  // },
+};
+
+// const translations = defineMessages({
+//   fetchUserFailure: {
+//     id: 'course.users.index.fetch.failure',
+//     defaultMessage: 'Failed to retrieve course user.',
+//   },
+// });
 
 const UserShow: FC<Props> = (props) => {
   const { intl } = props;
   //   const courseId = getCourseId();
   const [isLoading, setIsLoading] = useState(true);
-  const user = useSelector((state: AppState) => getUserEntity(state));
+  const dispatch = useDispatch<AppDispatch>();
+  const { userId } = useParams();
+  const user = useSelector((state: AppState) => getUserEntity(state, +userId!));
   //   const achievementPermissions = useSelector((state: AppState) =>
   //     getAchievementPermissions(state),
   //   );
-  const dispatch = useDispatch<AppDispatch>();
-  const { userId } = useParams();
 
   useEffect(() => {
     if (userId) {
@@ -57,7 +55,7 @@ const UserShow: FC<Props> = (props) => {
   }
 
   if (!user) {
-    return toast.error(intl.formatMessage(translations.fetchUserFailure));
+    return null;
   }
 
   const headerToolbars: ReactElement[] = [];
@@ -92,19 +90,16 @@ const UserShow: FC<Props> = (props) => {
         })}
         toolbars={headerToolbars}
       />
-      <Grid container>
-        <span> id: {user.id} </span>
-        <span> email: {user.email} </span>
-        <span> name: {user.name} </span>
-        <span> role: {user.role} </span>
-        <img src={user.imageUrl} />
-        <a href={user.manageEmailSubscriptionUrl}>
-          {' '}
-          Manage email subscriptions{' '}
-        </a>
-        <a href={user.experiencePointsRecordsUrl}> Exp records </a>
-        <span> achivements: {user.achievementCount} </span>
-      </Grid>
+      {/* <Grid container> */}
+      <p> id: {user.id} </p>
+      <p> email: {user.email} </p>
+      <p> name: {user.name} </p>
+      <p> role: {user.role} </p>
+      <Avatar src={user.imageUrl} sx={styles.courseUserImage} />
+      <a href={user.manageEmailSubscriptionUrl}> Manage email subscriptions </a>
+      <a href={user.experiencePointsRecordsUrl}> Exp records </a>
+      <p> achivements: {user.achievementCount} </p>
+      {/* </Grid> */}
     </>
   );
 };
