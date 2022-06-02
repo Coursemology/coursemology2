01519f621e14da82d8fd037419a39b1df0760968
yoopie <sunyupei1999@gmail.com>
2022-06-02 15:03:10 +0800

    feat(courses): add course pagination

--- client/app/bundles/course/courses/components/CourseDisplay.tsx
+++ client/app/bundles/course/courses/components/CourseDisplay.tsx
@@ -1,5 +1,5 @@
 import { injectIntl } from 'react-intl';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Pagination, Grid } from '@mui/material';
 import { CoursesEntity } from 'types/course/courses';
 import CourseInfoBox from './CourseInfoBox';
@@ -10,24 +10,44 @@ interface Props {
 
 const CourseDisplay: FC<Props> = (props) => {
   const { courses } = props;
+
+  const ITEMS_PER_PAGE = 12;
+  const count = Math.ceil(courses.length / ITEMS_PER_PAGE);
+  const [page, setPage] = useState(1);
+  const [slicedCourses, setSlicedCorses] = useState(
+    courses.slice(0, ITEMS_PER_PAGE),
+  );
+
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  const handleChange = (_e: React.ChangeEvent<unknown>, pageNum: number) => {
+    setPage(pageNum);
+  };
+
+  useEffect(() => {
+    const begin = (page - 1) * ITEMS_PER_PAGE;
+    setSlicedCorses(courses.slice(begin, begin + ITEMS_PER_PAGE));
+  }, [page]);
+
   return (
     <>
+      {count > 1 && (
+        <Pagination
+          style={{ padding: 24, display: 'flex', justifyContent: 'center' }}
+          count={count}
+          page={page}
+          onChange={handleChange}
+        />
+      )}
       <Grid
         container
         spacing={{ xs: 2, md: 3 }}
         columns={{ xs: 1, sm: 2, md: 3, lg: 4 }}
         p={5}
       >
-        {courses.map((course: CoursesEntity) => (
+        {slicedCourses.map((course: CoursesEntity) => (
           <CourseInfoBox key={course.id} course={course} />
         ))}
       </Grid>
-      <Pagination
-        style={{ padding: 24, display: 'flex', justifyContent: 'center' }}
-        count={10}
-        showFirstButton
-        showLastButton
-      />
     </>
   );
 };
