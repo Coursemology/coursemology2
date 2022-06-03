9f4e8569205ecb8194f5628ea07f9b99447db057
leeroy999 <leeroy.whatsup@rocketmail.com>
2022-06-03 12:16:16 +0800

    test(leaderboard): add test capybara WIP

--- spec/features/course/leaderboard_viewing_spec.rb
+++ spec/features/course/leaderboard_viewing_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'Course: Leaderboard: View' do
       login_as(user, scope: :user)
     end
 
-    context 'As a student' do
+    context 'As a student', js: true do
       let!(:students) { create_list(:course_student, 2, course: course) }
       let!(:phantom_user) { create(:course_student, :phantom, course: course) }
       let(:user) { students[0].user }
@@ -19,14 +19,15 @@ RSpec.describe 'Course: Leaderboard: View' do
         create(:course_experience_points_record, points_awarded: 200, course_user: students[0])
         visit course_leaderboard_path(course)
 
-        within find('.leaderboard-level') do
+        within find('#leaderboard-level') do
           sorted_course_users = course.course_users.students.without_phantom_users.
                                 ordered_by_experience_points
 
-          sorted_course_users.each.with_index(1) do |student, index|
+          sorted_course_users.each.with_index(1) do |student|
             within find(content_tag_selector(student)) do
-              expect(page).to have_text(index)
-              expect(page).to have_text(I18n.t('course.leaderboards.show.level'))
+              expect(page).to have_text(student.name)
+              # expect(page).to have_text(index)
+              expect(page).to have_link(nil, href: course_user_path(course, student))
             end
           end
         end
@@ -55,7 +56,7 @@ RSpec.describe 'Course: Leaderboard: View' do
         expect(page).to have_no_content_tag_for(phantom_user)
       end
 
-      context 'when the group leaderboard is enabled for the course' do
+      context 'when the group leaderboard is enabled for the course', js: true do
         let!(:groups) { create_list(:course_group, 2, course: course) }
         let!(:group_user1) do
           create(:course_group_user, course: course, course_user: students[0], group: groups[0])
