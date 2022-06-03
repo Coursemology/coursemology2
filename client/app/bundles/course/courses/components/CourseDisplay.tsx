99ad4d330444d922f4b4c211f5f1f4c95ed5e781
yoopie <sunyupei1999@gmail.com>
2022-06-03 16:12:48 +0800

    refactor(pagination): refactor pagination
    Abstracted out pagination as a component for easy reuse

--- client/app/bundles/course/courses/components/CourseDisplay.tsx
+++ client/app/bundles/course/courses/components/CourseDisplay.tsx
@@ -1,7 +1,8 @@
 import { injectIntl } from 'react-intl';
-import { FC, useEffect, useState } from 'react';
-import { Pagination, Grid } from '@mui/material';
+import { FC, useState } from 'react';
+import { Grid } from '@mui/material';
 import { CoursesEntity } from 'types/course/courses';
+import Pagination from 'lib/components/Pagination';
 import CourseInfoBox from './CourseInfoBox';
 
 interface Props {
@@ -13,36 +14,18 @@ const CourseDisplay: FC<Props> = (props) => {
   // Ideally 24. Divisble by 2, 3 and 4. Also makes the pagination less awkward and causes less
   // UI jumps when the scroll bar of the webpage is rendered (when expanding descriptions)
   const ITEMS_PER_PAGE = 24;
-  const count = Math.ceil(courses.length / ITEMS_PER_PAGE);
+  const [slicedCourses, setSlicedCorses] = useState(courses);
   const [page, setPage] = useState(1);
-  const [slicedCourses, setSlicedCorses] = useState(
-    courses.slice(0, ITEMS_PER_PAGE),
-  );
-
-  const handleChange: (
-    _e: React.ChangeEvent<unknown>,
-    pageNum: number,
-  ) => void = (_e, pageNum) => {
-    setPage(pageNum);
-  };
-
-  useEffect(() => {
-    const begin = (page - 1) * ITEMS_PER_PAGE;
-    setSlicedCorses(courses.slice(begin, begin + ITEMS_PER_PAGE));
-  }, [page]);
 
   return (
     <>
-      {count > 1 && (
-        <Pagination
-          color="primary"
-          variant="outlined"
-          style={{ padding: 24, display: 'flex', justifyContent: 'center' }}
-          count={count}
-          page={page}
-          onChange={handleChange}
-        />
-      )}
+      <Pagination
+        items={courses}
+        itemsPerPage={ITEMS_PER_PAGE}
+        setSlicedItems={setSlicedCorses}
+        page={page}
+        setPage={setPage}
+      />
       <Grid
         // MUI applies default marginLeft: -16
         style={{ padding: 0 }}
@@ -54,16 +37,13 @@ const CourseDisplay: FC<Props> = (props) => {
           <CourseInfoBox key={course.id} course={course} />
         ))}
       </Grid>
-      {count > 1 && (
-        <Pagination
-          color="primary"
-          variant="outlined"
-          style={{ padding: 24, display: 'flex', justifyContent: 'center' }}
-          count={count}
-          page={page}
-          onChange={handleChange}
-        />
-      )}
+      <Pagination
+        items={courses}
+        itemsPerPage={ITEMS_PER_PAGE}
+        setSlicedItems={setSlicedCorses}
+        page={page}
+        setPage={setPage}
+      />
     </>
   );
 };
