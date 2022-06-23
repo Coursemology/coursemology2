5de251a33ac474a5bb5091ec831d6e8ffd8c4a5b
yoopie <sunyupei1999@gmail.com>
2022-06-23 12:57:52 +0800

    test(course-home-page): fix tests

--- spec/features/course/registration_spec.rb
+++ spec/features/course/registration_spec.rb
@@ -12,52 +12,54 @@ RSpec.feature 'Courses: Registration' do
     context 'when the course has unconfirmed invitations' do
       let!(:invitation) { create(:course_user_invitation, course: course) }
 
-      scenario 'Users can register course using invitation code' do
+      scenario 'Users can register course using invitation code', js: true do
         visit course_path(course)
 
-        # Submit with empty code
-        click_button I18n.t('course.user_registrations.registration.register')
-        expect(page).to have_selector('span.help-block')
-
-        fill_in 'registration_code', with: invitation.invitation_key
-        click_button I18n.t('course.user_registrations.registration.register')
-
+        # No code input
+        find('#register-button').click
         expect(page).
-          to have_selector('div.alert', text: I18n.t('course.user_registrations.create.registered'))
+          to have_selector('div.Toastify__toast-body', text: 'Please enter an invitation code')
+
+        # Enter wrong registration code
+        fill_in 'registration-code', with: 'defintielyTheWrongCode'
+        find('#register-button').click
+        expect(page).
+          to have_selector('div.Toastify__toast-body', text: 'Your code is incorrect')
+
+        # Correct code
+        fill_in 'registration-code', with: invitation.invitation_key
+        find('#register-button').click
+        # Go to home page of an enrolled user, which will always have an announcement header
+        expect(page).
+          to have_selector('h2', text: 'Announcements')
       end
     end
 
     context 'when the course allows enrol requests' do
       let(:course) { create(:course, :enrollable) }
 
-      scenario 'Users can create and cancel enrol requests', type: :mailer do
+      scenario 'Users can create and cancel enrol requests', js: true do
         visit course_path(course)
 
-        expect(page).to have_text(course.description)
+        expect(page).to have_selector('div#course-description', text: course.description)
 
         expect(ActionMailer::Base.deliveries.count).to eq(0)
-        click_link I18n.t('course.user_registrations.registration.new_enrol_request_button')
-        expect(page).
-          to have_selector('div.alert', text: I18n.t('course.enrol_requests.create.success'))
+        find('#submit-enrol-request-button').click
+        expect(page).to have_selector('div.Toastify__toast-body', text: 'Enrol request has been submitted')
         expect(ActionMailer::Base.deliveries.count).not_to eq(0)
 
         # Cancel request
-        click_link I18n.t('course.user_registrations.registration.deregister')
-        expect(page).
-          to have_selector('div.alert', text: I18n.t('course.enrol_requests.destroy.success'))
+        find('#cancel-enrol-request-button').click
+        expect(page).to have_selector('div.Toastify__toast-body', text: 'Enrol request has been cancelled')
       end
 
       context 'when the user has been enrolled' do
         let!(:enrolled_student) { create(:course_student, course: course, user: user) }
 
-        scenario 'user cannot de-register or re-register for the course' do
+        scenario 'user cannot de-register or re-register for the course', js: true do
           visit course_path(course)
-
-          new_request = I18n.t('course.user_registrations.registration.new_enrol_request_button')
-          expect(page).not_to have_link(new_request)
-
-          delete_request = I18n.t('course.user_registrations.registration.deregister')
-          expect(page).not_to have_link(delete_request)
+          expect(page).not_to have_selector('#submit-enrol-request-button')
+          expect(page).not_to have_selector('#cancel-enrol-request-button')
         end
       end
     end
