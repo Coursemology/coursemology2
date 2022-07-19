5cddc32a1b8a888221d88e65389f6856a7704935
Jefferson <9080974+qreoct@users.noreply.github.com>
2022-07-19 14:53:39 +0800

    perf(invitations): replace virtualization with datatable

---
+++ client/app/bundles/course/user-invitations/components/tables/InvitationResultUsersTable.tsx
@@ -0,0 +1,154 @@
+import { FC, memo } from 'react';
+import { injectIntl, WrappedComponentProps } from 'react-intl';
+import { Box, Typography } from '@mui/material';
+import DataTable from 'lib/components/DataTable';
+import { CourseUserData } from 'types/course/courseUsers';
+import sharedConstants from 'lib/constants/sharedConstants';
+import { TableColumns, TableOptions } from 'types/components/DataTable';
+import tableTranslations from 'lib/components/tables/translations';
+import equal from 'fast-deep-equal';
+
+interface Props extends WrappedComponentProps {
+  title: JSX.Element;
+  users: CourseUserData[];
+}
+
+const InvitationResultUsersTable: FC<Props> = (props) => {
+  const { title, users, intl } = props;
+
+  if (users && users.length === 0) {
+    return <></>;
+  }
+
+  const options: TableOptions = {
+    download: true,
+    filter: false,
+    pagination: true,
+    print: false,
+    rowsPerPage: 15,
+    rowsPerPageOptions: [15],
+    search: false,
+    selectableRows: 'none',
+    setTableProps: (): object => {
+      return { size: 'small' };
+    },
+    setRowProps: (_row, dataIndex, _rowIndex): Record<string, unknown> => {
+      return {
+        key: `invitation_result_user_${users[dataIndex].id}`,
+        userid: `invitation_result_user_${users[dataIndex].id}`,
+        className: `invitation_result_user invitation_result_user_${users[dataIndex].id}`,
+      };
+    },
+    sortOrder: {
+      name: 'name',
+      direction: 'asc',
+    },
+    viewColumns: false,
+  };
+
+  const columns: TableColumns[] = [
+    {
+      name: 'id',
+      label: intl.formatMessage(tableTranslations.id),
+      options: {
+        display: false,
+        filter: false,
+        sort: false,
+      },
+    },
+    {
+      name: 'name',
+      label: intl.formatMessage(tableTranslations.name),
+      options: {
+        alignCenter: false,
+        customBodyRenderLite: (dataIndex: number): JSX.Element => {
+          const user = users[dataIndex];
+          return (
+            <Typography
+              key={`name-${user.id}`}
+              className="invitation_result_user_name"
+              variant="body2"
+            >
+              {user.name}
+            </Typography>
+          );
+        },
+      },
+    },
+    {
+      name: 'email',
+      label: intl.formatMessage(tableTranslations.email),
+      options: {
+        alignCenter: false,
+        customBodyRenderLite: (dataIndex: number): JSX.Element => {
+          const user = users[dataIndex];
+          return (
+            <Typography
+              key={`email-${user.id}`}
+              className="invitation_result_user_email"
+              variant="body2"
+            >
+              {user.email}
+            </Typography>
+          );
+        },
+      },
+    },
+    {
+      name: 'phantom',
+      label: intl.formatMessage(tableTranslations.phantom),
+      options: {
+        customBodyRenderLite: (dataIndex: number): JSX.Element => {
+          const user = users[dataIndex];
+          return (
+            <Typography
+              key={`phantom-${user.id}`}
+              className="invitation_result_user_phantom"
+              variant="body2"
+            >
+              {user.phantom ? 'Yes' : 'No'}
+            </Typography>
+          );
+        },
+      },
+    },
+    {
+      name: 'role',
+      label: intl.formatMessage(tableTranslations.role),
+      options: {
+        alignCenter: false,
+        customBodyRenderLite: (dataIndex: number): JSX.Element => {
+          const user = users[dataIndex];
+          return (
+            <Typography
+              key={`role-${user.id}`}
+              className="invitation_result_user_role"
+              variant="body2"
+            >
+              {sharedConstants.COURSE_USER_ROLES[user.role]}
+            </Typography>
+          );
+        },
+      },
+    },
+  ];
+
+  return (
+    <Box sx={{ margin: '12px 0px' }}>
+      <DataTable
+        title={title}
+        data={users}
+        columns={columns}
+        options={options}
+        includeRowNumber
+      />
+    </Box>
+  );
+};
+
+export default memo(
+  injectIntl(InvitationResultUsersTable),
+  (prevProps, nextProps) => {
+    return equal(prevProps.users, nextProps.users);
+  },
+);
