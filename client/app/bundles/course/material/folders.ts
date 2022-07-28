fb7edd6a18a61efe436409f8ed41956be7f920b2
Eko Widianto <ekowidianto@outlook.com>
2022-07-28 11:32:28 +0800

    style(folders): remove unused comment

--- client/app/bundles/course/material/folders.ts
+++ client/app/bundles/course/material/folders.ts
@@ -1,7 +1,2 @@
-// import initializeDownloadLink from 'lib/helpers/initializeDownloadLink';
-
-// initializeDownloadLink('.btn.download');
-
 // This is the entry point for the folders (workbin) page
-
 import './folders/index';
