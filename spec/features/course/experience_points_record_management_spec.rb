095da37faf066c8cab83d3ccbe1c41e010a7ed45
leeroy999 <leeroy.whatsup@rocketmail.com>
2022-08-09 01:46:04 +0800

    fix(records): test cases

--- spec/features/course/experience_points_record_management_spec.rb
+++ spec/features/course/experience_points_record_management_spec.rb
@@ -20,7 +20,7 @@ RSpec.feature 'Courses: Experience Points Records: Management', js: true do
     let(:inactive_record) do
       create(:course_experience_points_record, :inactive, course_user: course_student)
     end
-    let(:records) { [record, manual_record, inactive_record] }
+    let!(:records) { [record, manual_record, inactive_record] }
 
     before { login_as(user, scope: :user) }
 
@@ -30,9 +30,9 @@ RSpec.feature 'Courses: Experience Points Records: Management', js: true do
       scenario "I can view a course student's active experience points records", js: true do
         visit course_user_experience_points_records_path(course, course_student)
 
-        expect(page).to have_content_tag_for(record)
-        expect(page).to have_content_tag_for(manual_record)
-        expect(page).to have_no_content_tag_for(inactive_record)
+        expect(page).to have_selector("#record-#{record.id}")
+        expect(page).to have_selector("#record-#{manual_record.id}")
+        expect(page).to have_no_selector("#record-#{inactive_record.id}")
         submission_path =
           edit_course_assessment_submission_path(course, submission.assessment, submission)
         expect(page).to have_link(submission.assessment.title, href: submission_path)
@@ -41,24 +41,22 @@ RSpec.feature 'Courses: Experience Points Records: Management', js: true do
       scenario "I can update a course student's active manually-awarded points records", js: true do
         visit course_user_experience_points_records_path(course, course_student)
 
-        within find(content_tag_selector(record)) do
-          expect(page).to have_field('experience_points_record_points_awarded')
-          expect(page).not_to have_field('experience_points_record_reason')
-          expect(page).to have_button('update')
+        within find("#record-#{record.id}") do
+          expect(page).to have_selector("#points-#{record.id}")
+          expect(page).not_to have_selector("#reason-#{record.id}")
+          expect(page).to have_selector(".record-save-#{record.id}")
         end
 
         updated_points = manual_record.points_awarded + 1
         updated_reason = 'updated reason'
-        within find(content_tag_selector(manual_record)) do
-          fill_in 'experience_points_record_points_awarded', with: updated_points
-          fill_in 'experience_points_record_reason', with: updated_reason
-          click_button 'update'
+        within find("#record-#{manual_record.id}") do
+          find("#points-#{manual_record.id}").set(updated_points)
+          find("#reason-#{manual_record.id}").set(updated_reason)
+          find("button.record-save-#{manual_record.id}").click
         end
 
-        wait_for_ajax
+        sleep 0.2
 
-        update_success_message = I18n.t('course.experience_points_records.update.success')
-        expect(page).to have_selector('div', text: update_success_message)
         expect(manual_record.reload.reason).to eq(updated_reason)
         expect(manual_record.points_awarded).to eq(updated_points)
       end
@@ -66,16 +64,12 @@ RSpec.feature 'Courses: Experience Points Records: Management', js: true do
       scenario "I can delete a course student's active manually-awarded points records", js: true do
         visit course_user_experience_points_records_path(course, course_student)
 
-        record_path =
-          course_user_experience_points_record_path(course, course_student, record)
-        manual_record_path =
-          course_user_experience_points_record_path(course, course_student, manual_record)
-
-        expect(page).not_to have_link(nil, href: record_path)
-        find_link(nil, href: manual_record_path).click
+        expect(page).not_to have_selector(".record-delete-#{record.id}")
+        find("button.record-delete-#{manual_record.id}").click
+        accept_confirm_dialog
         expect(current_path).
           to eq(course_user_experience_points_records_path(course, course_student))
-        expect(page).to have_no_content_tag_for(manual_record)
+        expect(page).to have_no_selector("#record-#{manual_record.id}")
       end
     end
 
@@ -85,15 +79,15 @@ RSpec.feature 'Courses: Experience Points Records: Management', js: true do
       scenario 'I can view my active experience points records', js: true do
         visit course_user_experience_points_records_path(course, course_student)
 
-        expect(page).to have_content_tag_for(record)
-        expect(page).to have_content_tag_for(manual_record)
-        expect(page).to have_no_content_tag_for(inactive_record)
+        expect(page).to have_selector("#record-#{record.id}")
+        expect(page).to have_selector("#record-#{manual_record.id}")
+        expect(page).to have_no_selector("#record-#{inactive_record.id}")
 
         # Can view experience points attributes but cannot edit the experience points record
-        within find(content_tag_selector(manual_record)) do
-          expect(page).not_to have_field('experience_points_record_points_awarded')
-          expect(page).not_to have_field('experience_points_record_reason')
-          expect(page).not_to have_button('update')
+        within find("#record-#{manual_record.id}") do
+          expect(page).not_to have_selector("#points-#{manual_record.id}")
+          expect(page).not_to have_selector("#reason-#{manual_record.id}")
+          expect(page).not_to have_selector(".record-save-#{manual_record.id}")
         end
       end
     end
