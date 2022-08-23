d09c4c165b50cda9b6de72de64c1066136fb64d4
Eko Widianto <ekowidianto@outlook.com>
2022-08-23 16:17:58 +0800

    test(components): fix tests

--- client/app/lib/components/buttons/__test__/DeleteButton.test.tsx
+++ client/app/lib/components/buttons/__test__/DeleteButton.test.tsx
@@ -4,10 +4,61 @@ import DeleteButton from '../DeleteButton';
 let documentBody: RenderResult;
 
 describe('<DeleteButton />', () => {
-  describe('when the delete button is rendered with confirmation dialog', () => {
+  describe('when the delete button is rendered without confirmation dialog', () => {
     beforeEach(() => {
       documentBody = render(
         <DeleteButton disabled={false} onClick={jest.fn()} loading={false} />,
+      );
+    });
+
+    it('shows the delete icon button', () => {
+      expect(documentBody.getByTestId('DeleteIconButton')).toBeVisible();
+      expect(documentBody.getByTestId('DeleteIcon')).toBeVisible();
+    });
+
+    it('does not show the confirmation dialog when clicked', () => {
+      // Before clicking
+      expect(documentBody.queryByTestId('ConfirmationDialog')).toBeNull();
+
+      fireEvent.click(screen.getByTestId('DeleteIconButton'));
+      // After clicking
+      expect(documentBody.queryByTestId('ConfirmationDialog')).toBeNull();
+    });
+
+    it('matches the snapshot', () => {
+      const { baseElement } = documentBody;
+      expect(baseElement).toMatchSnapshot();
+    });
+  });
+
+  describe('when the delete button is disabled', () => {
+    beforeEach(() => {
+      documentBody = render(
+        <DeleteButton
+          disabled
+          onClick={jest.fn()}
+          loading={false}
+          confirmMessage="Are you sure you wish to delete?"
+        />,
+      );
+    });
+
+    it('shows the delete icon button', () => {
+      expect(documentBody.getByTestId('DeleteIconButton')).toBeVisible();
+      expect(documentBody.getByTestId('DeleteIcon')).toBeVisible();
+      expect(documentBody.getByTestId('DeleteIconButton')).toBeDisabled();
+    });
+  });
+
+  describe('when the delete button is rendered with confirmation dialog', () => {
+    beforeEach(() => {
+      documentBody = render(
+        <DeleteButton
+          disabled={false}
+          onClick={jest.fn()}
+          loading={false}
+          confirmMessage="Are you sure you wish to delete?"
+        />,
       );
     });
 
@@ -30,31 +81,6 @@ describe('<DeleteButton />', () => {
       // After clicking cancel in the confirmation dialog, it is closed
       fireEvent.click(screen.getByText('Cancel'));
       expect(documentBody.queryByTestId('ConfirmationDialog')).toBeNull();
-    });
-  });
-
-  describe('when the delete button is rendered with disabled buttons in the confirmation dialog', () => {
-    beforeEach(() => {
-      documentBody = render(
-        <DeleteButton disabled onClick={jest.fn()} loading={false} />,
-      );
-    });
-
-    it('shows the delete icon button', () => {
-      expect(documentBody.getByTestId('DeleteIconButton')).toBeVisible();
-      expect(documentBody.getByTestId('DeleteIcon')).toBeVisible();
-    });
-
-    it('shows the confirmation dialog when clicked', () => {
-      // Before clicking delete button
-      expect(documentBody.queryByTestId('ConfirmationDialog')).toBeNull();
-
-      fireEvent.click(screen.getByTestId('DeleteIconButton'));
-      // After clicking delete button
-      expect(documentBody.getByTestId('ConfirmationDialog')).toBeVisible();
-
-      expect(documentBody.getByText('Continue')).toBeDisabled();
-      expect(documentBody.getByText('Cancel')).toBeDisabled();
     });
   });
 });
