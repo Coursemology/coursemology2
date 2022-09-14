a68873df6a2b016534c0d95c0356c5915443e547
Phillmont Muktar <phillmont@u.nus.edu>
2022-09-14 11:12:30 +0800

    chore(webpack): split dev and prod configurations

---
+++ client/webpack.prod.js
@@ -0,0 +1,17 @@
+const { merge } = require('webpack-merge');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'production',
+  output: {
+    filename: '[name]-[contenthash].js',
+    publicPath: '/webpack/',
+  },
+  plugins: [
+    new HardSourceWebpackPlugin({
+      cacheDirectory: join(__dirname, 'hard-source-cache/[confighash]'),
+    }),
+  ],
+});
