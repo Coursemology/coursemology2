da4033bee78653d15e79b8532c574548dff63c4d
Phillmont Muktar <phillmont@u.nus.edu>
2022-09-19 20:23:46 +0800

    feat(admin): add react comments settings
    sqt comments

---
+++ client/app/api/course/Admin/Comments.ts
@@ -0,0 +1,23 @@
+import { AxiosResponse } from 'axios';
+
+import type {
+  CommentsSettingsData,
+  CommentsSettingsPostData,
+} from 'types/course/admin/comments';
+import BaseAdminAPI from './Base';
+
+export default class CommentsAdminAPI extends BaseAdminAPI {
+  override _getUrlPrefix(): string {
+    return `${super._getUrlPrefix()}/comments`;
+  }
+
+  index(): Promise<AxiosResponse<CommentsSettingsData>> {
+    return this.getClient().get(this._getUrlPrefix());
+  }
+
+  update(
+    data: CommentsSettingsPostData,
+  ): Promise<AxiosResponse<CommentsSettingsData>> {
+    return this.getClient().patch(this._getUrlPrefix(), data);
+  }
+}
