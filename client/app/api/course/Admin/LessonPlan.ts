02832b5f1edeebb2c256dba9ee8f681d3928d6b9
Phillmont Muktar <phillmont@u.nus.edu>
2022-09-19 20:18:30 +0800

    feat(admin): lesson plan settings fetches via json

---
+++ client/app/api/course/Admin/LessonPlan.ts
@@ -0,0 +1,29 @@
+import { AxiosResponse } from 'axios';
+
+import type { LessonPlanSettings } from 'types/course/admin/lessonPlan';
+import BaseAdminAPI from './Base';
+
+export default class LessonPlanSettingsAPI extends BaseAdminAPI {
+  override _getUrlPrefix(): string {
+    return `${super._getUrlPrefix()}/lesson_plan`;
+  }
+
+  index(): Promise<AxiosResponse<LessonPlanSettings>> {
+    return this.getClient().get(this._getUrlPrefix());
+  }
+
+  /**
+   * Update a lesson plan setting.
+   *
+   * @param {object} params
+   *   - params in the format of
+   *     { lesson_plan_settings: { lesson_plan_item_settings: { :component, :key, :enabled, :options } } }
+   *
+   * @return {Promise}
+   * success response: {}
+   * error response: {}
+   */
+  update(params): Promise<AxiosResponse<LessonPlanSettings>> {
+    return this.getClient().patch(this._getUrlPrefix(), params);
+  }
+}
