c971e7cec4aae556cd163b2c739a105dd70d8899
Phillmont Muktar <phillmont@u.nus.edu>
2022-09-19 20:24:28 +0800

    feat(admin): add react materials settings

---
+++ client/app/api/course/Admin/Materials.ts
@@ -0,0 +1,23 @@
+import { AxiosResponse } from 'axios';
+
+import type {
+  MaterialsSettingsData,
+  MaterialsSettingsPostData,
+} from 'types/course/admin/materials';
+import BaseAdminAPI from './Base';
+
+export default class MaterialsAdminAPI extends BaseAdminAPI {
+  override _getUrlPrefix(): string {
+    return `${super._getUrlPrefix()}/materials`;
+  }
+
+  index(): Promise<AxiosResponse<MaterialsSettingsData>> {
+    return this.getClient().get(this._getUrlPrefix());
+  }
+
+  update(
+    data: MaterialsSettingsPostData,
+  ): Promise<AxiosResponse<MaterialsSettingsData>> {
+    return this.getClient().patch(this._getUrlPrefix(), data);
+  }
+}
---
+++ client/app/bundles/course/admin/pages/MaterialsSettings/translations.ts
@@ -0,0 +1,8 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  materialsSettings: {
+    id: 'course.admin.materials.materialsSettings',
+    defaultMessage: 'Materials settings',
+  },
+});
