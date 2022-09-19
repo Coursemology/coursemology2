a09dc11c26d3edec13ea1a901e2442f559089217
Phillmont Muktar <phillmont@u.nus.edu>
2022-09-19 20:25:43 +0800

    feat(admin): add react course settings

---
+++ client/app/api/course/Admin/Course.ts
@@ -0,0 +1,37 @@
+import { AxiosResponse } from 'axios';
+
+import type {
+  CourseInfo,
+  CourseAdminOptions,
+  CourseInfoPostData,
+  TimeZones,
+} from 'types/course/admin/course';
+import BaseAdminAPI from './Base';
+
+export default class CourseAdminAPI extends BaseAdminAPI {
+  index(): Promise<AxiosResponse<CourseInfo>> {
+    return this.getClient().get(this._getUrlPrefix());
+  }
+
+  timeZones(): Promise<AxiosResponse<TimeZones>> {
+    return this.getClient().get(`${this._getUrlPrefix()}/time_zones`);
+  }
+
+  options(): Promise<AxiosResponse<CourseAdminOptions>> {
+    return this.getClient().get(`${this._getUrlPrefix()}/options`);
+  }
+
+  update(data: CourseInfoPostData): Promise<AxiosResponse<CourseInfo>> {
+    return this.getClient().patch(this._getUrlPrefix(), data);
+  }
+
+  updateLogo(image: File): Promise<AxiosResponse<CourseInfo>> {
+    const formData = new FormData();
+    formData.append('course[logo]', image);
+    return this.getClient().patch(this._getUrlPrefix(), formData);
+  }
+
+  delete(): Promise<AxiosResponse> {
+    return this.getClient().delete(this._getUrlPrefix());
+  }
+}
---
+++ client/app/types/course/admin/course.ts
@@ -0,0 +1,46 @@
+export interface CourseInfo {
+  title: string;
+  description: string;
+  logo: string;
+  published: boolean;
+  enrollable: boolean;
+  startAt: string;
+  endAt: string;
+  gamified: boolean;
+  showPersonalizedTimelineFeatures: boolean;
+  defaultTimelineAlgorithm: 'fixed' | 'fomo' | 'stragglers' | 'otot';
+  timeZone: string;
+  advanceStartAtDurationDays: number;
+}
+
+export interface CourseAdminOption {
+  title: string;
+  weight: number;
+  path: string;
+}
+
+export interface CourseInfoPostData {
+  course: {
+    title?: CourseInfo['title'];
+    description?: CourseInfo['description'];
+    published?: CourseInfo['published'];
+    enrollable?: CourseInfo['enrollable'];
+    start_at?: CourseInfo['startAt'];
+    end_at?: CourseInfo['endAt'];
+    logo?: CourseInfo['logo'];
+    gamified?: CourseInfo['gamified'];
+    show_personalized_timeline_features?: CourseInfo['showPersonalizedTimelineFeatures'];
+    default_timeline_algorithm?: CourseInfo['defaultTimelineAlgorithm'];
+    time_zone?: CourseInfo['timeZone'];
+    advance_start_at_duration_days?: CourseInfo['advanceStartAtDurationDays'];
+  };
+}
+
+export type CourseAdminOptions = CourseAdminOption[];
+
+export interface TimeZone {
+  name: string;
+  displayName: string;
+}
+
+export type TimeZones = TimeZone[];
