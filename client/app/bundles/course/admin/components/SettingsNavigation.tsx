c69921c6b67b312209a3937265a0ee791dc7e56c
Phillmont Muktar <phillmont@u.nus.edu>
2022-09-19 20:27:17 +0800

    feat(admin): add react main course settings entry point

---
+++ client/app/bundles/course/admin/components/SettingsNavigation.tsx
@@ -0,0 +1,62 @@
+import { createContext, Suspense, useContext, useState } from 'react';
+import { Chip } from '@mui/material';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+
+import CourseAPI from 'api/course';
+import { CourseAdminOptions } from 'types/course/admin/course';
+import LoadingIndicator from 'lib/components/LoadingIndicator';
+import suspend from 'lib/hooks/suspended';
+
+const fetchOptions = async (): Promise<CourseAdminOptions> => {
+  const response = await CourseAPI.admin.course.options();
+  return response.data;
+};
+
+const resource = suspend(fetchOptions());
+
+const OptionsReloaderContext = createContext(() => {});
+
+export const useOptionsReloader = (): (() => void) =>
+  useContext(OptionsReloaderContext);
+
+const SettingsNavigationLayout = (): JSX.Element => {
+  const [options, setOptions] = useState(resource.read());
+
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  const reloadOptions = (): void => {
+    fetchOptions().then(setOptions);
+  };
+
+  return (
+    <OptionsReloaderContext.Provider value={reloadOptions}>
+      <div className="flex flex-col">
+        <div className="-m-2 pb-10">
+          {options.map(({ title, path }) => (
+            <Chip
+              key={path}
+              label={title}
+              onClick={(): void => navigate(path)}
+              className={`m-2 ${path === pathname && 'p-[1px]'}`}
+              clickable={path !== pathname}
+              variant={path === pathname ? 'filled' : 'outlined'}
+            />
+          ))}
+        </div>
+
+        <Outlet />
+      </div>
+    </OptionsReloaderContext.Provider>
+  );
+};
+
+const SettingsNavigation = (): JSX.Element => {
+  return (
+    <Suspense fallback={<LoadingIndicator />}>
+      <SettingsNavigationLayout />
+    </Suspense>
+  );
+};
+
+export default SettingsNavigation;
