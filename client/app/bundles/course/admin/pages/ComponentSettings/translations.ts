38155d7ed10d99e048ab0265c8ab0b0d60eb9826
Phillmont Muktar <phillmont@u.nus.edu>
2022-09-19 20:24:02 +0800

    feat(admin): add react components settings

---
+++ client/app/bundles/course/admin/pages/ComponentSettings/translations.ts
@@ -0,0 +1,12 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  componentSettings: {
+    id: 'course.admin.component.componentSettings',
+    defaultMessage: 'Components settings',
+  },
+  componentSettingsSubtitle: {
+    id: 'course.admin.component.componentSettingsSubtitle',
+    defaultMessage: 'Turn Coursemology features in this course on or off.',
+  },
+});
