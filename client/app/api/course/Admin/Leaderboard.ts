a14ade3cef4a8ab6ee3e9bc124151d72ae6abc92
Phillmont Muktar <phillmont@u.nus.edu>
2022-09-19 20:25:26 +0800

    feat(admin): add react leaderboard settings

---
+++ client/app/api/course/Admin/Leaderboard.ts
@@ -0,0 +1,24 @@
+import { AxiosResponse } from 'axios';
+
+import {
+  LeaderboardSettingsData,
+  LeaderboardSettingsPostData,
+} from 'types/course/admin/leaderboard';
+
+import BaseAdminAPI from './Base';
+
+export default class LeaderboardAdminAPI extends BaseAdminAPI {
+  override _getUrlPrefix(): string {
+    return `${super._getUrlPrefix()}/leaderboard`;
+  }
+
+  index(): Promise<AxiosResponse<LeaderboardSettingsData>> {
+    return this.getClient().get(this._getUrlPrefix());
+  }
+
+  update(
+    data: LeaderboardSettingsPostData,
+  ): Promise<AxiosResponse<LeaderboardSettingsData>> {
+    return this.getClient().patch(this._getUrlPrefix(), data);
+  }
+}
