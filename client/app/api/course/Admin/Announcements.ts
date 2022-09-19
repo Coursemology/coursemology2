347da08dc0c325ad64060034cee24780520e4e4d
Phillmont Muktar <phillmont@u.nus.edu>
2022-09-19 20:23:15 +0800

    feat(admin): add react announcements settings

---
+++ client/app/api/course/Admin/Announcements.ts
@@ -0,0 +1,23 @@
+import { AxiosResponse } from 'axios';
+
+import type {
+  AnnouncementsSettingsData,
+  AnnouncementsSettingsPostData,
+} from 'types/course/admin/announcements';
+import BaseAdminAPI from './Base';
+
+export default class AnnouncementsAdminAPI extends BaseAdminAPI {
+  override _getUrlPrefix(): string {
+    return `${super._getUrlPrefix()}/announcements`;
+  }
+
+  index(): Promise<AxiosResponse<AnnouncementsSettingsData>> {
+    return this.getClient().get(this._getUrlPrefix());
+  }
+
+  update(
+    data: AnnouncementsSettingsPostData,
+  ): Promise<AxiosResponse<AnnouncementsSettingsData>> {
+    return this.getClient().patch(this._getUrlPrefix(), data);
+  }
+}
