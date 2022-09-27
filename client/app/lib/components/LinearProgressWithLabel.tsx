049d59f17c0887bf33ce1de8429325e5b8735552
Eko Widianto <ekowidianto@outlook.com>
2022-09-27 13:06:48 +0800

    fix(linear progress with label): display value as zero when it is empty

--- client/app/lib/components/LinearProgressWithLabel.tsx
+++ client/app/lib/components/LinearProgressWithLabel.tsx
@@ -12,7 +12,7 @@ const LinearProgressWithLabel: FC<Props> = (props: Props) => {
   return (
     <Box sx={{ display: 'flex', alignItems: 'center' }}>
       <Box sx={{ width: '100%', mr: 1 }}>
-        <LinearProgress variant="determinate" {...props} />
+        <LinearProgress variant="determinate" value={props.value ?? 0} />
       </Box>
       <Box sx={{ minWidth: 35 }}>
         <Typography variant="body2" color="text.secondary">{`${Math.round(
