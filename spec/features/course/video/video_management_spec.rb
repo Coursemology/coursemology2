982c992549702e1660665e045bc9ba75cf041c8a
Eko Widianto <ekowidianto@outlook.com>
2022-09-27 13:28:22 +0800

    test(react video port): fix tests

--- spec/features/course/video/video_management_spec.rb
+++ spec/features/course/video/video_management_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.feature 'Course: Videos: Management' do
+RSpec.feature 'Course: Videos: Management', js: true do
   let(:instance) { create(:instance, :with_video_component_enabled) }
 
   with_tenant(:instance) do
@@ -15,47 +15,49 @@ RSpec.feature 'Course: Videos: Management' do
         video = build_stubbed(:video)
         visit course_videos_path(course)
 
-        click_link(nil, href: new_course_video_path(course, tab: course.default_video_tab))
-        expect(page).to have_selector('h1', text: I18n.t('course.video.videos.new.header'))
+        find('button.new-video-button').click
+        expect(page).to have_selector('h2', text: 'New Video')
+        fill_in 'title', with: video.title
+        fill_in_react_ck 'textarea[name=description]', video.description
 
-        fill_in 'video_title', with: video.title
-        select 'default', from: 'video_tab_id'
-        fill_in 'video_description', with: video.description
-        fill_in 'video_start_at', with: video.start_at
-        fill_in 'video_url', with: video.url
+        find_field('startAt').click.set(video.start_at.strftime('%d/%m/%Y %I:%M'))
 
-        click_button 'submit'
+        fill_in 'url', with: video.url
 
-        expect(current_path).to eq(course_videos_path(course))
-        expect(page).to have_selector('div.alert.alert-success')
+        expect do
+          find('button.btn-submit').click
+          expect(page).not_to have_selector('h2', text: 'New Video')
+        end.to change { course.videos.count }.by(1)
+
+        expect_toastify("#{video.title} was created.")
 
         video_created = course.videos.last
-        expect(page).to have_content_tag_for(video_created)
+        expect(page).to have_selector("tr.video_#{video_created.id}")
       end
 
       scenario 'I can edit or delete a video from the videos page' do
         unpublished_video = create(:video, course: course)
-        edit_path = edit_course_video_path(course, unpublished_video)
 
         # Edit video Page
         visit course_videos_path(course)
-        within find(content_tag_selector(unpublished_video)) do
-          find('.btn.btn-default.edit').click
-        end
-        expect(current_path).to eq(edit_path)
+        find("button.video-edit-#{unpublished_video.id}").click
+
         new_title = 'zzz'
 
-        fill_in 'video_title', with: new_title
-        click_button 'submit'
+        fill_in 'title', with: new_title
+        find('.btn-submit').click
+
+        expect_toastify("#{new_title} has been successfully updated.")
 
         expect(unpublished_video.reload.title).to eq(new_title)
-        expect(current_path).to eq(course_videos_path(course))
+        within find("tr.video_#{unpublished_video.id}") do
+          expect(page).to have_text(new_title)
+        end
 
         # Delete video
         expect do
-          within find(content_tag_selector(unpublished_video)) do
-            find('.btn.btn-danger.delete').click
-          end
+          find("button.video-delete-#{unpublished_video.id}").click
+          accept_confirm_dialog
         end.to change { course.videos.count }.by(-1)
       end
     end
