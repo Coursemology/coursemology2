27ad56d4225ecaf85a119b95b6ab47a746eef9b7
Eko Widianto <ekowidianto@outlook.com>
2022-09-29 10:36:46 +0800

    refactor(materials): remove unused files

--- lib/extensions/materials/action_view/helpers/form_builder.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-module Extensions::Materials::ActionView::Helpers::FormBuilder
-  # Hepler to support f.folder in form
-  def folder
-    @template.render 'layouts/materials_uploader', f: self
-  end
-end
