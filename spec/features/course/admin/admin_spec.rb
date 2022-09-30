958dd40cd241836a3ce2ab39286fdcea61895733
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2022-09-30 21:06:26 +0800

    test(admin): adapt to new ui

--- spec/features/course/admin/admin_spec.rb
+++ spec/features/course/admin/admin_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.feature 'Course: Administration: Administration' do
+RSpec.feature 'Course: Administration: Administration', js: true do
   subject { page }
 
   let(:instance) { Instance.default }
@@ -19,59 +19,73 @@ RSpec.feature 'Course: Administration: Administration' do
         expect(page).to have_selector('li', text: 'layouts.course_admin.title')
       end
 
-      scenario 'I can view the course settings page' do
-        visit course_admin_path(course)
-
-        expect(page).to have_selector('h1', text: 'layouts.course_admin.title')
-      end
-
       scenario 'I can change the course attributes' do
         visit course_admin_path(course)
 
-        fill_in 'course_title', with: ''
-        click_button I18n.t('helpers.submit.course.update')
-        expect(course.reload.title).not_to eq('')
-        expect(page).to have_selector('div.has-error')
+        fill_in 'title', with: ''
+        click_button 'Save changes'
+        expect(course.reload.title).not_to be_empty
+        expect(page).to have_text('Course name is required')
 
         new_title = 'New Title'
         new_description = 'New Description'
-        fill_in 'course_title',          with: new_title
-        fill_in 'course_description',    with: new_description
-        attach_file :course_logo, File.join(Rails.root, '/spec/fixtures/files/picture.jpg')
+        fill_in 'title', with: new_title
+        fill_in_react_ck 'textarea[name="description"]', new_description
 
-        click_button I18n.t('helpers.submit.course.update')
+        click_button 'Save changes'
+        expect_toastify('Your changes have been saved.')
 
-        expect(page).to have_selector('div.alert.alert-success')
-        expect(page).to have_content(course.logo.medium.url) # TODO: This test is checking for nil right now.
         expect(course.reload.title).to eq(new_title)
-        expect(course.reload.description).to eq(new_description)
+        expect(course.reload.description).to include(new_description)
       end
 
-      scenario 'I can enable/disable self directed learning', js: true do
+      scenario 'I can change the course logo' do
+        visit course_admin_path(course)
+        logo = File.join(Rails.root, '/spec/fixtures/files/picture.jpg')
+
+        attach_file(logo) do
+          find('label', text: 'Choose a new image', visible: false).click
+        end
+
+        click_button 'Upload image'
+        sleep 0.5
+        expect_toastify('The new course logo was successfully uploaded.')
+
+        visit current_path
+
+        course_logo = find('#course-sidebar-logo img')
+        expect(course_logo[:src]).to include(course.reload.logo.medium.url)
+      end
+
+      scenario 'I can enable/disable self directed learning' do
         visit course_admin_path(course)
 
         days = 10
-        check 'self_directed_learning'
-        fill_in 'course_advance_start_at_duration_days', with: days
+        advance_start_at_duration_field = 'advanceStartAtDurationDays'
+        fill_in advance_start_at_duration_field, with: days
 
-        click_button 'submit'
+        click_button 'Save changes'
 
-        expect(page).to have_selector('div.alert-success')
+        expect_toastify('Your changes have been saved.')
         expect(course.reload.advance_start_at_duration).to be_within(1.hour).of(days.days)
 
-        uncheck 'self_directed_learning'
-        click_button 'submit'
+        fill_in advance_start_at_duration_field, with: ''
+        click_button 'Save changes'
 
-        expect(page).to have_selector('div.alert-success')
+        expect_toastify('Your changes have been saved.')
         expect(course.reload.advance_start_at_duration).to eq 0
       end
 
       scenario 'I can delete the course' do
         visit course_admin_path(course)
 
-        expect { click_link(I18n.t('course.admin.admin.index.delete.button')) }.
-          to change(instance.courses, :count).by(-1)
-        expect(page).to have_selector('div', text: I18n.t('course.admin.admin.destroy.success'))
+        expect_delete_action = expect do
+          click_button('Delete this course')
+          click_button('Delete course')
+          expect(page).to have_current_path(courses_path)
+        end
+
+        expect_delete_action.to change(instance.courses, :count).by(-1)
       end
     end
 
