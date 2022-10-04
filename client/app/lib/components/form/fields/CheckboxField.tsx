2908c44bac3e0fce047eb48429783c42c78b531f
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2022-10-04 20:01:41 +0800

    refactor(checkboxfield): abstract checkbox to lib/components

--- client/app/lib/components/form/fields/CheckboxField.tsx
+++ client/app/lib/components/form/fields/CheckboxField.tsx
@@ -1,95 +1,27 @@
-import { memo } from 'react';
-import {
-  Box,
-  Checkbox,
-  FormControlLabel,
-  FormHelperText,
-  Typography,
-} from '@mui/material';
+import { memo, ComponentProps } from 'react';
 import { ControllerFieldState } from 'react-hook-form';
 
+import Checkbox from 'lib/components/Checkbox';
 import { formatErrorMessage } from './utils/mapError';
 import propsAreEqual from './utils/propsAreEqual';
 
-interface Props {
+type FormCheckboxFieldProps = ComponentProps<typeof Checkbox> & {
   // react-hook-form ControllerRenderProps requires generics for field
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   field: any;
   fieldState: ControllerFieldState;
-  disabled?: boolean;
-  label?: string;
-  renderIf?: boolean;
-  icon?: JSX.Element;
-  checkedIcon?: JSX.Element;
-  description?: string;
-  disabledHint?: string | JSX.Element;
-}
-
-const styles = {
-  description: {
-    marginLeft: '30px',
-  },
-  disabledHint: {
-    marginLeft: '30px',
-    marginTop: '0.5rem',
-  },
 };
 
-const FormCheckboxField = (props: Props): JSX.Element => {
-  const {
-    field,
-    fieldState,
-    disabled,
-    label,
-    renderIf = true,
-    icon,
-    checkedIcon,
-    description,
-    disabledHint,
-  } = props;
-
-  const isError = !!fieldState.error;
-  if (!renderIf) return <></>;
+const FormCheckboxField = (props: FormCheckboxFieldProps): JSX.Element => {
+  const { field, fieldState, ...checkboxProps } = props;
 
   return (
-    <Box width="100%">
-      <FormControlLabel
-        control={
-          <Checkbox
-            {...field}
-            checked={field.value}
-            color="primary"
-            onChange={field.onChange}
-            className="py-0 px-4"
-            icon={icon}
-            checkedIcon={checkedIcon}
-          />
-        }
-        disabled={disabled}
-        label={label}
-        className="mb-0"
-      />
-
-      {description && (
-        <Typography
-          variant="body2"
-          className="ml-[34px]"
-          color={disabled ? 'text.disabled' : 'text.secondary'}
-        >
-          {description}
-        </Typography>
-      )}
-
-      {disabledHint && disabled && (
-        <div style={styles.disabledHint}>{disabledHint}</div>
-      )}
-
-      {isError && (
-        <FormHelperText error={isError} sx={styles.description}>
-          {formatErrorMessage(fieldState.error?.message)}
-        </FormHelperText>
-      )}
-    </Box>
+    <Checkbox
+      {...field}
+      checked={field.value}
+      error={formatErrorMessage(fieldState.error?.message)}
+      {...checkboxProps}
+    />
   );
 };
 
