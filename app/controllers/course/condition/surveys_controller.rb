525ef38de07b93d500eaf4de5eff902d948ade2f
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2022-10-04 19:49:01 +0800

    feat(condition): responds to json

--- app/controllers/course/condition/surveys_controller.rb
+++ app/controllers/course/condition/surveys_controller.rb
@@ -4,37 +4,27 @@ class Course::Condition::SurveysController < Course::ConditionsController
   before_action :set_course_and_conditional, only: [:new, :create]
   authorize_resource :survey_condition, class: Course::Condition::Survey.name
 
-  def new
-  end
-
   def create
-    if @survey_condition.save
-      redirect_to return_to_path, success: t('course.condition.surveys.create.success')
-    else
-      render 'new'
-    end
-  end
-
-  def edit
+    try_to_perform @survey_condition.save
   end
 
   def update
-    if @survey_condition.update(survey_condition_params)
-      redirect_to return_to_path, success: t('course.condition.surveys.update.success')
-    else
-      render 'edit'
-    end
+    try_to_perform @survey_condition.update(survey_condition_params)
   end
 
   def destroy
-    if @survey_condition.destroy
-      redirect_to return_to_path, success: t('course.condition.surveys.destroy.success')
-    else
-      redirect_to return_to_path, danger: t('course.condition.surveys.destroy.error')
-    end
+    try_to_perform @survey_condition.destroy
   end
 
   private
+
+  def try_to_perform(operation_succeeded)
+    if operation_succeeded
+      success_action
+    else
+      render json: { errors: @survey_condition.errors }, status: :bad_request
+    end
+  end
 
   def survey_condition_params
     params.require(:condition_survey).permit(:survey_id)
