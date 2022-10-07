194baa5fc82e0040704114cec3de2c171811fcbe
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2022-10-07 17:45:59 +0800

    fix(textfield): number fields keyboard inconsistencies

---
+++ client/app/lib/components/NumberTextField.tsx
@@ -0,0 +1,49 @@
+import { isNaN } from 'lodash';
+import {
+  ChangeEvent,
+  ChangeEventHandler,
+  ComponentProps,
+  FocusEvent,
+  FocusEventHandler,
+} from 'react';
+
+import TextField from './TextField';
+
+type NumberTextFieldProps = Omit<
+  ComponentProps<typeof TextField>,
+  'onChange' | 'onBlur'
+> & {
+  onChange?: (event: ChangeEvent<HTMLInputElement>, value: '' | number) => void;
+  onBlur?: (event: FocusEvent<HTMLInputElement>, value: number) => void;
+};
+
+const isValidInteger = (value: number): boolean =>
+  !isNaN(value) &&
+  value >= Number.MIN_SAFE_INTEGER &&
+  value <= Number.MAX_SAFE_INTEGER;
+
+const NumberTextField = (props: NumberTextFieldProps): JSX.Element => {
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e): void => {
+    const numberValue = parseInt(e.target.value, 10);
+    if (isValidInteger(numberValue)) props.onChange?.(e, numberValue);
+    if (isNaN(numberValue)) props.onChange?.(e, '');
+  };
+
+  const handleBlur: FocusEventHandler<HTMLInputElement> = (e): void => {
+    const numberValue = parseInt(e.target.value, 10) || 0;
+    if (isValidInteger(numberValue)) props.onChange?.(e, numberValue);
+  };
+
+  // type="number" is highly inconsistent across browsers. See below:
+  // https://stackoverflow.blog/2022/09/15/why-the-number-input-is-the-worst-input/
+  return (
+    <TextField
+      {...props}
+      type="text"
+      onChange={handleChange}
+      onBlur={handleBlur}
+    />
+  );
+};
+
+export default NumberTextField;
--- client/app/lib/components/form/fields/TextField.tsx
+++ client/app/lib/components/form/fields/TextField.tsx
@@ -1,23 +1,13 @@
-import {
-  ChangeEventHandler,
-  ComponentProps,
-  FocusEventHandler,
-  KeyboardEventHandler,
-  Ref,
-} from 'react';
+import { ComponentProps, Ref } from 'react';
 import { ControllerFieldState } from 'react-hook-form';
 
 import TextField from 'lib/components/TextField';
+import NumberTextField from 'lib/components/NumberTextField';
 import { formatErrorMessage } from './utils/mapError';
 
 // Adapted from old TextField.jsx and DebouncedTextField.jsx
 const styles = {
   textFieldStyle: { margin: '8px 10px 8px 0px' },
-};
-
-const onlyNumberInput: KeyboardEventHandler<HTMLInputElement> = (e): void => {
-  if (e.which === 8) return;
-  if (e.which < 48 || e.which > 57) e.preventDefault();
 };
 
 type FormTextFieldProps = ComponentProps<typeof TextField> & {
@@ -32,53 +22,41 @@ type FormTextFieldProps = ComponentProps<typeof TextField> & {
   disableMargins?: boolean;
 };
 
+type TextFieldPropsWithoutOnChange = Omit<
+  ComponentProps<typeof TextField>,
+  'onChange'
+>;
+
 const FormTextField = (props: FormTextFieldProps): JSX.Element => {
   const {
     field,
     fieldState: { error },
     disableMargins,
+    type,
     ...textFieldProps
   } = props;
 
-  const processValue = (
-    input?: string | number,
-    trimStart = false,
-  ): string | number => {
-    if (typeof input === 'number') return input;
-
-    const value = (trimStart ? input?.trimStart() : input?.trim()) ?? '';
-    if (!trimStart && props.type === 'number') return parseInt(value, 10) || 0;
-    return value;
+  const elementProps: TextFieldPropsWithoutOnChange = {
+    trims: true,
+    ...field,
+    error: Boolean(error),
+    helperText: error && formatErrorMessage(error.message),
+    ...(!disableMargins && { style: styles.textFieldStyle }),
+    ...textFieldProps,
   };
 
-  const handleChange: ChangeEventHandler<HTMLInputElement> = (e): void => {
-    e.persist();
-    field.onChange(processValue(e.target.value, true));
-  };
-
-  const handleBlur: FocusEventHandler<HTMLInputElement> = (e): void => {
-    field.onChange(processValue(e.target.value));
-    field.onBlur();
-  };
-
-  const handleKeyPress: KeyboardEventHandler<HTMLInputElement> = (e): void => {
-    if (props.type === 'number') onlyNumberInput(e);
-    if (e.key === 'Enter') {
-      e.preventDefault();
-      field.onChange(processValue(field.value));
-    }
-  };
+  if (type === 'number')
+    return (
+      <NumberTextField
+        {...elementProps}
+        onChange={(_, value): void => field.onChange(value)}
+      />
+    );
 
   return (
     <TextField
-      {...field}
-      onChange={handleChange}
-      onBlur={handleBlur}
-      onKeyDown={handleKeyPress}
-      error={Boolean(error)}
-      helperText={error && formatErrorMessage(error.message)}
-      {...(!disableMargins && { style: styles.textFieldStyle })}
-      {...textFieldProps}
+      {...elementProps}
+      onChange={(e): void => field.onChange(e.target.value)}
     />
   );
 };
