36da63d26586c27687586ddf062493290175006c
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2022-10-22 21:33:39 +0800

    test(email_management): adapt to new ui

--- spec/features/user/email_management_spec.rb
+++ spec/features/user/email_management_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.feature 'User: Emails' do
+RSpec.feature 'User: Emails', js: true do
   let(:instance) { Instance.default }
 
   with_tenant(:instance) do
@@ -16,52 +16,67 @@ RSpec.feature 'User: Emails' do
 
     scenario 'I can view all my emails' do
       user.reload.emails.each do |email|
-        expect(page).to have_selector('tr td', text: email.email)
+        expect(page).to have_selector('section', text: email.email)
+        section = find('section', text: email.email)
 
         unless email.confirmed?
-          expect(page).to have_selector("tr#user_email_#{email.id} td",
-                                        text: I18n.t('user.emails.email.unconfirmed'))
+          expect(section).to have_text('Unconfirmed')
+          expect(section).to have_content('Resend confirmation email')
         end
 
-        if email.primary?
-          expect(page).to have_selector("tr#user_email_#{email.id} td",
-                                        text: I18n.t('user.emails.email.primary'))
-        end
+        expect(section).to have_text('Primary') if email.primary?
+        expect(section).to have_content('Set as primary') if !email.primary? && email.confirmed?
       end
     end
 
     scenario 'I can add new emails' do
       invalid_email = 'test@@example'
       valid_email = build(:user_email).email
-      fill_in 'user_email_email', with: invalid_email
 
-      click_button I18n.t('user.emails.index.add')
-      expect(page).to have_selector('div.alert-danger')
+      expect do
+        fill_in 'newEmail', with: invalid_email
+        click_button 'Add email address'
+        expect(page).to have_text('is invalid')
+      end.not_to(change { user.emails.count })
 
-      fill_in 'user_email_email', with: valid_email
-      click_button I18n.t('user.emails.index.add')
-      expect(page).to have_selector('tr td', text: valid_email)
+      expect do
+        fill_in 'newEmail', with: valid_email
+        click_button 'Add email address'
+        expect(page).to have_selector('section', text: valid_email)
+      end.to change { user.emails.count }.by(1)
     end
 
     scenario 'I can delete a email' do
-      email_to_delete = user.reload.emails.sample
-      expect do
-        find_link(nil, href: user_email_path(email_to_delete)).click
-      end.to change { user.emails.count }.by(-1)
+      non_primary_emails = user.reload.emails.filter { |email| !email.primary? }
+      email_to_delete = non_primary_emails.sample
+      section = find('section', text: email_to_delete.email)
+      delete_button = section.find('button')
 
-      expect(page).to have_selector('div.alert.alert-success')
+      expect do
+        delete_button.click
+        click_button 'Remove email' if email_to_delete.confirmed?
+        expect_toastify("#{email_to_delete.email} was successfully removed.")
+      end.to change { user.emails.count }.by(-1)
     end
 
     scenario 'I can set an email as primary' do
       email_to_be_set_as_primary = confirmed_emails.sample
-      find_link(nil, href: set_primary_user_email_path(email_to_be_set_as_primary)).click
+      section = find('section', text: email_to_be_set_as_primary.email)
+      set_as_primary_link = section.find('a', text: 'Set as primary')
+
+      set_as_primary_link.click
+
+      expect_toastify("#{email_to_be_set_as_primary.email} was successfully set as your primary email.")
       expect(email_to_be_set_as_primary.reload).to be_primary
     end
 
     scenario 'I can request a new confirmation email' do
-      find_link(nil, href: send_confirmation_user_email_path(unconfirmed_email)).click
-      expect(page).to have_selector('div.alert.alert-success',
-                                    text: I18n.t('user.emails.send_confirmation.success'))
+      section = find('section', text: unconfirmed_email.email)
+      resend_confirmation_link = section.find('a', text: 'Resend confirmation email')
+
+      resend_confirmation_link.click
+
+      expect_toastify("A confirmation email has been sent to #{unconfirmed_email.email}.")
     end
   end
 end
