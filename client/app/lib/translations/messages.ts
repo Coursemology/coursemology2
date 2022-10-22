3b5c541aa015e79656a115075fc8d12b3f80c200
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2022-10-22 15:46:02 +0800

    feat(textfield): add new password visibility features

--- client/app/lib/translations/messages.ts
+++ client/app/lib/translations/messages.ts
@@ -1,9 +1,16 @@
 import { defineMessages } from 'react-intl';
 
-export default defineMessages({
+const messagesTranslations = defineMessages({
   fetchingError: {
-    id: 'lib.form.fetchingError',
+    id: 'lib.translations.messages.fetchingError',
     defaultMessage:
       'An error occurred when loading your data. Please reload and try again.',
   },
+  passwordIsVisible: {
+    id: 'lib.translations.messages.passwordIsVisible',
+    defaultMessage:
+      'Your password is visible! Watch out for eyes behind you...',
+  },
 });
+
+export default messagesTranslations;
