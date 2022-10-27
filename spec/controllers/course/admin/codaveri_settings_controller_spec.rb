7a843b779b82b17f6cd7b16194692ca1bf9868a8
Eko Widianto <ekowidianto@outlook.com>
2022-10-27 20:12:52 +0800

    test(codaveri component): add tests for codaveri component

---
+++ spec/controllers/course/admin/codaveri_settings_controller_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe Course::Admin::CodaveriSettingsController, type: :controller do
+  let!(:instance) { create(:instance, :with_codaveri_component_enabled) }
+  with_tenant(:instance) do
+    let(:user) { create(:user) }
+    let(:course) { create(:course, :with_codaveri_component_enabled, creator: user) }
+    before { sign_in(user) }
+
+    describe '#edit' do
+      subject { get :edit, params: { course_id: course, format: :json } }
+      it { is_expected.to render_template(:edit) }
+    end
+
+    describe '#update' do
+      before do
+        allow(course).to receive(:save).and_return(false)
+        allow(controller).to receive(:current_course).and_return(course)
+      end
+      context 'when course cannot be saved' do
+        subject { patch :update, params: { course_id: course, settings_codaveri_component: { is_only_itsp: '' } } }
+        it 'returns bad_request with errors' do
+          expect(subject).to have_http_status(:bad_request)
+          expect(JSON.parse(subject.body)['errors']).not_to be_nil
+        end
+      end
+    end
+  end
+end
---
+++ spec/features/course/admin/codaveri_settings_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.feature 'Course: Administration: Codaveri', js: true do
+  let!(:instance) { create(:instance, :with_codaveri_component_enabled) }
+
+  with_tenant(:instance) do
+    let(:course) { create(:course, :with_codaveri_component_enabled) }
+    before { login_as(user, scope: :user) }
+
+    context 'As a Course Manager' do
+      let(:user) { create(:course_manager, course: course).user }
+
+      scenario 'I can enable and disable codaveri ITSP setting' do
+        visit course_admin_codaveri_path(course)
+        expect(course.reload.codaveri_itsp_enabled?).to eq(nil)
+
+        control = find('label', text: 'Enable ITSP')
+        control.click
+        expect_toastify('Your changes have been saved.')
+
+        option = control.find('input', visible: false)
+        expect(option).to be_checked
+        expect(course.reload.codaveri_itsp_enabled?).to eq(true)
+
+        control.click
+        expect_toastify('Your changes have been saved.')
+
+        expect(option).not_to be_checked
+        expect(course.reload.codaveri_itsp_enabled?).to eq(nil)
+      end
+    end
+  end
+end
