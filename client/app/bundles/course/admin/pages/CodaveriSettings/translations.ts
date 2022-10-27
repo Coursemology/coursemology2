cf2b2f33d489028e48f241348d047b33d74f329e
Eko Widianto <ekowidianto@outlook.com>
2022-10-27 20:12:46 +0800

    feat(codaveri settings): add substitle

--- client/app/bundles/course/admin/pages/CodaveriSettings/translations.ts
+++ client/app/bundles/course/admin/pages/CodaveriSettings/translations.ts
@@ -5,6 +5,12 @@ export default defineMessages({
     id: 'course.admin.codaveri.codaveriSettings',
     defaultMessage: 'Codaveri settings',
   },
+  codaverSettingsSubtitle: {
+    id: 'course.admin.codaveri.codaverSettingsSubtitle',
+    defaultMessage:
+      "This is currently an experimental feature. \
+      Codaveri provides code evaluation and automated code feedback services for students' codes.",
+  },
   enableIsOnlyITSP: {
     id: 'course.admin.codaveri.enableIsOnlyITSP',
     defaultMessage: 'Enable ITSP',
