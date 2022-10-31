4f9fbe0b2d2db5d115e2c54a1f6c566a5074630f
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2022-10-31 17:30:13 +0800

    chore(jest): move config to jest.config.js

--- client/app/utilities/test-utils.tsx
+++ client/app/utilities/test-utils.tsx
@@ -1,22 +1,18 @@
-import { FC, ReactElement } from 'react';
+import { FC, ReactNode } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { render, RenderOptions } from '@testing-library/react';
+import { render, RenderOptions, RenderResult } from '@testing-library/react';
 import { IntlProvider } from 'react-intl';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const AllTheProviders: FC<any> = ({ children }) => {
-  return (
-    <IntlProvider locale="en" timeZone="Asia/Singapore">
-      {children}
-    </IntlProvider>
-  );
-};
+const AllTheProviders: FC<{ children: ReactNode }> = ({ children }) => (
+  <IntlProvider locale="en" timeZone="Asia/Singapore">
+    {children}
+  </IntlProvider>
+);
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const customRender: any = (
-  ui: ReactElement,
+const customRender = (
+  ui: JSX.Element,
   options?: Omit<RenderOptions, 'wrapper'>,
-) => render(ui, { wrapper: AllTheProviders, ...options });
+): RenderResult => render(ui, { wrapper: AllTheProviders, ...options });
 
 // eslint-disable-next-line import/no-extraneous-dependencies
 export * from '@testing-library/react';
