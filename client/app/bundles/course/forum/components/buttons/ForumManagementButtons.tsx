2d59d2bf27d92ea8f0ece191c5fbfed956002051
Eko Widianto <ekowidianto@outlook.com>
2022-11-02 16:06:10 +0800

    feat(react forum porting): add pages and ui components for forum page

---
+++ client/app/bundles/course/forum/components/buttons/ForumManagementButtons.tsx
@@ -0,0 +1,163 @@
+import { FC, useState } from 'react';
+import { defineMessages } from 'react-intl';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { ClickAwayListener, IconButton } from '@mui/material';
+import { MoreHoriz } from '@mui/icons-material';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import colors from 'tailwindcss/colors';
+import { ForumEntity } from 'types/course/forums';
+import { AppDispatch } from 'types/store';
+import DeleteButton from 'lib/components/core/buttons/DeleteButton';
+import EditButton from 'lib/components/core/buttons/EditButton';
+import { getCourseId } from 'lib/helpers/url-helpers';
+import useTranslation from 'lib/hooks/useTranslation';
+import SubscribeButton from './SubscribeButton';
+import ForumEdit from '../../pages/ForumEdit';
+import { deleteForum } from '../../operations';
+
+interface Props {
+  forum: ForumEntity;
+  navigateToIndexAfterDelete?: boolean;
+  navigateToShowAfterUpdate?: boolean;
+  disabled?: boolean;
+  showOnHover?: boolean;
+}
+
+const translations = defineMessages({
+  deletionSuccess: {
+    id: 'course.forum.components.buttons.forumManagement.delete.success',
+    defaultMessage: 'Forum {title} was deleted.',
+  },
+  deletionFailure: {
+    id: 'course.forum.components.buttons.forumManagement.delete.fail',
+    defaultMessage: 'Failed to delete forum - {error}',
+  },
+  deletionConfirm: {
+    id: 'course.forum.components.buttons.forumManagement.delete.confirm',
+    defaultMessage: 'Are you sure you wish to delete this forum "{title}"?',
+  },
+});
+
+const ForumManagementButtons: FC<Props> = (props) => {
+  const {
+    forum,
+    navigateToIndexAfterDelete,
+    navigateToShowAfterUpdate,
+    disabled,
+    showOnHover,
+  } = props;
+  const dispatch = useDispatch<AppDispatch>();
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+  const [showButtons, setShowButtons] = useState(false);
+  const [isEditOpen, setIsEditOpen] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const disableButton = isDeleting || !!disabled;
+  const handleEdit = (): void => {
+    setIsEditOpen(true);
+  };
+
+  const handleDelete = (): Promise<void> => {
+    setIsDeleting(true);
+    return dispatch(deleteForum(forum.id))
+      .then(() => {
+        toast.success(
+          t(translations.deletionSuccess, {
+            title: forum.name,
+          }),
+        );
+        if (navigateToIndexAfterDelete) {
+          navigate(`/courses/${getCourseId()}/forums`);
+        } else {
+          setIsDeleting(false);
+        }
+      })
+      .catch((error) => {
+        setIsDeleting(false);
+        const errorMessage = error.response?.data?.errors
+          ? error.response.data.errors
+          : '';
+        toast.error(
+          t(translations.deletionFailure, {
+            error: errorMessage,
+          }),
+        );
+      });
+  };
+
+  const managementButtons = (
+    <div
+      className={
+        showOnHover
+          ? `${
+              showButtons ? '' : 'invisible group-hover:visible'
+            } absolute right-0 top-0 flex h-full items-center border-0 pl-20`
+          : 'whitespace-nowrap'
+      }
+      style={
+        showOnHover || showButtons
+          ? {
+              background: `linear-gradient(90deg, transparent 0%, ${colors.neutral[100]} 20%)`,
+            }
+          : {}
+      }
+    >
+      <SubscribeButton
+        emailSubscription={forum.emailSubscription}
+        entityType="forum"
+        entityId={forum.id}
+        entityUrl={forum.forumUrl}
+        entityTitle={forum.name}
+        disabled={disableButton}
+      />
+
+      {forum.permissions.canEditForum && (
+        <EditButton
+          className={`forum-edit-${forum.id}`}
+          onClick={handleEdit}
+          disabled={disableButton}
+        />
+      )}
+      {forum.permissions.canDeleteForum && (
+        <DeleteButton
+          className={`forum-delete-${forum.id}`}
+          disabled={disableButton}
+          loading={isDeleting}
+          onClick={handleDelete}
+          confirmMessage={t(translations.deletionConfirm, {
+            title: forum.name,
+          })}
+        />
+      )}
+    </div>
+  );
+
+  return (
+    <ClickAwayListener onClickAway={(): void => setShowButtons(false)}>
+      <div className="group">
+        {showOnHover && !showButtons && (
+          <IconButton
+            className={`forum-action-${forum.id}`}
+            color="inherit"
+            onClick={(): void => setShowButtons((prevState) => !prevState)}
+          >
+            <MoreHoriz />
+          </IconButton>
+        )}
+        {managementButtons}
+        <ForumEdit
+          forum={forum}
+          isOpen={isEditOpen}
+          handleClose={(): void => {
+            setIsEditOpen(false);
+          }}
+          navigateToShowAfterUpdate={navigateToShowAfterUpdate}
+        />
+      </div>
+    </ClickAwayListener>
+  );
+};
+
+export default ForumManagementButtons;
---
+++ client/app/bundles/course/forum/components/buttons/SubscribeButton.tsx
@@ -0,0 +1,229 @@
+import { FC, useEffect, useState } from 'react';
+import { IconButton, Tooltip } from '@mui/material';
+import { toast } from 'react-toastify';
+import { useSearchParams } from 'react-router-dom';
+import { EmailSubscriptionSetting } from 'types/course/forums';
+import { defineMessages } from 'react-intl';
+import { NotificationsActive, NotificationsOff } from '@mui/icons-material';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from 'types/store';
+import useTranslation from 'lib/hooks/useTranslation';
+import {
+  updateForumSubscription,
+  updateForumTopicSubscription,
+} from '../../operations';
+
+const commonTranslations = {
+  updateSubscriptionFailure: {
+    id: 'course.forum.components.buttons.subcribeButton.updateSubscriptionFailure',
+    defaultMessage: 'Failed to update subscription - {error}',
+  },
+};
+
+const forumTranslations = defineMessages({
+  subscribeTag: {
+    id: 'course.forum.components.buttons.subcribeButton.subscribeTag',
+    defaultMessage:
+      'Subscribe to receive an email notification when a new topic is created.',
+  },
+  unsubscribeTag: {
+    id: 'course.forum.components.buttons.subcribeButton.unsubscribeTag',
+    defaultMessage:
+      'Unsubscribe to stop receiving email notifications when a new topic is created.',
+  },
+  userSettingSubscribed: {
+    id: 'course.forum.components.buttons.subcribeButton.userSettingSubscribed',
+    defaultMessage:
+      'You have unsubscribed from "New Topic" for forums in this course. Please go to "manage my subscription" to enable it.',
+  },
+  adminSettingSubscribed: {
+    id: 'course.forum.components.buttons.subcribeButton.adminSettingSubscribed',
+    defaultMessage:
+      'Subscription of new forum topic is disabled by the course admin.',
+  },
+  subscribeSuccess: {
+    id: 'course.forum.components.buttons.subcribeButton.subscribeSuccess',
+    defaultMessage: 'You have successfully been subscribed to {title}.',
+  },
+  unsubscribeSuccess: {
+    id: 'course.forum.components.buttons.subcribeButton.unsubscribeSuccess',
+    defaultMessage: 'You have successfully been unsubscribed from {title}.',
+  },
+});
+
+const forumTopicTranslations = defineMessages({
+  subscribeTag: {
+    id: 'course.forum.topic.components.buttons.subcribeButton.subscribeTag',
+    defaultMessage:
+      'Subscribe to receive email notifications when someone replies in this forum topic.',
+  },
+  unsubscribeTag: {
+    id: 'course.forum.topic.components.buttons.subcribeButton.unsubscribeTag',
+    defaultMessage:
+      'Unsubscribe to stop receiving email notifications when someone replies in this forum topic.',
+  },
+  userSettingSubscribed: {
+    id: 'course.forum.topic.components.buttons.subcribeButton.userSettingSubscribed',
+    defaultMessage:
+      'You have unsubscribed from "New Post and Reply" for forums in this course. Please go to "manage my subscription" to enable it.',
+  },
+  adminSettingSubscribed: {
+    id: 'course.forum.topic.components.buttons.subcribeButton.adminSettingSubscribed',
+    defaultMessage:
+      'Subscription of forum topics is disabled by the course admin.',
+  },
+  subscribeSuccess: {
+    id: 'course.forum.topic.components.buttons.subcribeButton.subscribeSuccess',
+    defaultMessage:
+      'You have successfully been subscribed to the forum topic {title}.',
+  },
+  unsubscribeSuccess: {
+    id: 'course.forum.topic.components.buttons.subcribeButton.unsubscribeSuccess',
+    defaultMessage:
+      'You have successfully been unsubscribed from the forum topic {title}.',
+  },
+});
+
+interface Props {
+  emailSubscription: EmailSubscriptionSetting;
+  entityType: 'forum' | 'topic';
+  entityId: number;
+  entityUrl: string;
+  entityTitle: string;
+  disabled?: boolean;
+}
+
+const SubscribeButton: FC<Props> = ({
+  emailSubscription,
+  entityType,
+  entityId,
+  entityUrl,
+  entityTitle,
+  disabled: disableButton,
+}: Props) => {
+  const [isUpdating, setIsUpdating] = useState(false);
+  const dispatch = useDispatch<AppDispatch>();
+  const { t } = useTranslation();
+  const translations =
+    entityType === 'forum' ? forumTranslations : forumTopicTranslations;
+  const isSubscribed = emailSubscription.isUserSubscribed;
+  const disabled =
+    !emailSubscription.isCourseEmailSettingEnabled ||
+    !emailSubscription.isUserEmailSettingEnabled ||
+    isUpdating ||
+    disableButton;
+
+  let subscribedTooltip = t(translations.unsubscribeTag);
+  let unsubscribedTooltip = t(translations.subscribeTag);
+
+  if (!emailSubscription.isUserEmailSettingEnabled) {
+    subscribedTooltip = t(translations.userSettingSubscribed);
+    unsubscribedTooltip = t(translations.userSettingSubscribed);
+  }
+
+  if (!emailSubscription.isCourseEmailSettingEnabled) {
+    subscribedTooltip = t(translations.adminSettingSubscribed);
+    unsubscribedTooltip = t(translations.adminSettingSubscribed);
+  }
+  const handleUpdate = (): Promise<void> => {
+    setIsUpdating(true);
+    return dispatch(
+      entityType === 'forum'
+        ? updateForumSubscription(
+            entityId,
+            entityUrl,
+            emailSubscription.isUserSubscribed,
+          )
+        : updateForumTopicSubscription(
+            entityId,
+            entityUrl,
+            emailSubscription.isUserSubscribed,
+          ),
+    )
+      .then(() => {
+        toast.success(
+          isSubscribed
+            ? t(translations.unsubscribeSuccess, {
+                title: entityTitle,
+              })
+            : t(translations.subscribeSuccess, {
+                title: entityTitle,
+              }),
+        );
+      })
+      .catch((error) => {
+        const errorMessage = error.response?.data?.errors
+          ? error.response.data.errors
+          : '';
+        toast.error(
+          t(commonTranslations.updateSubscriptionFailure, {
+            error: errorMessage,
+          }),
+        );
+      })
+      .finally(() => setIsUpdating(false));
+  };
+
+  const [searchParams] = useSearchParams();
+
+  // The following useEffect is to handle unsubscription triggerred from
+  // a user's email notification. The link from the email includes
+  // params of either subscribe_topic or subscribe_forum.
+  // When these params are detected, the following logic would either
+  // call handleUpdate in order to trigger unsubscription to the backend,
+  // or if the topic/forum has already been unsubscribed, we do not trigger
+  // any backend call and we just show a success toast message.
+  useEffect(() => {
+    // In a forum topic table rendered in the ForumShow page,
+    // there could be multiple SubscribeButton elements
+    // belonging to either the forum itself or the the individual topics inside the table.
+    // As we only want to trigger only the relevant unsubscription, we check the params key
+    // , of either subscribe_forum or subscribe_topic, with the entityType of the button.
+    if (
+      searchParams.get('subscribe_forum') === 'false' &&
+      entityType === 'forum'
+    ) {
+      if (!emailSubscription.isUserSubscribed) {
+        toast.success(
+          t(translations.unsubscribeSuccess, {
+            title: entityTitle,
+          }),
+        );
+      } else {
+        handleUpdate();
+      }
+    }
+
+    if (
+      searchParams.get('subscribe_topic') === 'false' &&
+      entityType === 'topic'
+    ) {
+      if (!emailSubscription.isUserSubscribed) {
+        toast.success(
+          t(translations.unsubscribeSuccess, {
+            title: entityTitle,
+          }),
+        );
+      } else {
+        handleUpdate();
+      }
+    }
+  }, [searchParams]);
+
+  return (
+    <Tooltip title={isSubscribed ? subscribedTooltip : unsubscribedTooltip}>
+      <span>
+        <IconButton
+          className={`${entityType}-subscribe-${entityId}`}
+          color="inherit"
+          disabled={disabled}
+          onClick={handleUpdate}
+        >
+          {isSubscribed ? <NotificationsOff /> : <NotificationsActive />}
+        </IconButton>
+      </span>
+    </Tooltip>
+  );
+};
+
+export default SubscribeButton;
