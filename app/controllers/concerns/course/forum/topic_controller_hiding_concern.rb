d4d226bd1bf5166710f018348732a8160d64f569
Eko Widianto <ekowidianto@outlook.com>
2022-11-02 15:53:47 +0800

    feat(react forum topic port): rewrite forum topics controller

--- app/controllers/concerns/course/forum/topic_controller_hiding_concern.rb
+++ app/controllers/concerns/course/forum/topic_controller_hiding_concern.rb
@@ -4,11 +4,9 @@ module Course::Forum::TopicControllerHidingConcern
 
   def set_hidden
     if @topic.update(hidden_params)
-      redirect_to course_forum_topic_path(current_course, @forum, @topic),
-                  success: hidden_state_text(true)
+      head :ok
     else
-      redirect_to course_forum_topic_path(current_course, @forum, @topic),
-                  danger: hidden_state_text(false)
+      render json: { errors: @topic.errors }, status: :bad_request
     end
   end
 
@@ -16,18 +14,5 @@ module Course::Forum::TopicControllerHidingConcern
 
   def hidden_params
     params.permit(:hidden)
-  end
-
-  def hidden_state_text(successful)
-    case [@topic.hidden, successful]
-    when [true, true]
-      t('course.forum.topics.hidden.success')
-    when [true, false]
-      t('course.forum.topics.hidden.failure')
-    when [false, true]
-      t('course.forum.topics.unhidden.success')
-    when [false, false]
-      t('course.forum.topics.unhidden.failure')
-    end
   end
 end
--- app/controllers/concerns/course/forum/topic_controller_subscription_concern.rb
+++ app/controllers/concerns/course/forum/topic_controller_subscription_concern.rb
@@ -5,32 +5,13 @@ module Course::Forum::TopicControllerSubscriptionConcern
   def subscribe
     authorize!(:read, @topic)
     if set_subscription_state
-      if request.get?
-        return redirect_to course_forum_topic_path(current_course, @forum, @topic),
-                           success: subscription_state_text(true)
-      else
-        flash.now[:success] = subscription_state_text(true)
-      end
+      head :ok
     else
-      flash.now[:danger] = subscription_state_text(false)
+      render json: { errors: @topic.errors }, status: :bad_request
     end
-    render 'course/forum/topics/update_subscribe_button'
   end
 
   private
-
-  def subscription_state_text(successful)
-    case [subscribe?, successful]
-    when [true, true]
-      t('course.forum.topics.subscribe.success', topic: @topic.title)
-    when [true, false]
-      t('course.forum.topics.subscribe.failure', topic: @topic.title)
-    when [false, true]
-      t('course.forum.topics.unsubscribe.success', topic: @topic.title)
-    when [false, false]
-      t('course.forum.topics.unsubscribe.failure', topic: @topic.title)
-    end
-  end
 
   def set_subscription_state
     if subscribe?
