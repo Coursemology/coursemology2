d79aef69bf5f94df1775585d56a4d27452581470
Eko Widianto <ekowidianto@outlook.com>
2022-11-02 15:50:48 +0800

    feat(react forum port): rewrite forums controller

--- app/controllers/course/forum/forums_controller.rb
+++ app/controllers/course/forum/forums_controller.rb
@@ -1,71 +1,75 @@
 # frozen_string_literal: true
 class Course::Forum::ForumsController < Course::Forum::Controller
+  include Course::UsersHelper
   load_resource :forum, class: Course::Forum.name, through: :course, only: [:index, :new, :create]
   before_action :add_forum_item_breadcrumb
 
   def index
-    @forums = @forums.order(:created_at).with_forum_statistics(current_user)
-    @unresolved_forums_ids = Course::Forum::Topic.filter_unresolved_forum(@forums.map(&:id))
+    respond_to do |format|
+      format.html
+      format.json do
+        @forums = @forums.order(:created_at).with_forum_statistics(current_user)
+        @unresolved_forums_ids = Course::Forum::Topic.filter_unresolved_forum(@forums.map(&:id))
+      end
+    end
   end
 
   def show
-    @topics = @forum.topics.accessible_by(current_ability).order_by_latest_post.with_topic_statistics.
-              page(page_param).with_read_marks_for(current_user).includes(:creator).with_latest_post
-  end
+    respond_to do |format|
+      format.html { render 'index' }
+      format.json do
+        @topics = @forum.topics.accessible_by(current_ability).order_by_latest_post.with_topic_statistics.
+                  with_read_marks_for(current_user).includes(:creator).with_latest_post
+        @subscribed_discussion_topic_ids = preload_topic_subscriptons
+        @course_users_hash = preload_course_users_hash(current_course)
 
-  def new
+        render 'show', locals: { forum: forum_with_statistics }
+      end
+    end
   end
 
   def create
     if @forum.save
-      redirect_to course_forums_path(current_course), success: t('.success', name: @forum.name)
+      render partial: 'forum_list_data',
+             locals: { forum: forum_with_statistics, isUnresolved: false },
+             status: :ok
     else
-      render 'new'
+      render json: { errors: @forum.errors }, status: :bad_request
     end
-  end
-
-  def edit
   end
 
   def update
     if @forum.update(forum_params)
-      redirect_to course_forum_path(current_course, @forum),
-                  success: t('.success', name: @forum.name)
+      render partial: 'forum_list_data',
+             locals: { forum: forum_with_statistics,
+                       isUnresolved: Course::Forum::Topic.filter_unresolved_forum(@forum.id).present? },
+             status: :ok
     else
-      render 'edit'
+      render json: { errors: @forum.errors }, status: :bad_request
     end
   end
 
   def destroy
     if @forum.destroy
-      redirect_to course_forums_path(current_course), success: t('.success', name: @forum.name)
+      head :ok
     else
-      redirect_to course_forum_path(current_course, @forum),
-                  danger: t('.failure', error: @forum.errors.full_messages.to_sentence)
+      render json: { errors: @forum.errors.full_messages.to_sentence }, status: :bad_request
     end
   end
 
   def subscribe
     if @forum.subscriptions.create(user: current_user)
-      flash.now[:success] = t('.success', name: @forum.name)
+      head :ok
     else
-      flash.now[:danger] = t('.failure', error: @forum.errors.full_messages.to_sentence)
+      render json: { errors: @forum.errors.full_messages.to_sentence }, status: :bad_request
     end
-    render 'update_subscribe_button'
   end
 
   def unsubscribe
-    if @forum.subscriptions.where(user: current_user).delete_all > 0
-      flash.now[:success] = t('.success', name: @forum.name)
+    if @forum.subscriptions.where(user: current_user).delete_all
+      head :ok
     else
-      flash.now[:danger] = t('.failure')
-    end
-    if request.get?
-      # When forum unsubscribe link is clicked from email
-      redirect_to course_forum_path(current_course, @forum),
-                  success: t('.success', name: @forum.name)
-    else
-      render 'update_subscribe_button'
+      render json: { errors: @forum.errors.full_messages.to_sentence }, status: :bad_request
     end
   end
 
@@ -87,29 +91,19 @@ class Course::Forum::ForumsController < Course::Forum::Controller
     @search = Course::Forum::Search.new(search_params)
   end
 
-  def next_unread
-    topic = Course::Forum::Topic.from_course(current_course).
-            accessible_by(current_ability).unread_by(current_user).first
-
-    if topic
-      redirect_to course_forum_topic_path(current_course, topic.forum, topic)
-    else
-      redirect_to course_forums_path(current_course), notice: t('.notice')
-    end
-  end
-
   def mark_all_as_read
     topics = Course::Forum::Topic.from_course(current_course).
              accessible_by(current_ability).unread_by(current_user).to_a
     Course::Forum::Topic.mark_as_read!(topics, for: current_user)
-    redirect_to course_forums_path(current_course), success: t('.success')
+
+    head :ok
   end
 
   def mark_as_read
     topics = @forum.topics.accessible_by(current_ability).to_a
     Course::Forum::Topic.mark_as_read!(topics, for: current_user)
 
-    redirect_to course_forum_path(current_course, @forum), success: t('.success')
+    head :ok
   end
 
   private
@@ -131,6 +125,21 @@ class Course::Forum::ForumsController < Course::Forum::Controller
   end
 
   def skip_load_forum?
-    [:index, :new, :create, :all_posts, :search, :next_unread, :mark_all_as_read].include?(action_name.to_sym)
+    [:index, :create, :all_posts, :search, :mark_all_as_read].include?(action_name.to_sym)
+  end
+
+  def forum_with_statistics
+    @forum.calculated(
+      :topic_count,
+      :topic_view_count,
+      :topic_post_count,
+      topic_unread_count: current_user
+    )
+  end
+
+  def preload_topic_subscriptons
+    discussion_topic_ids = @topics.map(&:discussion_topic).pluck(:id)
+    Course::Discussion::Topic::Subscription.where(topic_id: discussion_topic_ids,
+                                                  user_id: current_user.id).pluck(:topic_id).to_set
   end
 end
