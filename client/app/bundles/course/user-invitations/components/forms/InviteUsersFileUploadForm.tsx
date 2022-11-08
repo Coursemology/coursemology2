f524c7c66310104082001b349b49a59994bd7ec4
Eko Widianto <ekowidianto@outlook.com>
2022-11-08 14:45:00 +0800

    refactor(invite course user file upload form): use form dialog

--- client/app/bundles/course/user-invitations/components/forms/InviteUsersFileUploadForm.tsx
+++ client/app/bundles/course/user-invitations/components/forms/InviteUsersFileUploadForm.tsx
@@ -1,115 +1,82 @@
-import { FC, useEffect } from 'react';
-import { Controller, useForm, UseFormSetError } from 'react-hook-form';
+import { FC } from 'react';
+import { Controller, UseFormSetError } from 'react-hook-form';
 import FormSingleFileInput, {
   FilePreview,
 } from 'lib/components/form/fields/SingleFileInput';
-import ErrorText from 'lib/components/core/ErrorText';
-import { LoadingButton } from '@mui/lab';
-import { Grid, Button, Typography } from '@mui/material';
-import { defineMessages, injectIntl, WrappedComponentProps } from 'react-intl';
+import { defineMessages } from 'react-intl';
 import { InvitationFileEntity } from 'types/course/userInvitations';
+import FormDialog from 'lib/components/form/dialog/FormDialog';
+import useTranslation from 'lib/hooks/useTranslation';
 
-interface Props extends WrappedComponentProps {
+interface Props {
+  open: boolean;
   onSubmit: (
     data: InvitationFileEntity,
     setError: UseFormSetError<IFormInputs>,
   ) => void;
-  handleClose: (isDirty: boolean) => void;
-  setIsDirty?: (value: boolean) => void;
-  initialValues?: Object;
-  isLoading: boolean;
+  onClose: () => void;
+  formSubtitle: JSX.Element;
 }
 
 interface IFormInputs {
   file: { name: string; url: string };
 }
 
+const initialValues = {
+  file: { name: '', url: '', file: undefined },
+};
+
 const translations = defineMessages({
+  fileUpload: {
+    id: 'course.userInvitation.fileUpload',
+    defaultMessage: 'File Upload',
+  },
   invite: {
     id: 'course.userInvitation.fileUpload.invite',
     defaultMessage: 'Invite Users from File',
   },
-  header: {
-    id: 'course.userInvitation.fileUpload.header',
+  fileUploadField: {
+    id: 'course.userInvitation.fileUploadField',
     defaultMessage: 'File Upload (.csv)',
-  },
-  cancel: {
-    id: 'course.userInvitation.fileUpload.cancel',
-    defaultMessage: 'Cancel',
   },
 });
 
 const FileUploadForm: FC<Props> = (props) => {
-  const { onSubmit, handleClose, setIsDirty, initialValues, isLoading, intl } =
-    props;
-  const {
-    control,
-    handleSubmit,
-    setError,
-    formState: { errors, isDirty, isSubmitting },
-  } = useForm<IFormInputs>({
-    defaultValues: initialValues,
-  });
+  const { open, onSubmit, onClose, formSubtitle } = props;
+  const { t } = useTranslation();
 
-  useEffect(() => {
-    if (setIsDirty) {
-      if (isDirty) {
-        setIsDirty(true);
-      } else {
-        setIsDirty(false);
-      }
-    }
-  }, [isDirty]);
-
-  const disabled = isSubmitting || !isDirty;
   return (
-    <>
-      <Typography variant="body2">
-        <strong>{intl.formatMessage(translations.header)}</strong>
-      </Typography>
-      <form
-        encType="multipart/form-data"
-        id="invite-users-file-upload-form"
-        noValidate
-        onSubmit={handleSubmit((data) => onSubmit(data.file, setError))}
-      >
-        <ErrorText errors={errors} />
-        <Controller
-          name="file"
-          control={control}
-          render={({ field, fieldState }): JSX.Element => (
-            <FormSingleFileInput
-              disabled={isLoading}
-              field={field}
-              fieldState={fieldState}
-              accept={{ 'text/csv': [] }}
-              previewComponent={FilePreview}
-            />
-          )}
-        />
-      </form>
-      <Grid container justifyContent="space-between" sx={{ marginTop: '24px' }}>
-        <LoadingButton
-          loading={isLoading}
-          disabled={disabled}
-          variant="contained"
-          className="btn-submit file-upload-submit"
-          form="invite-users-file-upload-form"
-          key="invite-users-file-upload-form-submit-button"
-          type="submit"
-        >
-          {intl.formatMessage(translations.invite)}
-        </LoadingButton>
-        <Button
-          color="secondary"
-          onClick={(): void => handleClose(isDirty)}
-          disabled={isLoading}
-        >
-          {intl.formatMessage(translations.cancel)}
-        </Button>
-      </Grid>
-    </>
+    <FormDialog
+      open={open}
+      editing={false}
+      onClose={onClose}
+      onSubmit={onSubmit}
+      title={t(translations.fileUpload)}
+      formName="invite-users-file-upload-form"
+      initialValues={initialValues}
+      primaryActionText={t(translations.invite)}
+    >
+      {(control, formState): JSX.Element => (
+        <>
+          {formSubtitle}
+          <Controller
+            name="file"
+            control={control}
+            render={({ field, fieldState }): JSX.Element => (
+              <FormSingleFileInput
+                disabled={formState.isSubmitting}
+                field={field}
+                fieldState={fieldState}
+                label={t(translations.fileUploadField)}
+                accept={{ 'text/csv': [] }}
+                previewComponent={FilePreview}
+              />
+            )}
+          />
+        </>
+      )}
+    </FormDialog>
   );
 };
 
-export default injectIntl(FileUploadForm);
+export default FileUploadForm;
