84d7f45dbf1e36f43141e2c3903a61791abfe501
Eko Widianto <ekowidianto@outlook.com>
2022-11-08 19:01:13 +0800

    refactor(form): remove unused div

--- client/app/bundles/course/forum/components/forms/ForumForm.tsx
+++ client/app/bundles/course/forum/components/forms/ForumForm.tsx
@@ -79,6 +79,7 @@ const ForumForm: FC<Props> = (props) => {
               />
             )}
           />
+
           <Controller
             name="description"
             control={control}
@@ -97,6 +98,7 @@ const ForumForm: FC<Props> = (props) => {
               />
             )}
           />
+
           <Controller
             name="forumTopicsAutoSubscribe"
             control={control}
