779adafc509bc7a32aad630d7cedf3b680529ea9
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2022-11-14 20:27:03 +0800

    feat(tailwind): add shake animation

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -11,6 +11,17 @@ module.exports = {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      animation: {
+        shake: 'shake 0.82s cubic-bezier(.36,.07,.19,.97) both',
+      },
+      keyframes: {
+        shake: {
+          '10%, 90%': { transform: 'translate3d(-1px, 0, 0)' },
+          '20%, 80%': { transform: 'translate3d(2px, 0, 0)' },
+          '30%, 50%, 70%': { transform: 'translate3d(-4px, 0, 0)' },
+          '40%, 60%': { transform: 'translate3d(4px, 0, 0)' },
+        },
+      },
       colors: {
         'slot-1': `var(${SLOTTED_COLOR_VAR}-1)`,
         'slot-2': `var(${SLOTTED_COLOR_VAR}-2)`,
