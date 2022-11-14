7d51967c99dc41a9beafe49392838978a99aa1ec
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2022-11-14 20:09:58 +0800

    feat(link): support underline only on hover

--- client/app/lib/components/core/Link.tsx
+++ client/app/lib/components/core/Link.tsx
@@ -3,19 +3,27 @@ import { Link as MuiLink } from '@mui/material';
 
 interface LinkProps extends ComponentProps<typeof MuiLink> {
   opensInNewTab?: boolean;
+  underlinesOnHover?: boolean;
 }
 
 const Link = (props: LinkProps): JSX.Element => {
-  const { opensInNewTab, ...linkProps } = props;
+  const {
+    opensInNewTab,
+    underlinesOnHover: underlineOnHover,
+    ...linkProps
+  } = props;
 
   return (
     <MuiLink
+      color="links"
       {...linkProps}
       {...(opensInNewTab && {
         target: '_blank',
         rel: 'noopener noreferrer',
       })}
-      className={`cursor-pointer ${props.className}`}
+      className={`cursor-pointer ${
+        underlineOnHover && 'no-underline hoverable:hover:underline'
+      } ${props.className}`}
     />
   );
 };
