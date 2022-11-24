d72fdbd1987e3ce17cc7c2d415aa299360620f23
Eko Widianto <ekowidianto@outlook.com>
2022-11-24 13:23:02 +0800

    refactor(statistics): remove unused js

--- client/app/bundles/course/statistics.js
+++ client/app/bundles/course/statistics.js
@@ -1,10 +1,3 @@
 import initializeDownloadLink from 'lib/helpers/initializeDownloadLink';
 
-const MY_STUDENT_SELECTOR = 'a[href="#my-students"]';
-
-function initializeTabs() {
-  $(MY_STUDENT_SELECTOR).tab('show');
-}
-
-$(initializeTabs);
 initializeDownloadLink('.btn.download');
