39a5789b61a83d093afaf78bda071c4493419ad3
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2022-11-25 15:26:44 +0800

    test(assessments): remove unused rails assessment tabs tests

--- app/helpers/course/assessment/assessments_helper.rb
+++ app/helpers/course/assessment/assessments_helper.rb
@@ -3,24 +3,6 @@ module Course::Assessment::AssessmentsHelper
   include Course::Achievement::AchievementsHelper
   include Course::Condition::ConditionsHelper
 
-  def display_assessment_tabs
-    return nil if @category.tabs.count == 1
-
-    # Set the first tab as active if there's no tab parameter in the URL.
-    active_tab = params[:tab] || @category.tabs.first
-    tabs do
-      @category.tabs.each do |item|
-        # If this is the tab previously set as active because there was no tab parameter in the URL,
-        # pass a hash to `nav_to` so it will show the tab as active.
-        # See https://github.com/doabit/bootstrap-sass-extras/blob/6aa549b91a66055a5f5e37400dbe44f4d17f09c3/app/helpers/nav_helper.rb#L32
-        html_options = item == active_tab ? { active: true } : nil
-        concat(nav_to(format_inline_text(item.title),
-                      course_assessments_path(current_course, category: @category, tab: item),
-                      html_options))
-      end
-    end
-  end
-
   def condition_not_satisfied(can_attempt_assessment, assessment, assessment_time)
     (!can_attempt_assessment &&
       !assessment.conditions_satisfied_by?(current_course_user)) ||
