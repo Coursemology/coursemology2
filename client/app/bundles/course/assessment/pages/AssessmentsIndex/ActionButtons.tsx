c87a986ae71e36ef15c588160d38ae5a162b220c
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2022-12-02 13:24:15 +0800

    feat(assessmentstable): persistent edit, submissions for staff

--- client/app/bundles/course/assessment/pages/AssessmentsIndex/ActionButtons.tsx
+++ client/app/bundles/course/assessment/pages/AssessmentsIndex/ActionButtons.tsx
@@ -26,50 +26,54 @@ const ActionButtons = (props: ActionButtonsProps): JSX.Element => {
   const { for: assessment, student } = props;
   const { t } = useTranslation();
 
+  const actionButton = assessment.actionButtonUrl && (
+    <Button
+      className="min-w-[8.5rem]"
+      // TODO: Change to react-router Link once SPA
+      href={assessment.actionButtonUrl}
+      size="small"
+      variant={assessment.status === 'submitted' ? 'outlined' : 'contained'}
+      // Temporary fix for blue link on hover caused by Bootstrap
+      // TODO: Remove when Bootstrap is finally removed
+      {...((assessment.status === 'open' ||
+        assessment.status === 'attempting' ||
+        assessment.status === 'locked') && {
+        className: 'min-w-[8.5rem] hover:text-white',
+      })}
+    >
+      {t(ACTION_LABELS[assessment.status])}
+    </Button>
+  );
+
   return (
     <div className="flex items-center">
-      {(assessment.editUrl || assessment.submissionsUrl) && (
+      {student ? (
+        actionButton
+      ) : (
         <div className="hoverable:invisible group-hover?:visible flex h-full items-center transition-position no-hover:mr-4 hoverable:absolute hoverable:right-0 hoverable:pl-8 hoverable:opacity-0 hoverable:bg-fade-to-l-slot-2 group-hover?:right-full group-hover?:opacity-100">
-          {assessment.editUrl && (
-            <Tooltip disableInteractive title={t(translations.editAssessment)}>
-              <Link to={assessment.editUrl}>
-                <IconButton className="max-sm:!hidden">
-                  <Create />
-                </IconButton>
-              </Link>
-            </Tooltip>
-          )}
-
-          {assessment.submissionsUrl && (
-            <Tooltip disableInteractive title={t(translations.submissions)}>
-              <IconButton
-                // TODO: Change to react-router Link once SPA
-                href={assessment.submissionsUrl}
-              >
-                <Inventory />
-              </IconButton>
-            </Tooltip>
-          )}
+          {actionButton}
         </div>
       )}
 
-      {assessment.actionButtonUrl && (
-        <Button
-          className="min-w-[8.5rem]"
-          // TODO: Change to react-router Link once SPA
-          href={assessment.actionButtonUrl}
-          size="small"
-          variant={assessment.status === 'submitted' ? 'outlined' : 'contained'}
-          // Temporary fix for blue link on hover caused by Bootstrap
-          // TODO: Remove when Bootstrap is finally removed
-          {...((assessment.status === 'open' ||
-            assessment.status === 'attempting' ||
-            assessment.status === 'locked') && {
-            className: 'min-w-[8.5rem] hover:text-white',
-          })}
-        >
-          {t(ACTION_LABELS[assessment.status])}
-        </Button>
+      {assessment.editUrl && (
+        <Tooltip disableInteractive title={t(translations.editAssessment)}>
+          <Link to={assessment.editUrl}>
+            <IconButton className="max-sm:!hidden">
+              <Create />
+            </IconButton>
+          </Link>
+        </Tooltip>
+      )}
+
+      {assessment.submissionsUrl && (
+        <Tooltip disableInteractive title={t(translations.submissions)}>
+          <IconButton
+            // TODO: Change to react-router Link once SPA
+            href={assessment.submissionsUrl}
+          >
+            <Inventory />
+          </IconButton>
+        </Tooltip>
       )}
 
       {assessment.status === 'unavailable' && (
