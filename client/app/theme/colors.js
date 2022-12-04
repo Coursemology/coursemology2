6a3903299e7d71bde5542d1bad5b71dd828e87e3
Hanming Zhu <hanming@huawen.com>
2022-12-04 12:13:39 -0800

    feat: add chart colors

--- client/app/theme/colors.js
+++ client/app/theme/colors.js
@@ -28,3 +28,18 @@ export const primary = {
   light: '#F6F9FD',
   dark: '#0B48A0',
 };
+
+// CHART.JS COLOURS
+
+export const PURPLE_CHART_BACKGROUND = 'rgba(153, 102, 255, 0.2)';
+export const PURPLE_CHART_BORDER = 'rgba(153, 102, 255, 1)';
+export const GREEN_CHART_BACKGROUND = 'rgba(75, 192, 192, 0.2)';
+export const GREEN_CHART_BORDER = 'rgba(75, 192, 192, 1)';
+export const ORANGE_CHART_BACKGROUND = 'rgba(255, 159, 64, 0.2)';
+export const ORANGE_CHART_BORDER = 'rgba(255, 159, 64, 1)';
+export const RED_CHART_BACKGROUND = 'rgba(255, 99, 132, 0.2)';
+export const RED_CHART_BORDER = 'rgba(255, 99, 132, 1)';
+export const BLUE_CHART_BACKGROUND = 'rgba(54, 162, 235, 0.2)';
+export const BLUE_CHART_BORDER = 'rgba(54, 162, 235, 1)';
+
+export const INVISIBLE_CHART_COLOR = 'rgba(255, 255, 255, 0)';
