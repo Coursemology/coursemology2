6499af3614d5b7a760384ccce15e3c940fc70f04
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2022-12-15 18:11:57 +0700

    feat(reference_timeline): add title, weight

--- app/models/course/reference_timeline.rb
+++ app/models/course/reference_timeline.rb
@@ -6,11 +6,30 @@ class Course::ReferenceTimeline < ApplicationRecord
   has_many :course_users, foreign_key: :reference_timeline_id, inverse_of: :reference_timeline,
                           dependent: :restrict_with_error
 
-  validates :default, inclusion: { in: [true, false] }
+  before_validation :set_weight, if: :new_record?
+
   validates :course, presence: true
+  validates :title, presence: true, unless: :default
+  validates :weight, presence: true, numericality: { only_integer: true }
+
+  default_scope { order(:weight) }
 
   def initialize_duplicate(duplicator, _other)
     self.course = duplicator.options[:destination_course]
     self.reference_times = []
+  end
+
+  private
+
+  def set_weight
+    return if weight.present?
+
+    if default?
+      self.weight = 0
+      return
+    end
+
+    max_weight = course.reference_timelines.maximum(:weight)
+    self.weight ||= max_weight.nil? ? 1 : max_weight + 1
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_12_093308) do
+ActiveRecord::Schema.define(version: 2023_01_15_054448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -868,6 +868,8 @@ ActiveRecord::Schema.define(version: 2023_01_12_093308) do
     t.boolean "default", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.integer "weight"
     t.index ["course_id"], name: "index_course_reference_timelines_on_course_id"
   end
 
