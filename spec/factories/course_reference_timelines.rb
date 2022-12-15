b28191686697be12243ca3c3ee8a0c6b04fc0bdd
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2022-12-15 18:26:45 +0700

    test(factories): add factories for timelines, times

---
+++ spec/factories/course_reference_timelines.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+FactoryBot.define do
+  factory :course_reference_timeline, class: Course::ReferenceTimeline.name do
+    course
+    default { false }
+    sequence(:title) { |n| "Timeline #{n}" }
+
+    trait :default do
+      default { true }
+      title { nil }
+    end
+  end
+end
