39b9cca7f08bbf9205ba1c020f17956c5f651aa9
Eko Widianto <ekowidianto@outlook.com>
2023-01-12 10:19:06 +0800

    refactor(title bar): improve title bar's style

--- client/app/lib/components/navigation/TitleBar.jsx
+++ client/app/lib/components/navigation/TitleBar.jsx
@@ -1,36 +1,20 @@
 import { AppBar, Toolbar, Typography } from '@mui/material';
 import PropTypes from 'prop-types';
 
-const styles = {
-  bar: {
-    // Ensure title bar does not overlap coursemology topbar when page is scrolled.
-    // To be removed once top bar has been migrated to MaterialUI.
-    zIndex: 0,
-    position: 'static',
-    marginBottom: 0,
-  },
-  leftIcon: {
-    marginLeft: -16,
-    marginRight: 8,
-  },
-  text: {
-    flexGrow: 1,
-  },
-};
-
 // Hide menu icon by default. To revert once sidebar has been ported to MaterialUI.
 const TitleBar = ({ iconElementLeft, iconElementRight, title, ...props }) => (
-  <div style={{ flexGrow: 1 }}>
+  <div className="grow">
     <AppBar
-      style={{ ...styles.bar }}
       {...props}
-      className="-mx-6 w-screen sm:mx-0 sm:w-full sm:rounded-md"
+      className="static z-0 -mx-6 mb-0 w-screen sm:mx-0 sm:w-full sm:rounded-md"
       elevation={0}
     >
       <Toolbar>
-        <div style={styles.leftIcon}>{iconElementLeft}</div>
-        <Typography color="inherit" style={styles.text} variant="h5">
-          {title}
+        <div className="-ml-4 mr-2">{iconElementLeft}</div>
+        <Typography className="grow" color="inherit" variant="h5">
+          <label className="m-0 align-middle font-normal" title={title}>
+            <div className="line-clamp-2">{title}</div>
+          </label>
         </Typography>
         {iconElementRight}
       </Toolbar>
