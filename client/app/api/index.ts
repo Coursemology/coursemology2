a3ac9798cfb720cc96cb9c4c8f9d51d0d83ae529
Eko Widianto <ekowidianto@outlook.com>
2023-01-13 15:37:10 +0800

    feat(jobs api): add jobs API

--- client/app/api/index.ts
+++ client/app/api/index.ts
@@ -1,8 +1,10 @@
 import AnnouncementsAPI from './Announcements';
+import JobsAPI from './Jobs';
 import UsersAPI from './Users';
 
 const GlobalAPI = {
   announcements: new AnnouncementsAPI(),
+  jobs: new JobsAPI(),
   users: new UsersAPI(),
 };
 
