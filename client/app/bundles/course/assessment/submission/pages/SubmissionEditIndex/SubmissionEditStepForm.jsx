f30ad5f4293313c823dcc852cad02d2bf386400a
Eko Widianto <ekowidianto@outlook.com>
2023-01-24 12:07:01 +0800

    feat(submission stepper): user red color for question stepper when answer is wrong

--- client/app/bundles/course/assessment/submission/pages/SubmissionEditIndex/SubmissionEditStepForm.jsx
+++ client/app/bundles/course/assessment/submission/pages/SubmissionEditIndex/SubmissionEditStepForm.jsx
@@ -514,6 +514,26 @@ const SubmissionEditStepForm = (props) => {
     );
   };
 
+  const renderStepperIcon = (questionId, questionIndex) => {
+    let stepButtonColor = '';
+    const isCurrentQuestion = questionIndex === stepIndex;
+    if (explanations[questionId]?.correct) {
+      stepButtonColor = isCurrentQuestion ? green[700] : green[300];
+    } else if (explanations[questionId]?.correct === false) {
+      stepButtonColor = isCurrentQuestion ? red[700] : red[300];
+    } else {
+      stepButtonColor = isCurrentQuestion ? blue[800] : lightBlue[400];
+    }
+    return (
+      <SvgIcon htmlColor={stepButtonColor}>
+        <circle cx="12" cy="12" r="12" />
+        <text fill="#fff" fontSize="12" textAnchor="middle" x="12" y="16">
+          {questionIndex + 1}
+        </text>
+      </SvgIcon>
+    );
+  };
+
   const renderStepper = () => {
     if (!questionIds || questionIds.length <= 1) {
       return null;
@@ -527,31 +547,11 @@ const SubmissionEditStepForm = (props) => {
         style={{ justifyContent: 'center', flexWrap: 'wrap', padding: 24 }}
       >
         {questionIds.map((questionId, index) => {
-          let stepButtonColor = '';
-          const isCurrentQuestion = index === stepIndex;
-          if (explanations[questionId] && explanations[questionId].correct) {
-            stepButtonColor = isCurrentQuestion ? green[700] : green[300];
-          } else {
-            stepButtonColor = isCurrentQuestion ? blue[800] : lightBlue[400];
-          }
           if (published || skippable || graderView || index <= maxStep) {
             return (
               <Step key={questionId} active={index <= maxStep}>
                 <StepButton
-                  icon={
-                    <SvgIcon htmlColor={stepButtonColor}>
-                      <circle cx="12" cy="12" r="12" />
-                      <text
-                        fill="#fff"
-                        fontSize="12"
-                        textAnchor="middle"
-                        x="12"
-                        y="16"
-                      >
-                        {index + 1}
-                      </text>
-                    </SvgIcon>
-                  }
+                  icon={renderStepperIcon(questionId, index)}
                   onClick={() => handleStepClick(index)}
                   style={styles.stepButton}
                 />
