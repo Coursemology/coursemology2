df4c6827e08c9e27c3ded41588a5a759fdff7510
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-02-20 21:31:42 +0800

    fix(workbin): missing start times for linked folders

--- client/app/types/course/material/folders.ts
+++ client/app/types/course/material/folders.ts
@@ -12,7 +12,6 @@ export type FolderPermissions = Permissions<
   | 'canCreateSubfolder'
   | 'canUpload'
   | 'canEdit'
-  | 'canEditSubfolders'
 >;
 
 export type SubfolderPermissions = Permissions<
