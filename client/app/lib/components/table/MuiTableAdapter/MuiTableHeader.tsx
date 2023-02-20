11df786ef255e416493f6c1585b00a99786c4745
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-02-20 16:57:39 +0800

    feat(tables): add muitableadapter

---
+++ client/app/lib/components/table/MuiTableAdapter/MuiTableHeader.tsx
@@ -0,0 +1,44 @@
+import { TableCell, TableHead, TableRow, TableSortLabel } from '@mui/material';
+
+import { HeaderProps, isRowSelector } from '../adapters';
+
+import MuiFilterMenu from './MuiFilterMenu';
+import MuiTableRowSelector from './MuiTableRowSelector';
+
+const MuiTableHeader = <H,>(props: HeaderProps<H>): JSX.Element => (
+  <TableHead>
+    <TableRow>
+      {props.headers.map((header, index) => {
+        const headerProps = props.forEach(header, index);
+
+        return (
+          <TableCell key={headerProps.id} className="whitespace-nowrap">
+            {isRowSelector(headerProps.render) && (
+              <MuiTableRowSelector {...headerProps.render} />
+            )}
+
+            {!isRowSelector(headerProps.render) && headerProps.sorting && (
+              <TableSortLabel
+                active={headerProps.sorting.sorted}
+                direction={headerProps.sorting.direction}
+                onClick={headerProps.sorting.onClickSort}
+              >
+                {headerProps.render}
+              </TableSortLabel>
+            )}
+
+            {!isRowSelector(headerProps.render) &&
+              !headerProps.sorting &&
+              headerProps.render}
+
+            {headerProps.filtering && (
+              <MuiFilterMenu {...headerProps.filtering} />
+            )}
+          </TableCell>
+        );
+      })}
+    </TableRow>
+  </TableHead>
+);
+
+export default MuiTableHeader;
