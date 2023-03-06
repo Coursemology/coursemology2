2b8209edbe03071481f8cd5d71c3aef31c1f40e1
Eko Widianto <ekowidianto@outlook.com>
2023-03-06 23:08:10 +0800

    fix(auto feedback): fix failed job due to unimplemented function being called

--- app/jobs/course/assessment/submission/auto_feedback_job.rb
+++ app/jobs/course/assessment/submission/auto_feedback_job.rb
@@ -12,7 +12,11 @@ class Course::Assessment::Submission::AutoFeedbackJob < ApplicationJob
   def perform_tracked(submission)
     instance = Course.unscoped { submission.assessment.course.instance }
     ActsAsTenant.with_tenant(instance) do
-      submission.current_answers.each(&:generate_feedback)
+      submission.current_answers.each do |current_answer|
+        if current_answer.specific.self_respond_to?(:generate_feedback)
+          current_answer.specific.generate_feedback
+        end
+      end
     end
   end
 end
