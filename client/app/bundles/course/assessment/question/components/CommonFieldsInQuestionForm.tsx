fc70da8c483b8b9bb37fe34eb338966d76ddede9
bivanalhar <bivan.alzacky@gmail.com>
2023-03-06 16:52:27 +0800

    refactor(question_form): create common fields in question form and refactor mcq/mrq and voice response form

---
+++ client/app/bundles/course/assessment/question/components/CommonFieldsInQuestionForm.tsx
@@ -0,0 +1,140 @@
+import { Control, Controller, FieldValues } from 'react-hook-form';
+import { Alert } from '@mui/material';
+
+import Section from 'lib/components/core/layouts/Section';
+import Subsection from 'lib/components/core/layouts/Subsection';
+import Link from 'lib/components/core/Link';
+import FormRichTextField from 'lib/components/form/fields/RichTextField';
+import FormTextField from 'lib/components/form/fields/TextField';
+import useTranslation from 'lib/hooks/useTranslation';
+
+import translations from '../../translations';
+
+import SkillsAutocomplete from './SkillsAutocomplete';
+
+interface CommonFieldsProps {
+  disabled?: boolean;
+  control?: Control<FieldValues, unknown>;
+  availableSkills?: Record<
+    number,
+    {
+      id: number;
+      title: string;
+      description: string;
+    }
+  > | null;
+  skillsUrl?: string;
+}
+
+const CommonFieldsInQuestionForm = (props: CommonFieldsProps): JSX.Element => {
+  const { disabled: submitting, control, availableSkills, skillsUrl } = props;
+
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <Section sticksToNavbar title={t(translations.questionDetails)}>
+        <Controller
+          control={control}
+          name="title"
+          render={({ field, fieldState }): JSX.Element => (
+            <FormTextField
+              disabled={submitting}
+              field={field}
+              fieldState={fieldState}
+              fullWidth
+              label={t(translations.title)}
+              variant="filled"
+            />
+          )}
+        />
+
+        <Subsection title={t(translations.description)}>
+          <Controller
+            control={control}
+            name="description"
+            render={({ field, fieldState }): JSX.Element => (
+              <FormRichTextField
+                disabled={submitting}
+                field={field}
+                fieldState={fieldState}
+              />
+            )}
+          />
+        </Subsection>
+
+        <Subsection
+          subtitle={t(translations.staffOnlyCommentsHint)}
+          title={t(translations.staffOnlyComments)}
+        >
+          <Controller
+            control={control}
+            name="staffOnlyComments"
+            render={({ field, fieldState }): JSX.Element => (
+              <FormRichTextField
+                disabled={submitting}
+                field={field}
+                fieldState={fieldState}
+              />
+            )}
+          />
+        </Subsection>
+      </Section>
+
+      <Section sticksToNavbar title={t(translations.grading)}>
+        <Controller
+          control={control}
+          name="maximumGrade"
+          render={({ field, fieldState }): JSX.Element => (
+            <FormTextField
+              disabled={submitting}
+              field={field}
+              fieldState={fieldState}
+              fullWidth
+              label={t(translations.maximumGrade)}
+              required
+              variant="filled"
+            />
+          )}
+        />
+      </Section>
+
+      <Section
+        sticksToNavbar
+        subtitle={t(translations.skillsHint)}
+        title={t(translations.skills)}
+      >
+        {availableSkills && (
+          <Controller
+            control={control}
+            name="skillIds"
+            render={({ field, fieldState: { error } }): JSX.Element => (
+              <SkillsAutocomplete
+                availableSkills={availableSkills}
+                error={error}
+                field={field}
+              />
+            )}
+          />
+        )}
+
+        <Alert severity="info">
+          {t(
+            availableSkills
+              ? translations.canConfigureSkills
+              : translations.noSkillsCanCreateSkills,
+            {
+              url: (chunks) => (
+                <Link href={skillsUrl} opensInNewTab>
+                  {chunks}
+                </Link>
+              ),
+            },
+          )}
+        </Alert>
+      </Section>
+    </>
+  );
+};
+
+export default CommonFieldsInQuestionForm;
--- client/app/bundles/course/assessment/question/voice-responses/VoiceForm.tsx
+++ client/app/bundles/course/assessment/question/voice-responses/VoiceForm.tsx
@@ -1,21 +1,12 @@
 import { useState } from 'react';
-import { Controller } from 'react-hook-form';
-import { Alert } from '@mui/material';
 import {
   VoiceData,
   VoiceFormData,
 } from 'types/course/assessment/question/voice-responses';
 
-import Section from 'lib/components/core/layouts/Section';
-import Subsection from 'lib/components/core/layouts/Subsection';
-import Link from 'lib/components/core/Link';
-import FormRichTextField from 'lib/components/form/fields/RichTextField';
-import FormTextField from 'lib/components/form/fields/TextField';
 import Form, { FormEmitter } from 'lib/components/form/Form';
-import useTranslation from 'lib/hooks/useTranslation';
 
-import translations from '../../translations';
-import SkillsAutocomplete from '../components/SkillsAutocomplete';
+import CommonFieldsInQuestionForm from '../components/CommonFieldsInQuestionForm';
 
 import questionSchema from './validation';
 
@@ -28,7 +19,6 @@ const VoiceForm = <T extends 'new' | 'edit'>(
   props: VoiceFormProps<T>,
 ): JSX.Element => {
   const { with: data } = props;
-  const { t } = useTranslation();
 
   const [submitting, setSubmitting] = useState(false);
   const [form, setForm] = useState<FormEmitter>();
@@ -48,8 +38,6 @@ const VoiceForm = <T extends 'new' | 'edit'>(
     });
   };
 
-  const availableSkills = data.availableSkills;
-
   return (
     <Form
       disabled={submitting}
@@ -60,108 +48,12 @@ const VoiceForm = <T extends 'new' | 'edit'>(
       validates={questionSchema}
     >
       {(control): JSX.Element => (
-        <>
-          <Section sticksToNavbar title={t(translations.questionDetails)}>
-            <Controller
-              control={control}
-              name="title"
-              render={({ field, fieldState }): JSX.Element => (
-                <FormTextField
-                  disabled={submitting}
-                  field={field}
-                  fieldState={fieldState}
-                  fullWidth
-                  label={t(translations.title)}
-                  variant="filled"
-                />
-              )}
-            />
-
-            <Subsection title={t(translations.description)}>
-              <Controller
-                control={control}
-                name="description"
-                render={({ field, fieldState }): JSX.Element => (
-                  <FormRichTextField
-                    disabled={submitting}
-                    field={field}
-                    fieldState={fieldState}
-                  />
-                )}
-              />
-            </Subsection>
-
-            <Subsection
-              subtitle={t(translations.staffOnlyCommentsHint)}
-              title={t(translations.staffOnlyComments)}
-            >
-              <Controller
-                control={control}
-                name="staffOnlyComments"
-                render={({ field, fieldState }): JSX.Element => (
-                  <FormRichTextField
-                    disabled={submitting}
-                    field={field}
-                    fieldState={fieldState}
-                  />
-                )}
-              />
-            </Subsection>
-          </Section>
-
-          <Section sticksToNavbar title={t(translations.grading)}>
-            <Controller
-              control={control}
-              name="maximumGrade"
-              render={({ field, fieldState }): JSX.Element => (
-                <FormTextField
-                  disabled={submitting}
-                  field={field}
-                  fieldState={fieldState}
-                  fullWidth
-                  label={t(translations.maximumGrade)}
-                  required
-                  variant="filled"
-                />
-              )}
-            />
-          </Section>
-
-          <Section
-            sticksToNavbar
-            subtitle={t(translations.skillsHint)}
-            title={t(translations.skills)}
-          >
-            {availableSkills && (
-              <Controller
-                control={control}
-                name="skillIds"
-                render={({ field, fieldState: { error } }): JSX.Element => (
-                  <SkillsAutocomplete
-                    availableSkills={availableSkills}
-                    error={error}
-                    field={field}
-                  />
-                )}
-              />
-            )}
-
-            <Alert severity="info">
-              {t(
-                availableSkills
-                  ? translations.canConfigureSkills
-                  : translations.noSkillsCanCreateSkills,
-                {
-                  url: (chunks) => (
-                    <Link href={data.skillsUrl} opensInNewTab>
-                      {chunks}
-                    </Link>
-                  ),
-                },
-              )}
-            </Alert>
-          </Section>
-        </>
+        <CommonFieldsInQuestionForm
+          availableSkills={data.availableSkills}
+          control={control}
+          disabled={submitting}
+          skillsUrl={data.skillsUrl}
+        />
       )}
     </Form>
   );
