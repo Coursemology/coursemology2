1f4bab820fae908a55bb92dcd50b8e268d8dd5c0
bivanalhar <bivan.alzacky@gmail.com>
2023-03-13 15:13:14 +0800

    feat(text-response): add API for new and edit text response and file upload

--- client/app/api/course/Assessment/Question/index.ts
+++ client/app/api/course/Assessment/Question/index.ts
@@ -1,12 +1,14 @@
 import ForumPostResponseAPI from './ForumPostResponse';
 import McqMrqAPI from './McqMrq';
 import ScribingQuestionAPI from './Scribing';
+import TextResponseAPI from './TextResponse';
 import VoiceResponseAPI from './VoiceResponse';
 
 const QuestionAPI = {
   forumPostResponse: new ForumPostResponseAPI(),
   mcqMrq: new McqMrqAPI(),
   scribing: new ScribingQuestionAPI(),
+  textResponse: new TextResponseAPI(),
   voiceResponse: new VoiceResponseAPI(),
 };
 
---
+++ client/app/types/course/assessment/question/text-responses.ts
@@ -0,0 +1,59 @@
+import {
+  AvailableSkills,
+  OptionalIfNew,
+  QuestionData,
+  QuestionFormData,
+} from '../questions';
+
+interface SolutionData {
+  id: number | string;
+  solution: string;
+  solutionType: string;
+  grade: number;
+  explanation: string;
+}
+
+interface SolutionEntity extends SolutionData {
+  toBeDeleted?: boolean;
+  draft?: boolean;
+}
+
+export type TextResponseListData = QuestionData & { solutions: SolutionData[] };
+
+export interface TextResponseData<T extends 'new' | 'edit' = 'edit'> {
+  solutions: SolutionEntity[] | null | OptionalIfNew<T>;
+  question:
+    | (QuestionFormData & {
+        allowAttachment: boolean;
+        hideText: boolean;
+      })
+    | OptionalIfNew<T>;
+}
+
+export interface TextResponseFormData<T extends 'new' | 'edit' = 'edit'>
+  extends TextResponseData<T>,
+    AvailableSkills {}
+
+type TextResponseFormDataQuestion = TextResponseFormData['question'];
+
+export interface TextResponsePostData {
+  question_text_response: {
+    title?: TextResponseFormDataQuestion['title'];
+    description?: TextResponseFormDataQuestion['description'];
+    staff_only_comments?: TextResponseFormDataQuestion['staffOnlyComments'];
+    maximum_grade?: TextResponseFormDataQuestion['maximumGrade'];
+    allow_attachment?: TextResponseFormDataQuestion['allowAttachment'];
+    hide_text?: TextResponseFormDataQuestion['hideText'];
+    question_assessment?: {
+      skill_ids: TextResponseFormDataQuestion['skillIds'];
+    };
+    solutions_attributes?: {
+      id?: SolutionEntity['id'];
+      solution?: SolutionEntity['solution'];
+      solutionType?: SolutionEntity['solutionType'];
+      grade?: SolutionEntity['grade'];
+      explanation?: SolutionEntity['explanation'];
+      _destroy?: SolutionEntity['toBeDeleted'];
+    };
+  };
+}
