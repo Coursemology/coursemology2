c6893a15877db2ace2eb0b73c03b263a6a5531fd
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-04-03 20:48:05 +0800

    feat(submissions): unsubmit resumes monitoring session

--- app/jobs/course/assessment/submission/unsubmitting_job.rb
+++ app/jobs/course/assessment/submission/unsubmitting_job.rb
@@ -20,7 +20,7 @@ class Course::Assessment::Submission::UnsubmittingJob < ApplicationJob
     instance = Course.unscoped { assessment.course.instance }
     ActsAsTenant.with_tenant(instance) do
       submissions = assessment.submissions.find(submission_ids)
-      unsubmit_submission(submissions, question, unsubmitter)
+      unsubmit_submission(assessment, submissions, question, unsubmitter)
     end
 
     redirect_to redirect_to_path
@@ -33,13 +33,18 @@ class Course::Assessment::Submission::UnsubmittingJob < ApplicationJob
   # @param [Course::Submissions] submissions Submissions that are to be unsubmitted.
   # @param [Course::Assessment::Question] question Optional question that should have its answers deleted.
   # @param [User] unsubmitter The user object who would be unsubmitting the submission.
-  def unsubmit_submission(submissions, question, unsubmitter)
+  def unsubmit_submission(assessment, submissions, question, unsubmitter)
     User.with_stamper(unsubmitter) do
       Course::Assessment::Submission.transaction do
+        creator_ids = []
         submissions.each do |submission|
           submission.update!('unmark' => 'true') if submission.graded?
           submission.update!('unsubmit' => 'true') unless submission.attempting?
+          creator_ids << submission.creator_id
         end
+
+        Course::Assessment::Submission::MonitoringService.continue_listening_from(assessment, creator_ids)
+
         question&.answers&.destroy_all
       end
     end
