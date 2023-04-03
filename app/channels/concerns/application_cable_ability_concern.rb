a42249cee94c6004ff7badddd7ef66e5bcc507f1
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-04-03 20:17:14 +0800

    feat(cable): extend cancancan ability

---
+++ app/channels/concerns/application_cable_ability_concern.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+module ApplicationCableAbilityConcern
+  extend ActiveSupport::Concern
+
+  included do
+    before_subscribe :load_current_ability
+  end
+
+  def current_ability
+    @current_ability ||= Ability.new(current_user, current_course, current_course_user, request.session)
+  end
+
+  def can?(*args)
+    current_ability.can?(*args)
+  end
+
+  def cannot?(*args)
+    current_ability.cannot?(*args)
+  end
+
+  alias_method :load_current_ability, :current_ability
+end
--- app/channels/course_channel.rb
+++ app/channels/course_channel.rb
@@ -8,8 +8,16 @@
 # - `current_course`
 # - `current_course_user`
 # - `current_component_host`
+# - `current_ability`
+# - `can?` and `cannot?` from `CanCan::Ability`
+#
+# Note that the more inclusions are added, the more queries and operations are executed
+# during subscriptions or actions, depending on the callbacks used in each included modules.
+# These features are broken up into concerns so that future channels can opt in to only the
+# capabilities they need.
 class CourseChannel < ApplicationCable::Channel
   include ApplicationCableMultitenancyConcern
   include ApplicationCableCourseConcern
   include ApplicationCableComponentConcern
+  include ApplicationCableAbilityConcern
 end
