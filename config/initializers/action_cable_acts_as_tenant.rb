fb9b1a6d16f35706a3478f0d0f8200a93fec12f9
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-04-03 20:12:43 +0800

    feat(cable): extend `set_current_tenant_through_filter`

---
+++ config/initializers/action_cable_acts_as_tenant.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+module ActionCable::ActsAsTenantFilterConcern
+  extend ActiveSupport::Concern
+
+  private
+
+  # rubocop:disable Naming/AccessorMethodName
+  def set_current_tenant(current_tenant_object)
+    ActsAsTenant.current_tenant = current_tenant_object
+  end
+  # rubocop:enable Naming/AccessorMethodName
+end
+
+module ActionCable::ActsAsTenantExtensions
+  def set_current_tenant_through_filter
+    include ActionCable::ActsAsTenantFilterConcern
+  end
+end
+
+# Adds support for ActsAsTenant's `set_current_tenant_through_filter`
+# in Action Cable channels.
+#
+# Adapted from https://github.com/ErwinM/acts_as_tenant/pull/280
+ActiveSupport.on_load(:action_cable_channel) do |base|
+  base.extend ActionCable::ActsAsTenantExtensions
+  base.include ActsAsTenant::TenantHelper
+end
