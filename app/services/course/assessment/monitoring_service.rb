7114e38ad357633969e8513283393c61b606cc90
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-04-03 20:43:33 +0800

    feat(monitoring): add assessment monitoring service

---
+++ app/services/course/assessment/monitoring_service.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+class Course::Assessment::MonitoringService
+  class << self
+    def params
+      [:enabled, :seb_hash, :min_interval_ms, :max_interval_ms, :offset_ms]
+    end
+  end
+
+  def initialize(assessment)
+    @assessment = assessment
+  end
+
+  def monitor
+    @monitor ||= @assessment.monitor
+  end
+
+  def upsert!(params)
+    return unless monitor.present? || params[:enabled]
+
+    if monitor.present?
+      monitor.update!(params)
+    else
+      @monitor = Course::Monitoring::Monitor.create!(params)
+      @assessment.update!(monitor: @monitor)
+    end
+  end
+end
