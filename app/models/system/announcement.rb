fb58cd4d2eea36346d45b201c3e7e07983695658
Eko Widianto <ekowidianto@outlook.com>
2023-04-03 16:55:26 +0800

    refactor(announcement): generalise announcement list data - removed unused rails page

--- app/models/system/announcement.rb
+++ app/models/system/announcement.rb
@@ -1,8 +1,4 @@
 # frozen_string_literal: true
 class System::Announcement < GenericAnnouncement
   validates :instance, absence: true
-
-  def to_partial_path
-    'system/admin/announcements/announcement'
-  end
 end
