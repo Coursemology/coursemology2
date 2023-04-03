8caff12368cd146ebdc973d6e9ee80f3a8053a10
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-04-03 20:09:45 +0800

    feat(cable): allow connections from all hosts on dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { address: '127.0.0.1', port: 1025 }
   config.action_mailer.raise_delivery_errors = false
+
+  config.action_cable.disable_request_forgery_protection = true
 end
