5a94356ee985c920c14174d6e831bc1e99e25012
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-04-06 17:59:20 +0800

    feat(pulsegrid): add legend for sessionblob colours

--- client/app/bundles/course/assessment/pages/AssessmentMonitoring/components/SessionBlob.tsx
+++ client/app/bundles/course/assessment/pages/AssessmentMonitoring/components/SessionBlob.tsx
@@ -3,21 +3,27 @@ import { Tooltip } from '@mui/material';
 import { Snapshot } from 'types/channels/liveMonitoring';
 
 interface SessionBlobProps extends ComponentProps<'div'> {
-  for: Snapshot;
+  for?: Snapshot;
   className?: string;
 }
 
 const SessionBlob = (props: SessionBlobProps): JSX.Element => {
   const { for: snapshot, ...divProps } = props;
 
+  const blob = (
+    <div
+      {...divProps}
+      className={`m-1 shrink-0 rounded wh-8 hover:ring-2 hover:ring-offset-1 ${
+        snapshot?.selected ? 'ring-2 ring-offset-1' : ''
+      } ${snapshot?.hidden ? 'hidden' : ''} ${props.className ?? ''}`}
+    />
+  );
+
+  if (!snapshot) return blob;
+
   return (
     <Tooltip arrow disableInteractive title={snapshot.userName}>
-      <div
-        {...divProps}
-        className={`m-1 rounded wh-8 hover:ring-2 hover:ring-offset-1 ${
-          snapshot.selected ? 'ring-2 ring-offset-1' : ''
-        } ${snapshot.hidden ? 'hidden' : ''} ${props.className ?? ''}`}
-      />
+      {blob}
     </Tooltip>
   );
 };
---
+++ client/app/bundles/course/assessment/pages/AssessmentMonitoring/components/SessionBlobLegend.tsx
@@ -0,0 +1,68 @@
+import { memo } from 'react';
+import { Tooltip, Typography } from '@mui/material';
+
+import useTranslation from 'lib/hooks/useTranslation';
+
+import translations from '../../../translations';
+
+import { PRESENCE_COLORS } from './Session';
+import SessionBlob from './SessionBlob';
+
+interface SessionBlobLegendProps {
+  hasSEBHash: boolean;
+}
+
+const SessionBlobLegend = (props: SessionBlobLegendProps): JSX.Element => {
+  const { t } = useTranslation();
+
+  return (
+    <Tooltip
+      title={
+        <div className="flex flex-col space-y-4">
+          <div className="flex space-x-2">
+            <SessionBlob className="bg-neutral-100" />
+            <Typography className="mt-1" variant="body2">
+              {t(translations.noActiveSessions)}
+            </Typography>
+          </div>
+
+          <div className="flex space-x-2">
+            <SessionBlob className={PRESENCE_COLORS.alive} />
+
+            <Typography className="mt-1" variant="body2">
+              {props.hasSEBHash
+                ? t(translations.alivePresenceHintSEBHashMatches)
+                : t(translations.alivePresenceHint)}
+            </Typography>
+          </div>
+
+          <div className="flex space-x-2">
+            <SessionBlob className={PRESENCE_COLORS.late} />
+
+            <Typography className="mt-1" variant="body2">
+              {t(translations.latePresenceHint)}
+            </Typography>
+          </div>
+
+          <div className="flex space-x-2">
+            <SessionBlob className={PRESENCE_COLORS.missing} />
+
+            <Typography className="mt-1" variant="body2">
+              {t(translations.missingPresenceHint)}
+            </Typography>
+          </div>
+        </div>
+      }
+    >
+      <Typography
+        className="w-fit cursor-pointer underline"
+        color="text.secondary"
+        variant="body2"
+      >
+        What do these colours mean?
+      </Typography>
+    </Tooltip>
+  );
+};
+
+export default memo(SessionBlobLegend);
