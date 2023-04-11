533b454f4dbfd607b8003cd7c1233cb511a91c00
Eko Widianto <ekowidianto@outlook.com>
2023-04-11 08:43:42 +0800

    refactor: rename migration and nit function name change

--- db/migrate/20230410121228_add_allow_record_draft_answer_to_assessments.rb
+++ db/migrate/20230410121228_add_allow_record_draft_answer_to_assessments.rb
@@ -1,4 +1,4 @@
-class UpdateAssessmentTable < ActiveRecord::Migration[6.0]
+class AddAllowRecordDraftAnswerToAssessments < ActiveRecord::Migration[6.0]
   def change
     add_column :course_assessments, :allow_record_draft_answer, :boolean, default: false
   end
