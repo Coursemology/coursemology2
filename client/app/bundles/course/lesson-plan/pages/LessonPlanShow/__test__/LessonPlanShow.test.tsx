f115607e512f5ccb075834c193acf94c49ca9f6e
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-05-11 15:06:51 +0800

    test(LessonPlanShow): fix store, migrate to rtl

---
+++ client/app/bundles/course/lesson-plan/pages/LessonPlanShow/__test__/LessonPlanShow.test.tsx
@@ -0,0 +1,94 @@
+import { render } from 'utilities/test-utils';
+
+import { UnconnectedLessonPlanShow as LessonPlanShow } from '../index';
+
+const data = {
+  groups: [
+    {
+      id: 'milestone-group-76',
+      milestone: null,
+      items: [
+        {
+          id: 44,
+          start_at: '2017-01-04T00:00:00.000+08:00',
+          itemTypeKey: 'Event',
+          title: 'Ungrouped Item',
+        },
+      ],
+    },
+    {
+      id: 'milestone-group-78',
+      milestone: {
+        id: 63,
+        start_at: '2017-01-06T00:00:00.000+08:00',
+        title: 'Semester 1',
+      },
+      items: [
+        {
+          id: 45,
+          start_at: '2017-01-08T00:00:00.000+08:00',
+          itemTypeKey: 'Event',
+          title: 'First Lecture',
+        },
+      ],
+    },
+  ],
+  visibility: { Event: true },
+  isLoading: false,
+};
+
+describe('<LessonPlanShow />', () => {
+  describe('when all milestones are expanded by default', () => {
+    it('shows all visible items', () => {
+      const page = render(
+        <LessonPlanShow
+          canManageLessonPlan
+          milestonesExpanded="all"
+          {...data}
+        />,
+      );
+
+      data.groups.forEach((group) =>
+        group.items.forEach((item) =>
+          expect(page.getByText(item.title)).toBeVisible(),
+        ),
+      );
+    });
+  });
+
+  describe('when none of the milestones are expanded by default', () => {
+    it('shows no items', () => {
+      const page = render(
+        <LessonPlanShow
+          canManageLessonPlan
+          milestonesExpanded="none"
+          {...data}
+        />,
+      );
+
+      data.groups.forEach((group) =>
+        group.items.forEach((item) =>
+          expect(page.queryByText(item.title)).not.toBeInTheDocument(),
+        ),
+      );
+    });
+  });
+
+  describe('when only one of the current milestone is expanded by default', () => {
+    it('shows items for current group', () => {
+      const page = render(
+        <LessonPlanShow
+          canManageLessonPlan
+          milestonesExpanded="current"
+          {...data}
+        />,
+      );
+
+      const hiddenItem = data.groups[0].items[0].title;
+      const shownItem = data.groups[1].items[0].title;
+
+      expect(page.queryByText(hiddenItem)).not.toBeInTheDocument();
+      expect(page.getByText(shownItem)).toBeVisible();
+    });
+  });
+});
