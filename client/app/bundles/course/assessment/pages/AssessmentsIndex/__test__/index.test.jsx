02e528a2c233adc7c72bc8932190bc0c82c9bbb7
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-05-16 22:33:50 +0800

    test(AssessmentsIndex): fix store, migrate to rtl

---
+++ client/app/bundles/course/assessment/pages/AssessmentsIndex/__test__/index.test.jsx
@@ -0,0 +1,15 @@
+import { fireEvent, render } from 'test-utils';
+
+import AssessmentIndex from '../NewAssessmentFormButton';
+
+describe('<AssessmentIndex />', () => {
+  it('renders the index page', async () => {
+    const page = render(<AssessmentIndex categoryId={1} tabId={1} />);
+
+    const newButton = page.getByRole('button');
+    fireEvent.click(newButton);
+
+    expect(page.getByRole('heading', { name: 'New Assessment' })).toBeVisible();
+    expect(page.getByLabelText('Title', { exact: false })).toBeVisible();
+  });
+});
