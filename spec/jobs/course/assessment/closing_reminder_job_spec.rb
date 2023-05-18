e4f17c36d84ba8a090cc8ea660360d43d0a86e42
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-05-18 20:27:38 +0800

    test(closing_reminder_job): test if token is always updated

--- spec/jobs/course/assessment/closing_reminder_job_spec.rb
+++ spec/jobs/course/assessment/closing_reminder_job_spec.rb
@@ -8,17 +8,20 @@ RSpec.describe Course::Assessment::ClosingReminderJob do
 
     context 'when end_at of the assessment is changed' do
       it 'creates a closing reminder job' do
+        old_closing_reminder_token = assessment.closing_reminder_token
         assessment.end_at = 1.day.from_now
 
         expect { assessment.save }.to have_enqueued_job(Course::Assessment::ClosingReminderJob)
+        expect(assessment.closing_reminder_token).not_to eq(old_closing_reminder_token)
       end
 
       context 'when end_at is a past time' do
-        it 'does not do anything' do
+        it 'does not create a closing reminder job, but updates the token' do
+          old_closing_reminder_token = assessment.closing_reminder_token
           assessment.end_at = 1.day.ago
 
-          expect { assessment.save }.
-            not_to have_enqueued_job(Course::Assessment::ClosingReminderJob)
+          expect { assessment.save }.not_to have_enqueued_job(Course::Assessment::ClosingReminderJob)
+          expect(assessment.closing_reminder_token).not_to eq(old_closing_reminder_token)
         end
       end
     end
