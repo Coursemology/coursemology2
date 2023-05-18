19ff42d0a3add378b9481f0161f3a1be868bfc3e
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-05-18 10:29:54 +0800

    test(UserEmailSubscriptions): fix store, migrate to rtl

---
+++ client/app/bundles/course/user-email-subscriptions/__test__/index.test.jsx
@@ -0,0 +1,52 @@
+import { fireEvent, render, waitFor } from 'test-utils';
+
+import CourseAPI from 'api/course';
+
+import UserEmailSubscriptions from '../UserEmailSubscriptionsTable';
+
+const state = {
+  userEmailSubscriptions: {
+    settings: [
+      {
+        component: 'sample_component',
+        course_assessment_category_id: null,
+        setting: 'email_for_some_event',
+        enabled: false,
+      },
+    ],
+    pageFilter: {
+      show_all_settings: true,
+      component: null,
+      category_id: null,
+      setting: null,
+    },
+  },
+};
+
+const expectedPayload = {
+  show_all_settings: true,
+  component: null,
+  category_id: null,
+  setting: null,
+  user_email_subscriptions: {
+    component: 'sample_component',
+    course_assessment_category_id: null,
+    setting: 'email_for_some_event',
+    enabled: true,
+  },
+};
+
+describe('<UserEmailSubscriptions />', () => {
+  it('allow emails subscription settings to be set', async () => {
+    const spy = jest.spyOn(CourseAPI.userEmailSubscriptions, 'update');
+
+    const page = render(<UserEmailSubscriptions />, { state });
+
+    const toggle = page.getByRole('checkbox');
+    fireEvent.click(toggle);
+
+    await waitFor(() => {
+      expect(spy).toHaveBeenCalledWith(expectedPayload);
+    });
+  });
+});
