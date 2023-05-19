872efd8c8968566f4e79d1146b533cb425e17bd0
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-05-19 15:07:40 +0800

    refactor(video/submission): use react-toastify for notifications
    video/submission cannot use `NotificationPopup` because it is on a different store

--- client/app/bundles/course/video/submission/containers/Submission.jsx
+++ client/app/bundles/course/video/submission/containers/Submission.jsx
@@ -1,37 +1,18 @@
-import { connect } from 'react-redux';
 import { Grid } from '@mui/material';
-
-import NotificationBar, {
-  notificationShape,
-} from 'lib/components/core/NotificationBar';
 
 import Discussion from './Discussion';
 import VideoPlayer from './VideoPlayer';
 
-const propTypes = {
-  notification: notificationShape,
-};
-
-const Submission = (props) => (
-  <>
-    <Grid container spacing={2}>
-      <Grid item lg={8} xs={12}>
-        <VideoPlayer />
-      </Grid>
-      <Grid className="sticky top-0 h-[calc(95vh-70px)]" item lg={4} xs={12}>
-        <Discussion />
-      </Grid>
+const Submission = () => (
+  <Grid container spacing={2}>
+    <Grid item lg={8} xs={12}>
+      <VideoPlayer />
     </Grid>
-    <NotificationBar notification={props.notification} />
-  </>
+
+    <Grid className="sticky top-0 h-[calc(95vh-70px)]" item lg={4} xs={12}>
+      <Discussion />
+    </Grid>
+  </Grid>
 );
 
-Submission.propTypes = propTypes;
-
-function mapStateToProps(state) {
-  return {
-    notification: state.notification,
-  };
-}
-
-export default connect(mapStateToProps)(Submission);
+export default Submission;
