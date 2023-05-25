fde3448b9977c186fdbef1e46e7b8880e4e6fdd9
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-05-25 11:11:38 +0800

    fix(closing_reminder_concern): token not saved if not called before save

--- spec/jobs/course/assessment/closing_reminder_job_spec.rb
+++ spec/jobs/course/assessment/closing_reminder_job_spec.rb
@@ -7,21 +7,40 @@ RSpec.describe Course::Assessment::ClosingReminderJob do
     let(:assessment) { create(:assessment) }
 
     context 'when end_at of the assessment is changed' do
-      it 'creates a closing reminder job' do
-        old_closing_reminder_token = assessment.closing_reminder_token
-        assessment.end_at = 1.day.from_now
+      context 'when end_at is a time in the future' do
+        it 'creates a closing reminder job' do
+          old_closing_reminder_token = assessment.closing_reminder_token
+          new_end_at = 1.day.from_now
+          assessment.end_at = 1.day.from_now
 
-        expect { assessment.save }.to have_enqueued_job(Course::Assessment::ClosingReminderJob)
-        expect(assessment.closing_reminder_token).not_to eq(old_closing_reminder_token)
+          expect { assessment.save }.to have_enqueued_job(Course::Assessment::ClosingReminderJob).exactly(:once)
+          expect(assessment.reload.end_at.to_i).to eq(new_end_at.to_i)
+          expect(assessment.reload.closing_reminder_token).not_to eq(old_closing_reminder_token)
+        end
       end
 
       context 'when end_at is a past time' do
         it 'does not create a closing reminder job, but updates the token' do
           old_closing_reminder_token = assessment.closing_reminder_token
-          assessment.end_at = 1.day.ago
+          new_end_at = 1.day.ago
+          assessment.end_at = new_end_at
 
           expect { assessment.save }.not_to have_enqueued_job(Course::Assessment::ClosingReminderJob)
-          expect(assessment.closing_reminder_token).not_to eq(old_closing_reminder_token)
+          expect(assessment.reload.end_at.to_i).to eq(new_end_at.to_i)
+          expect(assessment.reload.closing_reminder_token).not_to eq(old_closing_reminder_token)
+        end
+      end
+
+      context 'when end_at becomes nil' do
+        let(:future_assessment) { create(:assessment, end_at: 1.day.from_now) }
+
+        it 'does not create a closing reminder job, but updates the token' do
+          old_closing_reminder_token = future_assessment.closing_reminder_token
+          future_assessment.end_at = nil
+
+          expect { future_assessment.save }.not_to have_enqueued_job(Course::Assessment::ClosingReminderJob)
+          expect(future_assessment.reload.end_at).to eq(nil)
+          expect(future_assessment.reload.closing_reminder_token).not_to eq(old_closing_reminder_token)
         end
       end
     end
