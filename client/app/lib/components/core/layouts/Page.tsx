09b5cdf329d7c6700675d590bf17f05b6b15c518
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-05-29 15:24:00 +0800

    feat(components): add `Page`, deprecate `PageHeader`

---
+++ client/app/lib/components/core/layouts/Page.tsx
@@ -0,0 +1,78 @@
+import { ReactNode } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ArrowBack } from '@mui/icons-material';
+import { IconButton, Typography } from '@mui/material';
+
+interface PageProps {
+  title?: ReactNode;
+  children?: ReactNode;
+  actions?: ReactNode;
+  backTo?: string | boolean;
+  className?: string;
+  unpadded?: boolean;
+}
+
+interface PageSectionProps {
+  className?: string;
+  children?: ReactNode;
+}
+
+const Page = (props: PageProps): JSX.Element => {
+  const { backTo: route } = props;
+
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      {(props.title || props.actions) && (
+        <header className="flex min-h-[6rem] items-center bg-white px-6 border-only-b-neutral-200">
+          <div className="flex w-full items-center justify-between space-x-4">
+            {props.title && (
+              <div className="flex items-center space-x-4">
+                {route && (
+                  <IconButton
+                    data-testid="ArrowBackIconButton"
+                    onClick={(): void =>
+                      route === true ? navigate(-1) : navigate(route)
+                    }
+                  >
+                    <ArrowBack data-testid="ArrowBack" />
+                  </IconButton>
+                )}
+
+                <Typography variant="h5">{props.title}</Typography>
+              </div>
+            )}
+
+            {props.actions && (
+              <div className="flex space-x-2">{props.actions}</div>
+            )}
+          </div>
+        </header>
+      )}
+
+      <main
+        className={`relative ${!props.unpadded ? 'p-6' : ''} ${
+          props.className ?? ''
+        }`}
+      >
+        {props.children}
+      </main>
+    </div>
+  );
+};
+
+const PaddedPageSection = (props: PageSectionProps): JSX.Element => (
+  <section className={`p-6 ${props.className ?? ''}`}>{props.children}</section>
+);
+
+const UnpaddedPageSection = (props: PageSectionProps): JSX.Element => (
+  <section className={`-m-6 ${props.className ?? ''}`}>
+    {props.children}
+  </section>
+);
+
+export default Object.assign(Page, {
+  PaddedSection: PaddedPageSection,
+  UnpaddedSection: UnpaddedPageSection,
+});
