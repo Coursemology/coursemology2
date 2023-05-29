7f78bc0477f83300f5b0283b2ad1ef720be707e8
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-05-29 15:24:00 +0800

    feat(components): add `Page`, deprecate `PageHeader`

--- client/app/lib/components/navigation/PageHeader.tsx
+++ client/app/lib/components/navigation/PageHeader.tsx
@@ -1,37 +1,51 @@
-import { FC, ReactNode } from 'react';
+import { ReactNode } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ArrowBack from '@mui/icons-material/ArrowBack';
-import { IconButton } from '@mui/material';
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
 
-import TitleBar from 'lib/components/navigation/TitleBar';
-
-interface Props {
-  title: ReactNode | string;
+interface PageHeaderProps {
+  title: NonNullable<ReactNode>;
   returnLink?: string;
   toolbars?: ReactNode;
   children?: ReactNode;
 }
 
-const PageHeader: FC<Props> = (props) => {
+/**
+ * @deprecated Pages should use `Page` and the `title` prop instead.
+ */
+const PageHeader = (props: PageHeaderProps): JSX.Element => {
   const { title, returnLink, toolbars, children } = props;
   const navigate = useNavigate();
 
   return (
-    <TitleBar
-      data-testid="TitleBar"
-      iconElementLeft={
-        returnLink ? (
-          <IconButton
-            data-testid="ArrowBackIconButton"
-            onClick={(): void => navigate(returnLink)}
-          >
-            <ArrowBack data-testid="ArrowBack" htmlColor="white" />
-          </IconButton>
-        ) : null
-      }
-      iconElementRight={children ?? toolbars ?? null}
-      title={title}
-    />
+    <AppBar
+      className="border-only-b-neutral-200"
+      color="transparent"
+      elevation={0}
+      position="static"
+    >
+      <Toolbar className="flex items-center justify-between px-8 py-4">
+        <div className="flex min-h-[4rem] w-full items-center">
+          {returnLink && (
+            <IconButton
+              data-testid="ArrowBackIconButton"
+              edge="start"
+              onClick={(): void => navigate(returnLink)}
+            >
+              <ArrowBack data-testid="ArrowBack" />
+            </IconButton>
+          )}
+
+          <Typography className="line-clamp-2" color="inherit" variant="h5">
+            {title}
+          </Typography>
+        </div>
+
+        <div className="flex shrink-0 items-center">
+          {children ?? toolbars ?? null}
+        </div>
+      </Toolbar>
+    </AppBar>
   );
 };
 
