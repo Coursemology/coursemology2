a48fb6261f39c5bc8d2b44e649ee7492cad145ce
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-05-29 14:29:37 +0800

    feat(router): add Dynamic Nest API

---
+++ client/app/lib/hooks/router/dynamicNest/crumbs.ts
@@ -0,0 +1,55 @@
+import produce from 'immer';
+
+import { Descriptor } from 'lib/hooks/useTranslation';
+
+import { isRecord, Match } from './utils';
+
+export type CrumbTitle = string | Descriptor | null | undefined;
+
+export interface CrumbContent {
+  url?: string;
+  title?: CrumbTitle;
+}
+
+export interface CrumbData {
+  id: string;
+  pathname: string;
+  activePath?: string | null;
+  content?: CrumbContent | CrumbContent[];
+}
+
+export interface CrumbPath {
+  pathname?: string;
+  activePath?: string | null;
+  content?: CrumbContent | CrumbContent[];
+}
+
+export type CrumbState = Record<string, CrumbData>;
+
+export const isCrumbPath = (value: unknown): value is CrumbPath =>
+  isRecord(value) && 'content' in value;
+
+export const buildCrumbData = (
+  match: Match,
+  data: CrumbTitle | CrumbPath,
+): CrumbData => ({
+  id: match.id,
+  pathname: match.pathname,
+  ...(isCrumbPath(data)
+    ? data
+    : {
+        content: {
+          title: data,
+          url: match.pathname,
+        },
+      }),
+});
+
+export const combineCrumbs = (
+  delta: CrumbData[],
+): ((state: CrumbState) => CrumbState) =>
+  produce((draft) => {
+    delta.forEach((crumb) => {
+      draft[crumb.pathname] = crumb;
+    });
+  });
