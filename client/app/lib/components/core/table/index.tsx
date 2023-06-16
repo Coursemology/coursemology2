6dfd65e99e50578658019282b585b8f4cf4a2ab3
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-06-17 00:08:33 +0800

    style(table): remove deprecated `HorizontalTable`

--- client/app/lib/components/core/table/index.tsx
+++ client/app/lib/components/core/table/index.tsx
@@ -1,4 +1,3 @@
-export { default as HorizontalTable } from './HorizontalTable';
 export { default as TableContainer } from './TableContainer';
 export type { ColumnTemplate } from './template';
 export { default as VerticalTable } from './VerticalTable';
