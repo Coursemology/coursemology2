52a14c226a8f0c3e9081de5b940e8cdaebe7f39f
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-06-24 22:25:21 +0800

    feat(layouts): separate renders for react and static pages

--- app/helpers/application_theming_helper.rb
+++ app/helpers/application_theming_helper.rb
@@ -4,11 +4,25 @@ module ApplicationThemingHelper
     # Used to include external javascript/css files, Currently it's empty
   end
 
+  # TODO: Remove this once fully SPA
   def page_class
     return nil if content_for?(:page_class_specified)
 
     page_class = super
     content_for(:page_class_specified) { page_class }
-    page_class
+    @page_class = page_class
+  end
+
+  # TODO: Remove this once fully SPA
+  def rails_page?
+    class_identifier = page_class.split.first
+
+    [
+      'high-voltage-pages',
+      'user-sessions',
+      'user-registrations',
+      'devise-passwords',
+      'devise-confirmations'
+    ].include? class_identifier
   end
 end
