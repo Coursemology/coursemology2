5e3af60b43e57d86b507ff5828576d0a1caebf85
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-06-24 21:24:27 +0800

    style(DescriptionCard): extract translation, flatten borders

--- client/app/lib/components/core/DescriptionCard.tsx
+++ client/app/lib/components/core/DescriptionCard.tsx
@@ -1,16 +1,29 @@
 import { FC } from 'react';
+import { defineMessages } from 'react-intl';
 import { Card, CardContent, CardHeader, Typography } from '@mui/material';
+
+import useTranslation from 'lib/hooks/useTranslation';
 
 interface Props {
   description: string;
 }
 
+const translations = defineMessages({
+  description: {
+    id: 'lib.components.core.DescriptionCard.description',
+    defaultMessage: 'Description',
+  },
+});
+
 const DescriptionCard: FC<Props> = (props) => {
   const { description } = props;
 
+  const { t } = useTranslation();
+
   return (
-    <Card className="mt-6">
-      <CardHeader title="Description" />
+    <Card className="mt-6" variant="outlined">
+      <CardHeader title={t(translations.description)} />
+
       <CardContent>
         <section className="rounded-lg bg-neutral-100 p-4">
           <Typography
