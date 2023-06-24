0d194e0974efae9dc3aaa0d159d1deae689132ab
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-06-24 21:08:46 +0800

    fix(table): cannot type space in search field in prod

--- client/app/lib/components/table/utils.ts
+++ client/app/lib/components/table/utils.ts
@@ -1,7 +1,7 @@
 const DEFAULT_CSV_FILENAME = 'data' as const;
 
 // TODO: Remove this suppression when more exports are added.
-// eslint-disable-next-line import/prefer-default-export
+
 export const downloadCsv = (csvData: string, filename?: string): void => {
   const csvContent = `data:text/csv;charset=utf-8,${csvData}`;
   const encodedUri = encodeURI(csvContent);
