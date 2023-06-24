066293913f3777f965db92853980a82340faae8b
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-06-24 22:13:48 +0800

    test(requests): remove redundant high_voltage_pages_spec

--- spec/requests/high_voltage_pages_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-require 'rails_helper'
-
-RSpec.describe 'High Voltage Pages' do
-  it 'displays the proper page action class' do
-    get '/'
-    expect(response.body).to have_tag('div.home')
-  end
-end
