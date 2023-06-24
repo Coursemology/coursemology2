1c9c12dc08e6aec340c6ee6fa8d9b9034251c14e
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-06-24 22:13:38 +0800

    test(requests): remove redundant devise_pages_spec

--- spec/requests/devise_pages_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-require 'rails_helper'
-
-RSpec.describe 'Devise Pages' do
-  it 'displays the proper page action class' do
-    get new_user_password_path
-    expect(response.body).to have_tag('div.devise-passwords')
-  end
-end
