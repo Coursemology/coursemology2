4e3de0606d0ae1ce2fd38b7b8a7e8559bacf4a4e
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-06-28 13:51:49 +0800

    style(PasswordTextField): remove visibility hint

--- client/app/lib/components/core/fields/PasswordTextField.tsx
+++ client/app/lib/components/core/fields/PasswordTextField.tsx
@@ -1,15 +1,11 @@
 import { ComponentProps, forwardRef, useState } from 'react';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
-import { IconButton, InputAdornment, Typography } from '@mui/material';
-
-import useTranslation from 'lib/hooks/useTranslation';
-import messagesTranslations from 'lib/translations/messages';
+import { IconButton, InputAdornment } from '@mui/material';
 
 import TextField from './TextField';
 
 type PasswordTextFieldProps = ComponentProps<typeof TextField> & {
   onChangePasswordVisibility?: (visibility: boolean) => void;
-  showPasswordVisibilityHint?: boolean;
   disablePasswordVisibilitySwitch?: boolean;
 };
 
@@ -17,12 +13,10 @@ const PasswordTextField = forwardRef<HTMLDivElement, PasswordTextFieldProps>(
   (props, ref): JSX.Element => {
     const {
       onChangePasswordVisibility,
-      showPasswordVisibilityHint,
       disablePasswordVisibilitySwitch,
       ...textFieldProps
     } = props;
 
-    const { t } = useTranslation();
     const [showPassword, setShowPassword] = useState(false);
 
     const handleChangePasswordVisibility = (): void =>
@@ -31,7 +25,7 @@ const PasswordTextField = forwardRef<HTMLDivElement, PasswordTextFieldProps>(
         return !state;
       });
 
-    const textField = (
+    return (
       <TextField
         {...textFieldProps}
         ref={ref}
@@ -53,23 +47,6 @@ const PasswordTextField = forwardRef<HTMLDivElement, PasswordTextFieldProps>(
         })}
       />
     );
-
-    if (showPasswordVisibilityHint && showPassword)
-      return (
-        <div>
-          {textField}
-
-          <div className="flex items-center space-x-4 text-neutral-500">
-            <Visibility />
-
-            <Typography variant="body2">
-              {t(messagesTranslations.passwordIsVisible)}
-            </Typography>
-          </div>
-        </div>
-      );
-
-    return textField;
   },
 );
 
