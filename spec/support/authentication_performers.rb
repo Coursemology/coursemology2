6130373239aa9acedd3e20d2654407f9df1ed90d
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-08-01 16:54:16 +0800

    test(capybara): add sign in, sign out performers

---
+++ spec/support/authentication_performers.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+module AuthenticationPerformersTestHelpers
+  include Warden::Test::Helpers
+
+  alias_method :warden_logout, :logout
+
+  def login_as(user, _ = {})
+    # For some reasons, sometimes new scenarios are automatically logged in as the previous user.
+    # Clearing cookies isn't enough and subsequent requests still carries over an old, authenticated
+    # session cookie. We force the server to log out all remaining sessions before logging in.
+    warden_logout
+
+    visit new_user_session_path
+
+    fill_in 'Email address', with: user.email
+    fill_in 'Password', with: password_for(user)
+    click_button 'Sign in'
+
+    wait_for_page
+  end
+
+  def logout(*_)
+    # We expect all pages should have a user menu button.
+    find('div[data-testid="user-menu-button"]').click
+    find('li', text: 'Sign out').click
+
+    super
+    wait_for_page
+  end
+
+  private
+
+  def password_for(user)
+    user.password || Application::Application.config.x.default_user_password
+  end
+end
+
+RSpec.configure do |config|
+  config.include AuthenticationPerformersTestHelpers, type: :feature
+end
--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -16,5 +16,4 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.extend DeviseControllerMacros, type: :controller
   config.include Warden::Test::Helpers, type: :request
-  config.include Warden::Test::Helpers, type: :feature
 end
