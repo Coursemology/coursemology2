cd8094b225a752bdcd51595d49c2f274d89f9cf9
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-08-01 17:32:17 +0800

    feat(ErrorBoundary): rewrite to TS, add raw error stack

---
+++ client/app/lib/components/wrappers/ErrorBoundary.tsx
@@ -0,0 +1,47 @@
+import { Component, ErrorInfo, ReactNode } from 'react';
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+  error: Error | null;
+  info: ErrorInfo | null;
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+
+    this.state = {
+      hasError: false,
+      error: null,
+      info: null,
+    };
+  }
+
+  override componentDidCatch(error: Error, info: ErrorInfo): void {
+    this.setState({ hasError: true, error, info });
+  }
+
+  override render(): ReactNode {
+    const { hasError, error, info } = this.state;
+
+    if (!hasError) return this.props.children;
+
+    return (
+      <main>
+        <h1>Something went wrong.</h1>
+        <h2>{error?.toString()}</h2>
+        <pre>
+          Component Stack
+          {info?.componentStack}
+        </pre>
+        <pre>{error?.stack}</pre>
+      </main>
+    );
+  }
+}
+
+export default ErrorBoundary;
