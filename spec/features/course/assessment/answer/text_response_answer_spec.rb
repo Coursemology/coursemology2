8be353eed6e3c65f6165c9a4df62a20e518a89d3
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-08-01 17:04:43 +0800

    test(text_response_answer): fix `submission` empty

--- spec/features/course/assessment/answer/text_response_answer_spec.rb
+++ spec/features/course/assessment/answer/text_response_answer_spec.rb
@@ -6,60 +6,59 @@ RSpec.describe 'Course: Assessments: Submissions: Text Response Answers', js: tr
 
   with_tenant(:instance) do
     let(:course) { create(:course) }
-    let(:assessment) { create(:assessment, :published_with_text_response_question, course: course) }
+    let(:user) { create(:course_student, course: course).user }
+    let(:submission) { create(:submission, :attempting, assessment: assessment, creator: user) }
+
     before { login_as(user, scope: :user) }
 
-    let(:submission) do
-      create(:submission, *submission_traits, assessment: assessment, creator: user)
-    end
-    let(:submission_traits) { nil }
-
     context 'As a Course Student' do
-      let(:user) { create(:course_student, course: course).user }
-      let(:file_path) do
-        File.join(Rails.root, '/spec/fixtures/files/text.txt')
-      end
+      let(:file_path) { File.join(Rails.root, '/spec/fixtures/files/text.txt') }
 
-      scenario 'I cannot update my submission after finalising' do
-        visit edit_course_assessment_submission_path(course, assessment, submission)
+      context 'when it is a file upload question' do
+        let(:assessment) { create(:assessment, :published_with_text_response_question, course: course) }
 
-        answer_id = submission.answers.first.id
-        find_field(name: "#{answer_id}.answer_text").set('Test')
-        click_button('Finalise Submission')
-        accept_confirm_dialog do
-          wait_for_job
+        scenario 'I cannot update my submission after finalising' do
+          visit edit_course_assessment_submission_path(course, assessment, submission)
+
+          answer_id = submission.answers.first.id
+          find_field(name: "#{answer_id}.answer_text").set('Test')
+          click_button('Finalise Submission')
+          accept_confirm_dialog do
+            wait_for_job
+          end
+          expect(page).not_to have_field(name: "#{answer_id}[answer_text]")
         end
-        expect(page).not_to have_field(name: "#{answer_id}[answer_text]")
+
+        scenario 'I upload an attachment to the answer' do
+          visit edit_course_assessment_submission_path(course, assessment, submission)
+          answer = submission.answers.last
+          file_view = all('div', text: 'Uploaded Files').last
+          dropzone = find('.dropzone-input')
+          file_input = dropzone.find('input', visible: false)
+
+          file_input.set(file_path)
+
+          # The file should show in the dropzone
+          expect(dropzone).to have_css('span', text: 'text.txt')
+
+          click_button('Save Draft')
+
+          expect(dropzone).to have_no_css('span', text: 'text.txt')
+          expect(file_view).to have_css('span', text: 'text.txt')
+          expect(file_view).to have_css('span', count: 2)
+          expect(answer.specific.attachments).not_to be_empty
+        end
       end
 
-      scenario 'I upload an attachment to the answer' do
-        visit edit_course_assessment_submission_path(course, assessment, submission)
-        answer = submission.answers.last
-        file_view = find('strong', text: 'Uploaded Files:').find(:xpath, '..')
-        dropzone = find('.dropzone-input')
-        file_input = dropzone.find('input', visible: false)
+      context 'when it is a text response question' do
+        let(:assessment) { create(:assessment, :published_with_file_upload_question, course: course) }
 
-        file_input.set(file_path)
+        scenario 'I cannot see the text box for a file upload question' do
+          visit edit_course_assessment_submission_path(course, assessment, submission)
 
-        # The file should show in the dropzone
-        expect(dropzone).to have_css('span', text: 'text.txt')
-
-        click_button('Save Draft')
-
-        expect(dropzone).to have_no_css('span', text: 'text.txt')
-        expect(file_view).to have_css('span', text: 'text.txt')
-        expect(file_view).to have_css('span', count: 2)
-        expect(answer.specific.attachments).not_to be_empty
-      end
-
-      scenario 'I cannot see the text box for a file upload question' do
-        assessment = create(:assessment, :published_with_file_upload_question, course: course)
-        submission = create(:submission, assessment: assessment, creator: user)
-
-        visit edit_course_assessment_submission_path(course, assessment, submission)
-
-        file_upload_answer = submission.answers.first
-        expect(page).not_to have_field(name: "#{file_upload_answer.id}[answer_text]")
+          file_upload_answer = submission.answers.first
+          expect(page).not_to have_field(name: "#{file_upload_answer.id}[answer_text]")
+        end
       end
     end
   end
