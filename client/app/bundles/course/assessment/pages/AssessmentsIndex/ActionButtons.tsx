315ce3c4d4461bd863e42937590ed57c2553a4e2
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-08-01 18:19:41 +0800

    feat(assessments): attempt buttons link to `/attempt` route

--- client/app/bundles/course/assessment/pages/AssessmentsIndex/ActionButtons.tsx
+++ client/app/bundles/course/assessment/pages/AssessmentsIndex/ActionButtons.tsx
@@ -1,14 +1,10 @@
-import { MouseEventHandler, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
 import { Create, Inventory, QuestionMark } from '@mui/icons-material';
 import { Button, IconButton, Tooltip } from '@mui/material';
 import { AssessmentListData } from 'types/course/assessment/assessments';
 
-import { JustRedirect } from 'api/types';
+import Link from 'lib/components/core/Link';
 import useTranslation, { Descriptor } from 'lib/hooks/useTranslation';
 
-import { attemptAssessment } from '../../operations';
 import translations from '../../translations';
 
 import UnavailableMessage from './UnavailableMessage';
@@ -28,42 +24,15 @@ interface ActionButtonsProps {
 
 const ActionButtons = (props: ActionButtonsProps): JSX.Element => {
   const { for: assessment, student } = props;
-  const [attempting, setAttempting] = useState(false);
-  const navigate = useNavigate();
   const { t } = useTranslation();
-  const actionButtonUrl =
-    assessment.status === 'open' ? '#' : assessment.actionButtonUrl;
-
-  const handleActionButton: MouseEventHandler<HTMLButtonElement> = (
-    e,
-  ): void => {
-    if (assessment.status !== 'open') return;
-    setAttempting(true);
-    e.preventDefault();
-    e.stopPropagation();
-    toast
-      .promise(attemptAssessment(assessment.id), {
-        pending: t(translations.attemptingAssessment),
-        success: t(translations.createSubmissionSuccessful),
-        error: {
-          render: ({ data }) => {
-            const error = (data as Error)?.message;
-            return t(translations.createSubmissionFailed, { error });
-          },
-        },
-      })
-      .then((data: JustRedirect) => navigate(data.redirectUrl))
-      .catch(() => setAttempting(false));
-  };
 
   return (
     <div className="flex items-center">
-      {actionButtonUrl && (
-        <Link to={actionButtonUrl}>
+      {assessment.actionButtonUrl && (
+        <Link to={assessment.actionButtonUrl}>
           <Button
+            aria-label={t(ACTION_LABELS[assessment.status])}
             className="mr-4 min-w-[8.5rem]"
-            disabled={attempting}
-            onClick={handleActionButton}
             size="small"
             variant={
               assessment.status === 'submitted' ? 'outlined' : 'contained'
