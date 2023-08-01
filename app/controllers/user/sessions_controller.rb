e4dfad80e0f1b62a3ed1a72882c45209c61d3f0f
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-08-01 17:17:10 +0800

    feat(sessions_controller): responds to JSON

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 class User::SessionsController < Devise::SessionsController
+  respond_to :json
+
   # before_filter :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
