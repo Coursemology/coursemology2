02afeac1f3c972c9bbf1c8cf8ac5048b186219ae
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-08-01 17:11:31 +0800

    feat(student_statistics): adapt to new UI

--- spec/features/course/students_statistics_spec.rb
+++ spec/features/course/students_statistics_spec.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.feature 'Course: Student Statistics' do
+RSpec.feature 'Course: Student Statistics', js: true do
   subject { page }
 
   let!(:instance) { Instance.default }
 
   with_tenant(:instance) do
     let(:course) { create(:course) }
-    let(:students) { create_list(:course_student, 2, course: course) }
+    let!(:students) { create_list(:course_student, 2, course: course) }
 
     before do
       login_as(user, scope: :user)
@@ -27,42 +27,32 @@ RSpec.feature 'Course: Student Statistics' do
         create(:course_group_user, group: other_group, course_user: students.last)
       end
 
-      scenario 'I can only view all student statistics when I am not a group manager', js: true do
-        pending 'Migrated students statistics to React-side'
-        students
-        visit course_statistics_all_students_path(course)
+      scenario 'I can only view all student statistics when I am not a group manager' do
+        visit course_statistics_path(course)
 
-        expect(page).to have_link(I18n.t('course.statistics.student.header'),
-                                  href: course_statistics_all_students_path(course))
-
-        students.each do |student|
-          expect(page).to have_content_tag_for(student)
-        end
-
-        expect(page).not_to have_text(I18n.t('course.statistics.tabs.my_students_tab'))
-        expect(page).
-          not_to have_text(I18n.t('course.statistics.course_student_statistics.phantom_students'))
+        students.each { |student| expect(page).to have_text(student.name) }
+        expect(page).not_to have_text('Show My Students Only')
+        expect(page).not_to have_text('Phantom')
 
         # Test that phantom students are rendered only if they exist
         phantom_student = students.first
         phantom_student.phantom = true
         phantom_student.save
-        visit course_statistics_all_students_path(course)
-        students.each do |student|
-          expect(page).to have_content_tag_for(student)
-        end
-        expect(page).
-          to have_text(I18n.t('course.statistics.course_student_statistics.phantom_students'))
+
+        visit course_statistics_path(course)
+
+        students.each { |student| expect(page).to have_text(student.name) }
+        expect(page).to have_text('Phantom')
       end
     end
 
     context 'As a Course Student' do
       let(:user) { create(:course_student, course: course).user }
 
-      scenario 'I cannot see the sidebar item' do
+      scenario 'I cannot see the statistics sidebar item' do
         visit course_path(course)
 
-        expect(page).not_to have_selector('li', text: I18n.t('course.statistics.header'))
+        expect(find_sidebar).not_to have_text(I18n.t('course.statistics.header'))
       end
 
       scenario 'I cannot access the statistics page' do
