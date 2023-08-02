2c63f5a0cefc66b9a20a6ff2ee9da3c3e77ed20a
Eko Widianto <ekowidianto@outlook.com>
2023-08-02 13:38:44 +0800

    refactor: remove announcements html slim

--- app/helpers/application_announcements_helper.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-module ApplicationAnnouncementsHelper
-  def global_announcements
-    render partial: 'announcements/global_announcements'
-  end
-end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  include ApplicationAnnouncementsHelper
   include ApplicationJobsHelper
   include ApplicationNotificationsHelper
 
