a02fe339f1a1ed175dc987be0314ffa617f2a850
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-08-02 12:14:33 +0800

    fix(AppLoader): prevent redirection if 404
    There is a risk of the page living in infinite loop if whatever request
    in `AppLoader` responds 404 too. Even if we limit the redirection to
    only happen when the response is `"Instance Not Found"`, there is a
    risk of infinite loop if for some reasons the default instance is also
    404. It's best to just throw it to `ErrorBoundary` and stop rendering.
    The page will show a 404 message anyway.

--- client/app/lib/containers/AppContainer/AppLoader.ts
+++ client/app/lib/containers/AppContainer/AppLoader.ts
@@ -1,10 +1,8 @@
 import { useLoaderData, useOutletContext } from 'react-router-dom';
-import { AxiosError } from 'axios';
 import { AnnouncementMiniEntity } from 'types/course/announcements';
 import { HomeLayoutData } from 'types/home';
 
 import GlobalAPI from 'api';
-import { redirectToDefaultNotFound } from 'lib/hooks/router/redirect';
 import { imperativeAuthenticator, setI18nConfig } from 'lib/hooks/session';
 
 interface AppLoaderData {
@@ -13,28 +11,21 @@ interface AppLoaderData {
 }
 
 export const loader = async (): Promise<AppLoaderData> => {
-  try {
-    const { data: home } = await GlobalAPI.home.fetch();
-    const { data: announcements } = await GlobalAPI.announcements.index(true);
+  const { data: home } = await GlobalAPI.home.fetch();
+  const { data: announcements } = await GlobalAPI.announcements.index(true);
 
-    setI18nConfig({
-      locale: home.locale,
-      timeZone: home.timeZone ?? undefined,
-    });
+  setI18nConfig({
+    locale: home.locale,
+    timeZone: home.timeZone ?? undefined,
+  });
 
-    if (home.user) {
-      imperativeAuthenticator.authenticate();
-    } else {
-      imperativeAuthenticator.deauthenticate();
-    }
-
-    return { home, announcements: announcements.announcements };
-  } catch (error) {
-    if (error instanceof AxiosError && error.response?.status === 404)
-      redirectToDefaultNotFound();
-
-    throw error;
+  if (home.user) {
+    imperativeAuthenticator.authenticate();
+  } else {
+    imperativeAuthenticator.deauthenticate();
   }
+
+  return { home, announcements: announcements.announcements };
 };
 
 export const useAppLoader = (): AppLoaderData =>
