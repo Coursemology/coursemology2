ca289d36fa8b749d3ea82e4959ce0951f1c4fa34
Eko Widianto <ekowidianto@outlook.com>
2023-08-03 11:46:21 +0800

    fix(markdown page): fix props destructuring

--- client/app/lib/components/core/layouts/MarkdownPage.tsx
+++ client/app/lib/components/core/layouts/MarkdownPage.tsx
@@ -10,43 +10,45 @@ interface MarkdownPageProps extends ComponentProps<typeof Page> {
   markdown: string;
 }
 
-const MarkdownPage = (props: MarkdownPageProps): JSX.Element => (
-  <Page {...props}>
-    <ReactMarkdown
-      components={{
-        h2: ({ children }) => (
-          <Typography className="mb-8" variant="h4">
-            {children}
-          </Typography>
-        ),
-        h3: ({ children }) => (
-          <Typography className="mb-2" variant="h6">
-            {children}
-          </Typography>
-        ),
-        p: ({ children }) => (
-          <Typography className="mb-6" variant="body2">
-            {children}
-          </Typography>
-        ),
-        li: ({ children }) => (
-          <Typography className="mb-2" component="li" variant="body2">
-            {children}
-          </Typography>
-        ),
-        ul: ({ children }) => <ul className="mb-5 ml-7">{children}</ul>,
-        a: ({ children, href }) => (
-          <Link external href={href} opensInNewTab>
-            {children}
-          </Link>
-        ),
-      }}
-    >
-      {props.markdown}
-    </ReactMarkdown>
+const MarkdownPage = (props: MarkdownPageProps): JSX.Element => {
+  const { markdown, ...pageProps } = props;
+  return (
+    <Page {...pageProps}>
+      <ReactMarkdown
+        components={{
+          h2: ({ children }) => (
+            <Typography className="mb-8" variant="h4">
+              {children}
+            </Typography>
+          ),
+          h3: ({ children }) => (
+            <Typography className="mb-2" variant="h6">
+              {children}
+            </Typography>
+          ),
+          p: ({ children }) => (
+            <Typography className="mb-6" variant="body2">
+              {children}
+            </Typography>
+          ),
+          li: ({ children }) => (
+            <Typography className="mb-2" component="li" variant="body2">
+              {children}
+            </Typography>
+          ),
+          ul: ({ children }) => <ul className="mb-5 ml-7">{children}</ul>,
+          a: ({ children, href }) => (
+            <Link external href={href} opensInNewTab>
+              {children}
+            </Link>
+          ),
+        }}
+      >
+        {markdown}
+      </ReactMarkdown>
 
-    {props.children}
-  </Page>
-);
-
+      {props.children}
+    </Page>
+  );
+};
 export default MarkdownPage;
