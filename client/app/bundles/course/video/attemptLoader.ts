4d535c7dfeb35bf16da8ae0d5c625919778d736a
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-08-08 13:10:28 +0800

    feat(AuthenticatedApp): toast when some loaders fail

--- client/app/bundles/course/video/attemptLoader.ts
+++ client/app/bundles/course/video/attemptLoader.ts
@@ -1,7 +1,18 @@
+import { defineMessages } from 'react-intl';
 import { LoaderFunction, Params, redirect } from 'react-router-dom';
 import { getIdFromUnknown } from 'utilities';
 
 import CourseAPI from 'api/course';
+import toast from 'lib/hooks/toast';
+import { Translated } from 'lib/hooks/useTranslation';
+
+const translations = defineMessages({
+  errorWatchVideo: {
+    id: 'client.video.attemptLoader.errorWatchVideo',
+    defaultMessage:
+      'An error occurred while attempting to watch this video. Try again later.',
+  },
+});
 
 const getSubmissionURL = (
   params: Params<string>,
@@ -14,16 +25,27 @@ const getSubmissionURL = (
   return `/courses/${courseId}/videos/${videoId}/submissions/${submissionId}/edit`;
 };
 
-const videoAttemptLoader: LoaderFunction = async ({ params }) => {
-  const videoId = getIdFromUnknown(params?.videoId);
-  if (!videoId) return redirect('/');
+const videoAttemptLoader: Translated<LoaderFunction> =
+  (t) =>
+  async ({ params }) => {
+    try {
+      const videoId = getIdFromUnknown(params?.videoId);
+      if (!videoId) return redirect('/');
 
-  const { data } = await CourseAPI.video.submissions.create(videoId);
+      const { data } = await CourseAPI.video.submissions.create(videoId);
 
-  const url = data.submissionUrl ?? getSubmissionURL(params, data.submissionId);
-  if (!url) return redirect('/');
+      const url = getSubmissionURL(params, data.submissionId);
+      if (!url) return redirect('/');
 
-  return redirect(url);
-};
+      return redirect(url);
+    } catch {
+      toast.error(t(translations.errorWatchVideo));
+
+      const { courseId } = params;
+      if (!courseId) return redirect('/');
+
+      return redirect(`/courses/${courseId}/videos`);
+    }
+  };
 
 export default videoAttemptLoader;
--- client/app/bundles/users/masqueradeLoader.ts
+++ client/app/bundles/users/masqueradeLoader.ts
@@ -1,13 +1,42 @@
+import { defineMessages } from 'react-intl';
 import { LoaderFunction, redirect } from 'react-router-dom';
 
 import GlobalAPI from 'api';
+import toast from 'lib/hooks/toast';
+import { Translated } from 'lib/hooks/useTranslation';
 
-export const masqueradeLoader: LoaderFunction = async ({ request }) => {
-  await GlobalAPI.users.masquerade(request.url);
-  return redirect('/');
-};
+const translations = defineMessages({
+  errorMasquerading: {
+    id: 'client.users.masqueradeLoader.errorMasquerading',
+    defaultMessage: 'An error occurred while masquerading. Try again later.',
+  },
+  errorStoppingMasquerade: {
+    id: 'client.users.masqueradeLoader.errorStoppingMasquerade',
+    defaultMessage:
+      'An error occurred while stopping masquerade. Try again later.',
+  },
+});
 
-export const stopMasqueradeLoader: LoaderFunction = async ({ request }) => {
-  await GlobalAPI.users.stopMasquerade(request.url);
-  return redirect('/admin/users');
-};
+export const masqueradeLoader: Translated<LoaderFunction> =
+  (t) =>
+  async ({ request }) => {
+    try {
+      await GlobalAPI.users.masquerade(request.url);
+      return redirect('/');
+    } catch {
+      toast.error(t(translations.errorMasquerading));
+      return redirect('/');
+    }
+  };
+
+export const stopMasqueradeLoader: Translated<LoaderFunction> =
+  (t) =>
+  async ({ request }) => {
+    try {
+      await GlobalAPI.users.stopMasquerade(request.url);
+      return redirect('/admin/users');
+    } catch {
+      toast.error(t(translations.errorStoppingMasquerade));
+      return redirect('/');
+    }
+  };
