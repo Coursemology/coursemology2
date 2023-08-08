8a249e8411b260942f5aef60f3a8cb0b97aecb3d
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-08-08 13:07:48 +0800

    feat(videos): watch buttons use link to `/attempt`

--- client/app/bundles/course/video/submission/containers/VideoControls/NextVideoButton.jsx
+++ client/app/bundles/course/video/submission/containers/VideoControls/NextVideoButton.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react';
 import { injectIntl } from 'react-intl';
 import { connect } from 'react-redux';
 import SkipNext from '@mui/icons-material/SkipNext';
 import { IconButton, Tooltip } from '@mui/material';
 import PropTypes from 'prop-types';
 
-import CourseAPI from 'api/course';
+import Link from 'lib/components/core/Link';
 
 import translations from '../../translations';
 
@@ -14,12 +13,9 @@ import styles from '../VideoPlayer.scss';
 const propTypes = {
   intl: PropTypes.object.isRequired,
   url: PropTypes.string,
-  nextVideoSubmissionExists: PropTypes.bool,
 };
 
 const NextVideoButton = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-
   if (!props.url) {
     return (
       <Tooltip title={props.intl.formatMessage(translations.noNextVideo)}>
@@ -34,23 +30,11 @@ const NextVideoButton = (props) => {
 
   return (
     <Tooltip title={props.intl.formatMessage(translations.watchNextVideo)}>
-      <IconButton
-        className={styles.nextVideo}
-        disabled={isLoading}
-        onClick={() => {
-          setIsLoading(true);
-          if (!props.nextVideoSubmissionExists) {
-            CourseAPI.video.submissions.attempt(props.url).then((response) => {
-              // TODO: Use `navigate` from `useNavigate` once bundle is ready.
-              window.location.href = response.data.submissionUrl;
-            });
-          } else {
-            window.location.href = props.url;
-          }
-        }}
-      >
-        <SkipNext />
-      </IconButton>
+      <Link className={styles.nextVideo} to={props.url}>
+        <IconButton>
+          <SkipNext />
+        </IconButton>
+      </Link>
     </Tooltip>
   );
 };
@@ -60,7 +44,6 @@ NextVideoButton.propTypes = propTypes;
 function mapStateToProps(state) {
   return {
     url: state.video.watchNextVideoUrl,
-    nextVideoSubmissionExists: state.video.nextVideoSubmissionExists,
   };
 }
 
