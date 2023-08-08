0c8e421f118493551453318d97a850db5ffaaedd
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-08-08 13:07:48 +0800

    feat(videos): watch buttons use link to `/attempt`

--- client/app/bundles/course/video/components/buttons/WatchVideoButton.tsx
+++ client/app/bundles/course/video/components/buttons/WatchVideoButton.tsx
@@ -1,13 +1,10 @@
 import { FC } from 'react';
 import { defineMessages, injectIntl, WrappedComponentProps } from 'react-intl';
-import { useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { Button } from '@mui/material';
 import { VideoListData } from 'types/course/videos';
 
-import CourseAPI from 'api/course';
-import { getEditVideoSubmissionURL } from 'lib/helpers/url-builders';
-import { getCourseId } from 'lib/helpers/url-helpers';
-import toast from 'lib/hooks/toast';
+import Link from 'lib/components/core/Link';
 
 interface Props extends WrappedComponentProps {
   video: VideoListData;
@@ -30,61 +27,27 @@ const translations = defineMessages({
 
 const WatchVideoButton: FC<Props> = (props) => {
   const { video, intl } = props;
-  const navigate = useNavigate();
+
+  const { courseId } = useParams();
 
   if (video.permissions.canAttempt) {
     if (video.videoSubmissionId) {
       return (
-        <Button
-          className="bg-white"
-          color="primary"
-          onClick={(): void =>
-            navigate(
-              getEditVideoSubmissionURL(
-                getCourseId(),
-                video.id,
-                video.videoSubmissionId,
-              ),
-            )
-          }
-          variant="outlined"
+        <Link
+          to={`/courses/${courseId}/videos/${video.id}/submissions/${video.videoSubmissionId}/edit`}
         >
-          {intl.formatMessage(translations.reWatch)}
-        </Button>
+          <Button className="bg-white" color="primary" variant="outlined">
+            {intl.formatMessage(translations.reWatch)}
+          </Button>
+        </Link>
       );
     }
     return (
-      <Button
-        className="bg-white"
-        color="primary"
-        onClick={(): void => {
-          CourseAPI.video.submissions
-            .create(video.id)
-            .then((response) =>
-              navigate(
-                getEditVideoSubmissionURL(
-                  getCourseId(),
-                  video.id,
-                  response.data.submissionId,
-                ),
-              ),
-            )
-            .catch((error) => {
-              const errorMessage = error.response?.data?.errors
-                ? error.response.data.errors
-                : '';
-
-              toast.error(
-                intl.formatMessage(translations.attemptFailure, {
-                  error: errorMessage,
-                }),
-              );
-            });
-        }}
-        variant="outlined"
-      >
-        {intl.formatMessage(translations.watch)}
-      </Button>
+      <Link to={`/courses/${courseId}/videos/${video.id}/attempt`}>
+        <Button className="bg-white" color="primary" variant="outlined">
+          {intl.formatMessage(translations.watch)}
+        </Button>
+      </Link>
     );
   }
   return (
