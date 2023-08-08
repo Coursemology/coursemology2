b08b35ec8cebfaf5ec9ed4e4638dc20e927c2707
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-08-08 13:06:41 +0800

    refactor(DashboardPage): use `useItems`, extract `CourseListItem`

--- client/app/bundles/common/DashboardPage.tsx
+++ client/app/bundles/common/DashboardPage.tsx
@@ -1,14 +1,15 @@
-import { useMemo, useState } from 'react';
 import { defineMessages } from 'react-intl';
 import { Navigate } from 'react-router-dom';
 import { ArrowForward } from '@mui/icons-material';
 import { Avatar, Typography } from '@mui/material';
 import moment from 'moment';
+import { HomeLayoutCourseData } from 'types/home';
 
 import SearchField from 'lib/components/core/fields/SearchField';
 import Page from 'lib/components/core/layouts/Page';
 import Link from 'lib/components/core/Link';
 import { useAppContext } from 'lib/containers/AppContainer';
+import useItems from 'lib/hooks/items/useItems';
 import useTranslation from 'lib/hooks/useTranslation';
 
 const translations = defineMessages({
@@ -26,30 +27,66 @@ const translations = defineMessages({
   },
   noCoursesMatch: {
     id: 'lib.components.navigation.CourseSwitcherPopupMenu.noCoursesMatch',
-    defaultMessage: "Oops, no courses matched ''{keyword}''.",
+    defaultMessage: 'Oops, no courses matched your search keyword.',
   },
 });
+
+interface CourseListItemProps {
+  course: HomeLayoutCourseData;
+}
+
+const CourseListItem = (props: CourseListItemProps): JSX.Element => {
+  const { course } = props;
+
+  const { t } = useTranslation();
+
+  return (
+    <Link
+      className="group flex items-center justify-between rounded-xl border border-solid border-neutral-200 p-5 transition-transform hover:bg-neutral-100 active:scale-95 active:bg-neutral-200"
+      color="inherit"
+      to={course.url}
+      underline="none"
+    >
+      <div className="flex space-x-5">
+        <Avatar
+          alt={course.title}
+          className="wh-20"
+          src={course.logoUrl}
+          variant="rounded"
+        />
+
+        <div className="flex flex-col justify-center">
+          <Typography variant="body1">{course.title}</Typography>
+
+          {course.lastActiveAt && (
+            <Typography color="text.secondary" variant="body2">
+              {t(translations.lastAccessed, {
+                at: moment(course.lastActiveAt).fromNow(),
+              })}
+            </Typography>
+          )}
+        </div>
+      </div>
+
+      <ArrowForward className="invisible group-hover:visible" color="primary" />
+    </Link>
+  );
+};
 
 const DashboardPage = (): JSX.Element => {
   const { courses } = useAppContext();
 
   const { t } = useTranslation();
 
-  const [filterKeyword, setFilterKeyword] = useState('');
-
-  const sortedCourses = useMemo(() => {
-    return courses?.slice().sort((a, b) => {
-      return moment(b.lastActiveAt).diff(moment(a.lastActiveAt));
-    });
-  }, [courses]);
-
-  const filteredCourses = useMemo(() => {
-    if (!filterKeyword) return sortedCourses;
-
-    return sortedCourses?.filter((course) =>
-      course.title.toLowerCase().includes(filterKeyword.toLocaleLowerCase()),
-    );
-  }, [filterKeyword]);
+  const { processedItems: filteredCourses, handleSearch } = useItems(
+    courses ?? [],
+    ['title'],
+    (rawCourses) =>
+      rawCourses?.slice().sort((a, b) => {
+        return moment(b.lastActiveAt).diff(moment(a.lastActiveAt));
+      }),
+    courses?.length ?? 0,
+  );
 
   return (
     <Page className="m-auto flex max-w-7xl flex-col justify-center space-y-7 pt-24">
@@ -59,53 +96,19 @@ const DashboardPage = (): JSX.Element => {
 
       <SearchField
         autoFocus
-        onChangeKeyword={setFilterKeyword}
+        onChangeKeyword={handleSearch}
         placeholder={t(translations.searchCourses)}
       />
 
       {Boolean(courses?.length) && (
         <section className="flex flex-col space-y-5">
           {filteredCourses?.map((course) => (
-            <Link
-              key={course.id}
-              className="group flex items-center justify-between rounded-xl border border-solid border-neutral-200 p-5 transition-transform hover:bg-neutral-100 active:scale-95 active:bg-neutral-200"
-              color="inherit"
-              to={course.url}
-              underline="none"
-            >
-              <div className="flex space-x-5">
-                <Avatar
-                  alt={course.title}
-                  className="wh-20"
-                  src={course.logoUrl}
-                  variant="rounded"
-                />
-
-                <div className="flex flex-col justify-center">
-                  <Typography variant="body1">{course.title}</Typography>
-
-                  {course.lastActiveAt && (
-                    <Typography color="text.secondary" variant="body2">
-                      {t(translations.lastAccessed, {
-                        at: moment(course.lastActiveAt).fromNow(),
-                      })}
-                    </Typography>
-                  )}
-                </div>
-              </div>
-
-              <ArrowForward
-                className="invisible group-hover:visible"
-                color="primary"
-              />
-            </Link>
+            <CourseListItem key={course.id} course={course} />
           ))}
 
           {!filteredCourses?.length && (
             <Typography color="text.secondary">
-              {t(translations.noCoursesMatch, {
-                keyword: filterKeyword,
-              })}
+              {t(translations.noCoursesMatch)}
             </Typography>
           )}
         </section>
