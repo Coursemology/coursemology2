0f56c8a3b1b2c95c310674c0965cdd2b2825a405
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-08-10 17:00:22 +0800

    style: fix ESLint warnings after prettier@3

--- client/app/bundles/course/assessment/components/FileManager/index.tsx
+++ client/app/bundles/course/assessment/components/FileManager/index.tsx
@@ -108,8 +108,9 @@ const FileManager = (props: FileManagerProps): JSX.Element => {
   const deleteFileWithRowIndex = async (index: number): Promise<void> => {
     const { id, name } = materials[index];
 
-    setMaterials((current) =>
-      current?.map((m) => (m.id === id ? { ...m, deleting: true } : m)),
+    setMaterials(
+      (current) =>
+        current?.map((m) => (m.id === id ? { ...m, deleting: true } : m)),
     );
 
     try {
@@ -117,8 +118,9 @@ const FileManager = (props: FileManagerProps): JSX.Element => {
       setMaterials((current) => current?.filter((m) => m.id !== id));
       toast.success(intl.formatMessage(t.deleteSuccess, { name }));
     } catch (error) {
-      setMaterials((current) =>
-        current?.map((m) => (m.id === id ? { ...m, deleting: false } : m)),
+      setMaterials(
+        (current) =>
+          current?.map((m) => (m.id === id ? { ...m, deleting: false } : m)),
       );
       toast.error(intl.formatMessage(t.deleteFail, { name }));
     }
