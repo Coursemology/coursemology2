4173b3d03f82ea63063c9b36e5a539654025b978
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-08-11 18:57:39 +0800

    feat(application_html_formatters): support Dailymotion oEmbed

--- config/initializers/oembed.rb
+++ config/initializers/oembed.rb
@@ -3,3 +3,13 @@ require 'oembed'
 
 OEmbed::Providers.register_all
 
+# HTML iframe points to https://geo.dailymotion.com/*. Ensure this is whitelisted in the sanitizer.
+# https://developers.dailymotion.com/news/player-api/embed-dailymotion-video-oembed/
+DailymotionProvider = OEmbed::Provider.new('https://www.dailymotion.com/services/oembed')
+DailymotionProvider << 'http://*.dailymotion.com/video/*'
+DailymotionProvider << 'https://*.dailymotion.com/video/*'
+DailymotionProvider << 'http://*.dai.ly/*'
+DailymotionProvider << 'https://*.dai.ly/*'
+
+# ruby-oembed's Dailymotion provider is from Embedly and requires an API key, so we make our own.
+OEmbed::Providers.register(DailymotionProvider)
