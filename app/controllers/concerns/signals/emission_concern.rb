6aab72ace33210f5785bd564d2573e86edaa9468
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-09-04 13:35:22 +0800

    feat(signals): add emission concern

---
+++ app/controllers/concerns/signals/emission_concern.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+module Signals::EmissionConcern
+  extend ActiveSupport::Concern
+
+  include ActiveSupport::Callbacks
+
+  HEADER_KEY = 'Signals-Sync'
+
+  module ClassMethods
+    private
+
+    def signals(slice_name, options = {})
+      prepend_after_action (lambda do
+        return unless response.successful?
+
+        self.class.include(slice_class(slice_name))
+        headers[HEADER_KEY] = generate_sync&.to_json
+      rescue NameError
+        return if Rails.env.production?
+
+        raise NameError, "Slice :#{slice_name} not defined, expected #{slice_class_name(slice_name)}"
+      end), only: options[:after], except: options[:except], if: options[:if]
+    end
+  end
+
+  private
+
+  def slice_class_name(slice_name)
+    "Signals::Slices::#{slice_name.to_s.camelize}"
+  end
+
+  def slice_class(slice_name)
+    slice_class_name(slice_name).constantize
+  end
+end
