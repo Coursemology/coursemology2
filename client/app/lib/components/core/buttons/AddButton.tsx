4aa2aa55fb41c664ce31b7410b63547b194cd19d
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-09-05 16:11:15 +0800

    feat(AddButton): renders a text button on wide screens

--- client/app/lib/components/core/buttons/AddButton.tsx
+++ client/app/lib/components/core/buttons/AddButton.tsx
@@ -1,24 +1,50 @@
-import { AddBoxOutlined } from '@mui/icons-material';
-import { IconButton, IconButtonProps, Tooltip } from '@mui/material';
+import { Add } from '@mui/icons-material';
+import { Button, IconButton, Tooltip } from '@mui/material';
 
-interface AddButtonProps extends IconButtonProps {
+import useMedia from 'lib/hooks/useMedia';
+
+interface AddButtonProps {
   onClick: () => void;
-  tooltip?: string;
+  id?: string;
+  className?: string;
+  disabled?: boolean;
+  children?: string;
+
+  /**
+   * If `true`, the button will always be a text button.
+   */
+  fixed?: boolean;
 }
 
+/**
+ * Displays an icon and tooltip on narrow screens and a text button on wide screens.
+ * Defaults to the latter if `fixed` is `true`.
+ */
 const AddButton = (props: AddButtonProps): JSX.Element => {
-  const { tooltip, ...otherProps } = props;
-  const button = (
-    <IconButton color="inherit" {...otherProps}>
-      <AddBoxOutlined />
-    </IconButton>
-  );
+  const wideScreen = useMedia.MaxWidth('md');
 
-  if (!tooltip) return button;
-
-  return (
-    <Tooltip title={tooltip}>
-      <span>{button}</span>
+  return props.fixed || wideScreen ? (
+    <Button
+      aria-label={props.children}
+      className={props.className}
+      disabled={props.disabled}
+      id={props.id}
+      onClick={props.onClick}
+      variant="outlined"
+    >
+      {props.children}
+    </Button>
+  ) : (
+    <Tooltip disableInteractive title={props.children}>
+      <IconButton
+        aria-label={props.children}
+        className={props.className}
+        color="primary"
+        id={props.id}
+        onClick={props.onClick}
+      >
+        <Add />
+      </IconButton>
     </Tooltip>
   );
 };
