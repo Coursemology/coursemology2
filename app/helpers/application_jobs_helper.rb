54ca1a083200eb6eeea69d7bc1dcb79f04d8afb1
Eko Widianto <ekowidianto@outlook.com>
2023-09-13 02:32:36 +0800

    refactor: differentiate timeout and time limit error

--- app/helpers/application_jobs_helper.rb
+++ app/helpers/application_jobs_helper.rb
@@ -5,8 +5,10 @@ module ApplicationJobsHelper
     return nil unless error
 
     case error['class']
-    when Timeout::Error.name, Docker::Error::ConflictError.name
+    when Docker::Error::ConflictError.name
       I18n.t('course.assessment.answer.programming_auto_grading.job.failure.time_limit_breached')
+    when Timeout::Error.name
+      I18n.t('course.assessment.answer.programming_auto_grading.job.failure.timeout_error')
     when Docker::Error::TimeoutError
       I18n.t('course.assessment.answer.programming_auto_grading.job.failure.container_unreachable')
     else
