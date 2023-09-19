1fd266a9198faa2c18a1079fe3340fcc09f271f8
Eko Widianto <ekowidianto@outlook.com>
2023-09-19 12:31:43 +0800

    refactor: change codaveri setting view to using radio buttons

--- client/app/bundles/course/admin/pages/CodaveriSettings/translations.ts
+++ client/app/bundles/course/admin/pages/CodaveriSettings/translations.ts
@@ -5,8 +5,8 @@ export default defineMessages({
     id: 'course.admin.CodaveriSettings.codaveriSettings',
     defaultMessage: 'Codaveri settings',
   },
-  codaverSettingsSubtitle: {
-    id: 'course.admin.CodaveriSettings.codaverSettingsSubtitle',
+  codaveriSettingsSubtitle: {
+    id: 'course.admin.CodaveriSettings.codaveriSettingsSubtitle',
     defaultMessage:
       "This is currently an experimental feature. \
       Codaveri provides code evaluation and automated code feedback services for students' codes.",
@@ -18,5 +18,30 @@ export default defineMessages({
   errorOccurredWhenUpdating: {
     id: 'course.admin.CodaveriSettings.error',
     defaultMessage: 'An error occurred while updating the codaveri setting.',
+  },
+  codaveriEngine: {
+    id: 'course.admin.CodaveriSettings.codaveriEngine',
+    defaultMessage: 'Codaveri Engine',
+  },
+  codaveriEngineDescription: {
+    id: 'course.admin.CodaveriSettings.codaveriEngineDescription',
+    defaultMessage:
+      'Type of codaveri engine used to generate programming code feedback',
+  },
+  defaultEngine: {
+    id: 'course.admin.CodaveriSettings.defaultEngine',
+    defaultMessage: 'Default Engine',
+  },
+  defaultEngineDescription: {
+    id: 'course.admin.CodaveriSettings.defaultEngineDescription',
+    defaultMessage: 'Uses generative AI and verification techniques',
+  },
+  itspEngine: {
+    id: 'course.admin.CodaveriSettings.itspEngine',
+    defaultMessage: 'ITSP Engine',
+  },
+  itspEngineDescription: {
+    id: 'course.admin.CodaveriSettings.itspEngineDescription',
+    defaultMessage: 'Uses automated program repair technique',
   },
 });
