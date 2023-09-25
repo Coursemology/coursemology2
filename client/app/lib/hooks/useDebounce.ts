3974187228b70a046b7773b8ce3b1bd6bab81812
bivanalhar <bivan.alzacky@gmail.com>
2023-09-25 10:26:31 +0800

    feat: implement useDebounce

---
+++ client/app/lib/hooks/useDebounce.ts
@@ -0,0 +1,22 @@
+import { DependencyList, useCallback, useEffect } from 'react';
+import type { DebouncedFunc } from 'lodash';
+import { debounce } from 'lodash';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const useDebounce = <T extends (...args: any[]) => any>(
+  callback: T,
+  delay: number,
+  deps: DependencyList,
+): DebouncedFunc<T> => {
+  const debouncedCallback = useCallback(debounce(callback, delay), [
+    delay,
+    ...deps,
+  ]);
+  useEffect(() => {
+    // To cancel any debounced functions when a page is unmounted
+    return () => debouncedCallback.cancel();
+  }, [debouncedCallback, delay, ...deps]);
+  return debouncedCallback;
+};
+
+export default useDebounce;
