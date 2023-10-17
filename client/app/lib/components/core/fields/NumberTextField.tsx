d34b9ea0c213f4a4f575dbc6e4e23f1554e09489
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-10-17 17:44:42 +0800

    feat(NumberTextField): support negatives, floats, "incomplete" numbers

--- client/app/lib/components/core/fields/NumberTextField.tsx
+++ client/app/lib/components/core/fields/NumberTextField.tsx
@@ -6,44 +6,87 @@ import {
   FocusEventHandler,
   forwardRef,
 } from 'react';
-import { isNaN } from 'lodash';
 
 import TextField from './TextField';
 
-type NumberTextFieldProps = Omit<
+type OverridableTextFieldProps = Omit<
   ComponentProps<typeof TextField>,
   'onChange' | 'onBlur'
-> & {
-  onChange?: (event: ChangeEvent<HTMLInputElement>, value: '' | number) => void;
-  onBlur?: (event: FocusEvent<HTMLInputElement>, value: number) => void;
+>;
+
+type NumberTextFieldProps = OverridableTextFieldProps & {
+  onBlur?: (e: FocusEvent<HTMLInputElement>, value: number) => void;
+
+  /**
+   * When the user is typing, `onChange` will allow inputs of "incomplete" numbers and disallow
+   * inputs that lead to invalid numbers (e.g., `'123.4.'`). In doing so, it will always be called
+   * with a `string`. It will only be called with a valid `number` on blur.
+   *
+   * @param value The (maybe) parsed number value.
+   */
+  onChange?: (e: ChangeEvent<HTMLInputElement>, value: string | number) => void;
+
+  /**
+   * The default value to fallback if our number conversion fails.
+   */
+  fallbackValue?: number;
 };
 
-const isValidInteger = (value: number): boolean =>
-  !isNaN(value) &&
-  value >= Number.MIN_SAFE_INTEGER &&
-  value <= Number.MAX_SAFE_INTEGER;
+const DEFAULT_FALLBACK_VALUE = 0;
 
+/**
+ * Checks if a string is a valid number or is on the way to become a valid number ("incomplete").
+ * For example, it will allow,
+ * - positive numbers (`'123'`),
+ * - minus mark only (`'-'`),
+ * - minus mark only in the beginning (`'-123'`),
+ * - period only (`'.'`),
+ * - numbers with only one period (`'123.'`, `'.123'`, or `'123.1'`), and
+ * - worst combinations of the above (`-.123`).
+ */
+const VALID_INTERMEDIATE_NUMBER_REGEX = /^-?\d*\.?\d*$/;
+
+/**
+ * A text field that only accepts the input of numbers, even incompletely. It doesn't support
+ * exponential notations (e.g., `'1e3'` or `'1e+3'`), and will never give you up (no `NaN`s).
+ */
 const NumberTextField = forwardRef<HTMLDivElement, NumberTextFieldProps>(
   (props, ref): JSX.Element => {
+    /**
+     * We do not parse the number here because `parseFloat` and `Number` both optimistically
+     * parse some "incomplete" number values as valid numbers. For example, `parseFloat` will
+     * parse `'123.'` as `123`, making it impossible for a user to type in a decimal point.
+     * If we want to have `onChange` optimistically parse a number, the parser must return
+     * `NaN` for "incomplete" numbers (see `VALID_INTERMEDIATE_NUMBER_REGEX` above).
+     */
     const handleChange: ChangeEventHandler<HTMLInputElement> = (e): void => {
-      const numberValue = parseInt(e.target.value, 10);
-      if (isValidInteger(numberValue)) props.onChange?.(e, numberValue);
-      if (isNaN(numberValue)) props.onChange?.(e, '');
+      if (!VALID_INTERMEDIATE_NUMBER_REGEX.test(e.target.value)) return;
+
+      props.onChange?.(e, e.target.value);
     };
 
     const handleBlur: FocusEventHandler<HTMLInputElement> = (e): void => {
-      const numberValue = parseInt(e.target.value, 10) || 0;
-      if (isValidInteger(numberValue)) props.onChange?.(e, numberValue);
+      const parsedValue = parseFloat(e.target.value);
+
+      // `Number.isFinite` is used as a validity check because it returns `false` if the
+      // value is `NaN` or infinity. Note that if the value is larger than `Number.MAX_VALUE`
+      // or smaller than `-Number.MAX_VALUE`, `parseFloat` will return infinity.
+      const value = Number.isFinite(parsedValue)
+        ? parsedValue
+        : props.fallbackValue ?? DEFAULT_FALLBACK_VALUE;
+
+      props.onChange?.(e, value);
+      props.onBlur?.(e, value);
     };
 
-    // type="number" is highly inconsistent across browsers. See below:
-    // https://stackoverflow.blog/2022/09/15/why-the-number-input-is-the-worst-input/
     return (
       <TextField
         {...props}
         ref={ref}
         onBlur={handleBlur}
         onChange={handleChange}
+        // `type="number"` is highly inconsistent across browsers. See below:
+        // https://stackoverflow.blog/2022/09/15/why-the-number-input-is-the-worst-input/
         type="text"
       />
     );
