a39c1707acab21fed882447e0c55c45cbd45d9f5
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-10-26 13:31:31 +0800

    feat(safe_mark_as_read): add informative error if model is not readable

--- app/models/concerns/safe_mark_as_read_concern.rb
+++ app/models/concerns/safe_mark_as_read_concern.rb
@@ -3,6 +3,10 @@ module SafeMarkAsReadConcern
   extend ActiveSupport::Concern
 
   def safely_mark_as_read!(options)
+    unless respond_to?(:mark_as_read!) || Rails.env.production?
+      raise "Did you have #{self.class.name} `acts_as_readable`?"
+    end
+
     mark_as_read!(options)
   rescue ActiveRecord::RecordNotUnique
     raise if unread?(options[:for])
