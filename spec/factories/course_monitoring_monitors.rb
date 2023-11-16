1758819646e5a296e52b05ff89eaca5e0e0290cc
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-11-16 16:47:51 +0800

    feat(monitor): validate assessment must be password-protected

--- spec/factories/course_monitoring_monitors.rb
+++ spec/factories/course_monitoring_monitors.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 FactoryBot.define do
   factory :course_monitoring_monitor, class: Course::Monitoring::Monitor.name do
-    assessment
+    association :assessment, :view_password
 
     enabled { true }
     min_interval_ms { Course::Monitoring::Monitor::DEFAULT_MIN_INTERVAL_MS }
