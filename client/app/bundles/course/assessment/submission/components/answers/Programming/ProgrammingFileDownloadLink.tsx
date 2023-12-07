30a36d3b2cf68f7d0e1cec0ee1d6444f7f9fe465
Eko Widianto <ekowidianto@outlook.com>
2023-12-07 16:58:13 +0800

    feat: add programming answer file download link component in frontend

---
+++ client/app/bundles/course/assessment/submission/components/answers/Programming/ProgrammingFileDownloadLink.tsx
@@ -0,0 +1,32 @@
+import { FC } from 'react';
+import { ProgrammingContent } from 'types/course/assessment/answer/programming';
+import { downloadFile } from 'utilities/downloadFile';
+
+import Link from 'lib/components/core/Link';
+import useTranslation from 'lib/hooks/useTranslation';
+
+interface ProgrammingFileDownloadLinkProps {
+  file: ProgrammingContent;
+}
+
+const ProgrammingFileDownloadLink: FC<ProgrammingFileDownloadLinkProps> = (
+  props,
+) => {
+  const { t } = useTranslation();
+  return (
+    <Link
+      className="inline"
+      onClick={(): void =>
+        downloadFile('text/plain', props.file.content, props.file.filename)
+      }
+      underline="hover"
+    >
+      {t({
+        id: 'course.assessment.submission.answers.Programming.ProgrammingFileDownloadLink.downloadFile',
+        defaultMessage: 'Download file',
+      })}
+    </Link>
+  );
+};
+
+export default ProgrammingFileDownloadLink;
