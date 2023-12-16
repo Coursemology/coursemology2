5d26ed6b875bb73f8587124577a934cb5bd427ac
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-12-16 14:23:54 +0800

    feat(oauth): add ApplicationUserOauthConcern

--- app/controllers/concerns/application_user_concern.rb
+++ app/controllers/concerns/application_user_concern.rb
@@ -2,9 +2,10 @@
 module ApplicationUserConcern
   extend ActiveSupport::Concern
   include ApplicationUserMasqueradeConcern
+  include ApplicationUserOauthConcern
 
   included do
-    before_action :authenticate_user!, unless: :publicly_accessible?
+    before_action :authenticate!, unless: :publicly_accessible?
     rescue_from CanCan::AccessDenied, with: :handle_access_denied
     helper_method :url_to_user_or_course_user
   end
@@ -18,6 +19,10 @@ module ApplicationUserConcern
     return user_path(user) if user.is_a?(User)
   end
 
+  def current_user
+    @current_user ||= current_user_from_devise || current_user_from_doorkeeper
+  end
+
   protected
 
   def publicly_accessible?
@@ -26,5 +31,22 @@ module ApplicationUserConcern
 
   def handle_access_denied(exception)
     render json: { errors: exception.message }, status: :forbidden
+  end
+
+  private
+
+  # This method is more or less a copy of Devise's `authenticate_user!`. Devise responds
+  # to `:warden` throws via `Devise::FailureApp`. This way, we keep the error message and
+  # status code consistent with Devise (as at before Doorkeeper was used).
+  #
+  # https://github.com/heartcombo/devise/blob/e2242a95f3bb2e68ec0e9a064238ff7af6429545/lib/devise/controllers/helpers.rb#L153
+  # https://github.com/heartcombo/devise/blob/e2242a95f3bb2e68ec0e9a064238ff7af6429545/lib/devise/controllers/helpers.rb#L120
+  # https://github.com/wardencommunity/warden/blob/88d2f59adf5d650238c1e93072635196aea432dc/lib/warden/proxy.rb#L134
+  def authenticate!
+    throw :warden unless devise_controller? || current_user
+  end
+
+  def current_user_from_devise
+    warden.authenticate(scope: :user)
   end
 end
