1dc6e463ad4a8c7427854d45b718b688b33159a8
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-12-16 14:23:22 +0800

    chore(oauth): configure doorkeeper

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_07_114521) do
+ActiveRecord::Schema.define(version: 2023_12_15_074458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1326,6 +1326,48 @@ ActiveRecord::Schema.define(version: 2023_11_07_114521) do
     t.datetime "updated_at"
   end
 
+  create_table "oauth_access_grants", force: :cascade do |t|
+    t.bigint "resource_owner_id", null: false
+    t.bigint "application_id", null: false
+    t.string "token", null: false
+    t.integer "expires_in", null: false
+    t.text "redirect_uri", null: false
+    t.string "scopes", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "revoked_at"
+    t.index ["application_id"], name: "index_oauth_access_grants_on_application_id"
+    t.index ["resource_owner_id"], name: "index_oauth_access_grants_on_resource_owner_id"
+    t.index ["token"], name: "index_oauth_access_grants_on_token", unique: true
+  end
+
+  create_table "oauth_access_tokens", force: :cascade do |t|
+    t.bigint "resource_owner_id"
+    t.bigint "application_id", null: false
+    t.string "token", null: false
+    t.string "refresh_token"
+    t.integer "expires_in"
+    t.string "scopes"
+    t.datetime "created_at", null: false
+    t.datetime "revoked_at"
+    t.string "previous_refresh_token", default: "", null: false
+    t.index ["application_id"], name: "index_oauth_access_tokens_on_application_id"
+    t.index ["refresh_token"], name: "index_oauth_access_tokens_on_refresh_token", unique: true
+    t.index ["resource_owner_id"], name: "index_oauth_access_tokens_on_resource_owner_id"
+    t.index ["token"], name: "index_oauth_access_tokens_on_token", unique: true
+  end
+
+  create_table "oauth_applications", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "uid", null: false
+    t.string "secret", null: false
+    t.text "redirect_uri", null: false
+    t.string "scopes", default: "", null: false
+    t.boolean "confidential", default: true, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["uid"], name: "index_oauth_applications_on_uid", unique: true
+  end
+
   create_table "polyglot_languages", id: :serial, force: :cascade do |t|
     t.string "type", limit: 255, null: false
     t.string "name", limit: 255, null: false
@@ -1615,6 +1657,10 @@ ActiveRecord::Schema.define(version: 2023_11_07_114521) do
   add_foreign_key "instance_user_role_requests", "users", name: "fk_instance_user_role_requests_user_id"
   add_foreign_key "instance_users", "instances", name: "fk_instance_users_instance_id"
   add_foreign_key "instance_users", "users", name: "fk_instance_users_user_id"
+  add_foreign_key "oauth_access_grants", "oauth_applications", column: "application_id"
+  add_foreign_key "oauth_access_grants", "users", column: "resource_owner_id"
+  add_foreign_key "oauth_access_tokens", "oauth_applications", column: "application_id"
+  add_foreign_key "oauth_access_tokens", "users", column: "resource_owner_id"
   add_foreign_key "polyglot_languages", "polyglot_languages", column: "parent_id", name: "fk_polyglot_languages_parent_id"
   add_foreign_key "user_emails", "users", name: "fk_user_emails_user_id"
   add_foreign_key "user_identities", "users", name: "fk_user_identities_user_id"
