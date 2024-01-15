5e9d5997e8eac51a6cea14ada3040cd64c1015cb
Eko Widianto <ekowidianto@outlook.com>
2024-01-15 15:20:12 +0800

    refactor(db schema): make last_session_id nullable

--- db/migrate/20231107114521_add_session_id_to_answer.rb
+++ db/migrate/20231107114521_add_session_id_to_answer.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
 class AddSessionIdToAnswer < ActiveRecord::Migration[6.0]
   def change
-    add_column :course_assessment_answers, :last_session_id, :string, null: false, default: ''
+    add_column :course_assessment_answers, :last_session_id, :string
     add_column :course_assessment_answers, :client_version, :bigint
   end
 end
