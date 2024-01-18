969b82bc9934e74b863d5af35796d453e06b7fdf
bivanalhar <bivan.alzacky@gmail.com>
2024-01-19 01:35:23 +0800

    design API to fetch mark per question statistics

--- app/controllers/course/statistics/assessments_controller.rb
+++ app/controllers/course/statistics/assessments_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 class Course::Statistics::AssessmentsController < Course::Statistics::Controller
+  include Course::UsersHelper
+
   def assessment
     @assessment = Course::Assessment.where(id: assessment_params[:id]).
                   calculated(:maximum_grade).
@@ -24,6 +26,31 @@ class Course::Statistics::AssessmentsController < Course::Statistics::Controller
     end
   end
 
+  def marks_per_question
+    authorize!(:read_statistics, current_course)
+
+    @assessment = Course::Assessment.where(id: assessment_params[:id]).
+                  preload(course: :course_users).first
+    submissions = Course::Assessment::Submission.preload(:answers, creator: :course_users).
+                  where(assessment_id: assessment_params[:id]).
+                  calculated(:grade, :grader_ids)
+    @course_users = current_course.course_users.students.order_alphabetically
+    @course_submission_hash = @course_users.to_h do |course_user|
+      [course_user, nil]
+    end
+    @course_users_hash = @course_users.to_h do |course_user| 
+      [course_user.user_id, course_user]
+    end
+    @question_order_hash = @assessment.question_assessments.to_h do |q|
+      [q.question_id, q.weight]
+    end
+    @question_maximum_grade_hash = @assessment.questions.to_h do |q|
+      [q.id, q.maximum_grade]
+    end
+
+    filter_only_student_submission(submissions)
+  end
+
   private
 
   def assessment_params
@@ -39,5 +66,16 @@ class Course::Statistics::AssessmentsController < Course::Statistics::Controller
       grade = submission.grade
       [submitter_course_user, submission.workflow_state, submission.submitted_at, end_at, grade]
     end.compact
+  end
+
+  def filter_only_student_submission(submissions)
+    submissions.map do |submission|
+      submitter_course_user = submission.creator.course_users.select { |u| u.course_id == @assessment.course_id }.first
+      next unless submitter_course_user&.student?
+
+      answers = submission.answers.select(&:current_answer).sort_by { |answer| @question_order_hash[answer.question_id] }
+
+      @course_submission_hash[submitter_course_user] = [submission, answers]
+    end
   end
 end
