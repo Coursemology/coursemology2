a995230b1a16e8662652fb623e50bf4884e9fec5
bivanalhar <bivan.alzacky@gmail.com>
2024-01-24 00:42:15 +0800

    refactor controller and pages for stats
    - move most functions in controller to concern
    - function to get mark cell classname out from component definition

--- client/app/bundles/course/assessment/pages/AssessmentStatistics/ColorGradationLevel.ts
+++ client/app/bundles/course/assessment/pages/AssessmentStatistics/ColorGradationLevel.ts
@@ -1,4 +1,4 @@
-export const lowerGradeBackgroundColorClassName = {
+const lowerGradeBackgroundColorClassName = {
   0: 'bg-red-50',
   100: 'bg-red-100',
   200: 'bg-red-200',
@@ -7,11 +7,32 @@ export const lowerGradeBackgroundColorClassName = {
   500: 'bg-red-500',
 };
 
-export const higherGradeBackgroundColorClassName = {
+const higherGradeBackgroundColorClassName = {
   0: 'bg-green-50',
   100: 'bg-green-100',
   200: 'bg-green-200',
   300: 'bg-green-300',
   400: 'bg-green-400',
   500: 'bg-green-500',
+};
+
+// calculate the gradient of the color in each grade cell
+// 1. we compute the distance between the grade and the mid-grade (half the maximum)
+// 2. then, we compute the fraction of it -> range becomes [0,1]
+// 3. then we convert it into range [0,3] so that the shades will become [100, 200, 300]
+const calculateColorGradientLevel = (
+  grade: number,
+  halfMaxGrade: number,
+): number => {
+  return Math.round((Math.abs(grade - halfMaxGrade) / halfMaxGrade) * 5) * 100;
+};
+
+export const getClassNameForMarkCell = (
+  grade: number,
+  maxGrade: number,
+): string => {
+  const gradientLevel = calculateColorGradientLevel(grade, maxGrade / 2);
+  return grade >= maxGrade / 2
+    ? `${higherGradeBackgroundColorClassName[gradientLevel]} p-[1rem]`
+    : `${lowerGradeBackgroundColorClassName[gradientLevel]} p-[1rem]`;
 };
