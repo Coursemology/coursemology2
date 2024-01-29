45c318b24ad8d22ed4600215b2ffe745e930d9ed
bivanalhar <bivan.alzacky@gmail.com>
2024-01-30 02:17:14 +0800

    refactor(statistics): emphasis on type name (display details)

--- client/app/bundles/course/assessment/pages/AssessmentStatistics/AnswerDetails/MultipleResponseDetails.tsx
+++ client/app/bundles/course/assessment/pages/AssessmentStatistics/AnswerDetails/MultipleResponseDetails.tsx
@@ -1,9 +1,9 @@
 import { Checkbox, FormControlLabel, Typography } from '@mui/material';
 import { green } from '@mui/material/colors';
-import { QuestionAnswerDetails } from 'types/course/statistics/assessmentStatistics';
+import { QuestionAnswerDisplayDetails } from 'types/course/statistics/assessmentStatistics';
 
 const MultipleResponseDetails = (
-  props: QuestionAnswerDetails<'MultipleResponse'>,
+  props: QuestionAnswerDisplayDetails<'MultipleResponse'>,
 ): JSX.Element => {
   const { question, answer } = props;
   return (
