1e310d44e936612c74f5ed5db653a47583e55f3d
bivanalhar <bivan.alzacky@gmail.com>
2024-01-31 00:28:48 +0800

    simplify controller and API Call
    - combine ancestor info and marks per question into main statistics
    - remove all unnecessary controllers

--- app/controllers/course/statistics/assessments_controller.rb
+++ app/controllers/course/statistics/assessments_controller.rb
@@ -28,7 +28,7 @@ class Course::Statistics::AssessmentsController < Course::Statistics::Controller
     submissions = Course::Assessment::Submission.preload(creator: :course_users).
                   where(assessment_id: assessment_params[:id]).
                   calculated(:grade)
-
+              
     load_course_user_students
 
     @student_submissions_hash = fetch_hash_for_ancestor_assessment(submissions, @all_students)
