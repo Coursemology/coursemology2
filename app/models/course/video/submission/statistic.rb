ec816db2b78e34fbd9166f84558b48776a7a105a
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2024-02-28 23:06:14 +0800

    feat(cikgo): publish video task completion

--- app/models/course/video/submission/statistic.rb
+++ app/models/course/video/submission/statistic.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 class Course::Video::Submission::Statistic < ApplicationRecord
+  include Course::Video::Submission::Statistic::CikgoTaskCompletionConcern
+
   belongs_to :submission, inverse_of: :statistic
 
   validates :percent_watched, numericality: { only_integer: true,
