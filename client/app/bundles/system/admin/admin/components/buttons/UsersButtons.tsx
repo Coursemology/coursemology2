def112bf23f109ff5bbabec588cf275cd078d099
bivanalhar <bivan.alzacky@gmail.com>
2024-02-29 10:26:07 +0800

    feat(user): modify prompt for delete user
    - differentiate prompt content when deleting user in 1 instance only
    - from user associated with more than 1 instance users

--- client/app/bundles/system/admin/admin/components/buttons/UsersButtons.tsx
+++ client/app/bundles/system/admin/admin/components/buttons/UsersButtons.tsx
@@ -5,9 +5,11 @@ import { UserMiniEntity } from 'types/users';
 
 import DeleteButton from 'lib/components/core/buttons/DeleteButton';
 import MasqueradeButton from 'lib/components/core/buttons/MasqueradeButton';
+import { PromptText } from 'lib/components/core/dialogs/Prompt';
 import { USER_ROLES } from 'lib/constants/sharedConstants';
 import { useAppDispatch } from 'lib/hooks/store';
 import toast from 'lib/hooks/toast';
+import useTranslation from 'lib/hooks/useTranslation';
 
 import { deleteUser } from '../../operations';
 
@@ -24,9 +26,22 @@ const translations = defineMessages({
     id: 'system.admin.admin.UsersButton.deletionFailure',
     defaultMessage: 'Failed to delete user - {error}',
   },
+  deletionConfirmTitle: {
+    id: 'system.admin.admin.UsersButton.deletionConfirmTitle',
+    defaultMessage: 'Deleting {role} {name} ({email})?',
+  },
+  deletionPromptContent: {
+    id: 'system.admin.admin.UsersButton.deletionPromptContent',
+    defaultMessage:
+      'After deleting this user, all the associated instance users in the following instances will be deleted',
+  },
+  associatedInstances: {
+    id: 'system.admin.admin.UsersButton.associatedInstances',
+    defaultMessage: '{index}. {instanceName}',
+  },
   deletionConfirm: {
     id: 'system.admin.admin.UsersButton.deletionConfirm',
-    defaultMessage: 'Are you sure you wish to delete {role} {name} ({email})?',
+    defaultMessage: 'Are you sure?',
   },
   deleteTooltip: {
     id: 'system.admin.admin.UsersButton.deleteTooltip',
@@ -38,6 +53,7 @@ const UserManagementButtons: FC<Props> = (props) => {
   const { intl, user } = props;
   const dispatch = useAppDispatch();
   const [isDeleting, setIsDeleting] = useState(false);
+  const { t } = useTranslation();
 
   const onDelete = (): Promise<void> => {
     setIsDeleting(true);
@@ -63,16 +79,31 @@ const UserManagementButtons: FC<Props> = (props) => {
     <div key={`buttons-${user.id}`} className="whitespace-nowrap">
       <DeleteButton
         className={`user-delete-${user.id} p-0`}
-        confirmMessage={intl.formatMessage(translations.deletionConfirm, {
+        disabled={isDeleting}
+        loading={isDeleting}
+        onClick={onDelete}
+        title={t(translations.deletionConfirmTitle, {
           role: USER_ROLES[user.role],
           name: user.name,
           email: user.email,
         })}
-        disabled={isDeleting}
-        loading={isDeleting}
-        onClick={onDelete}
-        tooltip={intl.formatMessage(translations.deleteTooltip)}
-      />
+        tooltip={t(translations.deleteTooltip)}
+      >
+        {user.instances.length > 1 && (
+          <>
+            <PromptText>{t(translations.deletionPromptContent)}</PromptText>
+            {user.instances.map((instance, index) => (
+              <PromptText key={`instance-${instance.host}`}>
+                {t(translations.associatedInstances, {
+                  index: index + 1,
+                  instanceName: instance.name,
+                })}
+              </PromptText>
+            ))}
+          </>
+        )}
+        <PromptText>{t(translations.deletionConfirm)}</PromptText>
+      </DeleteButton>
       <MasqueradeButton
         canMasquerade={Boolean(user.canMasquerade)}
         className={`user-masquerade-${user.id} ml-4 p-0`}
