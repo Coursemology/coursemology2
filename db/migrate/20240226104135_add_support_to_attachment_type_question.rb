4ffc752d66095eb590bee0f3733537056a2a1672
bivanalhar <bivan.alzacky@gmail.com>
2024-03-11 13:48:30 +0800

    refactor(migration): modify column to text response question
    - attachment_type -> max_attachments

--- db/migrate/20240226104135_add_support_to_attachment_type_question.rb
+++ db/migrate/20240226104135_add_support_to_attachment_type_question.rb
@@ -1,15 +1,15 @@
 class AddSupportToAttachmentTypeQuestion < ActiveRecord::Migration[6.0]
   def up
     add_column :course_assessment_question_text_responses, :is_attachment_required, :boolean, null: false, default: false
-    add_column :course_assessment_question_text_responses, :attachment_type, :integer, null: false, default: 0
+    add_column :course_assessment_question_text_responses, :max_attachments, :integer, null: false, default: 0
 
-    # migrate all values inside allow_attachment into attachment_type
+    # migrate all values inside allow_attachment into max_attachments
 
-    # attachment type will have the value either 0 (no attachment), 1 (single file attachment), or 2 (multiple file attachment)
+    # max_attachments will contain the maximum number of attachments allowed within one question
     # for the initial migration, we would like to maintain the allowance of attachment as much as possible
     # therefore, if allow_attachment is FALSE, we map it to 0 -> no attachment is allowed
-    # otherwise, we map it into 2 -> multiple attachment is allowed
-    Course::Assessment::Question::TextResponse.update_all('attachment_type = CASE WHEN allow_attachment THEN 2 ELSE 0 END')
+    # otherwise, we map it into 50 -> the highest number of attachments possible after this migration
+    Course::Assessment::Question::TextResponse.update_all('max_attachments = CASE WHEN allow_attachment THEN 50 ELSE 0 END')
     remove_column :course_assessment_question_text_responses, :allow_attachment
   end
 
@@ -19,10 +19,10 @@ class AddSupportToAttachmentTypeQuestion < ActiveRecord::Migration[6.0]
 
     # migrate all values inside attachment_type into allow_attachment
 
-    # in the reverse process, it would not be the exact conversion from the up process since there will be value 1 here
-    # in case attachment_type is zero, we map it back to FALSE -> no attachment allowed
-    # otherwise (either single or multiple files allowed), map it back to TRUE -> multiple attachment allowed
-    Course::Assessment::Question::TextResponse.update_all('allow_attachment = (attachment_type <> 0)')
-    remove_column :course_assessment_question_text_responses, :attachment_type
+    # in the reverse process, it would not be the exact conversion from the up process since we convert from number to bool
+    # in case max_attachments is zero, we map it back to FALSE -> no attachment allowed
+    # otherwise (nonzero max_attachments recorded), map it back to TRUE -> multiple attachment allowed
+    Course::Assessment::Question::TextResponse.update_all('allow_attachment = (max_attachments <> 0)')
+    remove_column :course_assessment_question_text_responses, :max_attachments
   end
 end
