5f2082ee5f86457076e449329961d9d1afd0da72
bivanalhar <bivan.alzacky@gmail.com>
2024-03-11 15:36:44 +0800

    feat(max_attachments): add configurable max attachments
    - in FE, user can now modify max number of attachments up to 50 (default 3)

--- client/app/bundles/course/assessment/question/text-responses/operations.ts
+++ client/app/bundles/course/assessment/question/text-responses/operations.ts
@@ -38,7 +38,7 @@ const adaptPostData = (data: TextResponseData): TextResponsePostData => ({
     description: data.question.description,
     staff_only_comments: data.question.staffOnlyComments,
     maximum_grade: data.question.maximumGrade,
-    attachment_type: data.question.attachmentType,
+    max_attachments: data.question.maxAttachments,
     is_attachment_required: data.question.isAttachmentRequired,
     hide_text: data.question.hideText,
     question_assessment: { skill_ids: data.question.skillIds },
---
+++ client/app/bundles/course/assessment/question/text-responses/utils.tsx
@@ -0,0 +1,32 @@
+import {
+  AttachmentType,
+  TextResponseQuestionFormData,
+} from 'types/course/assessment/question/text-responses';
+
+export const getAttachmentTypeFromMaxAttachment = (
+  maxAttachment: number | undefined,
+): AttachmentType => {
+  if (!maxAttachment || maxAttachment === 0) {
+    return AttachmentType.NO_ATTACHMENT;
+  }
+
+  if (maxAttachment === 1) {
+    return AttachmentType.SINGLE_FILE_ATTACHMENT;
+  }
+
+  return AttachmentType.MULTIPLE_FILE_ATTACHMENTS;
+};
+
+export const getMaxAttachmentFromAttachmentType = (
+  question: TextResponseQuestionFormData,
+): number => {
+  if (question.attachmentType === AttachmentType.NO_ATTACHMENT) {
+    return 0;
+  }
+
+  if (question.attachmentType === AttachmentType.SINGLE_FILE_ATTACHMENT) {
+    return 1;
+  }
+
+  return question.maxAttachments;
+};
