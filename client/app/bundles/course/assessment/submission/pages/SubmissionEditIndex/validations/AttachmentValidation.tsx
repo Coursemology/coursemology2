323d799ff03d5df2ea3e0b9e77fe42db9e1837fd
bivanalhar <bivan.alzacky@gmail.com>
2024-03-12 10:04:45 +0800

    refactor(types-errors): remove unnecessary types and errors
    - error of more files uploaded than allowed has been caught in FileInputField
    - only maxAttachments is used for Answer

--- client/app/bundles/course/assessment/submission/pages/SubmissionEditIndex/validations/AttachmentValidation.tsx
+++ client/app/bundles/course/assessment/submission/pages/SubmissionEditIndex/validations/AttachmentValidation.tsx
@@ -1,4 +1,3 @@
-import { AttachmentType } from 'types/course/assessment/question/text-responses';
 import { SubmissionQuestionData } from 'types/course/assessment/submission/question/types';
 
 import { Attachment } from 'course/assessment/submission/components/answers/types';
@@ -11,16 +10,8 @@ export const validateAttachmentInAnswer = (
     | SubmissionQuestionData<'FileUpload'>,
   attachments: Attachment[],
 ): ErrorType => {
-  const isAttachmentRequired = question.isAttachmentRequired;
-  const onlyOneAttachmentAllowed =
-    question.attachmentType === AttachmentType.SINGLE_FILE_ATTACHMENT;
-
-  if (isAttachmentRequired && attachments.length === 0) {
+  if (question.isAttachmentRequired && attachments.length === 0) {
     return ErrorType.AttachmentRequired;
-  }
-
-  if (onlyOneAttachmentAllowed && attachments.length > 1) {
-    return ErrorType.AtMostOneAttachmentAllowed;
   }
 
   return ErrorType.NoError;
