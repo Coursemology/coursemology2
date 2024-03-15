e1d99560b7a48d781dedbece7bdb6ce116bfb81c
bivanalhar <bivan.alzacky@gmail.com>
2024-03-15 15:50:41 +0800

    fix(style): numLate before numAttempting
    - also fix the styling in concern files
    - default of avg and stdev to 0 if assessment_id not found in hash

--- app/controllers/concerns/course/statistics/times_concern.rb
+++ app/controllers/concerns/course/statistics/times_concern.rb
@@ -5,18 +5,19 @@ module Course::Statistics::TimesConcern
   private
 
   def get_time_taken(submission)
-    created_at = submission["created_at"].to_i
-    submitted_at = submission["submitted_at"].to_i
+    created_at = submission['created_at'].to_i
+    submitted_at = submission['submitted_at'].to_i
 
     submitted_at - created_at
   end
 
   def duration_statistics_hash
-    duration_hash = @submissions.to_a
-                                .reject { |s| s["workflow_state"] == "attempting" }
-                                .map { |s| [s["assessment_id"], get_time_taken(s)] }
-                                .group_by { |assessment_id, duration| assessment_id }
-                                .transform_values { |pairs| pairs.map { |_, duration| duration } }
+    duration_hash = @all_submissions_info.
+                    to_a.
+                    reject { |s| s['workflow_state'] == 'attempting' }.
+                    map { |s| [s['assessment_id'], get_time_taken(s)] }.
+                    group_by { |assessment_id, _| assessment_id }.
+                    transform_values { |pairs| pairs.map { |_, duration| duration } }
 
     average_and_stdev_each_assessment(duration_hash)
   end
