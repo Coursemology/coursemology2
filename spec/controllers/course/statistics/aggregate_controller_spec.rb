c7e2182e9470a9376e37bc3581f90dc235cd88bf
bivanalhar <bivan.alzacky@gmail.com>
2024-03-21 16:59:30 +0800

    test(assessment): test for course stats assessment

--- spec/controllers/course/statistics/aggregate_controller_spec.rb
+++ spec/controllers/course/statistics/aggregate_controller_spec.rb
@@ -143,5 +143,47 @@ RSpec.describe Course::Statistics::AggregateController, type: :controller do
         it { expect(subject).to be_successful }
       end
     end
+
+    describe '#all_assessments' do
+      render_views
+      let!(:assessment) { create(:assessment, :published, :with_all_question_types, course: course) }
+      let!(:unpublished_assessment) { create(:assessment, :with_all_question_types, course: course) }
+      subject { get :all_assessments, format: :json, params: { course_id: course, user_id: course_user } }
+
+      context 'when a Normal User pings the endpoint' do
+        let(:user) { create(:user) }
+        before { controller_sign_in(controller, user) }
+        it { expect { subject }.to raise_exception(CanCan::AccessDenied) }
+      end
+
+      context 'when a Course Student pings the endpoint' do
+        let(:user) { create(:course_student, course: course).user }
+        before { controller_sign_in(controller, user) }
+        it { expect { subject }.to raise_exception(CanCan::AccessDenied) }
+      end
+
+      context 'when a Course Teaching Assistant pings the endpoint' do
+        let(:user) { create(:course_teaching_assistant, course: course).user }
+        before { controller_sign_in(controller, user) }
+        it { expect(subject).to be_successful }
+      end
+
+      context 'when a Course Manager pings the endpoint' do
+        let(:user) { create(:course_manager, course: course).user }
+        before { controller_sign_in(controller, user) }
+
+        it 'expects to render all published assessments' do
+          expect(subject).to be_successful
+          json_result = JSON.parse(response.body)
+          expect(json_result['assessments'].count).to eq(1)
+        end
+      end
+
+      context 'when a Course Observer pings the endpoint' do
+        let(:user) { create(:course_observer, course: course).user }
+        before { controller_sign_in(controller, user) }
+        it { expect(subject).to be_successful }
+      end
+    end
   end
 end
