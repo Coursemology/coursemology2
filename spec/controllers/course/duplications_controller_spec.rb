a629b2445f6024bde3bffde9f5561bc970837b2a
bivanalhar <bivan.alzacky@gmail.com>
2024-05-03 12:08:43 +0800

    test(duplication): add more test

--- spec/controllers/course/duplications_controller_spec.rb
+++ spec/controllers/course/duplications_controller_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 RSpec.describe Course::DuplicationsController, type: :controller do
   let(:instance) { Instance.default }
   let(:destination_instance) { create(:instance) }
+  let(:destination_instance_admin) { create(:instance_user, :instructor, instance: destination_instance).user }
 
   with_tenant(:instance) do
     let(:course) { create(:course) }
@@ -23,6 +24,21 @@ RSpec.describe Course::DuplicationsController, type: :controller do
         before { sign_in(instance_admin_course_user) }
 
         it { expect { subject }.to raise_exception(CanCan::AccessDenied) }
+      end
+
+      context 'when course manager in a course which is instance normal wants to duplicate to other instance' do
+        let(:instance_normal_user) do
+          ActsAsTenant.without_tenant do
+            create(:instance_user, user: destination_instance_admin, instance: instance).user
+          end
+        end
+        let!(:instance_course_manager) { create(:course_manager, user: instance_normal_user, course: course).user }
+        before { sign_in(instance_course_manager) }
+
+        it 'expects the duplication to be successful' do
+          subject
+          expect(response).to be_successful
+        end
       end
 
       context 'when admin user wants to duplicate course to another instance' do
