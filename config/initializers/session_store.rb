24471e0e1a20a38428cc3ee341f2caa8c97c2470
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2024-05-06 16:59:40 +0800

    feat(session_store): set `SameSite` to `Lax` on development

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_coursemology2_session',
-                                                      same_site: :strict
+Rails.application.config.session_store(
+  :cookie_store,
+  key: '_coursemology2_session',
+  same_site: :lax
+)
