b3388e9d0100be3fd0544a8f41fb6186be73a95e
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2024-05-17 16:42:23 +0800

    feat(stories): automatically set course user roles on Cikgo

--- app/services/cikgo/chats_service.rb
+++ app/services/cikgo/chats_service.rb
@@ -6,7 +6,8 @@ class Cikgo::ChatsService < Cikgo::Service
     def find_or_create_room(course_user)
       result = connection(:post, 'chats', body: {
         pushKey: push_key(course_user.course),
-        userId: cikgo_user_id(course_user)
+        userId: cikgo_user_id(course_user),
+        role: cikgo_role(course_user)
       })
 
       [result[:url], result[:openThreadsCount]]
