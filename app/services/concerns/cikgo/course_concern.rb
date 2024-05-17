77d81f5f453f47d39269816ba2879806c99dbbdc
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2024-05-17 16:42:23 +0800

    feat(stories): automatically set course user roles on Cikgo

--- app/services/concerns/cikgo/course_concern.rb
+++ app/services/concerns/cikgo/course_concern.rb
@@ -8,6 +8,17 @@ module Cikgo::CourseConcern
     course_user.user.cikgo_user&.provided_user_id
   end
 
+  # Maps Coursemology's `CourseUser` role to Cikgo's course user role.
+  # :manager, :owner               -> 'owner'
+  # :teaching_assistant, :observer -> 'instructor'
+  # :student                       -> 'student'
+  def cikgo_role(course_user)
+    return 'owner' if course_user.manager_or_owner?
+    return 'instructor' if course_user.staff?
+
+    'student'
+  end
+
   def push_key(course)
     stories_settings = course.settings.course_stories_component
     return unless stories_settings
