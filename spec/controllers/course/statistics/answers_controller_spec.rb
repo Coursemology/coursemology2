be5a54f7fc7d8a4055d5fd97869423c05573ccaa
bivanalhar <bivan.alzacky@gmail.com>
2024-08-01 13:21:27 +0800

    test(answer): add test for stats answer controller

---
+++ spec/controllers/course/statistics/answers_controller_spec.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe Course::Statistics::AnswersController, type: :controller do
+  let(:instance) { Instance.default }
+
+  with_tenant(:instance) do
+    let(:course) { create(:course, :enrollable) }
+    let(:course_user) { create(:course_user, course: course) }
+    let(:assessment) do
+      create(:assessment, :published_with_mrq_question, course: course, start_at: 1.day.from_now)
+    end
+    let(:submission) { create(:submission, :graded, assessment: assessment, creator: course_user.user) }
+    let(:answer) { submission.answers.first }
+
+    describe '#question_answer_details' do
+      render_views
+      subject { get :question_answer_details, format: :json, params: { course_id: course, id: answer.id } }
+
+      context 'when the Normal User get the question answer details for the statistics' do
+        let(:user) { create(:user) }
+        before { controller_sign_in(controller, user) }
+        it { expect { subject }.to raise_exception(CanCan::AccessDenied) }
+      end
+
+      context 'when the Course Student get the question answer details for the statistics' do
+        let(:user) { create(:course_student, course: course).user }
+        before { controller_sign_in(controller, user) }
+        it { expect { subject }.to raise_exception(CanCan::AccessDenied) }
+      end
+
+      context 'when the Course Manager get the question answer details for the statistics' do
+        let(:user) { create(:course_manager, course: course).user }
+        before { controller_sign_in(controller, user) }
+
+        it 'returns OK with right question id and answer grade being displayed' do
+          expect(subject).to have_http_status(:success)
+          json_result = JSON.parse(response.body)
+
+          expect(json_result['question']['id']).to eq(answer.question.id)
+          expect(json_result['answer']['grade'].to_f).to eq(answer.grade)
+        end
+      end
+
+      context 'when the administrator get the question answer details for the statistics' do
+        let(:administrator) { create(:administrator) }
+        before { controller_sign_in(controller, administrator) }
+
+        it 'returns OK with right question id and answer grade being displayed' do
+          expect(subject).to have_http_status(:success)
+          json_result = JSON.parse(response.body)
+
+          expect(json_result['question']['id']).to eq(answer.question.id)
+          expect(json_result['answer']['grade'].to_f).to eq(answer.grade)
+        end
+      end
+    end
+  end
+end
