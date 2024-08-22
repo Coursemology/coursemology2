f0673826b1af125146184905f7fd2ab635e7d23c
bivanalhar <bivan.alzacky@gmail.com>
2024-08-22 18:01:36 +0800

    feat(job): force submit for timed submission
    - use submission_id as token
    - if user delete submission then create new one, the previous job won't affect new one

--- app/models/course/assessment/submission.rb
+++ app/models/course/assessment/submission.rb
@@ -11,7 +11,7 @@ class Course::Assessment::Submission < ApplicationRecord
 
   acts_as_experience_points_record
 
-  BUFFER_TIME_TO_FORCE_SUBMIT_MINUTES = 5.minutes
+  FORCE_SUBMIT_DELAY = 5.minutes
 
   after_save :auto_grade_submission, if: :submitted?
   after_save :retrieve_codaveri_feedback, if: :submitted?
@@ -238,9 +238,9 @@ class Course::Assessment::Submission < ApplicationRecord
   def create_force_submission_job
     return unless assessment.time_limit
 
-    Course::Assessment::Submission::ForceSubmittingJob.
-      set(wait_until: created_at + assessment.time_limit.minutes + BUFFER_TIME_TO_FORCE_SUBMIT_MINUTES).
-      perform_later(assessment, [creator_id], [], creator)
+    Course::Assessment::Submission::ForceSubmitTimedSubmissionJob.
+      set(wait_until: created_at + assessment.time_limit.minutes + FORCE_SUBMIT_DELAY).
+      perform_later(assessment, id, creator)
   end
 
   # The answers with current_answer flag set to true, filtering out orphaned answers to questions which are no longer
