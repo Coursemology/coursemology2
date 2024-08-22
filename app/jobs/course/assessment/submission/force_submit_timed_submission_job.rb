82112d5ce854d1af91c103f1c02a118034ea2d83
bivanalhar <bivan.alzacky@gmail.com>
2024-08-22 18:01:36 +0800

    feat(job): force submit for timed submission
    - use submission_id as token
    - if user delete submission then create new one, the previous job won't affect new one

---
+++ app/jobs/course/assessment/submission/force_submit_timed_submission_job.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+# This job performs the force submission for timed assessment
+class Course::Assessment::Submission::ForceSubmitTimedSubmissionJob < ApplicationJob
+  include TrackableJob
+  include Rails.application.routes.url_helpers
+
+  protected
+
+  def perform_tracked(assessment, submission_id, submitter)
+    instance = Course.unscoped { assessment.course.instance }
+
+    ActsAsTenant.with_tenant(instance) do
+      submission = Course::Assessment::Submission.find_by(id: submission_id)
+      return unless submission
+
+      force_submit(submission, submitter)
+    end
+  end
+
+  private
+
+  def force_submit(submission, submitter)
+    User.with_stamper(submitter) do
+      ActiveRecord::Base.transaction do
+        submission.update!('finalise' => 'true')
+      end
+    end
+  end
+end
