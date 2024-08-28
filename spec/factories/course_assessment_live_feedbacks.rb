af8e5f9ce05aa34a98fdbc6817f17dc8572d1c81
yoopie <sunyupei1999@gmail.com>
2024-08-28 22:03:09 +0800

    test(live-feedback-statistics) add tests for live feedback statistics

--- spec/factories/course_assessment_live_feedbacks.rb
+++ spec/factories/course_assessment_live_feedbacks.rb
@@ -2,7 +2,14 @@
 FactoryBot.define do
   factory :course_assessment_live_feedback, class: Course::Assessment::LiveFeedback do
     assessment
-    question { association(:course_assessment_question_programming, assessment: assessment) }
-    creator { association(:course_user, course: assessment.course) }
+    question { association(:course_assessment_question, assessment: assessment) }
+
+    transient do
+      with_comment { false }
+    end
+
+    after(:create) do |live_feedback, evaluator|
+      create(:course_assessment_live_feedback_code, feedback: live_feedback, with_comment: evaluator.with_comment)
+    end
   end
 end
