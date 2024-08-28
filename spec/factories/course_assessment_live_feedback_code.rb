bff53513b21bd742c3561508af9c64721b7ca33c
yoopie <sunyupei1999@gmail.com>
2024-08-28 22:03:09 +0800

    test(live-feedback-statistics) add tests for live feedback statistics

--- spec/factories/course_assessment_live_feedback_code.rb
+++ spec/factories/course_assessment_live_feedback_code.rb
@@ -4,5 +4,13 @@ FactoryBot.define do
     feedback { association(:course_assessment_live_feedback) }
     filename { 'test_code.rb' }
     content { 'puts "Hello, World!"' }
+
+    transient do
+      with_comment { false }
+    end
+
+    after(:create) do |live_feedback_code, evaluator|
+      create(:course_assessment_live_feedback_comment, code: live_feedback_code) if evaluator.with_comment
+    end
   end
 end
