9ae527e9b61137ee88341fd76d631bca9f7dd6d1
adi-herwana-nus <adi.hwn@nus.edu.sg>
2024-09-05 16:15:18 +0800

    fix(codaveri-qns-generation): fix admin codaveri settings, amend test case

--- spec/controllers/course/admin/codaveri_settings_controller_spec.rb
+++ spec/controllers/course/admin/codaveri_settings_controller_spec.rb
@@ -5,12 +5,29 @@ RSpec.describe Course::Admin::CodaveriSettingsController, type: :controller do
   let!(:instance) { create(:instance) }
   with_tenant(:instance) do
     let(:user) { create(:user) }
-    let(:course) { create(:course, creator: user) }
+    let(:lang_valid_for_codaveri) { Coursemology::Polyglot::Language::Python::Python3Point12.instance }
+    let(:lang_invalid_for_codaveri) { Coursemology::Polyglot::Language::Java::Java8.instance }
+    let(:course) do
+      course = create(:course, creator: user)
+      assessment = create(:assessment, course: course)
+      create(:course_assessment_question_programming,
+             assessment: assessment, language: lang_valid_for_codaveri, template_package: true)
+      create(:course_assessment_question_programming,
+             assessment: assessment, language: lang_invalid_for_codaveri, template_package: true)
+      course
+    end
     before { controller_sign_in(controller, user) }
 
     describe '#edit' do
+      render_views
       subject { get :edit, params: { course_id: course, format: :json } }
-      it { is_expected.to render_template(:edit) }
+      it 'renders codaveri and assessment question settings' do
+        expect(subject).to render_template(:edit)
+        programming_questions = JSON.parse(subject.body).dig('assessments', 0, 'programmingQuestions')
+        expect(programming_questions).not_to be_nil
+        expect(programming_questions).to be_an_instance_of(Array)
+        expect(programming_questions.count).to eq(1)
+      end
     end
 
     describe '#update' do
