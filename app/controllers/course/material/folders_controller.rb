d9b2ebddf4bfc0f3beb90dcec19301abfd9ff86a
jon <cysjonathan@gmail.com>
2024-10-31 22:07:29 +0800

    feat(materials): add index route api for root folder
    - clean up folders_controller methods
    - add rspec tests for show and index
    - clean up rspec tests
    - remove material root folder query in sidebar

--- app/controllers/course/material/folders_controller.rb
+++ app/controllers/course/material/folders_controller.rb
@@ -1,30 +1,29 @@
 # frozen_string_literal: true
 class Course::Material::FoldersController < Course::Material::Controller
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found
+  skip_load_resource :folder, only: [:index]
   before_action :authorize_read_owner!, only: [:show, :download]
 
+  def index
+    load_root_folder_with_subfolders
+    render 'show' unless performed?
+  end
+
   def show
-    respond_to do |format|
-      format.json do
-        @subfolders = @folder.children.with_content_statistics.accessible_by(current_ability).
-                      order(:name).includes(:owner).without_empty_linked_folder
-        # Don't display the folder if the user cannot access its owner.
-        @subfolders.select! { |f| can?(:read_owner, f) }
-      end
-    end
+    load_subfolders
   end
 
   def update
     if @folder.update(folder_params)
       @folder = @folder.parent || @folder
-      show
-      render 'show', status: :ok
+      load_subfolders
+      render 'show'
     else
       render json: { errors: @folder.errors }, status: :bad_request
     end
   end
 
   def destroy
-    parent_folder = @folder.parent
     if @folder.destroy
       head :ok
     else
@@ -36,8 +35,8 @@ class Course::Material::FoldersController < Course::Material::Controller
     @subfolder = Course::Material::Folder.new(folder_params)
     @subfolder.course = current_course
     if @subfolder.save
-      show
-      render 'show', status: :ok
+      load_subfolders
+      render 'show'
     else
       render json: { errors: @subfolder.errors }, status: :bad_request
     end
@@ -45,20 +44,13 @@ class Course::Material::FoldersController < Course::Material::Controller
 
   def upload_materials
     @materials = @folder.build_materials(files_params[:files_attributes])
-    respond_to do |format|
-      if @folder.save
-        format.json do
-          if params[:render_show]
-            show
-            return render 'show', status: :ok
-          end
-        end
-      else
-        format.json do
-          render json: { errors: @folder.errors.full_messages.to_sentence },
-                 status: :bad_request
-        end
+    if @folder.save
+      if params[:render_show]
+        load_subfolders
+        render 'show'
       end
+    else
+      render json: { errors: @folder.errors.full_messages.to_sentence }, status: :bad_request
     end
   end
 
@@ -67,9 +59,7 @@ class Course::Material::FoldersController < Course::Material::Controller
                  map { |f| f.materials.accessible_by(current_ability) }.flatten
     zip_filename = @folder.root? ? root_folder_name : @folder.name
     job = Course::Material::ZipDownloadJob.perform_later(@folder, @materials, zip_filename).job
-    respond_to do |format|
-      format.json { render partial: 'jobs/submitted', locals: { job: job } }
-    end
+    render partial: 'jobs/submitted', locals: { job: job }
   end
 
   private
@@ -85,5 +75,24 @@ class Course::Material::FoldersController < Course::Material::Controller
 
   def files_params
     params.require(:material_folder).permit(files_attributes: [])
+  end
+
+  def load_subfolders
+    @subfolders = @folder.children.with_content_statistics.accessible_by(current_ability).
+                  order(:name).includes(:owner).without_empty_linked_folder
+    # Don't display the folder if the user cannot access its owner.
+    @subfolders.select! { |f| can?(:read_owner, f) }
+  end
+
+  def load_root_folder_with_subfolders
+    @folder = current_course.root_folder
+    load_subfolders
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Missing root folder' }, status: :not_found
+  end
+
+  def handle_not_found
+    load_root_folder_with_subfolders
+    render 'show', status: :not_found unless performed?
   end
 end
