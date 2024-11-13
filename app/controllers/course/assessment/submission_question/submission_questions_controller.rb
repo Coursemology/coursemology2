019b3cb257f8b4827c7b88eb35d9bb946715e930
adi-herwana-nus <adi.hwn@nus.edu.sg>
2024-11-13 13:21:39 +0800

    fix(answer): reintroduce past answer limit

--- app/controllers/course/assessment/submission_question/submission_questions_controller.rb
+++ app/controllers/course/assessment/submission_question/submission_questions_controller.rb
@@ -2,7 +2,8 @@
 class Course::Assessment::SubmissionQuestion::SubmissionQuestionsController < \
   Course::Assessment::SubmissionQuestion::Controller
   def past_answers
-    answers = @submission_question.past_answers
+    answers_to_load = past_answers_params[:answers_to_load]&.to_i || 10
+    answers = @submission_question.past_answers(answers_to_load)
 
     respond_to do |format|
       format.json { render 'past_answers', locals: { answers: answers } }
