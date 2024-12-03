c47dc5ccfa4549f646ecfc105065569c324d9e3c
bivanalhar <bivan.alzacky@gmail.com>
2024-12-03 19:30:11 +0800

    feat(liveChat): chat platform for get help
    - split pane between programming answer and chat page

--- client/app/bundles/course/assessment/submission/pages/SubmissionEditIndex/components/button/LiveFeedbackButton.tsx
+++ client/app/bundles/course/assessment/submission/pages/SubmissionEditIndex/components/button/LiveFeedbackButton.tsx
@@ -1,74 +1,21 @@
 import { FC } from 'react';
 import { Button } from '@mui/material';
 
-import {
-  generateLiveFeedback,
-  initializeLiveFeedback,
-} from 'course/assessment/submission/actions/answers';
-import { getAssessment } from 'course/assessment/submission/selectors/assessments';
-import { getLiveFeedbacks } from 'course/assessment/submission/selectors/liveFeedbacks';
-import { getQuestionFlags } from 'course/assessment/submission/selectors/questionFlags';
-import { getQuestions } from 'course/assessment/submission/selectors/questions';
-import { getSubmission } from 'course/assessment/submission/selectors/submissions';
+import { toggleLiveFeedbackChat } from 'course/assessment/submission/reducers/liveFeedbackChats';
 import translations from 'course/assessment/submission/translations';
-import LoadingIndicator from 'lib/components/core/LoadingIndicator';
-import { getSubmissionId } from 'lib/helpers/url-helpers';
-import { useAppDispatch, useAppSelector } from 'lib/hooks/store';
+import { useAppDispatch } from 'lib/hooks/store';
 import useTranslation from 'lib/hooks/useTranslation';
 
 interface Props {
-  questionId: number;
+  answerId?: number;
 }
 
 const LiveFeedbackButton: FC<Props> = (props) => {
   const { t } = useTranslation();
-  const { questionId } = props;
+  const { answerId } = props;
 
   const dispatch = useAppDispatch();
-
-  const assessment = useAppSelector(getAssessment);
-  const submission = useAppSelector(getSubmission);
-  const questions = useAppSelector(getQuestions);
-  const questionFlags = useAppSelector(getQuestionFlags);
-  const liveFeedback = useAppSelector(getLiveFeedbacks);
-
-  const submissionId = getSubmissionId();
-
-  const question = questions[questionId];
-
-  const { questionIds } = assessment;
-  const { graderView } = submission;
-  const { answerId, attemptsLeft } = question;
-  const { isResetting } = questionFlags[questionId] || {};
-
-  const isRequestingLiveFeedback =
-    liveFeedback?.feedbackByQuestion?.[questionId]?.isRequestingLiveFeedback ??
-    false;
-
-  const isPollingLiveFeedback =
-    (liveFeedback?.feedbackByQuestion?.[questionId]?.pendingFeedbackToken ??
-      false) !== false;
-
-  const onGenerateLiveFeedback = (): void => {
-    const questionIndex = questionIds.findIndex((id) => id === questionId) + 1;
-    const successMessage = t(translations.liveFeedbackSuccess, {
-      questionIndex,
-    });
-    const noFeedbackMessage = t(translations.liveFeedbackNoneGenerated, {
-      questionIndex,
-    });
-
-    dispatch(initializeLiveFeedback(questionId));
-    dispatch(
-      generateLiveFeedback({
-        submissionId,
-        answerId,
-        questionId,
-        successMessage,
-        noFeedbackMessage,
-      }),
-    );
-  };
+  if (!answerId) return null;
 
   // TODO: update logic pending #7418: allow [Live feedback] on all programming questions
 
@@ -76,19 +23,10 @@ const LiveFeedbackButton: FC<Props> = (props) => {
     <Button
       className="mb-2 mr-2"
       color="info"
-      disabled={
-        isResetting ||
-        isRequestingLiveFeedback ||
-        isPollingLiveFeedback ||
-        (!graderView && attemptsLeft === 0)
-      }
       id="get-live-feedback"
-      onClick={() => onGenerateLiveFeedback()}
-      startIcon={
-        (isRequestingLiveFeedback || isPollingLiveFeedback) && (
-          <LoadingIndicator bare size={20} />
-        )
-      }
+      onClick={() => {
+        dispatch(toggleLiveFeedbackChat({ answerId }));
+      }}
       variant="contained"
     >
       {t(translations.generateCodaveriLiveFeedback)}
