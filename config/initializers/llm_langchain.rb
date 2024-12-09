af2d6db43ce5e55fbd4eacd156fb6ca152ec071d
Jonaspng <jonaspng@hotmail.com>
2024-12-09 22:05:35 +0800

    feat(initialiser): add llm initialiser
    - initialise LLM models that will be used in code
    - LANGCHAIN_OPENAI model is used for normal RAG operations
    - RAGAS (Retrieval Augmented Generation Assessment) model is used for evaluation of RAG

---
+++ config/initializers/llm_langchain.rb
@@ -0,0 +1,15 @@
+if ENV['OPENAI_API_KEY'].present?
+  require 'langchain'
+  # Create a global OpenAI client instance
+  LANGCHAIN_OPENAI = Langchain::LLM::OpenAI.new(
+    api_key: ENV['OPENAI_API_KEY'],
+    default_options: { temperature: 0.5, chat_completion_model_name: 'gpt-4o' }
+  )
+  # RAGAS (Retrieval Augmented Generation Assessment) used to evaluate RAG response
+  RAGAS = Langchain::LLM::OpenAI.new(
+    api_key: ENV['OPENAI_API_KEY'],
+    default_options: { temperature: 0, chat_completion_model_name: 'gpt-4o' }
+  )
+else
+  Rails.logger.error('OPENAI_API_KEY is not set in the environment')
+end
