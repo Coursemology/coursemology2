85897aa8f5c1843caa1eff05db5951eb1069b696
Jonaspng <jonaspng@hotmail.com>
2024-12-09 22:14:02 +0800

    feat(controller): update materials_controller
    - add create_text_chunks that create text chunks from material
    - add destory_text_chunks that destroy materials's text chunks
    - modify update so that if the file contents was updated (i.e new file upload) it will destroy current text chunks related to previous material
    - modify destroy to ensure that material cannot be deleted while material is still undergoing text chunk job

--- app/controllers/course/material/materials_controller.rb
+++ app/controllers/course/material/materials_controller.rb
@@ -9,13 +9,16 @@ class Course::Material::MaterialsController < Course::Material::Controller
   end
 
   def update
-    if @material.update(material_params)
+    if @material.workflow_state != 'chunking' && @material.update(material_params)
+      # deletes material's text chunk if file has been changed and file has been chunked
+      delete_material_text_chunks if material_params['file'] && @material.workflow_state == 'chunked'
       course_user = @material.attachment.updater.course_users.find_by(course: current_course)
       user = course_user || @material.attachment.updater
       render json: { id: @material.id,
                      name: @material.name,
                      description: @material.description,
                      updatedAt: @material.attachment.updated_at,
+                     workflowState: @material.workflow_state,
                      updater: { id: user.id, name: user.name,
                                 userUrl: url_to_user_or_course_user(current_course, user) } },
              status: :ok
@@ -25,7 +28,27 @@ class Course::Material::MaterialsController < Course::Material::Controller
   end
 
   def destroy
-    if @material.destroy
+    if @material.destroy && @material.workflow_state != 'chunking'
+      head :ok
+    else
+      render json: { errors: @material.errors.full_messages.to_sentence }, status: :bad_request
+    end
+  end
+
+  def create_text_chunks
+    job = last_text_chunking_job
+    if job
+      render partial: 'jobs/submitted', locals: { job: job }
+    else
+      job = @material.text_chunking!(current_user)
+      render partial: 'jobs/submitted', locals: { job: job.job }
+    end
+  end
+
+  def destroy_text_chunks
+    if @material.text_chunks.destroy_all && @material.workflow_state == 'chunked'
+      @material.delete_chunks!
+      @material.save
       head :ok
     else
       render json: { errors: @material.errors.full_messages.to_sentence }, status: :bad_request
@@ -64,5 +87,19 @@ class Course::Material::MaterialsController < Course::Material::Controller
   def log_service
     @log_service ||=
       Course::Assessment::SessionLogService.new(@assessment, current_session_id, @submission)
+  end
+
+  def last_text_chunking_job
+    job = @material.text_chunking&.job
+    (job&.status == 'submitted') ? job : nil
+  end
+
+  def delete_material_text_chunks
+    if @material.text_chunks.destroy_all
+      @material.delete_chunks!
+      @material.save
+    else
+      render json: { errors: @material.errors.full_messages.to_sentence }, status: :bad_request
+    end
   end
 end
