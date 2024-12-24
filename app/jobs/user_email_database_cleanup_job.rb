636b2e469ba3bdee7a58fcc2fdba9257bc7e2641
adi-herwana-nus <adi.hwn@nus.edu.sg>
2024-12-24 17:13:47 +0800

    fix(email-cleanup): fix database cleanup job logic
    - users with confirmed emails will no longer be removed

--- app/jobs/user_email_database_cleanup_job.rb
+++ app/jobs/user_email_database_cleanup_job.rb
@@ -2,12 +2,25 @@
 class UserEmailDatabaseCleanupJob < ApplicationJob
   def perform
     ActsAsTenant.without_tenant do
-      unconfirmed_user_ids = User::Email.where(confirmed_at: nil).
-                             where('confirmation_sent_at < ?', 6.months.ago).pluck(:user_id)
+      unconfirmed_user_ids = User.where(
+        'NOT EXISTS (
+           SELECT 1 FROM user_emails
+           WHERE user_emails.user_id = users.id
+             AND (user_emails.confirmed_at IS NOT NULL OR user_emails.confirmation_sent_at >= ?)
+         )', 6.months.ago
+      ).pluck(:id)
+
       identity_user_ids = User::Identity.pluck(:user_id)
       user_ids_to_remove = unconfirmed_user_ids - identity_user_ids
-      user_ids_to_remove.each do |user_id|
-        User.find(user_id).destroy
+      ActiveRecord::Base.transaction do
+        user_ids_to_remove.each do |user_id|
+          User.find(user_id).destroy
+        end
+
+        User::Email.
+          where(confirmed_at: nil).
+          where('confirmation_sent_at < ?', 6.months.ago).
+          destroy_all
       end
     end
   end
