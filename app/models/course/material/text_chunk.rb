69c7202c0700e650ca1beabfe9bb589fedbc0471
Jonaspng <jonaspng@hotmail.com>
2025-01-07 18:11:05 +0800

    feat(chunk_references): add course_material_text_chunk_references

--- app/models/course/material/text_chunk.rb
+++ app/models/course/material/text_chunk.rb
@@ -1,10 +1,24 @@
 # frozen_string_literal: true
 class Course::Material::TextChunk < ApplicationRecord
   has_neighbors :embedding
-  belongs_to :material, inverse_of: :text_chunks, class_name: 'Course::Material',
-                        foreign_key: :course_material_id, autosave: true
-  validates :creator, presence: true
   validates :content, presence: true
   validates :embedding, presence: true
-  validates :course_id, presence: true
+  validates :name, presence: true
+  has_many :text_chunk_references, class_name: 'Course::Material::TextChunkReference',
+                                   dependent: :destroy
+
+  class << self
+    def existing_chunks(attributes)
+      file = attributes.delete(:file)
+      attributes[:name] = file_digest(file)
+      where(attributes)
+    end
+
+    private
+
+    def file_digest(file)
+      # Get the actual file by #tempfile if the file is an `ActionDispatch::Http::UploadedFile`.
+      Digest::SHA256.file(file.try(:tempfile) || file).hexdigest
+    end
+  end
 end
