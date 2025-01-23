66cb82621cf9a35e81f6f5d1f726098b3e68d037
Jonaspng <jonaspng@hotmail.com>
2025-01-23 20:59:22 +0800

    feat(auto-answering): add auto answering feature to forum
    - rag_workflow_service handles response generation
    - response_evaluation_service evaluates quality of generated response using faithfulness and answer_relevance scores
    - auto_answering controller logic is located in auto_answering_concern
    - reply will only be automatically generated for first post in forum topic
    - identity of automated response will be System
    - identity of manually requested AI response will be the teaching staff that clicks the publish/mark as answer and publish button

--- app/models/course/discussion/post.rb
+++ app/models/course/discussion/post.rb
@@ -11,8 +11,12 @@ class Course::Discussion::Post < ApplicationRecord
       event :publish, transitions_to: :published
     end
     state :delayed
+    state :answering do
+      event :answered, transitions_to: :published
+    end
     state :published do
       event :unpublish, transitions_to: :draft
+      event :answer, transitions_to: :answering
     end
   end
 
@@ -36,10 +40,15 @@ class Course::Discussion::Post < ApplicationRecord
   validates :topic, presence: true
   validates :workflow_state, length: { maximum: 255 }, presence: true
   validates :is_anonymous, inclusion: { in: [true, false] }
+  validates :is_ai_generated, inclusion: { in: [true, false] }
+  validates :faithfulness_score, presence: true
+  validates :answer_relevance_score, presence: true
 
   belongs_to :topic, inverse_of: :posts, touch: true
   has_many :votes, inverse_of: :post, dependent: :destroy
   has_one :codaveri_feedback, inverse_of: :post, dependent: :destroy
+  has_one :rag_auto_answering, class_name: 'Course::Forum::RagAutoAnswering',
+                               inverse_of: :post, dependent: :destroy
 
   accepts_nested_attributes_for :codaveri_feedback
 
@@ -65,6 +74,18 @@ class Course::Discussion::Post < ApplicationRecord
                                                             associations: :votes,
                                                             scope: votes)
       preloader.call
+    end
+  end)
+
+  # @!method self.include_drafts_for_teaching_staff(current_user)
+  #   Includes draft posts if the user is the teaching staff.
+  #
+  #   @param [User] current_user The user to determine access for.
+  scope :include_drafts_for_teaching_staff, (lambda do |current_course_user, current_course|
+    if current_course_user&.teaching_staff? && current_course.component_enabled?(Course::RagWiseComponent)
+      all
+    else
+      where.not(workflow_state: 'draft')
     end
   end)
 
@@ -135,6 +156,14 @@ class Course::Discussion::Post < ApplicationRecord
     course_user&.name || creator.name
   end
 
+  def rag_auto_answer!(topic, current_author, current_course_author, settings)
+    ensure_rag_auto_answering!
+    Course::Forum::AutoAnsweringJob.perform_later(self, topic, current_author,
+                                                  current_course_author, settings).tap do |job|
+      rag_auto_answering.update_column(:job_id, job.job_id)
+    end
+  end
+
   private
 
   def set_topic
@@ -171,5 +200,16 @@ class Course::Discussion::Post < ApplicationRecord
 
   def sanitize_text
     self.text = ApplicationController.helpers.sanitize_ckeditor_rich_text(text)
+  end
+
+  def ensure_rag_auto_answering!
+    ActiveRecord::Base.transaction(requires_new: true) do
+      rag_auto_answering || create_rag_auto_answering!
+    end
+  rescue ActiveRecord::RecordInvalid, ActiveRecord::RecordNotUnique => e
+    raise e if e.is_a?(ActiveRecord::RecordInvalid) && e.record.errors[:post_id].empty?
+
+    association(:rag_auto_answering).reload
+    rag_auto_answering
   end
 end
