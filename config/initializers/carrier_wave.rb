ceb4315724c381a2a9d352480fa09c41e590b78f
adi-herwana-nus <adi.hwn@nus.edu.sg>
2025-02-25 19:06:19 +0800

    fix(carrierwave): revert carrierwave config to match exactly dockerfiles version

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,8 +4,12 @@ CarrierWave.configure do |config|
     require 'carrierwave/storage/fog'
 
     config.storage = :fog
-    config.fog_credentials = { provider: 'AWS' }
-    config.fog_attributes = { s3_client: S3_CLIENT } # reuse credentials from aws.rb
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_REGION']
+    }
     config.fog_directory = ENV['AWS_BUCKET']
     config.fog_public = false
   else
