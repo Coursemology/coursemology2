6aa9c9f461b6be2456b519498cc497e71150f953
Jonaspng <jonaspng@hotmail.com>
2025-03-01 21:30:00 +0800

    feat(UI): add and modify UI for forum imports and multiple material chunking
    - forum import switch will be disabled if owner/manager of that course is not the owner/manager of the parent course

--- client/app/api/course/Admin/RagWise.ts
+++ client/app/api/course/Admin/RagWise.ts
@@ -1,10 +1,16 @@
 import { AxiosResponse } from 'axios';
 import {
+  Course,
   Folder,
+  ForumImport,
+  ForumImportData,
   Material,
   RagWiseSettings,
   RagWiseSettingsPostData,
 } from 'types/course/admin/ragWise';
+import { JobSubmitted } from 'types/jobs';
+
+import { APIResponse } from 'api/types';
 
 import BaseAdminAPI from './Base';
 
@@ -29,5 +35,24 @@ export default class RagWiseAdminAPI extends BaseAdminAPI {
 
   folders(): Promise<AxiosResponse<{ folders: Folder[] }>> {
     return this.client.get(`${this.urlPrefix}/folders`);
+  }
+
+  courses(): Promise<AxiosResponse<{ courses: Course[] }>> {
+    return this.client.get(`${this.urlPrefix}/courses`);
+  }
+
+  forums(): Promise<AxiosResponse<{ forums: ForumImport[] }>> {
+    return this.client.get(`${this.urlPrefix}/forums`);
+  }
+
+  importCourseForums(params: ForumImportData): APIResponse<JobSubmitted> {
+    return this.client.put(`${this.urlPrefix}/import_course_forums`, params);
+  }
+
+  destroyImportedDiscussions(params: ForumImportData): APIResponse {
+    return this.client.put(
+      `${this.urlPrefix}/destroy_imported_discussions`,
+      params,
+    );
   }
 }
