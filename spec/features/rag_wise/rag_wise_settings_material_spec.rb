7c3d2af525e947478a8d602d72eca6b519fde7a8
Jonaspng <jonaspng@hotmail.com>
2025-03-01 21:31:27 +0800

    test(rspec): add and modify test cases for forum-import and multiple material chunking

--- spec/features/rag_wise/rag_wise_settings_material_spec.rb
+++ spec/features/rag_wise/rag_wise_settings_material_spec.rb
@@ -7,95 +7,13 @@ RSpec.feature 'Course: Administration: RagWise', js: true do
   with_tenant(:instance) do
     let(:course) { create(:course, :with_rag_wise_component_enabled) }
 
-    before { login_as(user, scope: :user) }
+    before { login_as(user, scope: :user, redirect_url: course_material_folders_path(course)) }
 
     context 'As a Course Manager' do
       let(:user) { create(:course_manager, course: course).user }
       let(:parent_folder) { course.root_folder }
 
-      scenario 'I can enable and disable auto answer' do
-        visit course_admin_rag_wise_path(course)
-
-        do_not_respond_radio_button = find('label', text: 'Do not automatically respond')
-        do_not_respond_radio_button.click
-        click_button 'Save changes'
-        expect_toastify('Your changes have been saved.', dismiss: true)
-        expect(course.reload.rag_wise_response_workflow).to eq('no')
-
-        respond_radio_button = find('label', text: 'Automatically respond')
-        respond_radio_button.click
-        click_button 'Save changes'
-        expect_toastify('Your changes have been saved.', dismiss: true)
-        expect(course.reload.rag_wise_response_workflow).to eq('0')
-
-        slider_thumb = find('.MuiSlider-thumb')
-        target_low_trust = find('.MuiSlider-mark[data-index="1"]')
-        slider_thumb.drag_to(target_low_trust)
-
-        click_button 'Save changes'
-        expect_toastify('Your changes have been saved.', dismiss: true)
-        expect(course.reload.rag_wise_response_workflow).to eq('30')
-
-        target_high_trust = find('.MuiSlider-mark[data-index="2"]')
-        slider_thumb.drag_to(target_high_trust)
-
-        click_button 'Save changes'
-        expect_toastify('Your changes have been saved.', dismiss: true)
-        expect(course.reload.rag_wise_response_workflow).to eq('70')
-
-        target_publish = find('.MuiSlider-mark[data-index="3"]')
-        slider_thumb.drag_to(target_publish)
-
-        click_button 'Save changes'
-        expect_toastify('Your changes have been saved.', dismiss: true)
-        expect(course.reload.rag_wise_response_workflow).to eq('100')
-
-        target_draft = find('.MuiSlider-mark[data-index="0"]')
-        slider_thumb.drag_to(target_draft)
-
-        click_button 'Save changes'
-        expect_toastify('Your changes have been saved.', dismiss: true)
-        expect(course.reload.rag_wise_response_workflow).to eq('0')
-      end
-
-      scenario 'I can edit character prompt and click promp templates' do
-        visit course_admin_rag_wise_path(course)
-
-        char_prompt_field = 'Character prompt (Max 200 Characters)'
-        new_prompt = 'testing'
-        fill_in char_prompt_field, with: new_prompt
-
-        click_button 'Save changes'
-        expect_toastify('Your changes have been saved.', dismiss: true)
-        expect(course.reload.rag_wise_character_prompt).to eq(new_prompt)
-
-        no_roleplay_button = find('.MuiChip-label', text: 'No roleplay')
-        no_roleplay_button.click
-
-        click_button 'Save changes'
-        expect_toastify('Your changes have been saved.', dismiss: true)
-        expect(course.reload.rag_wise_character_prompt).to eq('')
-
-        deadpool_prompt = 'You must always impersonate Deadpool character in all your responses.'
-        deadpool_roleplay_button = find('.MuiChip-label', text: 'Deadpool')
-        deadpool_roleplay_button.click
-
-        click_button 'Save changes'
-        expect_toastify('Your changes have been saved.', dismiss: true)
-        expect(course.reload.rag_wise_character_prompt).to eq(deadpool_prompt)
-
-        yoda_prompt = 'You must always impersonate Master Yoda character in all your responses.'
-        yoda_roleplay_button = find('.MuiChip-label', text: 'Master Yoda')
-        yoda_roleplay_button.click
-
-        click_button 'Save changes'
-        expect_toastify('Your changes have been saved.', dismiss: true)
-        expect(course.reload.rag_wise_character_prompt).to eq(yoda_prompt)
-      end
-
       scenario 'I can see uploaded pdfs and txt in materials tree' do
-        visit course_material_folders_path(course)
-
         # inserting files
         txt_file = File.join(Rails.root, '/spec/fixtures/files/text.txt')
         pdf_file = File.join(Rails.root, '/spec/fixtures/files/one-page-document.pdf')
@@ -116,13 +34,11 @@ RSpec.feature 'Course: Administration: RagWise', js: true do
         expect(page).to_not have_selector('.MuiListItemText-root span', text: 'template_file')
       end
 
-      scenario 'I can add and remove file from knowledge base' do
+      scenario 'I can add and remove file from knowledge base (singular)' do
         allow_any_instance_of(RagWise::ChunkingService).to receive(:file_chunking).and_return(['test'])
         vector = JSON.parse(File.read(File.join(Rails.root, '/spec/features/rag_wise/vector.json')))['vector']
         allow_any_instance_of(RagWise::LlmService).to receive(:generate_embeddings_from_chunks).
-          and_return(vector)
-
-        visit course_material_folders_path(course)
+          and_return([vector])
 
         # inserting files
         pdf_file = File.join(Rails.root, '/spec/fixtures/files/two-page-document-with-text.pdf')
@@ -149,11 +65,45 @@ RSpec.feature 'Course: Administration: RagWise', js: true do
         expect(file.reload.workflow_state).to eq('not_chunked')
       end
 
+      scenario 'I can add and remove files from knowledge base (plural)' do
+        allow_any_instance_of(RagWise::ChunkingService).to receive(:file_chunking).and_return(['test'])
+        vector = JSON.parse(File.read(File.join(Rails.root, '/spec/features/rag_wise/vector.json')))['vector']
+        allow_any_instance_of(RagWise::LlmService).to receive(:generate_embeddings_from_chunks).
+          and_return([vector])
+
+        # inserting files
+        pdf_file = File.join(Rails.root, '/spec/fixtures/files/two-page-document-with-text.pdf')
+        txt_file = File.join(Rails.root, '/spec/fixtures/files/text.txt')
+        find('#upload-files-button').click
+        find('input[type="file"]', visible: false).attach_file([pdf_file, txt_file], make_visible: true)
+        find('button#material-upload-form-upload-button').click
+        wait_for_page
+        first_file = parent_folder.materials.first
+        second_file = parent_folder.materials[1]
+
+        visit course_admin_rag_wise_path(course)
+
+        find('label', text: 'Expand all folders').click
+        expect(first_file.reload.workflow_state).to eq('not_chunked')
+        expect(second_file.reload.workflow_state).to eq('not_chunked')
+
+        # toggle knowledge base button
+        folder_switch = find('.MuiListItemText-root span', text: 'Root').
+                        ancestor('div.items-center').find('.MuiSwitch-root')
+        folder_switch.click
+        wait_for_page
+        expect(first_file.reload.workflow_state).to eq('chunked')
+        expect(second_file.reload.workflow_state).to eq('chunked')
+        folder_switch.click
+        wait_for_page
+        expect(first_file.reload.workflow_state).to eq('not_chunked')
+        expect(second_file.reload.workflow_state).to eq('not_chunked')
+      end
+
       scenario 'I cannot add file to knowledge base' do
         allow_any_instance_of(RagWise::ChunkingService).to receive(:file_chunking).and_return(['test'])
         allow_any_instance_of(RagWise::LlmService).to receive(:generate_embeddings_from_chunks).
           and_return(nil)
-        visit course_material_folders_path(course)
 
         # inserting files
         txt_file = File.join(Rails.root, '/spec/fixtures/files/text.txt')
@@ -173,6 +123,7 @@ RSpec.feature 'Course: Administration: RagWise', js: true do
         # toggle knowledge base button
         txt_file_switch = find('.MuiListItemText-root span', text: txt_file_name).ancestor('li').find('.MuiSwitch-root')
         txt_file_switch.click
+        wait_for_page
         expect_toastify("#{txt_file_name} could not be added to knowledge base.", dismiss: true)
         expect(file.reload.workflow_state).to eq('not_chunked')
       end
