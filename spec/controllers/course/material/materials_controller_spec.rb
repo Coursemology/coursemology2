229fd925f4e5e7288e4aef02c3035e1014590de1
Jonaspng <jonaspng@hotmail.com>
2025-03-01 21:31:27 +0800

    test(rspec): add and modify test cases for forum-import and multiple material chunking

--- spec/controllers/course/material/materials_controller_spec.rb
+++ spec/controllers/course/material/materials_controller_spec.rb
@@ -73,51 +73,5 @@ RSpec.describe Course::Material::MaterialsController, type: :controller do
         it { expect(response.status).to eq(400) }
       end
     end
-
-    describe '#create_text_chunks' do
-      let(:text_chunking) { create(:text_chunking, :chunking) }
-      let(:chunking_material_stub) { create(:material, :chunking, text_chunking: text_chunking) }
-
-      let(:no_text_chunking) { create(:text_chunking, :no_chunking) }
-      let(:no_chunking_material_stub) { create(:material, :chunking, text_chunking: no_text_chunking) }
-      subject { put :create_text_chunks, params: { course_id: course, folder_id: folder, id: chunking_material_stub } }
-
-      context 'when last_text_chunking_job exists' do
-        before do
-          allow(controller).to receive(:render)
-          controller.instance_variable_set(:@material, chunking_material_stub)
-          subject
-        end
-        it 'returns existing job' do
-          expect(controller).to have_received(:render).with(partial: 'jobs/submitted',
-                                                            locals: { job: text_chunking.job })
-        end
-      end
-
-      context 'when last_text_chunking_job does not exists' do
-        before do
-          allow(controller).to receive(:render)
-          controller.instance_variable_set(:@material, no_chunking_material_stub)
-          subject
-        end
-        it 'creates a new job' do
-          expect(controller).to have_received(:render).with(partial: 'jobs/submitted',
-                                                            locals: { job: no_text_chunking.job })
-        end
-      end
-    end
-
-    describe '#destroy_text_chunks' do
-      let(:material) { create(:material, folder: folder) }
-      subject { delete :destroy_text_chunks, params: { course_id: course, folder_id: folder, id: material_stub } }
-
-      context 'when text chunks cannot be destroyed' do
-        before do
-          controller.instance_variable_set(:@material, material_stub)
-          subject
-        end
-        it { expect(response.status).to eq(400) }
-      end
-    end
   end
 end
