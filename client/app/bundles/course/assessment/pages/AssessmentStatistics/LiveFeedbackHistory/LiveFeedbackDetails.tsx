4688b008af5c582069c221fdfcb644246038bac1
Bivan Alzacky Harmanto <bivan.alzacky@gmail.com>
2025-03-10 17:22:16 +0800

    feat(liveFeedback): add option list offered in each message

--- client/app/bundles/course/assessment/pages/AssessmentStatistics/LiveFeedbackHistory/LiveFeedbackDetails.tsx
+++ client/app/bundles/course/assessment/pages/AssessmentStatistics/LiveFeedbackHistory/LiveFeedbackDetails.tsx
@@ -1,6 +1,7 @@
-import { FC, useState } from 'react';
+import { FC, useRef, useState } from 'react';
 import { Typography } from '@mui/material';
 
+import CustomSlider from 'lib/components/extensions/CustomSlider';
 import { useAppSelector } from 'lib/hooks/store';
 import useTranslation from 'lib/hooks/useTranslation';
 import { formatLongDateTime } from 'lib/moment';
@@ -16,12 +17,45 @@ const LiveFeedbackDetails: FC = () => {
 
   const messages = useAppSelector(getLiveFeedbackChatMessages);
 
+  const messageTimeMarkers = messages.map((message, idx) => {
+    return {
+      value: idx,
+      label:
+        idx === 0 || idx === messages.length - 1
+          ? formatLongDateTime(message.createdAt)
+          : '',
+    };
+  });
+
+  const scrollableRef = useRef<HTMLDivElement>(null);
+
   const [selectedMessageIndex, setSelectedMessageIndex] = useState(
     messages.length - 1,
   );
 
+  const currentMessageMarker = messageTimeMarkers[selectedMessageIndex];
+  const latestMessageMarker = messageTimeMarkers[messageTimeMarkers.length - 1];
+  const earliestMessageMarker = messageTimeMarkers[0];
+
   return (
     <>
+      <div className="w-[calc(100%_-_17rem)] mx-auto pt-8">
+        <CustomSlider
+          defaultValue={currentMessageMarker.value}
+          marks={messageTimeMarkers}
+          max={latestMessageMarker.value}
+          min={earliestMessageMarker.value}
+          onChange={(_, value) => {
+            const newIndex = value as number;
+            setSelectedMessageIndex(newIndex);
+          }}
+          step={null}
+          valueLabelDisplay="on"
+          valueLabelFormat={(value) =>
+            `${formatLongDateTime(messageTimeMarkers[value].label)} (${value + 1} of ${messages.length})`
+          }
+        />
+      </div>
       <Typography className="py-2" variant="h6">
         {t(translations.messageTimingTitle, {
           usedAt: formatLongDateTime(messages[selectedMessageIndex].createdAt),
@@ -38,7 +72,10 @@ const LiveFeedbackDetails: FC = () => {
           ))}
         </div>
 
-        <div className="absolute max-h-[100%] flex w-1/2 whitespace-nowrap right-0">
+        <div
+          ref={scrollableRef}
+          className="absolute max-h-full flex w-1/2 whitespace-nowrap right-0"
+        >
           <LiveFeedbackConversation
             messages={messages}
             selectedMessageIndex={selectedMessageIndex}
--- client/app/bundles/course/assessment/submission/components/GetHelpChatPage/utils.ts
+++ client/app/bundles/course/assessment/submission/components/GetHelpChatPage/utils.ts
@@ -1,3 +1,5 @@
+import { LiveFeedbackChatMessage } from 'types/course/assessment/submission/liveFeedback';
+
 export const justifyPosition = (
   isStudent: boolean,
   isError: boolean,
@@ -11,4 +13,79 @@ export const justifyPosition = (
   }
 
   return 'justify-start';
+};
+
+export const isAllFileIdsIdentical = (
+  fileIds: number[],
+  fileIdHash: Record<number, boolean>,
+): boolean => {
+  if (fileIds.length !== Object.keys(fileIdHash).map(Number).length) {
+    return false;
+  }
+
+  for (let i = 0; i < fileIds.length; i++) {
+    if (!fileIdHash[fileIds[i]]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+export const fetchAllIndexWithIdenticalFileIds = (
+  messages: LiveFeedbackChatMessage[],
+  selectedMessageIndex: number,
+): Record<number, boolean> => {
+  const selectedMessageFileIdHash: Record<number, boolean> = messages[
+    selectedMessageIndex
+  ].files.reduce(function (map, file) {
+    map[file.id] = true;
+    return map;
+  }, {});
+
+  const allIndexWithIdenticalFileIds: Record<number, boolean> = {};
+  allIndexWithIdenticalFileIds[selectedMessageIndex] = true;
+
+  let doneChoosingBackwardIndex = false;
+  let doneChoosingForwardIndex = false;
+
+  for (let offset = 1; offset < messages.length; offset++) {
+    if (!doneChoosingBackwardIndex) {
+      const backwardIndex = selectedMessageIndex - offset;
+      if (backwardIndex >= 0) {
+        if (
+          isAllFileIdsIdentical(
+            messages[backwardIndex].files.map((file) => file.id),
+            selectedMessageFileIdHash,
+          )
+        ) {
+          allIndexWithIdenticalFileIds[backwardIndex] = true;
+        } else {
+          doneChoosingBackwardIndex = true;
+        }
+      } else {
+        doneChoosingBackwardIndex = true;
+      }
+    }
+
+    if (!doneChoosingForwardIndex) {
+      const forwardIndex = selectedMessageIndex + offset;
+      if (forwardIndex <= messages.length - 1) {
+        if (
+          isAllFileIdsIdentical(
+            messages[forwardIndex].files.map((file) => file.id),
+            selectedMessageFileIdHash,
+          )
+        ) {
+          allIndexWithIdenticalFileIds[forwardIndex] = true;
+        } else {
+          doneChoosingForwardIndex = true;
+        }
+      } else {
+        doneChoosingForwardIndex = true;
+      }
+    }
+  }
+
+  return allIndexWithIdenticalFileIds;
 };
