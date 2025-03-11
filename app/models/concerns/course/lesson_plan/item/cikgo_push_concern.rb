d46db512efaafeb6958208fdc9b3bd91c3d66090
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2025-03-11 16:07:32 +0800

    fix(cikgo_push_concern): not pushing after committing edits

--- app/models/concerns/course/lesson_plan/item/cikgo_push_concern.rb
+++ app/models/concerns/course/lesson_plan/item/cikgo_push_concern.rb
@@ -5,18 +5,31 @@ module Course::LessonPlan::Item::CikgoPushConcern
   include Cikgo::PushableItemConcern
 
   included do
-    after_create_commit -> { push(:create) }, if: -> { published? }
-    after_update_commit -> { push(:create) }, if: -> { saved_change_to_published? && published? }
+    after_save :persist_dirty_states
 
-    after_update_commit -> { push(:delete) }, if: -> { saved_change_to_published? && !published? }
+    # We use `after_commit`s for these because we want to only push after the transaction succeeds.
+    after_create_commit -> { push(:create) }, if: -> { published? }
+    after_update_commit -> { push(:create) }, if: -> { @did_change_published && published? }
+
+    after_update_commit -> { push(:delete) }, if: -> { @did_change_published && !published? }
     after_destroy_commit -> { push(:delete) }, if: -> { published? }
 
     after_update_commit -> { push(:update) }, if: (lambda do
-      published? && (saved_change_to_title? || saved_change_to_description?)
+      published? && (@did_change_title || @did_change_description)
     end)
   end
 
   private
+
+  # We do this because these `saved_change_to_*?` are not available in `after_commit`. Presumably, the
+  # dirty states have been replaced by the update to `updated_at`.
+  def persist_dirty_states
+    return unless saved_change_to_title? || saved_change_to_description? || saved_change_to_published?
+
+    @did_change_title = saved_change_to_title?
+    @did_change_description = saved_change_to_description?
+    @did_change_published = saved_change_to_published?
+  end
 
   def create_payload
     kind = actable.class.name.demodulize
