ad5e511348ebf0504f95605690359e5b56e8427e
Bivan Alzacky Harmanto <bivan.alzacky@gmail.com>
2025-03-25 14:51:16 +0800

    refactor(statistics): optimise N+1 problems on main and ancestor statistics

--- app/controllers/course/statistics/assessments_controller.rb
+++ app/controllers/course/statistics/assessments_controller.rb
@@ -5,14 +5,14 @@ class Course::Statistics::AssessmentsController < Course::Statistics::Controller
   include Course::Statistics::UsersConcern
 
   def main_statistics
-    @assessment = Course::Assessment.where(id: assessment_params[:id]).
-                  calculated(:maximum_grade, :question_count).
+    @assessment = Course::Assessment.unscoped.
                   includes(programming_questions: [:language]).
-                  preload(course: :course_users).first
-    submissions = Course::Assessment::Submission.where(assessment_id: assessment_params[:id]).
-                  calculated(:grade, :grader_ids).
-                  preload(creator: :course_users)
-    @course_users_hash = preload_course_users_hash(@assessment.course)
+                  calculated(:maximum_grade, :question_count).
+                  find(assessment_params[:id])
+    submissions = Course::Assessment::Submission.unscoped.
+                  where(assessment_id: assessment_params[:id]).
+                  calculated(:grade, :grader_ids)
+    @course_users_hash = preload_course_users_hash(current_course)
 
     load_course_user_students_info
     load_ordered_questions
@@ -24,14 +24,16 @@ class Course::Statistics::AssessmentsController < Course::Statistics::Controller
   end
 
   def ancestor_statistics
-    @assessment = Course::Assessment.where(id: assessment_params[:id]).
+    @assessment = Course::Assessment.
+                  preload(lesson_plan_item: [:reference_times, personal_times: :course_user]).
                   calculated(:maximum_grade).
-                  preload(lesson_plan_item: [:reference_times, personal_times: :course_user],
-                          course: :course_users).first
+                  find(assessment_params[:id])
     authorize!(:read_ancestor, @assessment)
-    submissions = Course::Assessment::Submission.preload(creator: :course_users).
+    submissions = Course::Assessment::Submission.unscoped.
+                  preload(creator: :course_users).
                   where(assessment_id: assessment_params[:id]).
                   calculated(:grade)
+    @course_users_hash = preload_course_users_hash(current_course)
 
     @all_students = @assessment.course.course_users.students
     @student_submissions_hash = fetch_hash_for_ancestor_assessment(submissions, @all_students).compact
