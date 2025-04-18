992f14347cbe7dfcd72f0c1fcf2a8ba34b9058f2
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2025-04-19 00:49:49 +0800

    feat(webpack): allow configuring client port

--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -5,6 +5,7 @@ const common = require('./webpack.common');
 const packageJSON = require('./package.json');
 
 const SERVER_PORT = packageJSON.devServer.serverPort;
+const CLIENT_PORT = packageJSON.devServer.clientPort;
 const APP_HOST = packageJSON.devServer.appHost;
 
 const BLUE_ANSI = '\x1b[36m%s\x1b[0m';
@@ -24,6 +25,7 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'eval-cheap-module-source-map',
   devServer: {
+    port: CLIENT_PORT,
     allowedHosts: [`.${APP_HOST}`],
     historyApiFallback: true,
     devMiddleware: {
