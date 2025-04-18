4730f44279e2f5ef3781fc82c06736e9542550e6
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2025-04-19 00:41:18 +0800

    perf(router): lazy load all routes

--- client/app/bundles/common/TermsOfServicePage/index.tsx
+++ client/app/bundles/common/TermsOfServicePage/index.tsx
@@ -1,10 +1,8 @@
-import { lazy, Suspense } from 'react';
 import { defineMessages } from 'react-intl';
 
-const TermsOfServicePage = lazy(
-  () =>
-    import(/* webpackChunkName: "TermsOfServicePage" */ './TermsOfServicePage'),
-);
+import MarkdownPage from 'lib/components/core/layouts/MarkdownPage';
+
+import termsOfService from './terms-of-service.md';
 
 const translations = defineMessages({
   termsOfService: {
@@ -13,12 +11,10 @@ const translations = defineMessages({
   },
 });
 
-const SuspensedTermsOfServicePage = (): JSX.Element => (
-  <Suspense>
-    <TermsOfServicePage />
-  </Suspense>
+const TermsOfServicePage = (): JSX.Element => (
+  <MarkdownPage className="m-auto max-w-7xl" markdown={termsOfService} />
 );
 
 const handle = translations.termsOfService;
 
-export default Object.assign(SuspensedTermsOfServicePage, { handle });
+export default Object.assign(TermsOfServicePage, { handle });
