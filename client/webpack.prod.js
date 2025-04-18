028a8b8e169d427c0d5c397121ca8c1aafec42fc
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2025-04-19 01:00:06 +0800

    perf(webpack): trim moment-timezone data to only from 2014

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -1,5 +1,6 @@
 const { merge } = require('webpack-merge');
 const TerserPlugin = require('terser-webpack-plugin');
+const MomentTimezoneDataPlugin = require('moment-timezone-data-webpack-plugin');
 
 const common = require('./webpack.common');
 
@@ -43,4 +44,7 @@ module.exports = merge(common, {
       },
     ],
   },
+  plugins: [
+    new MomentTimezoneDataPlugin({ startYear: 2014 }),
+  ],
 });
