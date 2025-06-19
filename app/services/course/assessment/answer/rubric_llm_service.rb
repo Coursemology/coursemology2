fb0685a166811f29d2889846aba9c476ea2ac870
Nguyen Cao Duy <nguyen.cao.duy.0303@gmail.com>
2025-06-19 13:48:56 +0800

    refactor(rubric_auto_grading): remove bonus categories from input prompt and update output schema descriptions

--- app/services/course/assessment/answer/rubric_llm_service.rb
+++ app/services/course/assessment/answer/rubric_llm_service.rb
@@ -45,7 +45,7 @@ class Course::Assessment::Answer::RubricLlmService
   # @param [Course::Assessment::Question::TextResponse] question The question containing rubric categories
   # @return [String] Formatted string representation of rubric categories and criteria
   def format_rubric_categories(question)
-    question.categories.includes(:criterions).map do |category|
+    question.categories.without_bonus_category.includes(:criterions).map do |category|
       criterions = category.criterions.map do |criterion|
         "- [Grade: #{criterion.grade}, Criterion ID: #{criterion.id}]: #{criterion.explanation}"
       end
