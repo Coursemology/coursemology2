e996dd97b6964bdbe64fdf8ff3102184e8c46f2b
craigtonlian <craigton.lian@gmail.com>
2025-07-29 13:41:22 +0800

    feat(SystemAdminPanel): add commit hash display

--- app/controllers/system/admin/admin_controller.rb
+++ app/controllers/system/admin/admin_controller.rb
@@ -2,4 +2,10 @@
 class System::Admin::AdminController < System::Admin::Controller
   def index
   end
+
+  def deployment_info
+    render json: {
+      commit_hash: ENV.fetch('GIT_COMMIT', nil)
+    }
+  end
 end
--- spec/controllers/system/admin/admin_controller_spec.rb
+++ spec/controllers/system/admin/admin_controller_spec.rb
@@ -12,5 +12,51 @@ RSpec.describe System::Admin::AdminController do
       before { get :index, as: :json }
       it { is_expected.to render_template('index') }
     end
+
+    describe '#deployment_info' do
+      subject { get :deployment_info, as: :json }
+
+      context 'when GIT_COMMIT environment variable is set' do
+        before do
+          @original_git_commit = ENV.fetch('GIT_COMMIT', nil)
+          ENV['GIT_COMMIT'] = 'abc123def456'
+        end
+
+        after do
+          ENV['GIT_COMMIT'] = @original_git_commit
+        end
+
+        it 'returns the commit hash' do
+          subject
+          expect(response).to have_http_status(:ok)
+          expect(JSON.parse(response.body)).to eq({ 'commit_hash' => 'abc123def456' })
+        end
+      end
+
+      context 'when GIT_COMMIT environment variable is not set' do
+        before do
+          @original_git_commit = ENV.fetch('GIT_COMMIT', nil)
+          ENV.delete('GIT_COMMIT')
+        end
+
+        after do
+          ENV['GIT_COMMIT'] = @original_git_commit
+        end
+
+        it 'returns nil for commit hash' do
+          subject
+          expect(response).to have_http_status(:ok)
+          expect(JSON.parse(response.body)).to eq({ 'commit_hash' => nil })
+        end
+      end
+
+      context 'when user is not an administrator' do
+        let(:user) { create(:user) }
+
+        it 'raises CanCan::AccessDenied exception' do
+          expect { subject }.to raise_exception(CanCan::AccessDenied)
+        end
+      end
+    end
   end
 end
