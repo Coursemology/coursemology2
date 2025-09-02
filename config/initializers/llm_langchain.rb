3cd232b20aa34b846600c05b81c1caa8e23c1a27
jon <cysjonathan@gmail.com>
2025-09-02 18:28:01 +0800

    fix(llm): update langchain gem
    - downgrade to non-reasoning gpt model
    - json-schema gem major version upgrade 4.3.1 to 5.2.2 https://github.com/voxpupuli/json-schema/commit/55dfa66d1103443c96e6c58f5ddbbb1d0c8c1336

--- config/initializers/llm_langchain.rb
+++ config/initializers/llm_langchain.rb
@@ -3,12 +3,12 @@ require 'langchain'
 # Create a global OpenAI client instance
 LANGCHAIN_OPENAI = Langchain::LLM::OpenAI.new(
   api_key: ENV.fetch('OPENAI_API_KEY', nil),
-  default_options: { temperature: 0.5, chat_model: 'gpt-5-nano' }
+  default_options: { temperature: 0, chat_model: 'gpt-4.1-nano' }
 )
 # RAGAS (Retrieval Augmented Generation Assessment) used to evaluate RAG response
 RAGAS = Langchain::LLM::OpenAI.new(
   api_key: ENV.fetch('OPENAI_API_KEY', nil),
-  default_options: { temperature: 0, chat_model: 'gpt-5-mini' }
+  default_options: { temperature: 0, chat_model: 'gpt-4.1-mini' }
 )
 
 # Suppress logs to only show when there is an error
