b06bc281981c5f8343b2315fe273d68797267375
adi-herwana-nus <adi.hwn@nus.edu.sg>
2025-10-29 07:03:36 +0800

    feat(rubric-playground): support bulk evaluation, refactor state data getters into selector

---
+++ client/app/bundles/course/assessment/question/rubric-playground/AnswerEvaluationsTableHeader.tsx
@@ -0,0 +1,163 @@
+import { FC, useState } from 'react';
+import { Add, PlayArrow, Refresh } from '@mui/icons-material';
+import { Button, Typography } from '@mui/material';
+import sampleSize from 'lodash-es/sampleSize';
+import { dispatch } from 'store';
+
+import { useAppSelector } from 'lib/hooks/store';
+
+import {
+  actions as questionRubricsActions,
+  RubricState,
+} from '../reducers/rubrics';
+
+import { AnswerTableEntry } from './AnswerEvaluationsTable/types';
+import { isAnswerAlreadyEvaluated } from './AnswerEvaluationsTable/utils';
+import { createQuestionMockAnswer } from './operations/mockAnswers';
+import { requestRowEvaluation } from './operations/rowEvaluation';
+import AddSampleAnswersDialog, {
+  AddSampleAnswersFormData,
+  AddSampleMode,
+} from './AddSampleAnswersDialog';
+
+const AnswerEvaluationsTableHeader: FC<{
+  answerCount: number;
+  answerEvaluatedCount: number;
+  answerEvaluationTableData: AnswerTableEntry[];
+  compareCount: number;
+  isComparing: boolean;
+  selectedRubric: RubricState;
+}> = (props) => {
+  const {
+    answerCount,
+    answerEvaluatedCount,
+    answerEvaluationTableData,
+    compareCount,
+    isComparing,
+    selectedRubric,
+  } = props;
+
+  const rubricAnswers = useAppSelector(
+    (state) => state.assessments.question.rubrics.answers,
+  );
+  const selectableAnswers = Object.values(rubricAnswers).filter(
+    (answer) => !(answer.id in selectedRubric.answerEvaluations),
+  );
+
+  const maximumGrade = selectedRubric.categories.reduce(
+    (sum, category) => sum + category.maximumGrade,
+    0,
+  );
+
+  const [isAddingAnswers, setIsAddingAnswers] = useState(false);
+
+  const handleAddAnswers = async (
+    data: AddSampleAnswersFormData,
+  ): Promise<void> => {
+    switch (data.addMode) {
+      case AddSampleMode.SPECIFIC_ANSWER: {
+        dispatch(
+          questionRubricsActions.initializeAnswerEvaluations({
+            answerIds: data.addAnswerIds,
+            rubricId: selectedRubric.id,
+          }),
+        );
+        break;
+      }
+      case AddSampleMode.RANDOM_STUDENT: {
+        const randomAnswerIds = sampleSize(
+          selectableAnswers,
+          data.addRandomAnswerCount,
+        ).map((answer) => answer.id);
+        dispatch(
+          questionRubricsActions.initializeAnswerEvaluations({
+            answerIds: randomAnswerIds,
+            rubricId: selectedRubric.id,
+          }),
+        );
+        break;
+      }
+      case AddSampleMode.CUSTOM_ANSWER: {
+        const mockAnswerId = await createQuestionMockAnswer(
+          data.addMockAnswerText,
+        );
+        dispatch(
+          questionRubricsActions.initializeMockAnswer({
+            rubricId: selectedRubric.id,
+            mockAnswerId,
+            answerText: data.addMockAnswerText,
+          }),
+        );
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  };
+
+  return (
+    <>
+      <div className="flex flex-row space-x-4 items-center py-2">
+        <Typography variant="h6">Sample Answer Evaluations</Typography>
+        <Button
+          onClick={() => setIsAddingAnswers(true)}
+          size="small"
+          startIcon={<Add />}
+          variant="outlined"
+        >
+          Add Sample Answers
+        </Button>
+        {Boolean(answerCount) && (
+          <Button
+            disabled={answerEvaluationTableData.some((row) => row.isEvaluating)}
+            onClick={() => {
+              const rowsToEvaluate =
+                answerEvaluatedCount === answerCount
+                  ? answerEvaluationTableData
+                  : answerEvaluationTableData.filter(
+                      (answer) => !isAnswerAlreadyEvaluated(answer),
+                    );
+              Promise.all(
+                rowsToEvaluate.map((row) =>
+                  requestRowEvaluation(dispatch, row, selectedRubric.id),
+                ),
+              );
+            }}
+            size="small"
+            startIcon={
+              answerEvaluatedCount === answerCount ? <Refresh /> : <PlayArrow />
+            }
+            variant="outlined"
+          >
+            {!answerEvaluatedCount && `Evaluate All (${answerCount})`}
+            {Boolean(answerEvaluatedCount) &&
+              answerEvaluatedCount === answerCount &&
+              `Re-evaluate All (${answerCount})`}
+            {Boolean(answerEvaluatedCount) &&
+              answerEvaluatedCount !== answerCount &&
+              `Evaluate Remaining (${answerCount - answerEvaluatedCount})`}
+          </Button>
+        )}
+      </div>
+      {isComparing && (
+        <Typography variant="body2">
+          Comparing {compareCount} revisions
+        </Typography>
+      )}
+
+      <AddSampleAnswersDialog
+        answers={selectableAnswers}
+        maximumGrade={maximumGrade ?? 0}
+        onClose={() => setIsAddingAnswers(false)}
+        onSubmit={async (data: AddSampleAnswersFormData): Promise<void> => {
+          await handleAddAnswers(data);
+          setIsAddingAnswers(false);
+        }}
+        open={isAddingAnswers}
+      />
+    </>
+  );
+};
+
+export default AnswerEvaluationsTableHeader;
--- client/app/bundles/course/assessment/question/rubric-playground/operations/rowEvaluation.ts
+++ client/app/bundles/course/assessment/question/rubric-playground/operations/rowEvaluation.ts
@@ -1,7 +1,83 @@
 import { AxiosError } from 'axios';
+import { AppDispatch } from 'store';
 import { JobStatusResponse } from 'types/jobs';
 
 import CourseAPI from 'api/course';
+
+import { actions as questionRubricsActions } from '../../reducers/rubrics';
+import { AnswerTableEntry } from '../AnswerEvaluationsTable/types';
+
+import { deleteAnswerEvaluation, evaluatePlaygroundAnswer } from './answers';
+import {
+  deleteMockAnswerEvaluation,
+  evaluatePlaygroundMockAnswer,
+} from './mockAnswers';
+
+export const requestRowEvaluation = (
+  dispatch: AppDispatch,
+  answer: AnswerTableEntry,
+  rubricId: number,
+): void => {
+  if (answer.isMock) {
+    dispatch(
+      questionRubricsActions.requestMockAnswerEvaluation({
+        mockAnswerId: answer.id,
+        rubricId,
+      }),
+    );
+    evaluatePlaygroundMockAnswer(rubricId, answer.id).then((evaluation) => {
+      dispatch(
+        questionRubricsActions.updateMockAnswerEvaluation({
+          mockAnswerId: answer.id,
+          rubricId,
+          evaluation,
+        }),
+      );
+    });
+  } else {
+    dispatch(
+      questionRubricsActions.requestAnswerEvaluation({
+        answerId: answer.id,
+        rubricId,
+      }),
+    );
+    evaluatePlaygroundAnswer(rubricId, answer.id).then((evaluation) => {
+      dispatch(
+        questionRubricsActions.updateAnswerEvaluation({
+          answerId: answer.id,
+          rubricId,
+          evaluation,
+        }),
+      );
+    });
+  }
+};
+
+export const deleteRowEvaluation = (
+  dispatch: AppDispatch,
+  answer: AnswerTableEntry,
+  rubricId: number,
+): void => {
+  if (answer.isMock) {
+    deleteMockAnswerEvaluation(rubricId, answer.id).then(() => {
+      dispatch(
+        questionRubricsActions.deleteMockAnswerEvaluation({
+          mockAnswerId: answer.id,
+          rubricId,
+        }),
+      );
+    });
+  } else {
+    deleteAnswerEvaluation(rubricId, answer.id).then(() => {
+      dispatch(
+        questionRubricsActions.deleteAnswerEvaluation({
+          answerId: answer.id,
+          rubricId,
+        }),
+      );
+    });
+  }
+};
 
 export const exportEvaluations = async (
   rubricId: number,
