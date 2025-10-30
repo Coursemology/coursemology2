c8fdfd5a6136b8cd3b17240b722269ef24d3ff4c
adi-herwana-nus <adi.hwn@nus.edu.sg>
2025-10-30 23:27:42 +0800

    feat(ssid): recover if ssid folder already exists

--- app/services/course/ssid_folder_service.rb
+++ app/services/course/ssid_folder_service.rb
@@ -7,8 +7,12 @@ class Course::SsidFolderService
   def run_create_ssid_folder_service
     ssid_api_service = SsidAsyncApiService.new('folders', @folder_object)
     response_status, response_body = ssid_api_service.post
+
+    # If id is lost in our DB somehow, we can recover it if SSID returns a 409
+    return response_body['payload']['data']['existingFolderId'] if response_status == 409
+
     raise SsidError, { status: response_status, body: response_body } unless response_status == 200
 
-    response_body['payload']['data']
+    response_body['payload']['data']['id']
   end
 end
