25cb5d13a40d444f9002f4f79cc1c72f287bd3be
adi-herwana-nus <adi.hwn@nus.edu.sg>
2025-11-07 17:24:30 +0800

    feat(rubric-playground): add translation keys

--- client/app/bundles/course/assessment/question/rubric-playground/AnswerEvaluationsTable/UnevaluatedCell.tsx
+++ client/app/bundles/course/assessment/question/rubric-playground/AnswerEvaluationsTable/UnevaluatedCell.tsx
@@ -3,6 +3,9 @@ import { PlayArrow } from '@mui/icons-material';
 import { Button, IconButton, Tooltip } from '@mui/material';
 
 import LoadingIndicator from 'lib/components/core/LoadingIndicator';
+import useTranslation from 'lib/hooks/useTranslation';
+
+import translations from '../translations';
 
 import { AnswerTableEntry } from './types';
 
@@ -10,39 +13,44 @@ const UnevaluatedCell: FC<{
   answer: AnswerTableEntry;
   handleEvaluate: () => void;
   compact?: boolean;
-}> = ({ answer, compact, handleEvaluate }) => (
-  <div className="w-full h-full bg-gray-100 px-6 py-3 flex items-center justify-start">
-    {compact && (
-      <Tooltip title="Evaluate">
-        <IconButton
-          className="p-0"
+}> = ({ answer, compact, handleEvaluate }) => {
+  const { t } = useTranslation();
+  return (
+    <div className="w-full h-full bg-gray-100 px-6 py-3 flex items-center justify-start">
+      {compact && (
+        <Tooltip title={t(translations.evaluate)}>
+          <IconButton
+            className="p-0"
+            color="primary"
+            disabled={answer.isEvaluating}
+            onClick={handleEvaluate}
+          >
+            <PlayArrow />
+          </IconButton>
+        </Tooltip>
+      )}
+      {!compact && (
+        <Button
+          className="w-fit whitespace-nowrap"
           color="primary"
           disabled={answer.isEvaluating}
           onClick={handleEvaluate}
+          startIcon={
+            answer.isEvaluating ? (
+              <LoadingIndicator bare size={15} />
+            ) : (
+              <PlayArrow />
+            )
+          }
+          variant="outlined"
         >
-          <PlayArrow />
-        </IconButton>
-      </Tooltip>
-    )}
-    {!compact && (
-      <Button
-        className="w-fit whitespace-nowrap"
-        color="primary"
-        disabled={answer.isEvaluating}
-        onClick={handleEvaluate}
-        startIcon={
-          answer.isEvaluating ? (
-            <LoadingIndicator bare size={15} />
-          ) : (
-            <PlayArrow />
-          )
-        }
-        variant="outlined"
-      >
-        {answer.isEvaluating ? 'Evaluating' : 'Evaluate'}
-      </Button>
-    )}
-  </div>
-);
+          {answer.isEvaluating
+            ? t(translations.evaluating)
+            : t(translations.evaluate)}
+        </Button>
+      )}
+    </div>
+  );
+};
 
 export default UnevaluatedCell;
