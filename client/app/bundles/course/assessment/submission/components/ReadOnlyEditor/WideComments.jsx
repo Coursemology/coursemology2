38550183eb6d06da8e03d0ffc99da6df5643322c
adi-herwana-nus <adi.hwn@nus.edu.sg>
2026-02-26 16:24:06 +0800

    fix(ReadOnlyEditor): make active comment line expandable

--- client/app/bundles/course/assessment/submission/components/ReadOnlyEditor/WideComments.jsx
+++ client/app/bundles/course/assessment/submission/components/ReadOnlyEditor/WideComments.jsx
@@ -67,27 +67,28 @@ export default class WideComments extends Component {
         </div>
       );
     }
-    return (
-      <Paper
-        key={lineNumber}
-        elevation={1}
-        onClick={() => expandLine(lineNumber)}
-        style={styles.collapsed}
-      >
-        <PostPreview annotation={annotation} />
-      </Paper>
-    );
+    if (annotation) {
+      return (
+        <Paper
+          key={lineNumber}
+          elevation={1}
+          onClick={() => expandLine(lineNumber)}
+          style={styles.collapsed}
+        >
+          <PostPreview annotation={annotation} />
+        </Paper>
+      );
+    }
+    return null;
   }
 
   render() {
-    const { expanded, annotations } = this.props;
+    const { expanded, annotations, activeComment } = this.props;
     const comments = [];
     for (let i = 1; i <= expanded.length; i++) {
-      const filtered = annotations.filter(
-        (annotation) => annotation.line === i,
-      );
-      if (filtered.length > 0) {
-        comments.push(this.renderComments(i, filtered[0]));
+      const annotation = annotations.find((a) => a.line === i);
+      if (annotation || (activeComment === i && expanded[i - 1])) {
+        comments.push(this.renderComments(i, annotation));
       } else {
         comments.push(<div key={i} style={styles.collapsed} />);
       }
