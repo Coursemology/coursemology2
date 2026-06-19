8a25e45fdc5ebffa50ba074b6c0fb12a56d876b4
adi-herwana-nus <adi.hwn@nus.edu.sg>
2026-06-20 01:03:55 +0800

    fix(ci): fix rollbar include error, add git commit hash

--- config/initializers/rollbar_activejob.rb
+++ config/initializers/rollbar_activejob.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 if Rails.env.production?
-  require 'rollbar/active_job'
+  require 'rollbar'
 
   Rails.application.config.to_prepare do
     ApplicationJob.include(Rollbar::ActiveJob)
