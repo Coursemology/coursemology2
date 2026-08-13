6b54b846f1f4876c65cf31abc65cb64473e18808
adi-herwana-nus <adi.hwn@nus.edu.sg>
2026-08-13 08:49:09 +0800

    fix(db): partial fix of deltas between schema.rb and production state

--- db/migrate/20211024140630_add_last_graded_time_to_course_assessment_submissions.rb
+++ db/migrate/20211024140630_add_last_graded_time_to_course_assessment_submissions.rb
@@ -1,5 +1,11 @@
 class AddLastGradedTimeToCourseAssessmentSubmissions < ActiveRecord::Migration[6.0]
   def change
-    add_column :course_assessment_submissions, :last_graded_time, :datetime, default: Time.now
+    # The original line (commented) generated the default based on when the migration was run, which
+    # caused desync between the schema.rb (based on migration run time in local development) and the actual
+    # production schema (based on migration run time in production).
+    # To fix this, we set the default value to a fixed time matching the actual production value.
+    #
+    # add_column :course_assessment_submissions, :last_graded_time, :datetime, default: Time.now
+    add_column :course_assessment_submissions, :last_graded_time, :datetime, default: '2021-11-09 00:08:09.318947'
   end
 end
